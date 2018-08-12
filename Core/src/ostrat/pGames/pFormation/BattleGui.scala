6769c116375d92252785f1e4cf526b6b363d3df0
Rich <rr1@tutanota.com>
2018-08-13 00:08:50 +0100

    More pFormation

--- Core/src/ostrat/pGames/pFormation/BattleGui.scala
+++ Core/src/ostrat/pGames/pFormation/BattleGui.scala
@@ -3,14 +3,18 @@ package ostrat
 package pGames
 package pFormation
 import pDisp._
-import Colour._
 import geom._
 
 /** needs to inherit from MapCanvas */
-case class BattleGui(canv: CanvasPlatform) extends pDisp.CanvasSimple//UnfixedMapGui
+case class BattleGui(canv: CanvasPlatform, scen: BScen) extends pDisp.CanvasSimple//UnfixedMapGui
 {
-   val rs = for { y <- 1 to 3
-      x <- 0 to 9}
-   yield Rect.xy(30, 20, (x -4.5) * 40, (y - 2) * 40).fill(Blue)
+   val rs = scen.lunits.flatMap{c => 
+   
+   
+   for { y <- 1 to c.ranks
+      x <- 0 to c.rankLen
+      }
+   yield Rect.xy(15, 10, (x - c.rankMiddle) * 20 + c.posn.x, (y - 2) * 20 + c.posn.y).fill(c.colour)
+   }
    repaint(rs)
 }
---
+++ Core/src/ostrat/pGames/pFormation/Company.scala
@@ -0,0 +1,40 @@
+/* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
+package ostrat
+package pGames
+package pFormation
+import Colour._
+import geom._
+
+trait Company
+{
+   var posn: Vec2
+   def ranks: Int
+   def rankLen: Int
+   def colour: Colour
+   def rankMiddle = (rankLen + 1) / 2.0
+}
+
+case class French(var posn: Vec2)  extends Company
+{
+   val ranks = 3
+   val rankLen = 30
+   val colour = Blue
+}
+
+
+case class British(var posn: Vec2) extends Company
+{
+   val ranks = 2
+   val rankLen = 40
+   val colour = Red
+}
+
+trait BScen
+{
+   val lunits: List[Company]
+}
+
+object Nap1 extends BScen
+{
+   val lunits = French(Vec2(0, - 200)) :: British(Vec2(0, 200)) :: Nil
+}
