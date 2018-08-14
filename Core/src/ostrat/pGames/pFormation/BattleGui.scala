de2c0ad8d75477ba4d0b495e4c6f283cfa38d4bf
Rich <rr1@tutanota.com>
2018-08-14 17:08:39 +0100

    Restored lost changes.

--- Core/src/ostrat/pGames/pFormation/BattleGui.scala
+++ Core/src/ostrat/pGames/pFormation/BattleGui.scala
@@ -3,14 +3,14 @@ package ostrat
 package pGames
 package pFormation
 import pDisp._
-import Colour._
 import geom._
 
-/** needs to inherit from MapCanvas */
-case class BattleGui(canv: CanvasPlatform) extends pDisp.CanvasSimple//UnfixedMapGui
-{
-//   val rs = for { y <- 1 to 3
-//      x <- 0 to 9
-//      } yield Rect.xy(15, 10, (x - c.rankMiddle) * 20 + c.posn.x, (y - 2) * 20 + c.posn.y).fill(c.colour)  
-//   repaint(rs)
-}
+case class BattleGui(canv: CanvasPlatform, scen: BScen) extends pDisp.CanvasSimple//UnfixedMapGui
+{   
+   val rs = scen.lunits.flatMap{c =>
+      for { y <- 1 to c.ranks
+      x <- 0 to c.rankLen
+      } yield Rectangle(15, 10, (x - c.rankMiddle) * 20 + c.posn.x, (y - 2) * 20 + c.posn.y).fill(c.colour)
+   }
+   repaint(rs)
+} 
