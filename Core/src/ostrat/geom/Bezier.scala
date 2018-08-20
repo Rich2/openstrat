9461c7b5062525ed7f49e3934e5b577062161786
Richtype <rr1@tutanota.com>
2018-08-20 08:24:24 +0100

    #7

---
+++ Core/src/ostrat/geom/Bezier.scala
@@ -0,0 +1,9 @@
+/* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
+package ostrat
+package geom
+
+/** Cubic bezier curve */
+class Bezier (x1: Double, y1: Double, x2: Double, y2: Double, x3: Double, y3: Double, x4: Double, y4: Double)
+{
+  
+}
