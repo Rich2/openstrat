db43dc0597778640d073edc9d260f90d5aa6f271
Richtype <rr1@tutanota.com>
2018-08-26 20:58:53 +0100

    Remove closes #41

--- Core/src/ostrat/geom/Vec2.scala
+++ Core/src/ostrat/geom/Vec2.scala
@@ -88,9 +88,7 @@ final class Vec2 (val x: Double, val y: Double) extends Product2[Double, Double]
       val r = length / 2.0
       Vec2s(-r vv r, r vv r, r vv -r, -r vv -r).addMap(x, y) 
    }
-   //def toSeq[A <: Double2
-   //override def numOfPts = 1
-   //override def seqOfPts: Vec2s = Vec2s.xy(x, y)
+   
    def fillText(str: String, fontSize: Int, fontColour: Colour = Colour.Black) = TextGraphic(this, str, fontSize, fontColour)
    def arcControlPoint(pt2: Vec2, arcCentre: Vec2): Vec2 =
    {
@@ -111,157 +109,18 @@ object Vec2
    def apply(x: Double, y: Double): Vec2 = new Vec2(x, y)
    def unapply(orig: Vec2): Option[(Double, Double)] = Some((orig.x, orig.y))
    def fromAngle(angle: Angle, scalar: Double = 1.0): Vec2 = angle.toVec2 * scalar
-//   def xy(inp: Double*): Vec2s =
-//   {      
-//
-//      def loop(rem: Seq[Double], acc: Vec2s, posn: Int): Vec2s = rem match
-//      {
-//         
-//         case Seq() => acc
-//         case Seq(h) => excep("Odd number of Doubles in Vec2s.apply method")
-//         case Seq(x, y, tail @ _*) => loop(tail, acc :+ Vec2(x, y), posn + 2)
-//      }
-//      loop(inp, Seq(), 0)      
-//   }  
-//   def fromDoubles(elems: Double*): Seq[Vec2] =
-//   {      
-//      def loop(rem: Seq[Double], acc: Seq[Vec2]): Seq[Vec2] = rem match
-//      {
-//         case Seq() => acc
-//         case Seq(x, y, tail @ _*) => loop(tail, acc :+ Vec2(x, y))
-//      }
-//      if (elems.length.isOdd) excep("Odd number of Doubles passed to Vec2 Seq constructor")
-//      loop(elems, Seq())
-//   }
-   //def zero: Vec2 = new Vec2(0, 0)
+
    implicit class Vec2Implicit(thisVec: Vec2)
    {
       def * (operand: Dist): Dist2 = Dist2(thisVec.x * operand, thisVec.y * operand)
    }
    def circlePt(angle: Double): Vec2 = Vec2(cos(angle), sin(angle))
    def circlePtClockwise(angle: Double): Vec2 = Vec2(cos(angle), - sin(angle))
-   def gets(coods: Double *): Array[Vec2] =
-   {
-      (for ( i <- 0 until coods.length / 2) yield Vec2(coods(i * 2), coods(i * 2 + 1))).toArray
-   }
-   def rectBL(left: Double, bottom: Double, width: Double, height: Double): Array[Vec2] =
-      Array[Vec2](Vec2(left, bottom), Vec2(left, bottom + height), Vec2(left + width, bottom + height), Vec2(left + width, bottom))
-   def squareBL(left: Double, bottom: Double, length: Double): Array[Vec2] =
-      Array[Vec2](Vec2(left, bottom), Vec2(left, bottom + length), Vec2(left + length, bottom + length), Vec2(left + length, bottom))   
-   
-   implicit class ImpVec2srray(arr: Array[Vec2])
-   {
-      def +++ (offset: Vec2): Array[Vec2] = arr.map(_ + offset)
-      def xy_+ (xOff: Double, yOff: Double): Array[Vec2] = arr.map(orig => Vec2(orig.x + xOff, orig.y + yOff))
-      def --- (offset: Vec2): Array[Vec2] = arr.map(_ - offset)
-      def *** (factor: Double): Array[Vec2] = arr.map(_ * factor)
-      def a_*+ (factor: Double, offset: Vec2): Array[Vec2] = arr.map(_ * factor + offset)
-      def a_+* (offset: Vec2, factor: Double): Array[Vec2] = arr.map(i => (i + offset) * factor)
-      def xy_*+ (factor: Double, xOff: Double, yOff: Double): Array[Vec2] = arr.map(_ * factor + Vec2(xOff, yOff))
-      def a_+* (xOff: Double, yOff: Double, factor: Double): Array[Vec2] = arr.map(i => (i + Vec2(xOff, yOff)) * factor)
-   }
-   
-   import scala.collection._   
    
    implicit class Vec2SeqImplicit(thisSeq: Seq[Vec2])
    {
       def toVec2s: Vec2s = thisSeq.toProdD2
    }
-   
-   implicit class ImpVec2Traversible[Repr](travLike: TraversableLike[Vec2, Repr])
-   {
-      /** Translates each Vec2 member of collection equivalent to trav.map(_ + offset) */
-      def +++ (offset: Vec2)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (x <- travLike) b += x + offset
-         b.result
-      }
-      /** Translates each Vec2 member of collection equivalent to trav.map(_ + Vec2(xOff, yOff) */
-      def slate (xOff: Double, yOff: Double)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (orig <- travLike) b += Vec2(orig.x + xOff, orig.y + yOff)
-         b.result
-      }
-      
-      def --- (offset: Vec2)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (x <- travLike) b += x - offset
-         b.result
-      }
-      
-      def *** (factor: Double)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (x <- travLike) b += x * factor
-         b.result
-      }
-      
-      def a_*+ (factor: Double, offset: Vec2)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (x <- travLike) b += x * factor + offset
-         b.result
-      }
-      def a_+* (offset: Vec2, factor: Double)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (x <- travLike) b += (x + offset) * factor
-         b.result
-      }
-      
-      def rotate (angle: Angle)(implicit bf: generic.CanBuildFrom[Repr, Vec2, Repr]): Repr =
-      {
-         def builder =
-         { // extracted to keep method size under 35 bytes, so that it can be JIT-inlined
-            val b = bf(travLike.repr)
-            b.sizeHint(travLike)
-            b
-         }
-         val b = builder
-         for (x <- travLike) b += (x.rotate(angle))
-         b.result
-      }
-   }
-  implicit object Vec2Persist extends PersistD2[Vec2]('Vec2, v => (v.x, v.y), apply)
-  
 
+  implicit object Vec2Persist extends PersistD2[Vec2]('Vec2, v => (v.x, v.y), apply)
 }
