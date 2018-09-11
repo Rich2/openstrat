0600ef3f56e4548ff8c38644878158fc5a1a9e8d
Richtype <rr1@tutanota.com>
2018-09-11 16:28:04 +0100

    Minor

--- FxStrat/src/ostrat/pFx/CanvasFx.scala
+++ FxStrat/src/ostrat/pFx/CanvasFx.scala
@@ -4,53 +4,49 @@ package pFx
 import geom._, pCanv._, scalafx.Includes._, scalafx.scene._
 
 case class CanvasFx(canvFx: canvas.Canvas) extends CanvasTopLeft// with CanvSaver
-{
-   val gc: canvas.GraphicsContext = canvFx.graphicsContext2D
-   override def width = canvFx.width.value.max(100)
-   override def height = canvFx.height.value.max(100)
-   def getButton(e: input.MouseEvent): MouseButton = 
-   {
-      import javafx.scene.input.MouseButton._
-      e.button.delegate match
-      {
-         case PRIMARY => LeftButton
-         case MIDDLE => MiddleButton
-         case SECONDARY => RightButton
-         case _ => NoButton
-      }     
-   }
-   canvFx.onMouseReleased = (e: input.MouseEvent) => mouseUpTopLeft(e.x, e.y, getButton(e))   
-   //canvFx.onMousePressed = (e: input.MouseEvent) => mouseDownTopLeft(e.x, e.y, getButton(e))   
-   //canvFx.onMouseMoved = (e: input.MouseEvent) => mouseMovedTopLeft(e.x, e.y, getButton(e))    
-   canvFx.onMouseDragged = (e: input.MouseEvent) => mouseDraggedTopLeft(e.x, e.y, getButton(e))   
+{ val gc: canvas.GraphicsContext = canvFx.graphicsContext2D
+  override def width = canvFx.width.value.max(100)
+  override def height = canvFx.height.value.max(100)
+  def getButton(e: input.MouseEvent): MouseButton = 
+  { import javafx.scene.input.MouseButton._
+    e.button.delegate match
+    { case PRIMARY => LeftButton
+      case MIDDLE => MiddleButton
+      case SECONDARY => RightButton
+      case _ => NoButton
+    }     
+  }
+  canvFx.onMouseReleased = (e: input.MouseEvent) => mouseUpTopLeft(e.x, e.y, getButton(e))   
+  //canvFx.onMousePressed = (e: input.MouseEvent) => mouseDownTopLeft(e.x, e.y, getButton(e))   
+  //canvFx.onMouseMoved = (e: input.MouseEvent) => mouseMovedTopLeft(e.x, e.y, getButton(e))    
+  canvFx.onMouseDragged = (e: input.MouseEvent) => mouseDraggedTopLeft(e.x, e.y, getButton(e))   
    
-   canvFx.onScroll = (e: input.ScrollEvent) => e.deltaY match
-      {
-         case 0 => //("scroll delta 0")
-         case d if d > 0 => onScroll(true)
-         case _ => onScroll(false)
-      }
+  canvFx.onScroll = (e: input.ScrollEvent) => e.deltaY match
+  { case 0 => //("scroll delta 0")
+    case d if d > 0 => onScroll(true)
+    case _ => onScroll(false)
+  }
    import paint.Color   
-   def fxColor(colour: Colour): paint.Color = Color.rgb(colour.red, colour.green, colour.blue, colour.alpha / 255.0)
+   def toFxColor(colour: Colour): paint.Color = Color.rgb(colour.red, colour.green, colour.blue, colour.alpha / 255.0)
    override def tlPolyFill(fp: PolyFill): Unit =
    {
-      gc.fill = fxColor(fp.colour)
+      gc.fill = toFxColor(fp.colour)
       gc.fillPolygon(fp.xArray, fp.yArray, fp.verts.length)
    }
 
    /** Needs mod */
    override def tlPolyDraw(dp: PolyDraw): Unit =    
    {
-      gc.stroke = fxColor(dp.lineColour)
+      gc.stroke = toFxColor(dp.lineColour)
       gc.lineWidth = dp.lineWidth
       gc.strokePolygon(dp.xArray, dp.yArray, dp.vertsLength)
    }
  
    override def tlPolyFillDraw(pfd: PolyFillDraw): Unit =    
    {      
-      gc.fill = fxColor(pfd.fillColour)           
+      gc.fill = toFxColor(pfd.fillColour)           
       gc.fillPolygon(pfd.xArray, pfd.yArray, pfd.vertsLength)
-      gc.stroke = fxColor(pfd.lineColour)
+      gc.stroke = toFxColor(pfd.lineColour)
       gc.lineWidth = pfd.lineWidth
       gc.strokePolygon(pfd.xArray, pfd.yArray, pfd.vertsLength)  
    }
@@ -60,7 +56,7 @@ case class CanvasFx(canvFx: canvas.Canvas) extends CanvasTopLeft// with CanvSave
       gc.beginPath
       gc.moveTo(ld.xStart, ld.yStart)
       gc.lineTo(ld.xEnd, ld.yEnd)
-      gc.stroke = fxColor(ld.colour)
+      gc.stroke = toFxColor(ld.colour)
       gc.lineWidth = ld.lineWidth
       gc.stroke()
    }
@@ -70,7 +66,7 @@ case class CanvasFx(canvFx: canvas.Canvas) extends CanvasTopLeft// with CanvSave
       gc.beginPath
       gc.moveTo(ad.xStart, ad.yStart)
       ad.fControlEndRadius(gc.arcTo)
-      gc.stroke = fxColor(ad.colour)
+      gc.stroke = toFxColor(ad.colour)
       gc.stroke()
    }
    
@@ -87,7 +83,7 @@ case class CanvasFx(canvFx: canvas.Canvas) extends CanvasTopLeft// with CanvSave
    
    protected def tlBezierDraw(bd: BezierDraw): Unit =
    {     
-      gc.stroke = fxColor(bd.colour)      
+      gc.stroke = toFxColor(bd.colour)      
       gc.lineWidth = bd.lineWidth
       gc.beginPath
       gc.moveTo(bd.xStart, bd.yStart)
@@ -96,72 +92,67 @@ case class CanvasFx(canvFx: canvas.Canvas) extends CanvasTopLeft// with CanvSave
    }   
    
    override def tlTextGraphic(tg: TextGraphic): Unit = 
-   {
-      gc.setTextAlign(fxAlign(tg.align))
+   {  gc.setTextAlign(fxAlign(tg.align))
       gc.setTextBaseline(javafx.geometry.VPos.CENTER)
       gc.setFont(new text.Font(tg.fontSize))
-      gc.fill = fxColor(tg.colour)      
+      gc.fill = toFxColor(tg.colour)      
       gc.fillText(tg.str, tg.posn.x, tg.posn.y)
    }
    
    protected def tlLinesDraw(lineSegs: Line2s, lineWidth: Double, linesColour: Colour): Unit =
-   {           
-      gc.beginPath
+   {  gc.beginPath
       lineSegs.foreach(ls => { gc.moveTo(ls.xStart, ls.yStart);  gc.lineTo(ls.xEnd, ls.yEnd)})
       gc.lineWidth = lineWidth
-      gc.stroke = fxColor(linesColour)
+      gc.stroke = toFxColor(linesColour)
       gc.stroke()      
    }
    
    override def tlTextOutline(to: TextOutline): Unit = 
-   {
-      gc.setTextAlign(javafx.scene.text.TextAlignment.CENTER)
+   {  gc.setTextAlign(javafx.scene.text.TextAlignment.CENTER)
       gc.setTextBaseline(javafx.geometry.VPos.CENTER)
-      gc.stroke = fxColor(to.colour)
+      gc.stroke = toFxColor(to.colour)
       gc.lineWidth = 1
       gc.setFont(new text.Font(to.fontSize))      
       gc.strokeText(to.str, to.posn.x, to.posn.y)      
    }
    
    private def segsPath(segs: CurveSegs): Unit =
-   {
-      gc.beginPath      
-      var startPt = segs.last.pEnd
-      gc.moveTo(startPt.x, startPt.y)
-      segs.foreach{seg =>
-         seg.segDo(ls =>  gc.lineTo(ls.xEnd, ls.yEnd),
-              as => as.fControlEndRadius(startPt, gc.arcTo),
-              bs => gc.bezierCurveTo(bs.xC1, bs.yC1, bs.xUses, bs.yUses, bs.xEnd, bs.yEnd))
-         startPt = seg.pEnd 
+   { gc.beginPath
+     var startPt = segs.last.pEnd
+     gc.moveTo(startPt.x, startPt.y)
+     segs.foreach{seg =>
+       seg.segDo(ls =>  gc.lineTo(ls.xEnd, ls.yEnd),
+           as => as.fControlEndRadius(startPt, gc.arcTo),
+           bs => gc.bezierCurveTo(bs.xC1, bs.yC1, bs.xUses, bs.yUses, bs.xEnd, bs.yEnd)         
+        )
+        startPt = seg.pEnd 
       }
       gc.closePath 
    }
    
    override def tlShapeFill(segs: CurveSegs, fillColour: Colour): Unit =
-   {
-      segsPath(segs)  
-      gc.fill = fxColor(fillColour)
-      gc.fill()      
+   { segsPath(segs)  
+     gc.fill = toFxColor(fillColour)
+     gc.fill()      
    }
    
    override def tlShapeFillDraw(segs: CurveSegs, fillColour: Colour, lineWidth: Double, lineColour: Colour): Unit =
-   {
-      segsPath(segs)  
-      gc.fill = fxColor(fillColour)
-      gc.fill()
-      gc.stroke = fxColor(lineColour)
-      gc.stroke()
+   { segsPath(segs)  
+     gc.fill = toFxColor(fillColour)
+     gc.fill()
+     gc.stroke = toFxColor(lineColour)
+     gc.stroke()
    }   
    override def tlShapeDraw(segs: CurveSegs, lineWidth: Double, lineColour: Colour): Unit =
    {
       segsPath(segs)  
-      gc.stroke = fxColor(lineColour)
+      gc.stroke = toFxColor(lineColour)
       gc.stroke()      
    }
    
    override def clear(colour: Colour): Unit =
    {
-      gc.fill = fxColor(colour)
+      gc.fill = toFxColor(colour)
       gc.fillRect(0, 0, width, height)
    }  
    def getTime: Double = System.currentTimeMillis
