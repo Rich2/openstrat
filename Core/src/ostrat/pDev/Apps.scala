d2243e93f290c9b46f742c2a42a7abeb0a9fdbea
Rich <rr1@tutanota.com>
2018-09-16 08:52:26 +0100

    Added comment to AppNum val.

--- Core/src/ostrat/pDev/Apps.scala
+++ Core/src/ostrat/pDev/Apps.scala
@@ -1,7 +1,7 @@
 /* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
 package ostrat
 package pDev
-import pCanv._, pStrat._, pGames._, pLearn._
+import pCanv._, pStrat._
 
 /** This file is normally set by "git update-index --assume-unchanged Core/src/ostrat/pDev/Apps.scala" this is to allow the developer to make local
  *  changes to appNum without modifying the master project. If permanent changes are required run:
@@ -10,24 +10,26 @@ import pCanv._, pStrat._, pGames._, pLearn._
 object Apps
 {
   val theMap: Map[Int, (CanvasPlatform => Unit, String)] = Map(
-      (0, (new pCiv.CivGui(_), "ScalaFx Rise of Civs")),
-      (1, (pWW2.WWIIGui(_, pWW2.WW1940), "World War II") ),
-      (2, (p1783.Y1783Gui(_, p1783.Nap1), "1783")),
+      (0, (new pGames.pCiv.CivGui(_), "ScalaFx Rise of Civs")),
+      (1, (pGames.pWW2.WWIIGui(_, pGames.pWW2.WW1940), "World War II") ),
+      (2, (pGames.p1783.Y1783Gui(_, pGames.p1783.Nap1), "1783")),
       (3, (FlagsGui(_), "ScalaFx Flags")),
-      (4, (pSpace.Planets(_), "ScalaFx Planets")),
-      (5, (new pZug.ZugGui(_), "ScalaFx Zugfuhrer")),
-      (6, (new pDung.DungGui(_), "ScalaFx Dungeon")),
-      (7, (pCloseOrder.BattleGui(_, pCloseOrder.Nap1), "ScalaFx Formation")),         
+      (4, (pGames.pSpace.Planets(_), "ScalaFx Planets")),
+      (5, (new pGames.pZug.ZugGui(_), "ScalaFx Zugfuhrer")),
+      (6, (new pGames.pDung.DungGui(_), "ScalaFx Dungeon")),
+      (7, (pGames.pCloseOrder.BattleGui(_, pGames.pCloseOrder.Nap1), "ScalaFx Formation")),         
       (8, (ColourGen(_), "ScalaFx Some Colours")),
-      (9, (p305.BC305Gui(_, p305.Bc1), "BC 305")),
-      (10, (pCard.BlackJack(_), "ScalaFx BlackJack")),
-      (11, (new pChess.DraughtsGui(_), "Draughts")),
-      (21, (Lesson1(_), "ScalaFx Demonstration Canvas 1")),
-      (22, (Lesson2(_), "ScalaFx Demonstration Canvas 2")),
-      (23, (Lesson3(_), "ScalaFx Demonstration Canvas 3")),
-      (24, (Lesson4(_), "ScalaFx Demonstration Canvas 4")),         
+      (9, (pGames.p305.BC305Gui(_, pGames.p305.Bc1), "BC 305")),
+      (10, (pGames.pCard.BlackJack(_), "ScalaFx BlackJack")),
+      (11, (new pGames.pChess.DraughtsGui(_), "Draughts")),
+      (21, (pLearn.Lesson1(_), "ScalaFx Demonstration Canvas 1")),
+      (22, (pLearn.Lesson2(_), "ScalaFx Demonstration Canvas 2")),
+      (23, (pLearn.Lesson3(_), "ScalaFx Demonstration Canvas 3")),
+      (24, (pLearn.Lesson4(_), "ScalaFx Demonstration Canvas 4")),         
   )
-      val appNum: Int = 23
-      def curr: (CanvasPlatform => Unit, String) = theMap(appNum)
+  
+  /** Change appNum to change the loaded application. */
+  val appNum: Int = 1
+  def curr: (CanvasPlatform => Unit, String) = theMap(appNum)
     
 }
