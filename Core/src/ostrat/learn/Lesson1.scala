fe50e4c1da279226d958940bb71686a03bccc917
w0d <soccamedia@yahoo.com>
2018-09-30 22:56:55 +0100

    comment correction taken Vec2Z out of lesson1

--- Core/src/ostrat/learn/Lesson1.scala
+++ Core/src/ostrat/learn/Lesson1.scala
@@ -6,7 +6,7 @@ import ostrat._, geom._, pCanv._, Colour._
  *  Hopefully in you editor or IDE (integrated Developer Environment) the comments will appear in a different colour. */
 
 // This is also a comment. Everything after two forward-slashes to the end of line. You can add and remove //s from the beginning of the commands,
-// Assuming you are running the mill "-w name.runBack" when you do a save mill will automatically rebuild and you can see the result of your changes.
+// Assuming you are running the "mill -w name.runBackground" when you do a save mill will automatically rebuild and you can see the result of your changes.
 // The associated commands will appear / disappear from the screen. */
  
 case class Lesson1(canv: CanvasPlatform) extends Lesson
@@ -19,7 +19,7 @@ case class Lesson1(canv: CanvasPlatform) extends Lesson
   canv.arcDraw(-220 vv 0, 0 vv 0, 0 vv 220, 4, Pink)
   canv.bezierDraw(200 vv -350, -500 vv -300, -600 vv -300, -450 vv -200, 2, Green)   
   
-  canv.textGraphic(Vec2Z, "This text is centred on the centre of the canvas. The point from which postions are measured.", 18, Turquoise)
+  canv.textGraphic(0 vv 0, "This text is centred on the centre of the canvas. The point from which postions are measured.", 18, Turquoise)
   canv.textGraphic(100 vv -100, "Text centred at 100, -100", 25)
    //canv.textGraphic(100 vv -200, "This text can't be seen till you uncomment it and rebuild the programme.", 18, Violet)
 }
