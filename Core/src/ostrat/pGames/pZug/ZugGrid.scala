815801dc6e3cfe7ef1c59265d2fb82198a4507bb
Rich <rr1@tutanota.com>
2018-10-04 01:01:23 +0100

    #56

--- Core/src/ostrat/pGames/pZug/ZugGrid.scala
+++ Core/src/ostrat/pGames/pZug/ZugGrid.scala
@@ -4,6 +4,11 @@ package pGames
 package pZug
 import pGrid._
 
+case class Node(cood: Cood, gCost: Int, hCost: Int, parent: Cood)
+{
+  def fCost = gCost + hCost
+}
+
 class ZugGrid(xTileMin: Int, xTileMax: Int, yTileMin: Int, yTileMax: Int) extends HexGridReg[ZugTile, ZugSide](xTileMin, xTileMax, yTileMin, yTileMax)
 {   
    def placeSquads(triples: (Polity, Int, Int) *): Unit = triples.foreach {tr =>
@@ -12,6 +17,26 @@ class ZugGrid(xTileMin: Int, xTileMax: Int, yTileMin: Int, yTileMax: Int) extend
      val sd = Squad(tr._1, x, y)     
      val tile = getTile(x, y)
      tile.lunits ::=  sd //:: tile.lunits
+   }
+   
+   def getHCost(startCood: Cood, endCood: Cood): Int =
+   { val diff = endCood - startCood
+     val x: Int = diff.x.abs
+     val y: Int = diff.y.abs
+     y - x match
+     {
+       case 0 => x / 2
+       case n if n > 0 => y / 2
+       case n if n %% 4 == 0 => y / 2 - n / 4
+       case n => y / 2 - n /4 + 1
+     }
+   }
+   def findPath(startCood: Cood, endCood: Cood): Option[List[Cood]] =
+   {
+     
+     var open: List[Node] = Node(startCood, 0, getHCost(startCood,endCood), startCood) :: Nil
+     var closed: List[Node] = Nil
+     None
    }
 }
 
