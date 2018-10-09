bc7447f7525fcb9f19047c8f5168f8490954fa42
Rich <rr1@tutanota.com>
2018-10-09 13:12:30 +0100

    Minor

--- Core/src/ostrat/pGames/pZug/ZugGui.scala
+++ Core/src/ostrat/pGames/pZug/ZugGui.scala
@@ -9,61 +9,68 @@ import pGrid._
 import pStrat._ 
 
 class ZugGui(canv: CanvasPlatform) extends HexGridGui[ZugTile, ZugSide, ZugGrid](canv, Zug1)
-{
-   override def scaleMin = 10
-   override def eTop(): Unit = reTop(guButs :+ status)
-   mapPanel.backColour = Black
-   def fHex: OfHexReg[ZugTile, ZugSide, ZugGrid] => Disp2 = ofh =>  {
-         import ofh._         
-         val colour: Colour = tile.colour         
-         val tv = tile.terr match
-         {
-            case _ : Building => List(vertDispVecs.fillSubj(tile, LightGreen), Square.fill(pScale * 2.5, colour, cen))
-            case _ => vertDispVecs.fillSubj(tile, colour) :: Nil        
-         }
-         val tText = ifScaleCObj(60, TextGraphic(cen, xyStr, 14, colour.contrastBW))
-         val lunit = tile.lunits match
-         {
-            case ::(head, _) if tScale > 68 => Some(UnitCounters.infantry(30, head, head.colour, tile.colour).slate(cen))
-            case _ => None   
-         }         
-         Disp2(tv, tText ++ lunit)
+{ override def scaleMin = 10
+  override def eTop(): Unit = reTop(guButs :+ status)
+  mapPanel.backColour = Black
+  def fHex: OfHexReg[ZugTile, ZugSide, ZugGrid] => Disp2 = ofh =>
+  { import ofh._         
+    val colour: Colour = tile.colour         
+    
+    val tv = tile.terr match
+    { case _ : Building => List(vertDispVecs.fillSubj(tile, LightGreen), Square.fill(pScale * 2.5, colour, cen))
+      case _ => vertDispVecs.fillSubj(tile, colour) :: Nil        
+    }
+    
+    val tText = ifScaleCObj(60, TextGraphic(cen, xyStr, 14, colour.contrastBW))
+    
+    val lunit = tile.lunits match
+    { case ::(head, _) if tScale > 68 => Some(UnitCounters.infantry(30, head, head.colour, tile.colour).slate(cen))
+      case _ => None   
+    }
+    
+    Disp2(tv, tText ++ lunit)
+  }
+    
+  def fSide: OfHexSideReg[ZugTile, ZugSide, ZugGrid] => Disp2 = ofs =>
+  { import ofs._
+    
+    val line: GraphicElems = ifScaleCObjs(60, side.wall match
+      { case true => vertDispLine.draw(6, Gray) :: Nil
+        case _ => ifTiles(_.colour == _.colour, (t1, _) => vertDispLine.draw(1, t1.colour.contrastBW))
       }
-   def fSide: OfHexSideReg[ZugTile, ZugSide, ZugGrid] => Disp2 = ofs => {
-      import ofs._
-      val line: GraphicElems = ifScaleCObjs(60, side.wall match
-         {
-         case true => vertDispLine.draw(6, Gray) :: Nil
-         case _ => ifTiles(_.colour == _.colour, (t1, _) => vertDispLine.draw(1, t1.colour.contrastBW))
-      })
-      Disp2(Nil, line)
-   }
-   def dSides: Disp2 = ofSidesDisplayFold(fSide)
+    )
+    
+    Disp2(Nil, line)
+  }
+    
+  def dSides: Disp2 = ofSidesDisplayFold(fSide)
+  def mapObjs: GraphicElems = (ofTilesDisplayFold(fHex) ++ dSides ).collapse//ofHexsDisplayFold(fHex).collapse
      
-   def mapObjs: GraphicElems = (ofTilesDisplayFold(fHex) ++ dSides ).collapse//ofHexsDisplayFold(fHex).collapse
-     
-   mapPanel.mouseUp = (v, but: MouseButton, clickList) => (but, selected, clickList) match
-   {
-     case (LeftButton, _, cl) =>
-       { selected = clickList.fHead(Nil, List(_))
-         statusText = selected.headOption.fold("Nothing Clicked")(_.toString)
-         eTop()            
-       }
-     case (RightButton, List(squad : Squad), List(newTile: ZugTile)) =>
-       { val newCood = newTile.cood
-         val oldCood = squad.cood
-         if (HexGrid.adjTileCoodsOfTile(oldCood).contains(newCood) && squad.canMove(newTile))
-         { val oldTile = grid.getTile(oldCood)
-           oldTile.lunits = oldTile.lunits.removeFirst(_ == squad)
-           squad.cood = newCood
-           newTile.lunits ::= squad             
-           repaintMap
-         }
-         else deb("No Move" -- oldCood.toString -- newCood.toString)
-       }      
-     case _ => deb("Other" -- clickList.toString)
-   }   
-   eTop()
-   mapPanel.repaint(mapObjs)
+  mapPanel.mouseUp = (v, but: MouseButton, clickList) => (but, selected, clickList) match
+  {
+    case (LeftButton, _, cl) =>
+    { selected = clickList.fHead(Nil, List(_))
+      statusText = selected.headOption.fold("Nothing Clicked")(_.toString)
+      eTop()            
+    }
+    
+    case (RightButton, List(squad : Squad), List(newTile: ZugTile)) if
+    (HexGrid.adjTileCoodsOfTile(squad.cood).contains(squad.cood) && squad.canMove(newTile)) =>
+      {
+        val newCood = newTile.cood
+        val oldCood = squad.cood
+        val oldTile = grid.getTile(oldCood)
+        oldTile.lunits = oldTile.lunits.removeFirst(_ == squad)
+        squad.cood = newCood
+        newTile.lunits ::= squad             
+        repaintMap
+      }
+      
+    case (RightButton, List(squad : Squad), List(newTile: ZugTile)) => deb("No Move" -- squad.cood.toString -- newTile.cood.toString)
+    case _ => deb("Other" -- clickList.toString)
+  }   
+   
+  eTop()
+  mapPanel.repaint(mapObjs)
 }
   
