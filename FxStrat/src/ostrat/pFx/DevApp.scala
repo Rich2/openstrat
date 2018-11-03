0c8b23b0c69e880b7363127d3546cf5b990ee819
Richtype <rr1@tutanota.com>
2018-11-03 07:21:04 +0000

    Less Scalafx

--- FxStrat/src/ostrat/pFx/DevApp.scala
+++ FxStrat/src/ostrat/pFx/DevApp.scala
@@ -1,11 +1,11 @@
 /* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
 package ostrat
 package pFx
-import scalafx.application.JFXApp
+//import scalafx.application.JFXApp
 
 /** Name should possibly be DevAppFx */
-object DevApp  extends JFXApp
+object DevApp  extends javafx.application.Application//JFXApp
 {   
    val pair = pDev.Apps.curr//(fromRsonFileFindElse(openStratDir / "Dev/AppNum.txt", 1))
-   stage = new RStage(cf => pair._1(CanvasFx(cf)), pair._2)  
+   def start(stage: javafx.stage.Stage): Unit = new RStage(cf => pair._1(CanvasFxAlt(cf)), pair._2)  
 }
--- FxStrat/src/ostrat/pFx/RSApp.scala
+++ FxStrat/src/ostrat/pFx/RSApp.scala
@@ -1,22 +1,22 @@
 /* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
 package ostrat
 package pFx
-import scalafx.Includes._, scalafx.application.JFXApp, scalafx.scene._ 
+import scalafx.Includes._, scalafx.application.JFXApp//, scalafx.scene._ 
 
-class RSApp(f: canvas.Canvas => Unit, str: String) extends JFXApp
+class RSApp(f: javafx.scene.canvas.Canvas => Unit, str: String) extends JFXApp
 {   
    stage = new RStage(f, str)  
 }
 
 /** N needs tidying up. the x value needs to be a developer setting. */
-case class RStage(fDisp: canvas.Canvas => Unit, tStr: String) extends JFXApp.PrimaryStage  
+case class RStage(fDisp: javafx.scene.canvas.Canvas => Unit, tStr: String) extends JFXApp.PrimaryStage  
 {  
   x = settingFromFileElse('xOffset, "Dev/GeneralDevSettings.rson", 0)//Sets default x value
   //y = 400// settingFromFileElse('yOffset, "Dev/GeneralDevSettings.rson", -200)//Sets default y value
   class GuiDispFx extends scalafx.scene.Scene
   {
     val bounds = javafx.stage.Screen.getPrimary.getVisualBounds  
-    val canvasCanvas: canvas.Canvas = new canvas.Canvas(bounds.width - 8, bounds.height - 40)      
+    val canvasCanvas: javafx.scene.canvas.Canvas = new javafx.scene.canvas.Canvas(bounds.width - 8, bounds.height - 40)      
     content = canvasCanvas 
     fDisp(canvasCanvas)
   }
