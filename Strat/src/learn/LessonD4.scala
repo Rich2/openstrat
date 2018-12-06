0634589239097736a627f908cf549c776854658a
w0d <rr1@tutanota.com>
2018-12-06 23:43:36 +0000

    D4 updated for settings.

--- Strat/src/learn/LessonD4.scala
+++ Strat/src/learn/LessonD4.scala
@@ -4,15 +4,19 @@ import ostrat._, geom._, pCanv._
 
 /** Under construction */
 case class LessonD4(canv: CanvasPlatform) extends CanvasSimple("Lesson D4")
-{
-  val s1 = 5.str
-  val s2 = 2.1.str
-  val s3 = false.str
-  val s4 = "Hello World!".str
-  val s5 = Vec2(2.3, -43.8).str
-  //List(5, 2.1, false, "Hello World!", Vec2(2.3, -43.8)).map(_.str)
-    
-  val strs = List(s1, s2, s3, s4, s5)map(_.toString)
-  repaint(TextGraphic.lines(strs, lineSpacing = 1.5, posn = -250 vv 0, align = TextLeft))
+{  
+  val r1 = Rval(5) - 2.1 - false - "Hello World!" - (2.3 vv -43.8) - Array(4, 5, 6)
+  val s1 = r1.str
+  val s2 = Sett('Age, 5).ap('Average, -2.1).ap('Open, false).ap('Greeting, "Hello World!"). ap('Posn, 2.3 vv -43.8).
+  ap('Scores, Array(4, 5, 6)).str
+ 
+  val c1 = s2.findSetting[Boolean]('Open)
+  val c2 = s2.findSetting[Boolean]('Guilty)
+  val c3 = s2.findSetting[Int]('Posn)
+  val c4 = s2.findSetting[Vec2]('Posn)
+  val cc = TextGraphic.lines(List(c1, c2, c3, c4).map(_.toString), lineSpacing = 1.5, posn = -250 vv -100, align = TextLeft)
+  
+  def ft(y: Double, str: String)  = TextGraphic(str, 24, -250 vv y, align = TextLeft) 
+  repaint(ft(300 , s1) :: ft(100, s2) :: cc)
   
 }
--- Util/src/ostrat/Rson.scala
+++ Util/src/ostrat/Rson.scala
@@ -1,8 +1,26 @@
 /* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
 package ostrat
 
-
-object Rson
+class Rval(val str: String) extends AnyVal
 {
-   
+  def - [A](value: A)(implicit ev: Persist[A]): Rval = new Rval(str + "\n" + ev.persist(value) + ";")
 }
+
+object Rval
+{
+  def apply[A](value: A)(implicit ev: Persist[A]): Rval = new Rval(ev.persist(value) + ";")
+}
+
+class Sett(val str: String) extends AnyVal
+{
+  def ap[A](settSym: Symbol, value: A)(implicit ev: Persist[A]): Sett =
+  {
+    new Sett(str + "\n" + settSym.name + " = " + ev.persist(value) + ";")
+  }
+}
+
+object Sett
+{
+  def apply[A](settSym: Symbol, value: A)(implicit ev: Persist[A]): Sett = new Sett(settSym.name + " = " + ev.persist(value) + ";")
+}
+
