28183975940561fa9b01f8286281bde33977a9b1
w0d <rr1@tutanota.com>
2018-12-14 03:05:53 +0000

    Minor.

--- Util/src/ostrat/pParse/packageParse.scala
+++ Util/src/ostrat/pParse/packageParse.scala
@@ -2,10 +2,10 @@ package ostrat
 
 /** This is the RSON package. Readable Succinct Object Notation. It could be just as accurately be described as Readable Succinct Data Notation, but
  *  that wouldn't scan as well and indicate it as a better replacement for JSON. RSON uses the standard semicolon separated statements combined with
- *  multilevel bracket hierarchy syntax familiar from C, C++, Java and JavaScript. Its main innovations are allowing statements to be partitioned into
- *  comma delineated clauses, the empty statement and the empty clause. It uses a context free operator precedence hierarchy, with the exception of
- *  allowing the 4 ( - + ~ !) operator to be used as prefix operators. Beyond this it generally follows Scala syntax where this doesn't conflict with
- *  the preceding rules.
+ *  multilevel bracket hierarchy syntax familiar from C, C++, Java and JavaScript. Its main innovations over JSON and XML are allowing statements to
+ *  be partitioned into comma delineated clauses, the empty statement, the empty clause and type inference. It uses a context free operator
+ *  precedence hierarchy, with the exception of allowing the 4 ( - + ~ !) operator to be used as prefix operators. Beyond this it generally follows
+ *  Scala syntax where this doesn't conflict with the preceding rules.
  *  
  *  The above allows it to combine a high level of human readability, succinctness, non-programmer / non-expert write-ability, programmer flexibility
  *  and composability and fast-parsing. The initial motivating use case was strategy games, which require huge amounts of modable date files. XML and
