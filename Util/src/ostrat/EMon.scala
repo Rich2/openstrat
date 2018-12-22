e3477ad1f413406c3c470cd87356caedf9e4f380
w0d <rr1@tutanota.com>
2018-12-22 12:31:53 +0000

    Minor.

--- Util/src/ostrat/EMon.scala
+++ Util/src/ostrat/EMon.scala
@@ -13,44 +13,17 @@ sealed trait EMon[+A]
   }
   
   def errs: StrList
-  def map[B](f: A => B): EMon[B] = fold(Bad[B](_), a => Good[B](f(a)))   
-  def flatMap[B](f: A => EMon[B]): EMon[B] = fold(Bad(_), f(_))
-  def foreach(f: A => Unit): Unit = fold(errs => {}, f(_))
-  def getElse[A1 >: A](elseValue: => A1): A1 = fold[A1](errs => elseValue, a => a)
-  def biMap[L2, R2](fLeft: StrList => L2, fRight: A => R2): Either[L2, R2] = fold(errs => Left(fLeft(errs)), a => Right(fRight(a)))
-  def toEither: Either[StrList, A] = fold(Left(_), Right(_))
-  def eitherMap[D](f: A => D): Either[StrList, D] = fold(Left(_), a => Right(f(a)))
-  def eitherFlatMap[D](f: A => Either[StrList, D]): Either[StrList, D] = fold(Left(_), f(_))
-   
-  def map2[A2, B](eMon2: EMon[A2], f: (A, A2) => B): EMon[B] = this match
-  {
-    case Good(a) => eMon2.map(a2 => f(a, a2))
-    case Bad(errs1) => Bad[B](errs1 ::: eMon2.errs)
-  }
-   
-  def map3[A2, A3, B](eMon2: EMon[A2], eMon3: EMon[A3], f: (A, A2, A3) => B): EMon[B] = this match
-  {
-    case Bad(errs1) => Bad[B](errs1 ::: eMon2.errs ::: eMon3.errs)
-    case Good(a1) => eMon2 match
-    {
-      case Bad(errs2) => Bad[B](errs2 ::: eMon3.errs)
-      case Good(a2) => eMon3.map(a3 => f(a1, a2, a3))        
-    }            
-  }
- 
-  def map4[A2, A3, A4, B](eMon2: EMon[A2], eMon3: EMon[A3], eMon4: EMon[A4], f: (A, A2, A3, A4) => B): EMon[B] = this match
-  {
-    case Bad(errs1) => Bad[B](errs1 ::: eMon2.errs ::: eMon3.errs ::: eMon4.errs)
-    case Good(a1) => eMon2 match
-    {
-      case Bad(errs2) => Bad[B](errs2 ::: eMon3.errs ::: eMon4.errs)
-      case Good(a2) => eMon3 match
-      {
-        case Bad(errs3) => Bad[B](errs3 ::: eMon4.errs)
-        case Good(a3) =>eMon4.map(a4 => f(a1, a2, a3, a4))        
-      }
-    }            
-  }
+  def map[B](f: A => B): EMon[B]  
+  def flatMap[B](f: A => EMon[B]): EMon[B]
+  def foreach(f: A => Unit): Unit
+  def getElse[A1 >: A](elseValue: => A1): A1 
+  def biMap[L2, R2](fLeft: StrList => L2, fRight: A => R2): Either[L2, R2]
+  def toEither: Either[StrList, A] 
+  def eitherMap[D](f: A => D): Either[StrList, D]
+  def eitherFlatMap[D](f: A => Either[StrList, D]): Either[StrList, D]   
+  def map2[A2, B](eMon2: EMon[A2], f: (A, A2) => B): EMon[B]
+  def map3[A2, A3, B](eMon2: EMon[A2], eMon3: EMon[A3], f: (A, A2, A3) => B): EMon[B]
+  def map4[A2, A3, A4, B](eMon2: EMon[A2], eMon3: EMon[A3], eMon4: EMon[A4], f: (A, A2, A3, A4) => B): EMon[B]
 }
 
 /** The Good sub class of EMon[+A]. This corresponds, but is not functionally equivalent to an Either[List[String], +A] based 
@@ -58,10 +31,50 @@ sealed trait EMon[+A]
 case class Good[+A](val value: A) extends EMon[A] 
 {
   def errs: StrList = Nil
+  override def map[B](f: A => B): EMon[B] = Good[B](f(value))   
+  override def flatMap[B](f: A => EMon[B]): EMon[B] = f(value)
+  override def foreach(f: A => Unit): Unit = f(_)
+  override def getElse[A1 >: A](elseValue: => A1): A1 = value
+  override def biMap[L2, R2](fLeft: StrList => L2, fRight: A => R2): Either[L2, R2] = Right(fRight(value))
+  override def toEither: Either[StrList, A] = Right(value)
+  override def eitherMap[D](f: A => D): Either[StrList, D] = Right(f(value))
+  override def eitherFlatMap[D](f: A => Either[StrList, D]): Either[StrList, D] = f(value)
+  override def map2[A2, B](eMon2: EMon[A2], f: (A, A2) => B): EMon[B] = eMon2.map(a2 => f(value, a2))
+  
+  override def map3[A2, A3, B](eMon2: EMon[A2], eMon3: EMon[A3], f: (A, A2, A3) => B): EMon[B] = eMon2 match
+  {
+    case Bad(errs2) => Bad[B](errs2 ::: eMon3.errs)
+    case Good(a2) => eMon3.map(a3 => f(value, a2, a3))                
+  }
+ 
+  override def map4[A2, A3, A4, B](eMon2: EMon[A2], eMon3: EMon[A3], eMon4: EMon[A4], f: (A, A2, A3, A4) => B): EMon[B] = eMon2 match
+  {
+    case Bad(errs2) => Bad[B](errs2 ::: eMon3.errs ::: eMon4.errs)
+    case Good(a2) => eMon3 match
+    {
+      case Bad(errs3) => Bad[B](errs3 ::: eMon4.errs)
+      case Good(a3) =>eMon4.map(a4 => f(value, a2, a3, a4))        
+    }
+  }  
 }
 
 /** The errors case of EMon[+A]. This corresponds, but is not functionally equivalent to an Either[List[String], +A] based Left[List[String], +A]. */
-case class Bad[+A](errs: StrList) extends EMon[A]// = Left[List[ParseErr], B]
+case class Bad[+A](errs: StrList) extends EMon[A]
+{
+  override def map[B](f: A => B): EMon[B] = Bad[B](errs)   
+  override def flatMap[B](f: A => EMon[B]): EMon[B] = Bad(errs)
+  override def foreach(f: A => Unit): Unit = {}
+  override def getElse[A1 >: A](elseValue: => A1): A1 = elseValue
+  override def biMap[L2, R2](fLeft: StrList => L2, fRight: A => R2): Either[L2, R2] = Left(fLeft(errs))
+  override def toEither: Either[StrList, A] = Left(errs)
+  override def eitherMap[D](f: A => D): Either[StrList, D] = Left(errs)
+  override def eitherFlatMap[D](f: A => Either[StrList, D]): Either[StrList, D] = (Left(errs))
+  override def map2[A2, B](eMon2: EMon[A2], f: (A, A2) => B): EMon[B] = Bad[B](errs ::: eMon2.errs)   
+  override def map3[A2, A3, B](eMon2: EMon[A2], eMon3: EMon[A3], f: (A, A2, A3) => B): EMon[B] = Bad[B](errs ::: eMon2.errs ::: eMon3.errs) 
+  
+  override def map4[A2, A3, A4, B](eMon2: EMon[A2], eMon3: EMon[A3], eMon4: EMon[A4], f: (A, A2, A3, A4) => B): EMon[B] = 
+    Bad[B](errs ::: eMon2.errs ::: eMon3.errs ::: eMon4.errs)    
+}
 
 object ParseErr
 {
