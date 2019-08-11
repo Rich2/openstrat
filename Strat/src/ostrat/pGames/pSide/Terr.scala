b1b3e10b1944fc0469103cacf576ba9d8c5ce540
richtype <rr1@tutanota.com>
2019-08-11 21:31:35 +0100

    Trying new type of tile Grid.

--- Strat/src/ostrat/pGames/pSide/Terr.scala
+++ Strat/src/ostrat/pGames/pSide/Terr.scala
@@ -7,17 +7,29 @@ object Sea extends Terr { override def str = "Sea"}
 case class Coast(p1: Int, p2: Int = 0, p3: Int = 0, p4: Int = 0, p5: Int = 0, p6: Int = 0) extends Terr
 { def str = "Coast"}
 
-trait HGrid[TileT]
+trait TGrid[TileT]
 {
+  type GridT[A] <: TGrid[A]
   def ts: Array[TileT]
-  def map[B](newGrid: HGrid[B], f: TileT => B): HGrid[B] =
-  {
-    val res = newGrid
+  def arrRows: Array[Int]
+  def numRows: Int = arrRows.length / 2 - 1
+  def rowIndex(rowNum: Int): Int = arrRows(rowNum * 2)
+  def xRowStart(rowNum: Int): Int = arrRows(rowNum * 2 + 1)
+  //def getTile
+
+  def map[B](mapGrid: Array[Int] => GridT[B], f: TileT => B): GridT[B] =
+  { val res = mapGrid(arrRows)
     ts.iForeach((t, i) => res.ts(i) = f(t))
     res
   }
 }
-case class TGrid(ts: Array[Terr]) extends HGrid[Terr]
+
+trait HGrid[TileT] extends TGrid[TileT]
+{
+  type GridT[A] = HGrid[A]
+}
+
+class MyGrid(val ts: Array[Terr], val arrRows: Array[Int]) extends HGrid[Terr]
 
 object Game extends App
 {
