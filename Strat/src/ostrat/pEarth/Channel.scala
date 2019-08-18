e450a5249a65547dd20de26d554926094d7faa1b
w0d <rr1@tutanota.com>
2019-08-18 01:41:00 +0100

    Minor.

--- Strat/src/ostrat/pEarth/Channel.scala
+++ Strat/src/ostrat/pEarth/Channel.scala
@@ -12,16 +12,14 @@ object Channel
   {
     import WTile._, RowMulti.{apply => rm}
     val v204463 = HVDnLt2(2, 2)
-
-
     val v206463 = HVUpRt2(2, 2)
-    //val v206463s = StraitsUpRt(2, 2)
+    val v198463 = HVDown1(3)
 
     TGrid.rowMulti(460, MyGrid.apply,
       rm(468, 180, sea, hills * 3, plain * 3, sea * 2),
       rm(466, 178, sea, hills * 2, plain * 4, sea * 2),
-      rm(464, 180, sea * 2, plain * 4, Coast(dnRt = v206463, down = v204463, sideDR = Some(())), Coastal(dnLt = v206463), plain),
-      rm(462, 178, sea , hills * 3, sea * 2, Coastal(upRt = v204463), Coast(up = HVUpRt2(2, 2), upLt = v204463), plain),
+      rm(464, 180, sea * 2, plain * 2, Coast(dnRt = v198463), Coast(dnLt = v198463), Coast(dnRt = v206463, down = v204463, sideDR = Some(())), Coastal(dnLt = v206463), plain),
+      rm(462, 178, sea , hills * 3, sea, Coastal(up = v198463), Coastal(upRt = v204463), Coast(up = HVUpRt2(2, 2), upLt = v204463), plain),
       rm(460, 180, sea * 6, plain * 3)
     )
   }
@@ -30,7 +28,7 @@ object Channel
 object ChannelApp extends App
 {
   import EGrid80km.{coodToLatLong0Off200 => ctl }
-  val a = ctl(204 cc 464).degStr
+  val a = ctl(198 cc 463).degStr
   deb(a.toString)
 
 }
