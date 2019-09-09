ef0c7cad688d15f0c5529b9afe15311735732ef2
w0d <rr1@tutanota.com>
2019-09-09 11:09:07 +0100

    Changes to chess.

--- Strat/src/ostrat/pGames/pChess/ChessGrid.scala
+++ Strat/src/ostrat/pGames/pChess/ChessGrid.scala
@@ -1,35 +1,29 @@
 package ostrat
 package pGames.pChess
-import pGrid._
+import pGrid._, Colour._
 
+sealed trait Piece
+object Pawn extends Piece
+object Rook extends Piece
+object Knight extends Piece
+object Bishop extends Piece
+object Queen extends Piece
+object King extends Piece
 
-
-sealed trait Player
-{
-  outer =>
-  def isWhite: Boolean
-  sealed trait Piece
-  { def isWhite: Boolean = outer.isWhite
-  }
-  object Pawn extends Piece
-  object Rook extends Piece
-  object Knight extends Piece
-  object Bishop extends Piece
-  object Queen extends Piece
-  object King extends Piece
-}
+sealed trait Player extends WithColour
 
 object PWhite extends Player
-{
-  def isWhite = true
+{ def colour = White
 }
 
 object PBlack extends Player
-{
-  def isWhite: Boolean = false
+{ def colour = Black
 }
 
-class ChessGrid (val array: Array[Option[Player#Piece]]) extends AnyVal with SqSqArr[Option[Player#Piece]]
+/** Player Piece */
+case class PPiece(player: Player, piece: Piece)
+
+class ChessGrid (val array: Array[Option[PPiece]]) extends AnyVal with SqSqArr[Option[PPiece]]
 { type GridT = ChessGrid
   override def thisFac = new ChessGrid(_)
   def size = 8
