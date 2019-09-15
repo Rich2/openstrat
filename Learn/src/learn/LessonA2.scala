24f834ae91dbe8ae65e87c697383d58c0ad69095
w0d <soccamedia@yahoo.com>
2019-09-15 01:00:35 +0100

    comment update

--- Learn/src/learn/LessonA2.scala
+++ Learn/src/learn/LessonA2.scala
@@ -8,7 +8,7 @@ case class LessonA2(canv: CanvasPlatform) extends CanvasSimple("Lesson A2")
     TextGraphic("This text is in the centre of the frame.", 18),
 
     //Here we create another the second number is the number of pixels up or down
-    TextGraphic("This text is 200 pixels up.", 18, 0 vv 200, Blue),
+    TextGraphic("This text is 300 pixels up.", 18, 0 vv 300, Red),
 
     TextGraphic("This text is 400 pixels right, down 200", 25, 400 vv -200),
     //TextGraphic("This text can't be seen till you uncomment it and rebuild the programme.", 18, 100 vv -125, Violet)
