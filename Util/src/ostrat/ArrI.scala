1d7cdfde32463082bde2aaeda6679a3ec459264a
w0d <rr1@tutanota.com>
2019-10-07 12:48:58 +0100

    #54

--- Util/src/ostrat/ArrI.scala
+++ Util/src/ostrat/ArrI.scala
@@ -8,10 +8,10 @@ trait ArrN[+T] extends Any
   //@uncheckedVariance def array: Array[T]
   //def length: Int = array.length
 }
-object ArrB
+/*object ArrB
 {
 
-}
+}*/
 
 class ArrR[+T <: AnyRef](val array: Array[T] @uncheckedVariance) extends AnyVal with
   ArrN[T]
@@ -20,7 +20,7 @@ class ArrR[+T <: AnyRef](val array: Array[T] @uncheckedVariance) extends AnyVal 
   @inline def apply(index: Int): T = array(index)
   def ++[AA >: T <: AnyRef](operand: ArrR[AA] @uncheckedVariance)(implicit ct: ClassTag[AA]): ArrR[AA] =
   {
-    var newArray: Array[AA] = new Array[AA](length + operand.length)
+    val newArray: Array[AA] = new Array[AA](length + operand.length)
     var count = 0
     while (count < length)
     { newArray(count) = apply(count)
@@ -36,16 +36,87 @@ class ArrR[+T <: AnyRef](val array: Array[T] @uncheckedVariance) extends AnyVal 
 }
 
 object ArrR
-{
-  def apply[A <: AnyRef](inp: A*)(implicit ct: ClassTag[A]): ArrR[A] = new ArrR[A](inp.toArray)
+{ def apply[A <: AnyRef](inp: A*)(implicit ct: ClassTag[A]): ArrR[A] = new ArrR[A](inp.toArray)
+}
+
+trait ArrValue[T] extends Any with ArrN[T]
+{ def array: Array[T]
+  @inline def length: Int = array.length
+  //def newArray(length: Int): Array[T]
+  def newArr(length: Int): ArrValue[T]
+  @inline def apply(index: Int): T = array(index)
+
+  def ++(operand: ArrValue[T] ): ArrValue[T] =
+  {
+    val res: ArrValue[T] = newArr(length + operand.length)
+    val resArray: Array[T] = res.array
+    var count = 0
+    while (count < length)
+    { resArray(count) = apply(count)
+      count += 1
+    }
+    var count2 = 0
+    while (count2 < operand.length)
+    { resArray(count) = operand(count2)
+      count += 1; count2 += 1
+    }
+    res
+  }
 }
 
 
-class ArrI(val array: Array[Int]) extends AnyVal with ArrN[Int]
-{
-
+class ArrI(val array: Array[Int]) extends AnyVal with ArrValue[Int]
+{ //override def newArray(length: Int): Array[Int] = new Array[Int](length)
+  override def newArr(length: Int): ArrI = new ArrI(new Array[Int](length))
 }
-class ArrD(val array: Array[Double]) extends AnyVal with ArrN[Double]
-{
 
+object ArrI
+{
+  def apply(inp: Int*): ArrI = new ArrI(inp.toArray)
 }
+
+class ArrD(val array: Array[Double]) extends AnyVal with ArrValue[Double]
+{// override def newArray(length: Int): Array[Double] = new Array[Double](length)
+  override def newArr(length: Int): ArrD = new ArrD(new Array[Double](length))
+}
+
+/*
+/** Using Att as temporary name, can be switched to Arr later to replace type alias for ArraySeq. */
+class Att[+A](val array: Array[A] @scala.annotation.unchecked.uncheckedVariance) extends AnyVal
+{
+  @inline def foreach[U](f: A => U): Unit = array.foreach(f)
+
+  def map[B](f: A => B)(implicit ct: ClassTag[B]): Att[B] =
+  { var count = 0
+    val newArray: Array[B] = new Array[B](length)
+    while (count < length) { newArray(count) = f(array(count)); count += 1}
+    new Att(newArray)
+  }
+
+  //def flatMap[B](f: A => B)(implicit ct: ClassTag[B]): Att[B] =
+
+  /* Maps from A to B like normal map,but has an additional accumulator of type C that is discarded once the traversal is completed */
+  def mapWithAcc[B, C](initC: C)(f: (A, C) => (B, C))(implicit ct: ClassTag[B]): Arr[B] =
+  { val accB: Buff[B] = Buff()
+    var accC: C = initC
+    foreach { a =>
+      val (newB, newC) = f(a, accC)
+      accB += newB
+      accC = newC
+    }
+    accB.toArr
+  }
+
+  /** Replaces all instances of the old value with the new value */
+ // def replace(oldValue: A, newValue: A): Att[A] = map { it => if (it == oldValue) newValue else it }
+
+  def reverseForeach(f: A => Unit): Unit =
+  { var count = length - 1
+    while(count >= 0){ f(apply(count)); count -= 1}
+  }
+
+  /*def ifAppendArr[B >: A](b: Boolean, newElems: => Att[B]): Att[B] = ife(b, this ++ newElems, this)
+  def optAppend[B >: A](optElem: Option[B]): Att[B] = optElem.fold[Arr[B]](this)(b => this :+ b)
+  def optAppends[B >: A](optElems: Option[Att[B]]): Arr[B] = optElems.fold[Arr[B]](this)(bs => this ++ bs)*/
+}
+*/
--- Util/test/src/ostrat/ArrTest.scala
+++ Util/test/src/ostrat/ArrTest.scala
@@ -3,18 +3,23 @@ import utest._
 
 object ArrTest extends TestSuite
 {
-  trait MyT
+  trait MyT{ def i: Int}
   case class My1(i: Int) extends MyT
-  case class My2(j: Int) extends MyT
+  case class My2(i: Int) extends MyT
   val tests = Tests
   {
     val ar1: ArrR[My1] = ArrR(My1(1), My1(2), My1(3))
     val ar2: ArrR[My2] = ArrR(My2(4), My2(5))
     val ar12: ArrR[MyT] = ar1 ++ ar2
+    val ai1 = ArrI(1, 2, 3, 4)
+    val ai2 = ArrI(5, 6)
+    val ai12 = ai1 ++ ai2
     'test1 -
     {
       ar1.length ==> 3
       ar12.length ==> 5
+      ar12(4).i ==> 5
+      ai12.length ==> 6
       //assert(at2.array.isInstanceOf[Array[Int]])
      // assert(at3(1) == true)
       //assert(at3.array.isInstanceOf[Array[Boolean]])*/
