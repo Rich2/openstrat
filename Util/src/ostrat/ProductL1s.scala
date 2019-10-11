90e9fdd6779b7713a9a2cdb22b02c7a5321096dd
Rich2 <rr1@tutanota.com>
2019-10-12 00:31:53 +0100

    Added ProductLongs.

---
+++ Util/src/ostrat/ProductL1s.scala
@@ -0,0 +1,38 @@
+/* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
+package ostrat
+
+trait ProdL1 extends Any
+{ def intValue: Long
+  @inline def _1 : Long = intValue
+}
+
+trait ProductL1s[A <: ProdL1] extends Any with ProductLongs[A]
+{
+  final override def productSize: Int = 1
+  def newElem(intValue: Long): A
+  final override def apply(index: Int): A = newElem(array(index))
+  final override def setElem(index: Int, elem: A): Unit = array(index) = elem.intValue
+  
+  /** This method could be made more general. */
+  def findIndex(value: A): Option[Int] =
+  {
+    var count = 0
+    var acc: Option[Int] = None
+    var continue = true
+    while (continue == true & count < length)
+    {
+      if (value.intValue == array(count))
+      { acc = Some(count)
+        continue = false
+      }
+      count += 1
+    }
+    acc
+  }
+  def toArrs: Arr[Arr[Long]] = map(el => Arr(el.intValue))
+  def foreachArr(f: Arr[Long] => Unit): Unit = foreach(el => f(Arr(el.intValue)))
+}
+
+trait ProductL1sBuff[A <: ProdL1, M <: ProductL1s[A]] extends Any with ProductLongsBuff[A, M]
+{ override def append(newElem: A): Unit = buffer.append(newElem._1)
+}
---
+++ Util/src/ostrat/ProductLongs.scala
@@ -0,0 +1,46 @@
+package ostrat
+import collection.mutable.ArrayBuffer
+
+/** An immutable collection of Elements that inherit from a Product of an Atomic value: Double, Long, Long or Float. They are stored with a backing
+ * Array[Long] They are named ProductLongs rather than ProductIs because that name can easlily be confused with ProductI1s. */
+trait ProductLongs[A] extends Any with ProductVals[A]
+{ def array: Array[Long]
+  def arrLen = array.length
+  def toArrs: Arr[Arr[Long]]
+  def foreachArr(f: Arr[Long] => Unit): Unit
+
+  override def toString: String =
+  { var body = ""
+    var start = true
+    foreachArr { arr =>
+      val el = arr.toStrsCommaNoSpaceFold(_.toString)
+      if(start == true) {body = el; start = false}
+      else  body = body + ";  " + el
+    }
+    typeStr + body.enParenth
+  }
+}
+
+/** A mutable collection of Elements that inherit from a Product of an Atomic value: Double, Long, Long or Float. They are stored with a backing
+ * ArrayBuffer[Long] They are named ProductLongsBuff rather than ProductIsBuff because that name can easlily be confused with ProductI1sBuff. */
+trait ProductLongsBuff[A, M <: ProductLongs[A]] extends Any with ProductValsBuff[A, M]
+{ def buffer: ArrayBuffer[Long]
+  def toArray: Array[Long] = buffer.toArray[Long]
+  def unBuff: M
+  def append(newElem: A): Unit
+  def addAll(newElems: M): Unit = buffer.addAll(newElems.array)
+}
+
+abstract class ProductLongsBuilder[A, M <: ProductLongs[A]](typeStr: String) extends ProductValsBuilder[A, M](typeStr)
+{ type VT = Long
+  override def fromBuffer(buf: Buff[Long]): M = fromArray(buf.toArray)
+  override def newBuffer: Buff[Long] = Buff[Long](0)
+}
+
+trait ProductLongsCompanion[M]
+{ def fromBuffer(buff: Buff[Long]): M = fromArray(buff.toArray[Long])
+  val factory: Long => M
+  def fromArray(array: Array[Long]): M
+}
+
+
