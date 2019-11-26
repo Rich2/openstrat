ac40912715945c771cd5ca925f7d3843a616ceb3
w0d <35996314+w0d@users.noreply.github.com>
2019-11-26 02:52:48 +0000

    keydown & keyUp => key & keyName

--- Graphic/examples/src/learn/LessonC6.scala
+++ Graphic/examples/src/learn/LessonC6.scala
@@ -6,9 +6,10 @@ import ostrat._, geom._, pCanv._, Colour._
 case class LessonC6(canv: CanvasPlatform) extends CanvasSimple("Lesson C6")
 {
   deb("Lesson C6")
-  val startText = TextGraphic("Press a key.", 28, 0 vv 400)
+  val startText = TextGraphic("Press a key.", 28, 0 vv 300)
   repaints(startText)  
   
-  canv.keyReleased = () => {deb("key"); repaints(TextGraphic("You pressed a key.", 28, 0 vv 0, Green)) }          
+  canv.keyPressed = (key: String, code: String) => { repaints(TextGraphic("key down '"+key+"' with code = "+code, 28, 0 vv 0, Blue)) }          
+  canv.keyReleased = (key: String, code: String) => { repaints(TextGraphic("key up '"+key+"' with code = "+code, 28, 0 vv 100, Green)) }          
 //  }
 }
