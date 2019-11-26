fe44b281929bb538481ce72a4a8ff1e3a6bbcc05
w0d <35996314+w0d@users.noreply.github.com>
2019-11-26 04:40:52 +0000

    keyDown, keyUp standardised for jFx & js

--- Graphic/examples/src/learn/LessonC6.scala
+++ Graphic/examples/src/learn/LessonC6.scala
@@ -9,7 +9,7 @@ case class LessonC6(canv: CanvasPlatform) extends CanvasSimple("Lesson C6")
   val startText = TextGraphic("Press a key.", 28, 0 vv 300)
   repaints(startText)  
   
-  canv.keyPressed = (key: String, code: String) => { repaints(TextGraphic("key down '"+key+"' with code = "+code, 28, 0 vv 0, Blue)) }          
-  canv.keyReleased = (key: String, code: String) => { repaints(TextGraphic("key up '"+key+"' with code = "+code, 28, 0 vv 100, Green)) }          
+  canv.keyDown = (key: String) => { repaints(TextGraphic("key down '"+key+"'", 28, 0 vv 0, Blue)) }          
+  canv.keyUp = (key: String) => { repaints(TextGraphic("key up '"+key+"'", 28, 0 vv 100, Green)) }          
 //  }
 }
