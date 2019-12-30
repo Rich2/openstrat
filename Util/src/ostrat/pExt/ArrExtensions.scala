653cd03ad1fcb9261f83197b4e39f71c2991a91a
Rich2 <rr1@tutanota.com>
2019-12-30 17:09:26 +0000

    #79

--- Util/src/ostrat/pExt/ArrExtensions.scala
+++ Util/src/ostrat/pExt/ArrExtensions.scala
@@ -1,20 +1,8 @@
 package ostrat
 package pExt
-import scala.reflect.ClassTag
 
 class ArrExtensions[A](thisArr: Arr[A])
-{ /* Maps from A to B like normal map,but has an additional accumulator of type C that is discarded once the traversal is completed */
-  def mapWithAcc[B, C](initC: C)(f: (A, C) => (B, C))(implicit ct: ClassTag[B]): Arr[B] =
-  { val accB: Buff[B] = Buff()
-    var accC: C = initC
-    thisArr.foreach { a =>
-      val (newB, newC) = f(a, accC)
-      accB += newB
-      accC = newC
-    }
-    accB.toArr
-  }
-
+{
   def optAppend(optElems: Option[Arr[A]]): Arr[A] = optElems.fold[Arr[A]](thisArr)(bs => thisArr ++ bs)
 
   /** Concatenates Arr if Some. Returns original Arr if operand is None. */
