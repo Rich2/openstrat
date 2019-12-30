3bbd63dc7a992529b01d742b65a5940f1058cb9a
Rich2 <rr1@tutanota.com>
2019-12-30 17:26:13 +0000

    #79 Removed optAppend from ArraySeq.

--- Graphic/src/ostrat/pCanv/Dist2Gui.scala
+++ Graphic/src/ostrat/pCanv/Dist2Gui.scala
@@ -38,16 +38,11 @@ trait Dist2Gui extends MapGui
    def invCanv(canvPoint: Vec2): Vec2 = ???//(canvPoint / scale).rotate(-rotation) + mapFocus   
    /** Translates an array of map points to an array of Canvas Display positions */
    def arrCanv(inp: Dist2s): Polygon = inp.pMap(toCanv(_))
-   final def repaintMap(): Unit =
-   {
-      val o2 = mapObjs      
-      mapPanel.repaintOld(o2)
-   }
-//   final def repaintMap(newObjs: Seq[CanvObj[_]]): Unit =
-//   {
-//      mapObjs = newObjs
-//      repaintMap
-//   }   
+
+  final def repaintMap(): Unit =
+  { val o2 = mapObjs
+    mapPanel.repaintOld(o2)
+  }
    
    def reFocus(newFocus: Dist2): Unit =
    {
--- Util/src/ostrat/pExt/ArrExtensions.scala
+++ Util/src/ostrat/pExt/ArrExtensions.scala
@@ -3,8 +3,6 @@ package pExt
 
 class ArrExtensions[A](thisArr: Arr[A])
 {
-  def optAppend(optElems: Option[Arr[A]]): Arr[A] = optElems.fold[Arr[A]](thisArr)(bs => thisArr ++ bs)
-
   /** Concatenates Arr if Some. Returns original Arr if operand is None. */
-  def optConcat[B >: A](optElems: Option[Arr[B]]): Arr[B] = optElems.fold[Arr[B]](thisArr)(bs => thisArr ++ bs)
+  @deprecated def optConcat[B >: A](optElems: Option[Arr[B]]): Arr[B] = optElems.fold[Arr[B]](thisArr)(bs => thisArr ++ bs)
 }
