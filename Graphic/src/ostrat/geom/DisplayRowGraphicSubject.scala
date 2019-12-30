6e311b895990b79f68ba06224a7a8b3db5d0653b
Rich2 <rr1@tutanota.com>
2019-12-30 17:07:33 +0000

    #79 DisplayRowGraphicsSubject now uses Refs

--- Graphic/src/ostrat/geom/DisplayRowGraphicSubject.scala
+++ Graphic/src/ostrat/geom/DisplayRowGraphicSubject.scala
@@ -3,9 +3,9 @@ package ostrat
 package geom
 
 /** Creates a row of GraphicSubject. Not sure about the usefulness of this specific class, or its implementation*/
-case class DisplayRowGraphicSubject(margin: Double, subjs: Arr[GraphicSubject])
+case class DisplayRowGraphicSubject(margin: Double, subjs: Refs[GraphicSubject])
 { /** Horrible code. Needs rewriting. */
-  def fromLeft(leftPt: Vec2): Arr[GraphicSubject] = subjs.mapWithAcc(leftPt.x + margin)((head, x) =>
+  def fromLeft(leftPt: Vec2): Refs[GraphicSubject] = subjs.mapWithAcc(leftPt.x + margin)((head, x) =>
     (head.slateX(x + head.width / 2),//.asInstanceOf[GraphicSubject]
       x + head.width + margin))
 }
