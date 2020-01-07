0963b7e12d8236e9453337b9878494d5f5d0a274
Rich2 <rr1@tutanota.com>
2020-01-07 14:14:42 +0000

    #79 Converted ArrOld object to factory method in OstratPackage object.

--- Util/src/ostrat/ArrOld.scala
+++
@@ -1,8 +0,0 @@
-/* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
-package ostrat
-import scala.collection.immutable.ArraySeq, reflect.ClassTag, collection.SeqFactory.UnapplySeqWrapper
-
-object ArrOld
-{ @inline def apply[A](inp: A *)(implicit ct: ClassTag[A]): ArrOld[A] = ArraySeq.apply(inp: _*)
-  @inline def unapplySeq[A](arr: ArraySeq[A]): UnapplySeqWrapper[A] = ArraySeq.unapplySeq(arr)
-}
