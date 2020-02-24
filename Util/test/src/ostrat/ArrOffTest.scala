1ada93d5e911f540f64b8042901102fc6870d299
Rich2 <rr1@tutanota.com>
2020-02-24 20:47:27 +0000

    Minor.

--- Util/test/src/ostrat/ArrOffTest.scala
+++ Util/test/src/ostrat/ArrOffTest.scala
@@ -3,15 +3,15 @@ import utest._
 
 object ArrOffTest  extends TestSuite
 {
-  implicit val array: Array[String] = Array("a1", "a2", "a3", "a4")
-  //val o1 = new ArrOff[String](0)
+  implicit val array: Refs[String] = Refs("a1", "a2", "a3", "a4")
+  val o1 = new RefsOff[String](0)
 
- /* val r1 = o1 match
-  { case ArrOff1("a1", _) => true
+  val r1 = o1 match
+  { case RefsOff1("a1") => true
     case _ => false
   }
 
-  val o2 = o1.drop1
+  /*val o2 = o1.drop1
 
   val r2 = o2 match
   { case ArrOff1("a2", _) => true
@@ -33,17 +33,17 @@ object ArrOffTest  extends TestSuite
   val r5 = r4 match
   { case ArrOff0(_) => true
     case _ => false
-  }*/
+  } */
 
   val tests = Tests
   {
-    'test1
-    { /*r1 ==> true
-      r2 ==> true
-      r2a ==> true
-      r3.offset ==> 2
-      r4.length ==> 0
-      r5 ==> true*/
+    "Test1" -
+    { r1 ==> true
+      //r2 ==> true
+      //r2a ==> true
+      //r3.offset ==> 2
+      //r4.length ==> 0
+      //r5 ==> true
     }
   }
 }
