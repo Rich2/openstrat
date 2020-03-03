2014387ab7aee990a37feb78b6be837fa3903f77
Rich2 <rr1@tutanota.com>
2020-03-03 17:27:26 +0000

    Various.

---
+++ Strat/src/gOne/MPlayer.scala
@@ -0,0 +1,28 @@
+/* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
+package ostrat
+package gOne
+import pGrid._
+
+/** A Player has a very simple token with a letter and colour for recognition." */
+case class MPlayer(player: Player, cood: Cood, move: Option[Cood] = None) extends WithColour
+{  
+  override def toString = player.toString
+  override def colour = player.colour
+}
+
+object MPlayer
+{
+  implicit val persistMPlayer: PersistEq[MPlayer] = new Persist3[Player, Cood, Option[Cood], MPlayer](
+    "MPlayer", "player", _.player, "cood",_.cood, "move", _.move, apply)
+}
+
+case class Player(val char: Char, val colour: Colour)
+{
+  override def toString = "Player " + char
+}
+
+object Player
+{
+  implicit val persistPlayer: PersistEq[Player] = Persist2[Char, Colour, Player]("Player", "char", _.char, "colour", _.colour, apply)
+}
+
--- Strat/src/gOne/OneGrid.scala
+++ Strat/src/gOne/OneGrid.scala
@@ -1,8 +1,11 @@
 package ostrat
 package gOne
-import pGrid._
+import pGrid._, Colour._
 
 object OneGrid
 {
+  object PlayerA extends Player('A', Red)
+  object PlayerB extends Player('B', Orange)
+  object PlayerC extends Player('C', Green)
   val grid = new HexGridReg(2, 10, 2, 6)
 }
