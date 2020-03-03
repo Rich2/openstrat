d198e4ce5c23b5485974af07b123ccc5fe82cfc0
Rich2 <rr1@tutanota.com>
2020-03-03 17:09:22 +0000

    #78

--- Strat/src/pGrid/HexGridReg.scala
+++ Strat/src/pGrid/HexGridReg.scala
@@ -4,27 +4,36 @@ package pGrid
 case class HexGridReg(xTileMin: Int, xTileMax: Int, yTileMin: Int, yTileMax: Int) extends HexGrid
 {
   /** Minimum x for Rows where y.Div4Rem2. */
-  def row2sxMin: Int = xTileMax.roundUpTo(_.div4Rem2)
+  def xRow2sMin: Int = xTileMax.roundUpTo(_.div4Rem2)
 
   /** Maximum x for Rows where y.Div4Rem2. */
-  def row2sxMax: Int = xTileMax.roundDownTo(_.div4Rem2)
+  def xRow2sMax: Int = xTileMax.roundDownTo(_.div4Rem2)
 
-  def row2sTileLen = ((row2sxMax - row2sxMin + 4) / 4).min0
+  def row2sTileLen = ((xRow2sMax - xRow2sMin + 4) / 4).min0
 
   /** Minimum x for Rows where y.Div4Rem0. */
-  def row0sxMin: Int = xTileMax.roundUpTo(_.div4Rem0)
+  def xRow0sMin: Int = xTileMax.roundUpTo(_.div4Rem0)
 
   /** Maximum x for Rows where y.Div4Rem0. */
-  def row0sxMax: Int = xTileMax.roundDownTo(_.div4Rem0)
+  def xRow0sMax: Int = xTileMax.roundDownTo(_.div4Rem0)
 
-  def row0sTileLen = ((row0sxMax - row0sxMin + 4) / 4).min0
+  def row0sTileLen = ((xRow0sMax - xRow0sMin + 4) / 4).min0
 
+  def yRow2sMin: Int = yTileMin.roundUpTo(_.div4Rem2)
+  def yRow2sMax: Int = yTileMax.roundDownTo(_.div4Rem2)
   /** Number of Rows where y.Div4Rem2. */
-  def numOfRow2s: Int = ((yTileMax.roundDownTo(_.div4Rem2) - yTileMin.roundUpTo(_.div4Rem2) + 4) / 4).min0
+  def numOfRow2s: Int = ((yRow2sMax - yRow2sMin + 4) / 4).min0
+
+  def yRow0sMin: Int = yTileMin.roundUpTo(_.div4Rem0)
+  def yRow0sMax: Int = yTileMax.roundDownTo(_.div4Rem0)
 
   /** Number of Rows where y.Div4Rem0. */
-  def numOfRow0s: Int = ((yTileMax.roundDownTo(_.div4Rem0) - yTileMin.roundUpTo(_.div4Rem0) + 4) / 4).min0
+  def numOfRow0s: Int = ((yRow0sMax - yRow0sMin + 4) / 4).min0
 
   override def numOfTiles: Int = numOfRow2s * row2sTileLen + numOfRow0s * row0sTileLen
-  override def allTilesForeach(f: Cood => Unit): Unit = ???
+
+  override def allTilesForeach(f: Cood => Unit): Unit =
+  { ijToForeach(yRow2sMin, yRow2sMax, 4)(xRow2sMin, xRow2sMax, 4)((y, x) => f(Cood(x, y)))
+    ijToForeach(yRow0sMin, yRow0sMax, 4)(xRow0sMin, xRow0sMax, 4)((y, x) => f(Cood(x, y)))
+  }
 }
--- Strat/src/pGrid/SquareGrid.scala
+++ Strat/src/pGrid/SquareGrid.scala
@@ -8,5 +8,5 @@ case class SquareGrid(xTileMin: Int, xTileMax: Int, yTileMin: Int, yTileMax: Int
   def numOfRows: Int = ((yTileMax.roundDownToEven - yTileMin + 2) / 2).min0
   def numOfTiles: Int = numOfRows * numOfTiles
 
-  override def allTilesForeach(f: Cood => Unit): Unit = ???
+  override def allTilesForeach(f: Cood => Unit): Unit = ijToForeach(yTileMin, yTileMax, 2)(xTileMin, xTileMax, 2)((y, x) => f(Cood(x, y)))
 }
