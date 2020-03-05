a534f4a96cb9e175866b782c210205da02b98aa5
Rich2 <rr1@tutanota.com>
2020-03-05 17:27:34 +0000

    Various.

--- Util/src/EMon/EMonInt.scala
+++ Util/src/EMon/EMonInt.scala
@@ -2,6 +2,7 @@ package ostrat
 
 sealed trait EMonInt extends EMonBase[Int]
 { def map[B, BB <: EMonBase[B]](f: Int => B)(implicit build: EMonBuild[B, BB]): BB
+  def getElse(elseValue: => Int): Int
 }
 
 final case class GoodInt(value: Int) extends EMonInt with GoodBase[Int]
@@ -12,12 +13,15 @@ final case class GoodInt(value: Int) extends EMonInt with GoodBase[Int]
   @inline override def foldErrs[B](fGood: Int => B)(fBad: Strings => B): B = fGood(value)
   override def get: Int = value
   override def foldDo(fGood: Int => Unit)(fBad: Strings => Unit): Unit = fGood(value)
+  override def getElse(elseValue: => Int): Int = value
 }
+
 case class BadInt(errs: Refs[String]) extends EMonInt with BadBase[Int]
 { override def map[B, BB <: EMonBase[B]](f: Int => B)(implicit build: EMonBuild[B, BB]): BB = build.newBad(errs)
   override def flatMap[B, BB <: EMonBase[B]](f: Int => BB)(implicit build: EMonBuild[B, BB]): BB = build.newBad(errs)
   override def fold[B](noneValue: => B)(fGood: Int => B): B = noneValue
   @inline override def foldErrs[B](fGood: Int => B)(fBad: Strings => B): B = fBad(errs)
+  override def getElse(elseValue: => Int): Int = elseValue
 }
 
 object NoInt extends BadInt(Refs())
@@ -35,6 +39,7 @@ final case class GoodInts(value: Ints) extends EMonInts with GoodBase[Ints]
   @inline override def foldErrs[B](fGood: Ints => B)(fBad: Strings => B): B = fGood(value)
   override def get: Ints = value
   override def foldDo(fGood: Ints => Unit)(fBad: Strings => Unit): Unit = fGood(value)
+
 }
 case class BadInts(errs: Refs[String]) extends EMonInts with BadBase[Ints]
 { override def map[B, BB <: EMonBase[B]](f: Ints => B)(implicit build: EMonBuild[B, BB]): BB = build.newBad(errs)
