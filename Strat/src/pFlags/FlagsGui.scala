b1bf7c8c5a742ce63dd6398cb183fc3d38d0dfa0
Rich2 <rr1@tutanota.com>
2020-03-06 22:46:01 +0000

    Minor.

--- Strat/src/pFlags/FlagsGui.scala
+++ Strat/src/pFlags/FlagsGui.scala
@@ -18,6 +18,7 @@ case class FlagsGui(canv: CanvasPlatform) extends CanvasNoPanels("Flags Gui")
       us.scale(150).slate(400, 350),
       uk.scale(200).slate(300, -350).rotate(10.degs),
       japan.scale(200).slate(650, 200).rotate(10.degs),
+      TextOutline("This is text outline at x = -400").slateX(-400),
    )
                
   mouseUp = (v, b, s) =>
