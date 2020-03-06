9c3aacbd9901cf66403a41cbab3b79362f3bbcc0
Rich2 <rr1@tutanota.com>
2020-03-06 17:45:31 +0000

    Minor.

--- Strat/src/pGrid/TileGrid.scala
+++ Strat/src/pGrid/TileGrid.scala
@@ -1,13 +1,13 @@
 package ostrat
 package pGrid
 
-/** A TileGrid is a description of an abstract TileGrid. It contains no data for the elements any TileGrid. The Data for TileGrids is stored in flat
- * arrays. The TileGrid gives the dimensions of a tileGrid. It has methods to interpret the data in flat Arrays created for that TileGrid
- * specification. It has methods to map the elements of an Array to the the 2 dimensional geometry of the Tile Grid. On its own a TileGrid can produce the
- * outlines of the grid, coordinates vector positions and other pure mathematical data. Combined with a simple function it can for example produce a
- * Chess board. Combined with a 64 length array it can produce a Chess board position. For anything but the most simple games, you will probably want
- * multiple arrays to describe the game state. The terrain for example may remain invariant, so the terrain data does not need to be reproduced with
- * every move.*/
+/** A TileGrid is a description of an abstract TileGrid. It contains no data for the elements of any particular TileGrid. The Data for TileGrids is
+ *  stored in flat arrays. The TileGrid gives the dimensions of a tileGrid. It has methods to interpret the data in flat Arrays created for that
+ *  TileGrid specification. It has methods to map the elements of an Array to the the 2 dimensional geometry of the Tile Grid. On its own a TileGrid
+ *  can produce the outlines of the grid, coordinates vector positions and other pure mathematical data. Combined with a simple function it can for
+ *  example produce a Chess board. Combined with a 64 length array it can produce a Chess board position. For anything but the most simple games, you
+ *  will probably want multiple arrays to describe the game state. The terrain for example may remain invariant, so the terrain data does not need to
+ *  be reproduced with every move.*/
 trait TileGrid
 { def xTileMin: Int
   def xTileMax: Int
