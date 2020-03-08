c100eb3ff0d2f32ecba859e795645e981e5c134c
Rich2 <rr1@tutanota.com>
2020-03-08 19:31:47 +0000

    Minor.

--- Util/src/EMon/Opt.scala
+++ Util/src/EMon/Opt.scala
@@ -1,24 +1,26 @@
 package ostrat
 
 trait Opt[A] extends Any
-{
-   def foreach(f: A => Unit): Unit
+{ def foreach(f: A => Unit): Unit
 }
 
 class OptRef[+A <: AnyRef](val value: A) extends AnyVal
-{
-   def foreach(f: A => Unit): Unit = if(value != null) f(value)
-   @inline def empty: Boolean = value != null
-   override def toString: String = if(value == null) "NoOpt" else "Some" + value.toString.enParenth
+{ def foreach(f: A => Unit): Unit = if(value != null) f(value)
+  @inline def empty: Boolean = value != null
+  override def toString: String = if(value == null) "NoOpt" else "Some" + value.toString.enParenth
 }
 
 trait OptInt extends Opt[Int]
+{ def map(f: Int => Int): OptInt
+}
 
 case class SomeInt(value: Int) extends OptInt
 { override def foreach(f: Int => Unit): Unit = f(value)
+  override def map(f: Int => Int): OptInt = SomeInt(f(value))
   //override def toString: String = "Some" + value.toString.enparenth
 }
 
 case object NoInt extends OptInt
 { override def foreach(f: Int => Unit): Unit = {}
+  override def map(f: Int => Int): OptInt = NoInt
 }
