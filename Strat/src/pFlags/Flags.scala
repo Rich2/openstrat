bcec9946601bf41cda93184be21af5d0bbbc9846
w0d <35996314+w0d@users.noreply.github.com>
2020-03-11 09:13:24 +0000

    updated usa star

--- Strat/src/pFlags/Flags.scala
+++ Strat/src/pFlags/Flags.scala
@@ -195,7 +195,7 @@ object UnitedStates extends Flag
   val starX = 1.9*2/5/12             //star horizontal spacing
   val starY = 1.0*7/13/10            //star vertical spacing
   val starScale = 4.0/5/13/1.9    //Diameter of star = four-fifths of the stripe width, Width of stripe = Height/13, Height = Width/1.9)
-  val star = Star5(0.4).scale(starScale).fill(White).slate(-0.95, 0.5)
+  val star = Star5().scale(starScale).fill(White).slate(-0.95, 0.5)
   val apply = 
   { val blueField = Rectangle.fromTL(0.76, 7.0/ 13, -0.95 vv 0.5).fill(Colour(0xFF3C3B6E))
     val stars = ijToMap(0, 5)(0, 4)((i, j) => (star.slate(starX + 2 * i * starX, - starY - 2 * j * starY)))
