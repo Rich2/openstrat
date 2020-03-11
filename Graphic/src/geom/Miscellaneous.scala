214f2e39f1df91922486cc5c51efc552b4a88804
w0d <35996314+w0d@users.noreply.github.com>
2020-03-11 12:57:22 +0000

    7 pointed star added to Miscellaneous.scala

--- Graphic/src/geom/Miscellaneous.scala
+++ Graphic/src/geom/Miscellaneous.scala
@@ -1,6 +1,8 @@
 package ostrat
 package geom
 
+import math.{Pi}
+
 object Star5
 {
   val classicRatio = 0.38
@@ -21,4 +23,18 @@ object Star5
 
   /** Not quite sure what this does. */
   def ptUpYCentred(ratio: Double = classicRatio): Polygon = apply(ratio).slateY(deg36.cos / 2 - 0.5)
+}
+
+object Star7
+{
+  val classicRatio = 0.38
+
+  /** The ratio for the standard star is approx 0.4. */
+  def apply(ratio: Double = classicRatio): Polygon =
+  { val l2: Vec2s = Vec2s(Vec2(0, 1), Vec2(0, ratio).rotate(-Angle(Pi / 7)))
+    iToFlatMap(0, 6)(i => l2.rotate(-Angle(Pi2 / 7) * i)).toPolygon
+  }
+
+  def fill(colour: Colour, ratio: Double = classicRatio): PolyFill = apply(ratio).fill(colour)
+
 }
