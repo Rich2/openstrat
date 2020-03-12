cf281cdfdb3f88824d219f29bda09b56436e191e
w0d <35996314+w0d@users.noreply.github.com>
2020-03-12 09:08:40 +0000

    Pentagram & Star3 Added
    Having troubles making Star6 so using 2 Star3 - can you help

--- Graphic/src/geom/Miscellaneous.scala
+++ Graphic/src/geom/Miscellaneous.scala
@@ -38,3 +38,27 @@ object Star7
   def fill(colour: Colour, ratio: Double = classicRatio): PolyFill = apply(ratio).fill(colour)
 }
 
+object Star3
+{
+  def apply(sideLength: Double = 1): Polygon = 
+  { Polygon((0 vv sideLength * math.sqrt(3) / 3),
+              (sideLength / 2 vv -sideLength * math.sqrt(3) / 6),
+              (-sideLength / 2 vv -sideLength * math.sqrt(3) / 6))
+  }
+    
+  def draw(lineWidth: Double = 1, colour: Colour): PolyDraw = apply().draw(lineWidth, colour)
+
+  def fill(colour: Colour): PolyFill = apply().fill(colour)
+
+}
+
+object Pentagram
+{
+  def apply(): Polygon =
+  { val l2: Vec2s = Vec2s(Vec2(0, 1), Vec2(0, -1).rotate(-deg36))
+    iToFlatMap(0, 4)(i => l2.rotate(-deg72 * i)).toPolygon
+  }
+
+  def draw(lineWidth: Double = 1, colour: Colour): PolyDraw = apply().draw(lineWidth, colour)
+
+}
