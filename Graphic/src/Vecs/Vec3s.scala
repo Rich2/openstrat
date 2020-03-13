214e18e5f7d0dd4b2a2149ffd591b6cb54fba05a
Rich2 <rr1@tutanota.com>
2020-03-13 22:44:35 +0000

    Created Vec3s class.

---
+++ Graphic/src/Vecs/Vec3s.scala
@@ -0,0 +1,10 @@
+package ostrat
+package geom
+
+class Vec3s(val array: Array[Double]) extends AnyVal with ArrProdDbl3[Vec3]
+{ type ThisT = Vec3s
+  override def typeStr: String = "Vec3s"
+  //override def elemBuilder(d1: Double, d2: Double, d3): Vec2 = Vec2.apply(d1, d2)
+  def newElem(d1: Double, d2: Double, d3: Double): ostrat.geom.Vec3 = ???
+  def unsafeFromArray(array: Array[Double]): Vec3s = ???
+}
