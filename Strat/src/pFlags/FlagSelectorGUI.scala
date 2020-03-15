34f02f29330d4f6144995b2c902548bd5378ea33
w0d <35996314+w0d@users.noreply.github.com>
2020-03-15 19:06:25 +0000

    flag selector dual bi directional scrolling

--- Strat/src/pFlags/FlagSelectorGUI.scala
+++ Strat/src/pFlags/FlagSelectorGUI.scala
@@ -22,31 +22,32 @@ case class FlagSelectorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Flags 
 
   val background = Rectangle.curvedCorners(dimensions("width"), dimensions("height"), 10).fill(Gray)
   val aTitle = TextGraphic("Flags", 40, 0 vv headerYpos)
-  val btnLeft = clickButton("<", (mb: MouseButton) => { prevPage }).slate(-150, headerYpos)
-  val btnRight = clickButton(">", (mb: MouseButton) => { nextPage }).slate(-100, headerYpos)   
-  val btnUp = clickButton("up", (mb: MouseButton) => { prevPage }).slate(-300, headerYpos)
-  val btnDown = clickButton("down", (mb: MouseButton) => { nextPage }).slate(-225, headerYpos)   
+  val btnLeft = clickButton("<", (mb: MouseButton) => { scrollLeft }).slate(-150, headerYpos)
+  val btnRight = clickButton(">", (mb: MouseButton) => { scrollRight }).slate(-100, headerYpos)   
+  val btnUp = clickButton("up", (mb: MouseButton) => { scrollUp }).slate(-300, headerYpos)
+  val btnDown = clickButton("down", (mb: MouseButton) => { scrollDown }).slate(-225, headerYpos)   
   val everythingNotFlag: Refs[GraphicElem] = Refs(background, aTitle, btnLeft, btnRight, btnUp, btnDown)
 
-  val scrollDirection = 'Horizontal     // 'Horizontal OR 'Vertical
   var viewIndex: Int = 0
   var selectedFlagIndex: Int = -1
-  def nextPage(): Unit = { showGridView( Math.min( viewIndex+1 ,flagCount%pageSize+1 ) ) }
-  def prevPage(): Unit = { showGridView( Math.max( viewIndex-1 ,0 ) ) }
+  def scrollRight(): Unit = { showGridView( 'Horizontal, Math.min( viewIndex+flagsPerCol ,flagCount%pageSize+1 ) ) }
+  def scrollLeft(): Unit = { showGridView( 'Horizontal, Math.max( viewIndex-flagsPerCol ,0 ) ) }
+  def scrollDown(): Unit = { showGridView( 'Vertical, Math.min( viewIndex+flagsPerRow ,flagCount%pageSize+1 ) ) }
+  def scrollUp(): Unit = { showGridView( 'Vertical, Math.max( viewIndex-flagsPerRow ,0 ) ) }
 //**NB Scroll <> print col by row
 //     Scroll ^\/ print row then col
 //NB2  From Left | Right 
-  def showGridView(firstFlagIndexToShow:Int): Unit =
+  def showGridView(scrollDirection: Symbol, firstFlagIndexToShow:Int): Unit =
   { var r1 = listOfFlags(firstFlagIndexToShow).parent((firstFlagIndexToShow).toString).scale(commonScale)
 
     var pageOfFlags:Refs[PolyParent] = Refs( r1.slate( firstFlagsPosition ))
-    if (scrollDirection == 'Horizontal) {
+    if (scrollDirection == 'Vertical) {
       for( j <- 0 to flagsPerCol-1; i <- 0 to flagsPerRow-1 if i+j > 0; if firstFlagIndexToShow+i+j*flagsPerRow < flagCount)
       { val thisIndex = firstFlagIndexToShow+i+j*flagsPerRow
         r1 = listOfFlags(thisIndex).parent((thisIndex).toString).scale(commonScale)
         pageOfFlags = pageOfFlags +- r1.slate(i*dimensions("cellWidth"), -j*dimensions("cellHeight")).slate( firstFlagsPosition )
       }
-    } else {
+    } else if (scrollDirection == 'Horizontal) {
       for( i <- 0 to flagsPerRow-1 ; j <- 0 to flagsPerCol-1 if i+j > 0; if firstFlagIndexToShow+j+i*flagsPerCol < flagCount)
       { val thisIndex = firstFlagIndexToShow+j+i*flagsPerCol
         r1 = listOfFlags(thisIndex).parent((thisIndex).toString).scale(commonScale)
@@ -58,7 +59,7 @@ case class FlagSelectorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Flags 
     viewIndex = firstFlagIndexToShow
   }
 
-  showGridView(viewIndex)
+  showGridView('Horizontal, viewIndex)
 
   mouseUp = (v, button: MouseButton, clickList) => button match
     {
@@ -73,36 +74,3 @@ case class FlagSelectorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Flags 
     case _ => deb("uncaught non left mouse button")
   }
 }
-
-  // mapPanel.mouseUp = (v, button: MouseButton, clickList) => button match
-  //   {
-  //     case LeftButton =>
-  //     { selected = clickList//.fHead(Arr(), Arr(_))
-  //       statusText = selected.headToStringElse("Nothing Clicked")
-  //       eTop()
-  //     }
-
-  //     case RightButton => (selected, clickList) match
-  //     { case (Refs1(army: Army), Refs1(newTile: W2TileOld)) =>
-  //       { army.tile.lunits = army.tile.lunits.removeFirst(_ == army)
-  //         val newArmy = army.copy(newTile)
-  //         newTile.lunits +-= newArmy
-  //         selected = Refs(newArmy)
-  //         repaintMap
-  //       }
-  //       case (Refs1(army: Army), as) => debvar(as.length)
-  //       case _ =>
-  //     }
-  //     case _ =>
-  //   }
-  // mouseUp = 
-  // {
-  //   case (v, b, Refs1(MButtonCmd(cmd))) => cmd.apply(b)
-  //  // case (v, b, s) => deb(Refs1(s).toString)
-  //   case (v, b, Refs1(s: Flag)) => {
-  //     deb(s.toString)
-  //     // deb(s(0).name + " : " & s(0).ratio )
-  //     // s.parent.scale(200)
-  //   }
-  //   case _ => deb("nothing")
-  // }
