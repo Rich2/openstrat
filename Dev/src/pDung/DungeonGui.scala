cbdd1c20ac7d48a0b4a03bdf8f3231367f11be06
Rich2 <rr1@tutanota.com>
2020-03-17 19:37:24 +0000

    Minor.

--- Dev/src/pDung/DungeonGui.scala
+++ Dev/src/pDung/DungeonGui.scala
@@ -10,21 +10,21 @@ class DungeonGui(canv: CanvasPlatform) extends SquareGridGui[DTileOld, SideOldBa
   var focus: Vec2 = grid.cen
   override def eTop(): Unit = reTop(guButs +- status)
    
-  def fSquare: OfSquareReg[DTileOld, SideOldBare, DungeonGridOld] => GraphicElemsOld = tog =>
+  def fSquare: OfSquareReg[DTileOld, SideOldBare, DungeonGridOld] => GraphicElems = tog =>
   { import tog._
     val colour: Colour = tile.colour
     val tv = vertDispVecs.fillActive(colour, tile)
-    val sides = ifScaleCObjsOld(60, ownSideLinesOld.map(_.draw(1, colour.contrastBW)))
-    val tText = ifScaleCObjOld(60, TextGraphic(xyStr, 14, cen, colour.contrastBW))
+    val sides = ifScaleCObjs(60, ownSideLines.map(_.draw(1, colour.contrastBW)))
+    val tText = ifScaleCObj(60, TextGraphic(xyStr, 14, cen, colour.contrastBW))
     val player = ifScaleOptObjsNew(60, tile.charac) { charac =>
       val poly1 = Rectangle(1.5).insVerts(1, -0.25 vv 0.5, 0 vv 0.8, 0.25 vv 0.5)
       val poly2 = poly1.scale(tog.tScale / 2.5).rotate(charac.facing.angle).slate(tog.cen)      
       Refs(poly2.parentFillDraw(charac, charac.colour, 1), TextGraphic(charac.iden.toString, 16, cen, charac.colour.contrast))
     }
-    tv.toArraySeq ++ tText ++ player.toArraySeq ++ sides
+    tv++ tText ++ player ++ sides
   }
   
-  def mapObjs: GraphicElems = (ofTilesDisplayFoldOld[OfSquareReg[DTileOld, SideOldBare, DungeonGridOld]](fSquare)).toRefs
+  def mapObjs: GraphicElems = (ofTilesDisplayFold[OfSquareReg[DTileOld, SideOldBare, DungeonGridOld]](fSquare))
 
   mapPanel.mouseUp = (v, but: MouseButton, clickList) => (but, selected, clickList) match
   {
