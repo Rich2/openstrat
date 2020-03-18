e2ef78ab7b553082b3db3b0502225f522eab4472
w0d <soccamedia@yahoo.com>
2020-03-18 13:15:49 +0000

    filltextactive help

--- Strat/src/pFlags/FlagSelectorGUI.scala
+++ Strat/src/pFlags/FlagSelectorGUI.scala
@@ -6,16 +6,21 @@ import geom._, pCanv._, Colour._
 /** NB: Assumes Flag.ratio is always <=2. :NB: From Left | Right */
 case class FlagSelectorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Flags Are Ace")
 { val commonScale: Int = 95
-  val listOfFlags: Refs[Flag] = Refs(Armenia, Austria, Belgium, Chad, China, England, France, Germany, Germany1871, Italy,
+/*  var listOfFlags: Refs[PaintElem] = Refs(Armenia, Austria, Belgium, Chad, China, England, France, Germany, Germany1871, Italy,
                           Ireland, Japan, Russia, USSR, Swastika, UnitedKingdom, UnitedStates, WhiteFlag, CommonShapesInFlags, WhiteFlag,
                           WhiteFlag, Armenia, WhiteFlag, WhiteFlag, Chad, WhiteFlag, WhiteFlag, WhiteFlag,
                           Chad, England, WhiteFlag, WhiteFlag, WhiteFlag, USSR, WhiteFlag, WhiteFlag,
                           WhiteFlag, UnitedKingdom, WhiteFlag)
-
-  val flagCount: Int = listOfFlags.length
+*/
+  val flagCount: Int = 44//listOfFlags.length
+  deb(flagCount.toString)
   val flagsPerRow: Int = 4
   val flagsPerCol: Int = 5
   val pageSize: Int = flagsPerRow * flagsPerCol
+  
+  var listOfFlags:Refs[GraphicElem] = Refs[GraphicElem]()
+  for( i <- 0 to flagCount-1) { listOfFlags = listOfFlags ++ Rectangle(2).fillTextActive(Violet, "Hello", "This Text") }
+
   val dimensions = Map("width"->800, "height"->600, "headerSize"->50, "cellWidth"->200, "cellHeight"->110)
   val headerYpos = dimensions("height")/2-dimensions("headerSize")/2
   val firstFlagsPosition = (-(dimensions("width")-dimensions("cellWidth"))/2 vv (dimensions("height")-dimensions("cellHeight"))/2-dimensions("headerSize"))
@@ -33,15 +38,16 @@ case class FlagSelectorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Flags 
   if (isScrollHorizontal) { flagsPerScroll = flagsPerCol; iScrollStep = flagsPerScroll; jScrollStep = 1 }
   else                    { flagsPerScroll = flagsPerRow; iScrollStep = 1; jScrollStep = flagsPerScroll }
 
-  def scrollMore(): Unit = { showGridView( isScrollHorizontal, Math.min( viewIndex + flagsPerScroll, flagCount % pageSize + 1 ) ) }
+  def scrollMore(): Unit = { showGridView( isScrollHorizontal, Math.min( viewIndex + flagsPerScroll, flagCount - pageSize) ) }
   def scrollLess(): Unit = { showGridView( isScrollHorizontal, Math.max( viewIndex - flagsPerScroll, 0 ) ) }
 
   def showGridView(isScrollHorizontal: Boolean, firstFlagIndexToShow:Int): Unit =
   { var pageOfFlags:Refs[PolyParent] = Refs()
     for( j <- 0 to flagsPerCol-1; i <- 0 to flagsPerRow-1 if firstFlagIndexToShow + i*iScrollStep + j*jScrollStep < flagCount)
     { val thisIndex = firstFlagIndexToShow + i*iScrollStep + j*jScrollStep
-      val r1 = listOfFlags(thisIndex).parent(thisIndex.toString).scale(commonScale)
-      pageOfFlags = pageOfFlags +- r1.slate(i*dimensions("cellWidth"), -j*dimensions("cellHeight")).slate( firstFlagsPosition )
+//      val thisFlag = listOfFlags(thisIndex).parent(thisIndex.toString).scale(commonScale)
+//     val thisFlag = listOfFlags(thisIndex).scale(commonScale)
+//      pageOfFlags = pageOfFlags +- thisFlag.slate(i*dimensions("cellWidth"), -j*dimensions("cellHeight")).slate( firstFlagsPosition )
     }
     
     repaint(everythingNotFlag ++ pageOfFlags ++ Refs(Rectangle.curvedCorners(50, 30, 10).fill(Pink).slate(-225, headerYpos).slate(firstFlagIndexToShow*2.5, 0) ))
@@ -53,12 +59,14 @@ case class FlagSelectorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Flags 
   mouseUp = (v, button: MouseButton, clickList) => button match
   { case LeftButton => clickList match
     { case List(MButtonCmd(cmd)) => cmd.apply(button)
-      case List(flagIndex) => deb(flagIndex.toString)
+      case List(flagIndex) =>
+        { deb(flagIndex.toString)
+        } 
       case l => deb(l.toString)
     }
     case _ => deb("uncaught non left mouse button")
   }
-  canv.mouseDown = (v, b) => deb("mouseDown")
-  canv.keyDown = (thekey) => if (thekey == "ArrowUp" || thekey == "ArrowLeft") scrollLess else if (thekey == "ArrowDown" || thekey == "ArrowRight") scrollMore
+  canv.mouseDown = (v, b) => ()
+  canv.keyDown = (thekey) => if (thekey=="ArrowUp" || thekey=="ArrowLeft") scrollLess else if (thekey=="ArrowDown" || thekey=="ArrowRight") scrollMore
   canv.onScroll = (isScrollUp) => if (isScrollUp)  scrollLess else scrollMore
 }
