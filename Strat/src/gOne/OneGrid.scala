7894c241a0ad807f1379fdfb29b5ec23853a6469
Rich2 <rr1@tutanota.com>
2020-03-22 20:57:30 +0000

    Minor.

--- Strat/src/gOne/OneGrid.scala
+++ Strat/src/gOne/OneGrid.scala
@@ -3,12 +3,11 @@ package gOne
 import pGrid._, Colour._
 
 object OneGrid
-{
-  object PlayerA extends Player('A', Red)
+{ object PlayerA extends Player('A', Red)
   object PlayerB extends Player('B', Orange)
   object PlayerC extends Player('C', Green)
   val grid = new HexGridReg(2, 10, 2, 6)
-  implicit val arr = grid.newOptRefs[Player]
+  implicit val arr: OptRefs[Player] = grid.newOptRefs[Player]
   grid.setTileSome[Player](4, 4, PlayerA)
   grid.setTileSome[Player](8, 4, PlayerB)
   grid.setTileSome[Player](10, 6, PlayerC)
