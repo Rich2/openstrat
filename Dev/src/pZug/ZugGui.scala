e79a02ca5396e4b00675fdf58ed4106f2eac7084
Rich2 <rr1@tutanota.com>
2020-04-01 12:42:56 +0100

    78 ZugGui.

--- Dev/src/pZug/ZugGui.scala
+++ Dev/src/pZug/ZugGui.scala
@@ -4,5 +4,11 @@ import pCanv._, geom._, pGrid._
 
 case class ZugGui(canv: CanvasPlatform) extends CmdBarGui("ZugFuhrer Gui")
 {
+  implicit val grid = Zug1.grid
+  val scale = grid.fullDisplayScale(mainWidth, mainHeight)
+  val sides = grid.sideLinesAll(scale).draw(2.0)
   var statusText = "Welcome to ZugFuhrer"
+  def thisTop(): Unit = reTop(Refs(status))
+  thisTop()
+  mainRepaints(sides)
 }
