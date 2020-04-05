eb6050f09dd417c750695a185003cd08b7486803
Rich2 <rr1@tutanota.com>
2020-04-05 19:26:10 +0100

    Minor.

--- Graphic/src/pGrid/EGrid.scala
+++ Graphic/src/pGrid/EGrid.scala
@@ -1,7 +1,7 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0 */
 package ostrat
-package pEarth
-import geom._, pGrid._, collection.mutable.ArrayBuffer
+package pGrid
+import geom._, collection.mutable.ArrayBuffer
 
 trait EGrid80Km extends EGrid
 {
