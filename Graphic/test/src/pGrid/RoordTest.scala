d8aad1b8f7b66edc89eadabe3d310c016bb88953
Rich2 <rr1@tutanota.com>
2020-04-05 23:06:05 +0100

    Various.

--- Graphic/test/src/pGrid/RoordTest.scala
+++ Graphic/test/src/pGrid/RoordTest.scala
@@ -7,8 +7,6 @@ object RoordTest extends TestSuite
   val r1 = 5 rr -96
   val r2 = r1 + Roord(-5, 96)
 
-  val ll = EGrid80Km.roordToLatLong0(464 rr 4)
-  debvar(ll.degStr)
   val tests = Tests
   {
     "test1" -
