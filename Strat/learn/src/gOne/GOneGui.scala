6024c79460edf893a22f7ff68de45f7fe879e131
Rich2 <rr1@tutanota.com>
2020-04-07 17:53:26 +0100

    Minor.

--- Strat/learn/src/gOne/GOneGui.scala
+++ Strat/learn/src/gOne/GOneGui.scala
@@ -4,11 +4,12 @@ import pCanv._, geom._, pGrid._
 
 /** Graphical user interface for GOne example game. */
 case class GOneGui(canv: CanvasPlatform, scenStart: OneScen) extends CmdBarGui("Game One Gui")
-{ var scen = scenStart
+{ var scen = scenStart.turn(Refs(HTileAndStep(4, 4, HTStepUL), HTileAndStep(4, 8, HTStepUL), HTileAndStep(6, 10, HTStepLt)))
   var statusText = "Let click on Player to select. Right click on adjacent Hex to set move."
   implicit def grid = scen.grid
   def players = scen.oPlayers
   val moves0: OptRefs[HTStep] = grid.newOptRefs[HTStep]
+  moves0.foreachSome(println)
   var moves: OptRefs[HTStep] = moves0
 
   val scale = grid.fullDisplayScale(mainWidth, mainHeight)
@@ -21,18 +22,19 @@ case class GOneGui(canv: CanvasPlatform, scenStart: OneScen) extends CmdBarGui("
 
   val sls = grid.sidesDraw(2.0)
 
-  def mMoves: Refs[LineDraw] = moves.gridMapSomes{(r, step) =>
+  def mMoves: Refs[LineDraw] ={
+    moves.gridMapSomes{(r, step) =>
     val newR = r + step.roord
-    RoordLine(r, newR).gridLine2.draw(2, players.gridElemGet(r).colour)
-  }
+    RoordLine(r, newR).gridLine2.draw(2, {deb("Get Colour"); players.gridElemGet(r).colour} )
+  } }
 
   def getOrders = moves.gridMapSomes((r, s) => r.andStep(s))
   val bTurn = clickButton("Turn " + (scen.turn + 1).toString, _ => {
-    debvar(getOrders)
+
     scen = scen.turn(getOrders)
-    deb("Done")
+
     moves = moves0
-    deb("About to paint")
+    moves.foreachSome(a => deb(a.toString))
     repaint()
 
   })
@@ -54,7 +56,11 @@ case class GOneGui(canv: CanvasPlatform, scenStart: OneScen) extends CmdBarGui("
        case (_, h, _) => deb("Other; " + h.toString)
     }
   thisTop()
-  def frame = (tiles +- sls ++ roardTexts ++ lunits ++ mMoves).gridTrans(scale)
+  def frame = {
+    val r = (tiles +- sls ++ roardTexts ++ lunits ++ mMoves)
+
+      r.gridTrans(scale)
+  }
   def repaint() = mainRepaint(frame)
   repaint()
 }
