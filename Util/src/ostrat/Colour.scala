5a57e2d7a3b92ab920980bdf3449511a901a2190
w0d <35996314+w0d@users.noreply.github.com>
2020-04-12 17:11:24 +0100

    Colour.scala updated to CSS Color Module Level 4
    fixed error in comment

--- Util/src/ostrat/Colour.scala
+++ Util/src/ostrat/Colour.scala
@@ -125,7 +125,8 @@ object Colour
     def none: OptOldColour = NoColour
   }*/
   
-  //** named colors & values following CSS Color Module Level 4 - though names are UpperCamelCase here - plus 4 non standard colors: BrightSkyBlue, DarkYellow, LemonLime, LightRed
+   /** named colors & values following CSS Color Module Level 4 - though names are UpperCamelCase here
+       - plus 4 non standard colors: BrightSkyBlue, DarkYellow, LemonLime, LightRed*/
    val AntiqueWhite: Colour = new Colour(0xFFFAEBD7)
    val Aqua: Colour = new Colour(0xFF00FFFF)
    val Aquamarine = new Colour(0xFF7FFFD4)
