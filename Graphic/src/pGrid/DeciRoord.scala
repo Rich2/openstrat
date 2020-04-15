a3fbc8cca753312d6971f2b89bab08f51969e535
Rich2 <rr1@tutanota.com>
2020-04-15 12:18:03 +0100

    #108 Changed name DeciRoord => DecRo.

--- Graphic/src/pGrid/DeciRoord.scala
+++ Graphic/src/pGrid/DeciRoord.scala
@@ -1,24 +1,27 @@
 package ostrat
 package pGrid
 
-class DeciRoord private(val bLong: Long) extends AnyVal with ProdInt2
+/** A deci-Roordinate. Divides the axes into graduations of ten. */
+class DeciRo private(val bLong: Long) extends AnyVal with ProdInt2
 { def yd: Int = bLong.>>(32).toInt
   def cd: Int = bLong.toInt
   def _1 = yd
   def _2 = cd
-  def canEqual(a: Any) = a.isInstanceOf[DeciRoord]
+  def canEqual(a: Any) = a.isInstanceOf[DeciRo]
   @inline def yStr: String = (yd.toDouble / 10).toString
   @inline def cStr: String = (cd.toDouble / 10).toString
   override def toString: String = "DeciRo".appendSemicolons(yStr, cStr)
   def ycStr: String = yStr.appendCommas(cStr)
 }
 
-object DeciRoord
+object DeciRo
 {
-  def apply(yIn: Double, cIn: Double): DeciRoord =
+  def apply(yIn: Double, cIn: Double): DeciRo =
   {
     val yd = (yIn * 10).toInt
     val cd = (cIn * 10).toInt
-    new DeciRoord(yd.toLong.<<(32) | (cd & 0xFFFFFFFFL))
+    new DeciRo(yd.toLong.<<(32) | (cd & 0xFFFFFFFFL))
   }
+
+  def fromLong(value: Long): DeciRo = new DeciRo(value)
 }
