10e1405856563c127944e01372548f0078543e76
w0d <35996314+w0d@users.noreply.github.com>
2020-04-20 03:17:22 +0100

    Added Chile Flag

--- Strat/src/pFlags/AmericasFlags.scala
+++ Strat/src/pFlags/AmericasFlags.scala
@@ -37,3 +37,19 @@ object Colombia extends Flag
  val ratio = 1.5
  val apply: Refs[PaintElem] = topToBottom(Colour(0xFFFCD116), Colour(0xFFFCD116), Colour(0xFF003893), Colour(0xFFCE1126))
 }
+
+
+object Chile extends Flag
+{
+ val name = "Chile"
+ val ratio = 1.5
+ val apply: Refs[PaintElem] = 
+  {
+    Refs[PaintElem](
+      Rectangle(ratio, 1).fill(White),
+      Rectangle(ratio, 0.5, 0 vv -0.25).fill(Colour(0xFFD52B1E)),
+      Rectangle(0.5, 0.5, -0.5 vv 0.25).fill(Colour(0xFF0039A6)),
+      Star5().scale(0.125).slate(-0.5 vv 0.25).fill(White)
+    )
+  }
+}
