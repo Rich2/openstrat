63d71489d3001201031b1a6ca3c81d29ced22c55
Rich2 <rr1@tutanota.com>
2020-04-24 15:04:38 +0100

    Minor.

--- Graphic/learn/src/LessonA5.scala
+++ Graphic/learn/src/LessonA5.scala
@@ -27,7 +27,8 @@ case class LessonA5(canv: CanvasPlatform) extends CanvasNoPanels("Lesson A5")
   val a1 = Arr(d1, d2, d3, d4, d5, d6)
   val a2 = a1 ++ a1.flatMap(_.startCenEndTexts)
 
-  repaint(a2)
+  val cen6 = TextGraphic("c6 cen", 14, -200 vv 100)
+  repaint(a2 +- cen6)
 }
 
 /** There are three types of values above. Numbers, text and Colours. Try changing the numbers, save the file and you should things move around the
