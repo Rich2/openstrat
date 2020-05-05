b84928d45bdafc923e3ef147d48a5e21daf3b3c1
richtype <rr1@tutanota.com>
2020-05-05 20:40:05 +0100

    #113

--- Graphic/src/Trans/TransAlign.scala
+++ Graphic/src/Trans/TransAlign.scala
@@ -7,7 +7,7 @@ trait TransAligner extends Any
 { type AlignT <: TransAligner
   def slate(offset: Vec2): AlignT
 
-
+  def scale(operand: Double): AlignT
   def ySlate(yDelta: Double): AlignT = slate(0 vv yDelta)
 
   /** Translate in 2 dimensional space. */
@@ -17,30 +17,29 @@ trait TransAligner extends Any
 /** A Rigid or Euclidean transformations type class. */
 trait TransAlign[T]
 { def slate(obj: T, offset: Vec2): T
-
+  def scale(obj: T, operand: Double): T
 }
 
 object TransAlign
 {
   implicit def transRigiderImplicit[T <: TransAligner]: TransAlign[T] = new TransAlign[T]
-  {
-    override def slate(obj: T, offset: Vec2): T = obj.slate(offset).asInstanceOf[T]
-
+  { override def slate(obj: T, offset: Vec2): T = obj.slate(offset).asInstanceOf[T]
+    override def scale(obj: T, operand: Double): T = obj.scale(operand).asInstanceOf[T]
   }
 
   implicit def arrImplicit[A, AA <: ArrBase[A]](implicit build: ArrBuild[A, AA], ev: TransAlign[A]): TransAlign[AA] = new TransAlign[AA]
   { override def slate(obj: AA, offset: Vec2): AA = obj.map(ev.slate(_, offset))
-   // override def rotateRadians(obj: AA, radians: Double): AA = obj.map(ev.rotateRadians(_, radians))
+    override def scale(obj: AA, operand: Double): AA = obj.map(ev.scale(_, operand))
   }
 
   implicit def functorImplicit[A, F[_]](implicit evF: Functor[F], evA: TransAlign[A]): TransAlign[F[A]] = new TransAlign[F[A]]
   { override def slate(obj: F[A], offset: Vec2): F[A] = evF.map(obj, evA.slate(_, offset))
-    //override def rotateRadians(obj: F[A], radians: Double): F[A] = evF.map(obj, evA.rotateRadians(_, radians))
+    override def scale(obj: F[A], operand: Double): F[A] = evF.map[A, A](obj, ts => evA.scale(ts, operand))
   }
 
   implicit def arrayImplicit[A](implicit ct: ClassTag[A], ev: TransAlign[A]): TransAlign[Array[A]] = new TransAlign[Array[A]]
   { override def slate(obj: Array[A], offset: Vec2): Array[A] = obj.map(ev.slate(_, offset))
-   // override def rotateRadians(obj: Array[A], radians: Double): Array[A] = obj.map(ev.rotateRadians(_, radians))
+   override def scale(obj: Array[A], operand: Double): Array[A] = obj.map(ev.scale(_, operand))
   }
 }
 
@@ -60,4 +59,12 @@ class TransAlignExtension[T](value: T, ev: TransAlign[T])
 
   /** this.asInstanceOf[T] */
   def identity: T = this.asInstanceOf[T]
+
+  def scale(operand: Double): T = ev.scale(value, operand)
+
+  /** The scale transformation on 2 dimensional vectors. */
+  def scaleSlate(factor: Double, addVec: Vec2): T =
+  { val r1 = ev.scale(value, factor)
+    ev.slate(r1, addVec)
+  }
 }
--- Graphic/src/Trans/TransSim.scala
+++ Graphic/src/Trans/TransSim.scala
@@ -5,7 +5,6 @@ import reflect.ClassTag
 
 trait TransSimer extends Any with TransAligner
 {// type RigidT <: TransSimer
-  def scale(operand: Double): AlignT
   def shear(xScale: Double, yScale: Double): TransAffer
   def mirror(line: Line2): AlignT
   def rotateRadians(radians: Double): AlignT
@@ -25,9 +24,7 @@ trait TransSimerUser extends TransSimer
 
 /** A Similar Transformations type class */
 trait TransSim[T] extends TransAlign[T]
-{ def scale(obj: T, operand: Double): T
-  def mirror(obj: T, line: Line2): T
-  //override def rotateRadians(obj: T, radians: Double): T = obj.rotateRadians(radians).asInstanceOf[T]
+{ def mirror(obj: T, line: Line2): T
   def rotateRadians(obj: T, radians: Double): T
 }
 
@@ -36,53 +33,40 @@ object TransSim
   implicit def transSimerImplicit[T <: TransSimer]: TransSim[T] = new TransSim[T]
   { override def rotateRadians(obj: T, radians: Double): T = obj.rotateRadians(radians).asInstanceOf[T]
     override def slate(obj: T, offset: Vec2): T = obj.slate(offset).asInstanceOf[T]
-    override def scale(obj: T, operand: Double): T = obj.scale(operand).asInstanceOf[T]
     override def mirror(obj: T, line: Line2): T = obj.mirror(line).asInstanceOf[T]
-
-    //override def mirror(obj: T, line: Line2): T = obj.mirror(line).asInstanceOf[T]
+    override def scale(obj: T, operand: Double): T = obj.scale(operand).asInstanceOf[T]
   }
 
   implicit def arrImplicit[A, AA <: ArrBase[A]](implicit build: ArrBuild[A, AA], ev: TransSim[A]): TransSim[AA] = new TransSim[AA]
-  { override def scale(obj: AA, operand: Double): AA = obj.map(ev.scale(_, operand))
-    override def slate(obj: AA, offset: Vec2): AA = obj.map(ev.slate(_, offset))
+  { override def slate(obj: AA, offset: Vec2): AA = obj.map(ev.slate(_, offset))
     override def rotateRadians(obj: AA, radians: Double): AA = obj.map(ev.rotateRadians(_, radians))
     override def mirror(obj: AA, line: Line2): AA = obj.map(ev.mirror(_, line))
+    override def scale(obj: AA, operand: Double): AA = obj.map(ev.scale(_, operand))
   }
 
   implicit def functorImplicit[A, F[_]](implicit evF: Functor[F], evA: TransSim[A]): TransSim[F[A]] = new TransSim[F[A]]
-  { override def scale(obj: F[A], operand: Double): F[A] = evF.map[A, A](obj, ts => evA.scale(ts, operand))
-    override def slate(obj: F[A], offset: Vec2): F[A] = evF.map(obj, ts => evA.slate(ts, offset))
+  { override def slate(obj: F[A], offset: Vec2): F[A] = evF.map(obj, ts => evA.slate(ts, offset))
     override def rotateRadians(obj: F[A], radians: Double): F[A] = evF.map(obj, ts => evA.rotateRadians(ts, radians))
     override def mirror(obj: F[A], line: Line2): F[A] = evF.map(obj, evA.mirror(_, line))
+    override def scale(obj: F[A], operand: Double): F[A] = evF.map[A, A](obj, ts => evA.scale(ts, operand))
   }
 
   implicit def arrayImplicit[A](implicit ct: ClassTag[A], ev: TransSim[A]): TransSim[Array[A]] = new TransSim[Array[A]]
-  { override def scale(obj: Array[A], operand: Double): Array[A] = obj.map(ev.scale(_, operand))
-    override def slate(obj: Array[A], offset: Vec2): Array[A] = obj.map(ev.slate(_, offset))
+  { override def slate(obj: Array[A], offset: Vec2): Array[A] = obj.map(ev.slate(_, offset))
     override def rotateRadians(obj: Array[A], radians: Double): Array[A] = obj.map(ev.rotateRadians(_, radians))
     override def mirror(obj: Array[A], line: Line2): Array[A] = obj.map(ev.mirror(_, line))
+    override def scale(obj: Array[A], operand: Double): Array[A] = obj.map(ev.scale(_, operand))
   }
 }
 
 class TransSimExtension[T](value: T, ev: TransSim[T]) extends TransSimGenExtension[T]
-{ def scale(operand: Double): T = ev.scale(value, operand)
-
-  /** The scale transformation on 2 dimensional vectors. */
-  def scaleSlate(factor: Double, addVec: Vec2): T =
-  { val r1 = ev.scale(value, factor)
-    ev.slate(r1, addVec)
-  }
-
-  def mirrorParallelX(yOffset: Double): T = mirror(-1, yOffset, 1, yOffset)
+{ def mirrorParallelX(yOffset: Double): T = mirror(-1, yOffset, 1, yOffset)
   def mirrorParallelY(xOffset: Double): T = mirror(xOffset, -1, xOffset, 1)
   def mirrorY: T = mirror(0, -1, 0, 1)
   def mirrorX: T = mirror(-1, 0, 1, 0)
   def mirror(line: Line2) = ev.mirror(value, line)
   def mirror(v1: Vec2, v2: Vec2): T = ev.mirror(value, v1.lineTo(v2))
   def mirror(x1: Double, y1: Double, x2: Double, y2: Double): T = ev.mirror(value, new Line2(x1, y1, x2, y2))
-
   override def rotateRadians(radians: Double): T = ev.rotateRadians(value, radians)
-
   override def rotate(angle: Angle): T = ev.rotateRadians(value, angle.radians)
-
 }
