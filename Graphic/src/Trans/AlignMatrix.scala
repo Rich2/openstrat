ddc03579a12c89d6c68cb2b23b83d40b38416e4d
richtype <rr1@tutanota.com>
2020-05-12 00:56:56 +0100

    Minor.

--- Graphic/src/Trans/AlignMatrix.scala
+++ Graphic/src/Trans/AlignMatrix.scala
@@ -9,3 +9,11 @@ case class AlignMatrix(xFactor: Double, yFactor: Double, xDelta: Double, yDelta:
   def slate(xOperand: Double, yOperand: Double): AlignMatrix = AlignMatrix(xFactor, yFactor, xDelta + xOperand, yDelta + yOperand)
   def xyScale(xOperand: Double, yOperand: Double): AlignMatrix = AlignMatrix(xFactor * xOperand, yFactor * yOperand, xDelta * xOperand, yDelta * yOperand)
 }
+
+/** A matrix for proportionate and aligned to X and Y axes transformations. */
+case class ProlignMatrix(vFactor: Double, up: Boolean, right: Boolean, xDelta: Double, yDelta: Double)
+{ def vDelta: Vec2 = Vec2(xDelta, yDelta)
+  def scale(operand: Double): ProlignMatrix = ProlignMatrix(vFactor * operand, up, right, xDelta * operand, yDelta * operand)
+  def slate(operand: Vec2): ProlignMatrix = ProlignMatrix(vFactor, up, right, xDelta + operand.x, yDelta + operand.y)
+  def slate(xOperand: Double, yOperand: Double): ProlignMatrix = ProlignMatrix(vFactor, up, right, xDelta + xOperand, yDelta + yOperand)
+}
