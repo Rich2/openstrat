15b93badd26f7d3e042bda968f71dce18b6901ed
richtype <rr1@tutanota.com>
2020-05-13 13:53:08 +0100

    Minor.

---
+++ Graphic/src/Trans/Scale.scala
@@ -0,0 +1,22 @@
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+package geom
+import reflect.ClassTag
+
+/** Type class for tranScale 2 dimensional vector transformations. Each transformation method has been given its own Type class and associated
+ * extension class. Different sets of transformations can then be combined. */
+trait Scale[T]
+{ def scale(obj: T, operand: Double): T
+}
+
+object Scale
+{
+  implicit def transAlignerImplicit[T <: TransAligner]: Scale[T] = (obj, operand) => obj.scaleOld(operand).asInstanceOf[T]
+
+  implicit def arrImplicit[A, AA <: ArrBase[A]](implicit build: ArrBuild[A, AA], ev: Scale[A]): Scale[AA] =
+    (obj, offset) => obj.map(ev.scale(_, offset))
+
+  implicit def functorImplicit[A, F[_]](implicit evF: Functor[F], evA: Scale[A]): Scale[F[A]] = (obj, operand) => evF.map(obj, evA.scale(_, operand))
+
+  implicit def arrayImplicit[A](implicit ct: ClassTag[A], ev: Scale[A]): Scale[Array[A]] = (obj, operand) => obj.map(ev.scale(_, operand))
+}
