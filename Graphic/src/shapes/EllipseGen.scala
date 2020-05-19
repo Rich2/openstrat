f203a4cef6ac71d96698a242b8e264999419212a
richtype <rr1@tutanota.com>
2020-05-19 18:51:45 +0100

    Minor.

--- Graphic/src/shapes/EllipseGen.scala
+++ Graphic/src/shapes/EllipseGen.scala
@@ -7,17 +7,19 @@ trait EllipseLike extends Transer with Shape//Affer
   //override def shear(xScale: Double, yScale: Double): Ellipse
 }
 
-class EllipseGen(val xCen: Double, val yCen: Double, val xRight: Double, val yRight: Double, val upRadius: Double) extends EllipseLike with Transer//ffer
+class EllipseGen(val xLeft: Double, val yLeft: Double, val xRight: Double, val yRight: Double, val upRadius: Double) extends EllipseLike with Transer//ffer
 { //type AlignT = Ellipse
   override def fTrans(f: Vec2 => Vec2): EllipseLike = ???
-  def vCen: Vec2 = xCen vv yCen
+  def xCen: Double = (xLeft + xRight) /2
+  def yCen: Double = (yLeft + yRight) / 2
+  def cen: Vec2 = xCen vv yCen
   def vRight: Vec2 = xRight vv yRight
   override def shear(xScale: Double, yScale: Double): EllipseGen = this
   override def rotate(angle: Angle): EllipseGen = this
   override def rotateRadians(radians: Double):  EllipseGen = this
-  override def slateOld(offset: Vec2): EllipseGen = EllipseGen(vCen + offset, vRight + offset, upRadius)
-  override def scaleOld(operand: Double):  EllipseGen = EllipseGen(vCen * operand * operand, vRight * operand, upRadius * operand)
-  override def mirror(line: Line2): EllipseGen = EllipseGen(vCen.mirror(line), vRight.mirror(line), upRadius)
+  override def slateOld(offset: Vec2): EllipseGen = EllipseGen(cen + offset, vRight + offset, upRadius)
+  override def scaleOld(operand: Double):  EllipseGen = EllipseGen(cen * operand * operand, vRight * operand, upRadius * operand)
+  override def mirror(line: Line2): EllipseGen = EllipseGen(cen.mirror(line), vRight.mirror(line), upRadius)
 
   override def fill(colour: Colour): GraphicElem = ???
 }
