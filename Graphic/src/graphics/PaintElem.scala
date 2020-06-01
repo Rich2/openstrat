2c7ccc0a6742999779160518276605f38dbfecc0
Rich2 <rr1@tutanota.com>
2020-06-01 14:39:48 +0100

    #114

--- Graphic/src/graphics/PaintElem.scala
+++ Graphic/src/graphics/PaintElem.scala
@@ -20,12 +20,33 @@ trait PaintElem extends GraphicElem
 
   /** Mirror, reflection transformation across the line x = xOffset, which is parallel to the X axis. */
   def mirrorYOffset(xOffset: Double): PaintElem
+
+  /** Mirror, reflection transformation across the line y = yOffset, which is parallel to the X axis. */
+  def mirrorXOffset(yOffset: Double): PaintElem
+
+  /** Mirror, reflection transformation across the X axis. This method has been left abstract in GeomElemNew to allow the return type to be narrowed
+   * in sub classes. */
+  def mirrorX: PaintElem
+
+  /** Mirror, reflection transformation across the X axis. This method has been left abstract in GeomElemNew to allow the return type to be narrowed
+   * in sub classes. */
+  def mirrorY: PaintElem
+
+  def prolign(matrix: ProlignMatrix): PaintElem
 }
 
 /** Companion object for PaintElem contains various implicit instances for the transformation type classes. */
 object PaintElem
 { implicit val slateImplicit: Slate[PaintElem] = (obj: PaintElem, offset: Vec2) => obj.slate(offset)
   implicit val scaleImplicit: Scale[PaintElem] = (obj: PaintElem, operand: Double) => obj.scale(operand)
+
+  implicit val mirrorAxisImplicit: MirrorAxis[PaintElem] = new MirrorAxis[PaintElem]
+  { /** Reflect, mirror across a line parallel to the X axis. */
+    override def mirrorXOffset(obj: PaintElem, yOffset: Double): PaintElem = obj.mirrorXOffset(yOffset)
+
+    /** Reflect, mirror across a line parallel to the Y axis. */
+    override def mirrorYOffset(obj: PaintElem, xOffset: Double): PaintElem = obj.mirrorYOffset(xOffset)
+  }
 }
 
 /** Trait to be removed. */
@@ -40,4 +61,12 @@ trait PaintElemOld extends GraphicElemOld with PaintElem
   def scale(operand: Double): AlignT
 
   def mirrorYOffset(xOffset: Double): AlignT
+
+  def mirrorXOffset(yOffset: Double): AlignT
+
+  override def mirrorX: AlignT
+
+  override def mirrorY: AlignT
+
+  //def prolign(matrix: Any): AlignT
 }
