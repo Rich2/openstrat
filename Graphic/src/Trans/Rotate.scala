fba0dc864e72ce1c858c33ab00550703db770fe3
Rich2 <rr1@tutanota.com>
2020-06-02 13:58:00 +0100

    #114 Created some Rotate typeclass inferences.

--- Graphic/src/Trans/Rotate.scala
+++ Graphic/src/Trans/Rotate.scala
@@ -1,7 +1,7 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-//import scala.reflect.ClassTag
+import reflect.ClassTag
 
 /** Type class for the rotation of objects of type T */
 trait Rotate[T]
@@ -10,7 +10,13 @@ trait Rotate[T]
 
 object Rotate
 {
+  implicit def transSimerImplicit[T <: TransSimer]: Rotate[T] = (obj, radians) => obj.rotateRadians(radians).asInstanceOf[T]
+  
   implicit def arrImplicit[A, AA <: ArrBase[A]](implicit build: ArrBuild[A, AA], ev: Rotate[A]): Rotate[AA] =
     (obj, radians) => obj.map(ev.rotateRadiansT(_, radians))
 
+  implicit def functorImplicit[A, F[_]](implicit evF: Functor[F], evA: Rotate[A]): Rotate[F[A]] =
+    (obj, radians) => evF.map(obj, evA.rotateRadiansT(_, radians))
+
+  implicit def arrayImplicit[A](implicit ct: ClassTag[A], ev: Rotate[A]): Rotate[Array[A]] = (obj, radians) => obj.map(ev.rotateRadiansT(_, radians))
 }
