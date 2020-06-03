4f986a59b7255647c43ab9873295269b1638bb5c
Rich2 <rr1@tutanota.com>
2020-06-03 17:50:04 +0100

    #114

--- Graphic/src/shapes/CircleGraphic.scala
+++ Graphic/src/shapes/CircleGraphic.scala
@@ -7,72 +7,34 @@ trait CircleGraphic extends ShapeGraphic
 { type GraphicT <: CircleGraphic
   def circle: Circle
   def fTrans(newCircle: Circle): GraphicT
+  override def mirrorXOffset(yOffset: Double): GraphicT = fTrans(circle.mirrorXOffset(yOffset))
+  override def mirrorX: GraphicT = fTrans(circle.mirrorX)
+  override def mirrorYOffset(xOffset: Double): GraphicT = fTrans(circle.mirrorYOffset(xOffset))
+  override def mirrorY: GraphicT = fTrans(circle.mirrorY)
+  override def slate(offset: Vec2): GraphicT = fTrans(circle.slate(offset))
+  @inline def slate(xOffset: Double, yOffset: Double): GraphicT = fTrans(circle.slate(xOffset, yOffset))
+  override def scale(operand: Double): GraphicT = fTrans(circle.scale(operand))
+  override def prolign(matrix: ProlignMatrix): GraphicT = fTrans(circle.prolign(matrix))
+  override def rotate90: GraphicT = fTrans(circle.rotate90)
+  override def rotate180: GraphicT = fTrans(circle.rotate180)
+  override def rotate270: GraphicT = fTrans(circle.rotate270)
+  override def rotateRadians(radians: Double): GraphicT = fTrans(circle.rotateRadians(radians))
+  override def mirror(line: Line2): GraphicT = fTrans(circle.mirror(line))
 }
 
 final case class CircleFill(circle: Circle, fillColour: Colour) extends CircleGraphic with ShapeFill
 { type GraphicT = CircleFill
   override def fTrans(newCircle: Circle): GraphicT = CircleFill(newCircle, fillColour)
-  override def rendToCanvas(cp: CanvasPlatform): Unit = cp.circleFill(this)
-
-  override def mirrorXOffset(yOffset: Double): CircleFill = copy(circle.mirrorXOffset(yOffset))
-  override def mirrorX: CircleFill = copy(circle.mirrorX)
-  override def mirrorYOffset(xOffset: Double): CircleFill = CircleFill(circle.mirrorYOffset(xOffset), fillColour)
-  override def mirrorY: CircleFill = copy(circle.mirrorY)
-
-  override def slate(offset: Vec2): CircleFill = copy(circle.slate(offset))
-  
-  /** Translate geometric transformation. */
-  @inline def slate(xOffset: Double, yOffset: Double): CircleFill = copy(circle.slate(xOffset, yOffset))
-
-  override def scale(operand: Double): CircleFill = copy(circle.scale(operand))
-
-  override def prolign(matrix: ProlignMatrix): CircleFill = copy(circle.prolign(matrix))
-
-  override def rotate90: CircleFill = copy(circle.rotate90)
-
-  override def rotate180: CircleFill = copy(circle.rotate180)
-
-  override def rotate270: CircleFill = copy(circle.rotate270)
-
-  override def rotateRadians(radians: Double): CircleFill = copy(circle.rotateRadians(radians))
-
-  override def mirror(line: Line2): CircleFill = copy(circle.mirror(line))
+  override def rendToCanvas(cp: CanvasPlatform): Unit = cp.circleFill(this)   
 }
 
 final case class CircleDraw(circle: Circle, lineWidth: Double, lineColour: Colour) extends CircleGraphic with ShapeDraw
 { type GraphicT = CircleDraw
   override def fTrans(newCircle: Circle): CircleDraw = CircleDraw(newCircle, lineWidth, lineColour)
-  override def rendToCanvas(cp: CanvasPlatform): Unit = ??? // cp.circleDraw(this)
-
-  override def mirrorXOffset(yOffset: Double): CircleDraw = copy(circle.mirrorXOffset(yOffset))
-  override def mirrorX: CircleDraw = copy(circle.mirrorX)
-  override def mirrorYOffset(xOffset: Double): CircleDraw = copy(circle.mirrorYOffset(xOffset))
-  override def mirrorY: CircleDraw = copy(circle.mirrorY)
-
-  override def slate(offset: Vec2): CircleDraw = copy(circle.slate(offset))
-
-  /** Translate geometric transformation. */
-  @inline def slate(xOffset: Double, yOffset: Double): CircleDraw = copy(circle.slate(xOffset, yOffset))
-
-  override def scale(operand: Double): CircleDraw = copy(circle.scale(operand))
-
-  override def prolign(matrix: ProlignMatrix): CircleDraw = copy(circle.prolign(matrix))
-
-  override def rotate90: CircleDraw = copy(circle.rotate90)
-
-  override def rotate180: CircleDraw = copy(circle.rotate180)
-
-  override def rotate270: CircleDraw = copy(circle.rotate270)
-
-  override def rotateRadians(radians: Double): CircleDraw = copy(circle.rotateRadians(radians))
-
-  override def mirror(line: Line2): CircleDraw = copy(circle.mirror(line))
+  override def rendToCanvas(cp: CanvasPlatform): Unit = ??? // cp.circleDraw(this)  
 }
 
-
 case class CircleFillIcon(fillColour: Colour) extends ShapeFillIcon
-{
-  override def scaleSlate(scale: Double, cen: Vec2): CircleFill = CircleFill(Circle(scale, cen), fillColour)
-
+{ override def scaleSlate(scale: Double, cen: Vec2): CircleFill = CircleFill(Circle(scale, cen), fillColour)
   override def scaleSlate(scale: Double, xCen: Double, yCen: Double): CircleFill = CircleFill(Circle(scale, xCen, yCen), fillColour)
 }
