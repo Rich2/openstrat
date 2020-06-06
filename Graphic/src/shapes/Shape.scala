21f20738f10efe8f6db28e8e9daa9db1cfd6f1df
Rich2 <rr1@tutanota.com>
2020-06-06 10:06:01 +0100

    Various.

--- Graphic/src/shapes/Shape.scala
+++ Graphic/src/shapes/Shape.scala
@@ -1,8 +1,7 @@
-/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0 */
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
 
 trait Shape extends TransElem
-{
-  def fill(colour: Colour): DisplayElem
+{ def fill(colour: Colour): DisplayElem
 }
