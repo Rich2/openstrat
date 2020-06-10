d6ce6a847a5c5a50237e1247349cea36da3da3e1
Rich2 <rr1@tutanota.com>
2020-06-10 23:10:00 +0100

    Changes to Lesson C7.

--- Graphic/learn/src/LessonC7.scala
+++ Graphic/learn/src/LessonC7.scala
@@ -7,46 +7,50 @@ case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exp
 { // defines the size of the circles that represent the points of the bezier
   val circleRadius = 5
 
+  val p1 = -50 vv 0
+  val p2 = 50 vv  0
+  val c1 = -50 vv -125
+  val c2 = 50 vv 25
   // bp (bezier points) holds the points that define the bezier to be displayed
-  var bp:Map[String, Vec2] = Map("p1" -> Vec2(-50, 0), "p2" -> Vec2(50, 0), "c1" -> Vec2(-50, -125), "c2" -> Vec2(50, 25))
+  //var bp:Map[String, Vec2] = Map(", ", , )
 
   // when one of the bezier points is being dragged, this will hold its corresponding key in bp
-  var theDragee = "" 
+  var theDragee: Option[Vec2] = None 
   
   drawBezier()
 
   def drawBezier():Unit = 
-  { val pt1 = Circle(circleRadius, bp("p1")).fill(Red) // the start point is represented as a red circle on screen
-    val pt2 = Circle(circleRadius, bp("p2")).fill(Red) // the end point is represented as a red circle on screen
-    val cp1 = Circle(circleRadius, bp("c1")).fill(Gray)// the control point for start point is represented as a grey circle on screen
-    val cp2 = Circle(circleRadius, bp("c2")).fill(Grey)// the control point for end point is represented as a grey circle on screen
-    val cl1 = LineDraw(bp("p1"), bp("c1"), 1, Grey)    // line between the start point and its control point
-    val cl2 = LineDraw(bp("p2"), bp("c2"), 1, Grey)    // line between the end point and its control point
+  { val cf1 = Circle(circleRadius, p1).fill(Red) // the start point is represented as a red circle on screen
+    val cf2 = Circle(circleRadius, p2).fill(Red) // the end point is represented as a red circle on screen
+    val cp1 = Circle(circleRadius, c1).fill(Gray)// the control point for start point is represented as a grey circle on screen
+    val cp2 = Circle(circleRadius, c2).fill(Grey)// the control point for end point is represented as a grey circle on screen
+    val cl1 = LineDraw(p1, c1, 1, Grey)    // line between the start point and its control point
+    val cl2 = LineDraw(p2, c2, 1, Grey)    // line between the end point and its control point
 
-    val bez = BezierDraw(bp("p1"), bp("c1"), bp("c2"), bp("p2"), 2, Green) // the bezier to be displayed
+    val bez = BezierDraw(p1, c1, c2, p2, 2, Green) // the bezier to be displayed
 
     // this holds the syntax required to draw the current bezier (bez) (NB: replace ; with , )
-    val txt = TextGraphic("BezierDraw(" + bp("p1") + ", " + bp("c1") + ", " + bp("c2") + ", " + bp("p2") + ", 2, Green)", 18, 0 vv 300)
+    val txt = TextGraphic("BezierDraw(" + p1 + ", " + c1 + ", " + c2 + ", " + "p2" + ", 2, Green)", 18, 0 vv 300)
 
-    val elementsToPaint = Arr(txt, pt1, pt2, cp1, cp2, cl1, cl2, bez)
+    val elementsToPaint = Arr(txt, cf1, cf2, cp1, cp2, cl1, cl2, bez)
 
     repaint(elementsToPaint)
   }
 
   // update the dragged bezier point with its new position and then redraw the screen
   def dragging(newPosition:Vec2):Unit = if (theDragee != "") 
-  { bp(theDragee) = newPosition
+  { theDragee = Some(newPosition)
     drawBezier()
   }
 
   // test to see if drag operation has started
   canv.mouseDown = (position, button) => 
   { // if the mouseDown is on one of the represented bezier points then set theDragee to its corresponding key in bp
-    if ((bp("c1") - position).magnitude <= circleRadius) theDragee = "c1"
-    else if ((bp("c2") - position).magnitude <= circleRadius) theDragee = "c2"
-    else if ((bp("p1") - position).magnitude <= circleRadius) theDragee = "p1"
-    else if ((bp("p2") - position).magnitude <= circleRadius) theDragee = "p2"
-    else theDragee = ""
+    if ((c1 - position).magnitude <= circleRadius) theDragee = Some(c1)
+    else if ((c2 - position).magnitude <= circleRadius) theDragee = Some(c2)
+    else if ((p1 - position).magnitude <= circleRadius) theDragee = Some(p1)
+    else if ((p2 - position).magnitude <= circleRadius) theDragee = Some(p2)
+    else theDragee = None
     dragging(position)
   }
 
@@ -55,6 +59,6 @@ case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exp
 
   mouseUp = (button, clickList, position) =>
   { dragging(position)    
-    theDragee = "" // dragging has finished so reset theDragee
+    theDragee = None // dragging has finished so reset theDragee
   }
 }
