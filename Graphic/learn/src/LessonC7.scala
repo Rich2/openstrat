f4ea6ff545ab73d32d0eea8d540c0c692ef2579c
w0d <35996314+w0d@users.noreply.github.com>
2020-06-15 09:28:58 +0100

    better val names

--- Graphic/learn/src/LessonC7.scala
+++ Graphic/learn/src/LessonC7.scala
@@ -4,52 +4,49 @@ import ostrat._, geom._, pCanv._, Colour._
 /** This lesson displays an interactive Bezier curve whose points can be dragged and also displays the syntax required to draw it */
 case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exploring Beziers")
 { /** defines the size of the circles that represent the points of the bezier */
-  val circleRadius = 15
-  case class Drag(var v: Vec2, c: Colour)
+  val circleRadius = 10
+  case class DragCircle(var loc: Vec2, color: Colour)
   
   /** start point bezier. */
-  val p1 = Drag(-100 vv 0, Gray)
+  val startPoint = DragCircle(-100 vv 0, Red)
   /** End point of bezier curve. */
-  val p2 = Drag(100 vv  0, Gray)
+  val endPoint = DragCircle(100 vv  0, Red)
   /** control point for start point */
-  val c1 = Drag(-100 vv -250, Red)
+  val controlStart = DragCircle(-100 vv -250, Gray)
   /** control point for end point */
-  val c2 = Drag(100 vv 50, Red) 
+  val controlEnd = DragCircle(100 vv 50, Gray) 
   
-  val cCen = Drag(0 vv 150, Violet)
-  val cRad = Drag(-70 vv 150, Orange)
-  
-  val pts = Arr(p1, p2, c1, c2, cCen, cRad)
+  val bezierPoints = Arr(startPoint, endPoint, controlStart, controlEnd)
 
   /** when one of the bezier points is being dragged, this will indicate which */
-  var theDragee: Option[Drag] = None 
+  var theDragee: Option[DragCircle] = None 
   
   drawBezier()
 
   def drawBezier():Unit =
-  { val cds = pts.map(dr => Circle(circleRadius, dr.v).fill(dr.c))
+  { val dragCircles = bezierPoints.map(dr => Circle(circleRadius, dr.loc).fill(dr.color))
 
-    val cl1 = LineDraw(p1.v, c1.v, 1, Grey)    /** line between the start point and its control point */
-    val cl2 = LineDraw(p2.v, c2.v, 1, Grey)    /** line between the end point and its control point */
+    val startControlLine = LineDraw(startPoint.loc, controlStart.loc, 1, Grey)    /** line between the start point and its control point */
+    val endControlLine = LineDraw(endPoint.loc, controlEnd.loc, 1, Grey)    /** line between the end point and its control point */
 
-    val bez = BezierDraw(p1.v, c1.v, c2.v, p2.v, 2, Green) /** the bezier to be displayed */
+    val bezier = BezierDraw(startPoint.loc, controlStart.loc, controlEnd.loc, endPoint.loc, 2, Green) /** the bezier to be displayed */
 
-    /** this holds the syntax required to draw the current bezier (bez) (NB: replace ; with , ) */
-    val txt = TextGraphic("BezierDraw(" + p1.v + ", " + c1.v + ", " + c2.v + ", " + p2.v + ", 2, Green)", 18, 0 vv 300)
-    val circAdj = Circle((cCen.v - cRad.v).magnitude, cCen.v).draw(2.0, Blue)
-
-    val elementsToPaint = cds ++ Arr(txt, cl1, cl2, bez, circAdj)
+    /** this holds the syntax required to draw the current bezier  (NB: replace ; with , ) */
+    val txt = TextGraphic("BezierDraw(" + startPoint.loc + ", " + controlStart.loc + ", " + controlEnd.loc + ", " + endPoint.loc + ", 2, Green)", 18, 0 vv 300)
+    
+    val elementsToPaint = dragCircles ++ Arr(txt, startControlLine, endControlLine, bezier)
 
     repaint(elementsToPaint)
   }
 
   /* test to see if drag operation has started. if the mouseDown is on one of the represented bezier points then set theDragee to its corresponding
    option */
-  canv.mouseDown = (position, _) => theDragee = pts.find(i => (i.v - position).magnitude <= circleRadius)
+  canv.mouseDown = (position, _) => theDragee = bezierPoints.find(i => (i.loc - position).magnitude <= circleRadius)
 
   // When a point is being dragged update the correspondin bezier point with its new position and then redraw the screen. */
   canv.mouseDragged = (position, button) => theDragee match
-  { case Some(drag) => drag.v = position; drawBezier()
+  { case Some(drag) if (1 == 1) => drag.loc = position; drawBezier()
+    case Some(drag) => drag.loc = position; drawBezier()
     case _ => theDragee = None
   }
 
