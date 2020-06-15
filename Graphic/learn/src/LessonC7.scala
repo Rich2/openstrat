35c324b79382726514386ba8ed067d02937f9652
w0d <35996314+w0d@users.noreply.github.com>
2020-06-15 10:39:53 +0100

    LessonC7 control point moves with respective point

--- Graphic/learn/src/LessonC7.scala
+++ Graphic/learn/src/LessonC7.scala
@@ -13,8 +13,10 @@ case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exp
   val endPoint = DragCircle(100 vv  0, Red)
   /** control point for start point */
   val controlStart = DragCircle(-100 vv -250, Gray)
+  var controlStartOffset = 0 vv -250
   /** control point for end point */
   val controlEnd = DragCircle(100 vv 50, Gray) 
+  var controlEndOffset = 0 vv 50
   
   val bezierPoints = Arr(startPoint, endPoint, controlStart, controlEnd)
 
@@ -24,7 +26,7 @@ case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exp
   drawBezier()
 
   def drawBezier():Unit =
-  { val dragCircles = bezierPoints.map(dr => Circle(circleRadius, dr.loc).fill(dr.color))
+  { val dragCircles = bezierPoints.map(dc => Circle(circleRadius, dc.loc).fill(dc.color))
 
     val startControlLine = LineDraw(startPoint.loc, controlStart.loc, 1, Grey)    /** line between the start point and its control point */
     val endControlLine = LineDraw(endPoint.loc, controlEnd.loc, 1, Grey)    /** line between the end point and its control point */
@@ -32,7 +34,7 @@ case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exp
     val bezier = BezierDraw(startPoint.loc, controlStart.loc, controlEnd.loc, endPoint.loc, 2, Green) /** the bezier to be displayed */
 
     /** this holds the syntax required to draw the current bezier  (NB: replace ; with , ) */
-    val txt = TextGraphic("BezierDraw(" + startPoint.loc + ", " + controlStart.loc + ", " + controlEnd.loc + ", " + endPoint.loc + ", 2, Green)", 18, 0 vv 300)
+    val txt = TextGraphic("BezierDraw(" + startPoint.loc + ", " + controlStart.loc + ", " + controlEnd.loc + ", " + endPoint.loc + ", 2, Green)", 18, 0 vv 300, Green)
     
     val elementsToPaint = dragCircles ++ Arr(txt, startControlLine, endControlLine, bezier)
 
@@ -43,10 +45,13 @@ case class LessonC7(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C7: Exp
    option */
   canv.mouseDown = (position, _) => theDragee = bezierPoints.find(i => (i.loc - position).magnitude <= circleRadius)
 
-  // When a point is being dragged update the correspondin bezier point with its new position and then redraw the screen. */
+  // When a point is being dragged update the correspondin bezier point with its new position and then redraw the screen.
+  // case Some(drag) => drag.loc = position; drawBezier()
   canv.mouseDragged = (position, button) => theDragee match
-  { case Some(drag) if (1 == 1) => drag.loc = position; drawBezier()
-    case Some(drag) => drag.loc = position; drawBezier()
+  { case Some(drag) if (drag == startPoint) => drag.loc = position; controlStart.loc = drag.loc +  controlStartOffset; drawBezier()
+    case Some(drag) if (drag == endPoint) => drag.loc = position; controlEnd.loc = drag.loc +  controlEndOffset; drawBezier()
+    case Some(drag) if (drag == controlStart) => drag.loc = position; controlStartOffset = drag.loc - startPoint.loc; drawBezier()
+    case Some(drag) if (drag == controlEnd) => drag.loc = position; controlEndOffset = drag.loc - endPoint.loc; drawBezier()
     case _ => theDragee = None
   }
 
