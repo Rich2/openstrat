7ebfe4c04994c6f92b66574f523a3a573f0d4252
Rich2 <rr1@tutanota.com>
2020-06-17 18:56:30 +0100

    Minor.

--- Graphic/src/shapes/Ellipselign.scala
+++ Graphic/src/shapes/Ellipselign.scala
@@ -1,6 +1,8 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
+import ostrat.pCanv.CanvasPlatform
+import ostrat.pXml.Attrib
 
 /** An Ellipse whose axes are aligned to the x and y axes. The width axis is not necessarily the major axis. */
 case class Ellipselign(xCen: Double, yCen: Double, xRadius: Double, yRadius: Double) extends Ellipse
@@ -27,4 +29,22 @@ case class Ellipselign(xCen: Double, yCen: Double, xRadius: Double, yRadius: Dou
 object Ellipselign
 { def apply(cen: Vec2, xRadius: Double, yRadius: Double): Ellipselign = new Ellipselign(cen.x, cen.y, xRadius, yRadius)
   def cenV1V3(cen: Vec2, v1: Vec2, v3: Vec2): Ellipselign = new Ellipselign(cen.x, cen.y, (v1.x - cen.x).abs, (v3.y - cen.y).abs)
+}
+
+case class EllipselignFill(shape: Ellipselign, fillColour: Colour) extends ShapeFill with TransProlignElem
+{
+  override type ThisT = EllipselignFill
+
+  override def fTrans(f: Vec2 => Vec2): EllipselignFill = EllipselignFill(shape.fTrans(f), fillColour)
+
+  override def attribs: Arr[Attrib] = ???
+
+  /** Renders this functional immutable GraphicElem, using the imperative methods of the abstract [[ostrat.pCanv.CanvasPlatform]] interface. */
+  override def rendToCanvas(cp: CanvasPlatform): Unit = ??? // cp.ellipseFill(thi)
+
+  override def rotateRadians(radians: Double): GraphicElem = ???
+
+  override def mirror(line: Line2): GraphicElem = ???
+
+  override def scaleXY(xOperand: Double, yOperand: Double): GraphicElem = ???
 }
