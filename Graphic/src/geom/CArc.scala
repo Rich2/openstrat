8efbdb3dfcd3d33c8cf0a229181ac268395a1e9e
Rich2 <rr1@tutanota.com>
2020-06-20 17:52:20 +0100

    Minor.

--- Graphic/src/geom/CArc.scala
+++ Graphic/src/geom/CArc.scala
@@ -6,20 +6,20 @@ package geom
 final case class CArc(xStart: Double, yStart: Double, xCen: Double, yCen: Double, deltaRadians: Double) extends SimilarPreserve
 { type ThisT = CArc
   override def fTrans(f: Vec2 => Vec2): CArc = ???
-  def pCen: Vec2 = xCen vv yCen
+  def cen: Vec2 = xCen vv yCen
   
-  def startAngleRadians: Double = (pStart - pCen).angleRadians
-  def startAngleRadiansPos: Double = (pStart - pCen).angleRadiansPos
-  def startAngle: Angle = (pStart - pCen).angle
+  def startAngleRadians: Double = (pStart - cen).angleRadians
+  def startAngleRadiansPos: Double = (pStart - cen).angleRadiansPos
+  def startAngle: Angle = (pStart - cen).angle
   def endAngleRadians: Double = startAngleRadians + deltaRadians
   def endAngleRadiansPos: Double = startAngleRadiansPos + deltaRadians
   def endAngle: Angle = Angle(endAngleRadians)
   def pStart: Vec2 = xStart vv yStart
-  def pEnd: Vec2 = pCen + endAngle.toVec2(radius)
+  def pEnd: Vec2 = cen + endAngle.toVec2(radius)
   def midArcAngleRadians = startAngleRadians + deltaRadians / 2
   def midArcAngle = Angle(midArcAngleRadians)
-  def pMidArc: Vec2 = pCen + midArcAngle.toVec2(radius)
-  def radius: Double = (pStart - pCen).magnitude
+  def pMidArc: Vec2 = cen + midArcAngle.toVec2(radius)
+  def radius: Double = (pStart - cen).magnitude
   def clock: Boolean = deltaRadians < 0
   def antiClock: Boolean = deltaRadians >= 0
   def detltaDegs = deltaRadians.radiansToDegrees
@@ -28,7 +28,7 @@ final case class CArc(xStart: Double, yStart: Double, xCen: Double, yCen: Double
   { val sAng: Angle = startAngle
     val resultAngle = sAng.bisect(endAngle)
     val alphaAngle =  sAng.angleTo(endAngle) / 2
-    pCen + resultAngle.toVec2(radius / alphaAngle.cos)
+    cen + resultAngle.toVec2(radius / alphaAngle.cos)
   }
 
   def xCtrl: Double = pCtrl.x
@@ -36,14 +36,14 @@ final case class CArc(xStart: Double, yStart: Double, xCen: Double, yCen: Double
 
   def xEnd: Double = pEnd.x
   def yEnd: Double = pEnd.y
-  override def slate(offset: Vec2): CArc = CArc(pStart + offset, pCen + offset, deltaRadians)
-  override def scale(operand: Double): CArc = CArc(pStart * operand, pCen * operand, deltaRadians)
+  override def slate(offset: Vec2): CArc = CArc(pStart + offset, cen + offset, deltaRadians)
+  override def scale(operand: Double): CArc = CArc(pStart * operand, cen * operand, deltaRadians)
 
   override def rotateRadians(operandRadians: Double): CArc =
-    CArc(pStart.rotateRadians(operandRadians), pCen.rotateRadians(operandRadians), deltaRadians)
+    CArc(pStart.rotateRadians(operandRadians), cen.rotateRadians(operandRadians), deltaRadians)
 
-  override def mirrorYOffset(xOffset: Double): CArc = CArc(pStart.mirrorYOffset(xOffset), pCen.mirrorYOffset(xOffset), -deltaRadians)
-  override def mirrorXOffset(yOffset: Double): CArc = CArc(pStart.mirrorXOffset(yOffset), pCen.mirrorXOffset(yOffset), -deltaRadians)
+  override def mirrorYOffset(xOffset: Double): CArc = CArc(pStart.mirrorYOffset(xOffset), cen.mirrorYOffset(xOffset), -deltaRadians)
+  override def mirrorXOffset(yOffset: Double): CArc = CArc(pStart.mirrorXOffset(yOffset), cen.mirrorXOffset(yOffset), -deltaRadians)
 
   override def mirror(line: Line2): CArc = ???
   def draw(lineWidth: Double = 2.0, colour: Colour = Colour.Black) = CArcDraw(this,lineWidth, colour)
