ff62f1bbe083fc42978d7c9f477fba1487c8d2ca
Rich2 <rr1@tutanota.com>
2020-06-29 11:19:34 +0100

    #114 Remived CircleOld.

--- Graphic/learn/src/LessonA3.scala
+++ Graphic/learn/src/LessonA3.scala
@@ -5,11 +5,10 @@ case class LessonA3(canv: CanvasPlatform) extends CanvasNoPanels("Lesson A3")
 {
   val x1 = 100
   val y1 = 200
-  val circles = Arr(CircleOld(100).fill(Green), CircleOld(100, 0, 200).fill(Violet), CircleOld(100, 200, 0).fill(SandyBrown),
-    CircleOld(100, 0, -200).fill(Turquoise))
+  val circles = Arr(Circle(100).fill(Green), Circle(100, 0, 200).fill(Violet), Circle(100, 200, 0).fill(SandyBrown),
+    Circle(100, 0, -200).fill(Turquoise))
   val crosses =  Vec2s(0 vv 0, -100 vv 0, 100 vv 0, 0 vv 100).flatMap(v => Cross(1, v))
   repaint(circles ++ crosses)
-
 }
 
 
--- Graphic/learn/src/LessonC4.scala
+++ Graphic/learn/src/LessonC4.scala
@@ -4,16 +4,14 @@ import ostrat._, geom._, pCanv._, Colour._
 
 /** We will use this helper class for Lesson C4 and C5 */
 case class ARect(val cen: Vec2, val width: Double = 200, val height: Double = 100, colour: Colour = Red) extends WithColour
-{
-  def mutateColour(newColour: Colour): ARect = copy(colour = newColour) 
+{ def mutateColour(newColour: Colour): ARect = copy(colour = newColour) 
   def graphic = Rectangle(width, height, cen).parentFill(this, colour)
   def sGraphic = Rectangle(width, height, cen).parentFillDraw(this, colour, 2, contrast)
 }
 
 /** This lesson is working, but has what looks like a very dubious implementation. */
 case class LessonC4(canv: CanvasPlatform) extends CanvasNoPanels("Lesson C4")
-{
-  val r0 = Rectangle(200, 100)//.fill(Red)
+{ val r0 = Rectangle(200, 100)//.fill(Red)
   val r1 = r0.slate(-300 vv 300)
   val r2 = r0.slate(300 vv 300)
   val r3 = r0.slate(300 vv -300)
