e698b93da8e413d2682f59708056a3977fc24b91
Rich2 <rr1@tutanota.com>
2020-07-10 01:07:03 +0100

    Minor.

--- Graphic/src/dist/LatLongs.scala
+++ Graphic/src/dist/LatLongs.scala
@@ -1,6 +1,8 @@
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
 
+/** Immutable flat efficient Array[Double] based collection class for [[geom.LatLong]]s */
 class LatLongs(val arrayUnsafe: Array[Double]) extends AnyVal with ArrProdDbl2[LatLong]
 { type ThisT = LatLongs
   override def unsafeFromArray(array: Array[Double]): LatLongs = new LatLongs(array)
