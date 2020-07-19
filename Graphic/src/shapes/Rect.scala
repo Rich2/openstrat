627a442690c03b7e1b1388787923578ef78dd756
Rich2 <rr1@tutanota.com>
2020-07-19 19:44:47 +0100

    Minor.

--- Graphic/src/shapes/Rect.scala
+++ Graphic/src/shapes/Rect.scala
@@ -3,10 +3,12 @@ package ostrat
 package geom
 
 /** A rectangle class that has position and may not be aligned to the X and Y axes. */
-final case class Rect(xCen: Double, yCen: Double, x0: Double, y0: Double, x1: Double, y1: Double) extends Rectangle with AffinePreserve
+final case class Rect(x0: Double, y0: Double, x1: Double, y1: Double, width: Double) extends Rectangle with AffinePreserve
 { override type ThisT = Rect
-  override def width: Double = ??? // (cenRight - cen).magnitude * 2
+  //override def width: Double = ??? // (cenRight - cen).magnitude * 2
   override def height: Double = ??? //(topCen - cen).magnitude * 2
+  override def xCen: Double = ???
+  override def yCen: Double = ???
   override def cen: Vec2 = xCen vv yCen
   override def v0: Vec2 = x0 vv y0
   override def v1: Vec2 = x1 vv y1
@@ -24,5 +26,5 @@ final case class Rect(xCen: Double, yCen: Double, x0: Double, y0: Double, x1: Do
 
 
 object Rect
-{ def points(cen: Vec2, topRight: Vec2, bottomRight: Vec2): Rect = new Rect(cen.x, cen.y, topRight.x, topRight.y, bottomRight.x, bottomRight.y)
+{ def points(cen: Vec2, topRight: Vec2, bottomRight: Vec2): Rect = ??? // new Rect(cen.x, cen.y, topRight.x, topRight.y, bottomRight.x, bottomRight.y)
 }
