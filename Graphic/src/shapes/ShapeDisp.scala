ed68987b54c02ad1551ef0f00c044309c85da43b
Rich2 <rr1@tutanota.com>
2020-08-22 18:07:14 +0100

    #119

--- Graphic/src/shapes/ShapeDisp.scala
+++ Graphic/src/shapes/ShapeDisp.scala
@@ -1,9 +1,9 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-import pXml._
+import pXml._, Colour.Black
 
-/** A shape based graphic. */
+/** A shape based graphic. Will probably change the name back to ShapeGraphic. */
 trait ShapeDisp extends DisplayElem
 { def shape: Shape
   def facets: Arr[ShapeFacet]
@@ -21,4 +21,14 @@ class FillColour(value: Int) extends ShapeFacet
   def fillAttrib: FillAttrib = FillAttrib(colour)
 }
 
+case class ShapeActive(id: Any) extends ShapeFacet
+{ override def attribs: Arr[Attrib] = Arr()
+}
+
 trait CurveFacet extends ShapeFacet
+
+case class CurveDraw(width: Double = 2.0, colour: Colour = Black) extends CurveFacet
+{ def strokeWidthAttrib: StrokeWidthAttrib = StrokeWidthAttrib(width)
+  def strokeAttrib: StrokeAttrib = StrokeAttrib(colour)
+  override def attribs: Arr[Attrib] = Arr(strokeWidthAttrib, strokeAttrib)
+}
