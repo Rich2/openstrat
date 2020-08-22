9407c21487841c9285ab63669f3b0e608cf3f830
Rich2 <rr1@tutanota.com>
2020-08-22 20:53:16 +0100

    #119

--- Graphic/src/shapes/CircleDisplay.scala
+++ Graphic/src/shapes/CircleDisplay.scala
@@ -2,8 +2,8 @@
 package ostrat
 package geom
 
-case class CircleDisplay(shape: Circle, members: Arr[ShapeMember]) extends ShapeDisplay  with SimilarPreserve
-{ override type ThisT = CircleDisplay
+case class CircleDisplay(shape: Circle, members: Arr[ShapeMember]) extends ShapeDisplay// with SimilarPreserve
+{ /*override type ThisT = CircleDisplay
 
   override def fTrans(f: Vec2 => Vec2): ThisT =
   {
@@ -12,7 +12,49 @@ case class CircleDisplay(shape: Circle, members: Arr[ShapeMember]) extends Shape
       case sd: ShapeDisplay => sd
     }
     CircleDisplay(shape.fTrans(f), newMems)
-  }
+  }*/
+
+  /** Translate geometric transformation. */
+  override def slate(offset: Vec2): CircleDisplay = ???
+
+  /** Translate geometric transformation. */
+  override def slate(xOffset: Double, yOffset: Double): ShapeDisplay = ???
+
+  /** Uniform scaling transformation. The scale name was chosen for this operation as it is normally the desired operation and preserves Circles and
+   * Squares. Use the xyScale method for differential scaling. */
+  override def scale(operand: Double): ShapeDisplay = ???
+
+  /** Mirror, reflection transformation across the line x = xOffset, which is parallel to the X axis. */
+  override def reflectYOffset(xOffset: Double): ShapeDisplay = ???
+
+  /** Mirror, reflection transformation across the line y = yOffset, which is parallel to the X axis. */
+  override def reflectXOffset(yOffset: Double): ShapeDisplay = ???
+
+  /** Mirror, reflection transformation across the X axis. This method has been left abstract in GeomElemNew to allow the return type to be narrowed
+   * in sub classes. */
+  override def reflectX: ShapeDisplay = ???
+
+  /** Mirror, reflection transformation across the X axis. This method has been left abstract in GeomElemNew to allow the return type to be narrowed
+   * in sub classes. */
+  override def reflectY: ShapeDisplay = ???
+
+  override def prolign(matrix: ProlignMatrix): ShapeDisplay = ???
+
+  /** Rotates 90 degrees or Pi/2 radians anticlockwise. */
+  override def rotate90: ShapeDisplay = ???
+
+  /** Rotates 180 degrees or Pi radians. */
+  override def rotate180: ShapeDisplay = ???
+
+  /** Rotates 90 degrees or Pi/2 radians clockwise. */
+  override def rotate270: ShapeDisplay = ???
+
+  override def rotateRadians(radians: Double): ShapeDisplay = ???
+
+  override def reflect(line: Line): ShapeDisplay = ???
+
+  override def reflect(line: Sline): TransElem = ???
+
   override def svgStr: String = ???
 
   override def scaleXY(xOperand: Double, yOperand: Double): ShapeDisplay = ???
