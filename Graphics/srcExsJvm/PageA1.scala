7d444c6d1947e68936ca62700d88b59fffd77c76
Rich2 <rr1@tutanota.com>
2020-09-05 03:19:50 +0100

    Minor.

--- Graphics/srcExsJvm/PageA1.scala
+++ Graphics/srcExsJvm/PageA1.scala
@@ -8,6 +8,7 @@ object PageA1 extends LessonPage
 { val head = HtmlHead(Arr(HtmlTitle("Lesson A1")))
   val bodyStr =
     """<h1>Lesson A1</h1>
-      |<p>Covers Circles and Ellipses.</p>""".stripMargin
+      |<p>This lesson covers Circles and Ellipses. As with the other lessons there will be a summary for those familiar with Scala and anumber of step
+      |by step parts for those new to Scala, programming or geometry and graphics. </p>""".stripMargin
   override val page: HtmlPage = HtmlPage(head, HtmlBody(bodyStr))    
 }
