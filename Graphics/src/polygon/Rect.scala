62f3d63b456153f2e4b406dfd9e5e94441712c27
Rich2 <rr1@tutanota.com>
2020-09-11 01:50:20 +0100

    Minor.

--- Graphics/src/polygon/Rect.scala
+++ Graphics/src/polygon/Rect.scala
@@ -3,11 +3,15 @@ package ostrat
 package geom
 
 /** A rectangle class that has position and may not be aligned to the X and Y axes. */
-final case class Rect(x0: Double, y0: Double, x1: Double, y1: Double, width: Double) extends RectV0V1
+final class Rect(val x0: Double, val y0: Double, val x1: Double, val y1: Double, val width: Double) extends RectV0V1
 { override type ThisT = Rect
   override def height: Double = (v1 - v2).magnitude
   override def fTrans(f: Vec2 => Vec2): Rect = Rect.points(f(cen), f(v0), f(v1))
- 
+
+  override def productArity: Int = 5
+
+  override def productElement(n: Int): Any = ???
+
   override def rotation: Angle = (v0 - v3).angle
 
   override def rotateRadians(radians: Double): Rect = ???
@@ -29,9 +33,11 @@ final case class Rect(x0: Double, y0: Double, x1: Double, y1: Double, width: Dou
   override def fillDrawOld(fillColour: Colour, lineWidth: Double, lineColour: Colour): ShapeFillDraw = ???
 }
 
-
 object Rect
 {
+  def apply(width: Double, height: Double, posn: Vec2, rotation: Angle = 0.degs): Rect = new Rect(posn.x, 0, 0, 0, width)
+  def xy(width: Double, height: Double, xCen: Double, yCen: Double, rotation: Angle = 0.degs): Rect = new Rect(xCen, 0, 0, 0, 0)
+  
   def v0v1(v0: Vec2, v1: Vec2, width: Double): Rect = new Rect(v0.x, v0.y, v1.x, v1.y, width)
   def points(cen: Vec2, topRight: Vec2, bottomRight: Vec2): Rect = ??? // new Rect(cen.x, cen.y, topRight.x, topRight.y, bottomRight.x, bottomRight.y)
 }
