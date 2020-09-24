f34f0a78b475a9f8116324afdf14af788adf28c6
Rich2 <rr1@tutanota.com>
2020-09-24 17:36:10 +0100

    #120

--- Graphics/src/shapes/EllipseGraphic.scala
+++ Graphics/src/shapes/EllipseGraphic.scala
@@ -9,3 +9,22 @@ trait EllipseGraphic extends ShapeGraphic
   @inline final def xCen: Double = shape.xCen
   @inline final def yCen: Double = shape.yCen
 }
+
+/** A Simple circle based graphic. Not sure if this trait is useful. */
+trait EllipseGraphicSimple extends EllipseGraphic with ShapeGraphicSimple with SimilarPreserve
+{ type ThisT <: EllipseGraphicSimple
+
+}
+/** A simple single colour fill of a circle graphic. */
+final case class EllipseFill(shape: Ellipse, colour: Colour) extends EllipseGraphicSimple with ShapeFill
+{ type ThisT = EllipseFill
+  override def fTrans(f: Vec2 => Vec2): ThisT = EllipseFill(shape.fTrans(f), colour)
+  override def rendToCanvas(cp: pCanv.CanvasPlatform): Unit = cp.ellipseFill(shape, colour)
+  override def xyScale(xOperand: Double, yOperand: Double): GraphicSimple = ???
+  override def xShear(operand: Double): TransElem = ???
+
+  override def yShear(operand: Double): TransElem = ???
+  override def svgElem(bounds: BoundingRect): SvgElem = ???
+
+  override def svgStr: String = ???
+}
