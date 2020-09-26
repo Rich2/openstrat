1ff60a705b583cc77d02894d1de7d82075ed65e4
Rich2 <rr1@tutanota.com>
2020-09-26 11:59:40 +0100

    $1

--- Graphics/src/polygon/PolygonGen.scala
+++ Graphics/src/polygon/PolygonGen.scala
@@ -4,7 +4,7 @@ package geom
 import Colour.Black, pWeb._
 
 /** A General Polygon as opposed to a specific Polygon such as a Square or a Rectangle is encoded as a sequence of plain 2 dimension (mathematical)
- *  vectors. Minimum length 3. Clockwise is the default */
+ *  vectors. Minimum length 3. Clockwise is the default. Polygon may be altered to include a centre. */
 final class PolygonGen(val arrayUnsafe: Array[Double]) extends Polygon with Vec2sLikeProdDbl2 with AffinePreserve
 { type ThisT = PolygonGen
   def unsafeFromArray(array: Array[Double]): PolygonGen = new PolygonGen(array)
