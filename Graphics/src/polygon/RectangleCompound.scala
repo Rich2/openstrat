0c23a0374477c6a90bdc28cf6d7aa0f2e51d1a3f
Rich2 <rr1@tutanota.com>
2020-10-01 12:49:37 +0100

    #122 RectanlgeCompound is now a trait.

--- Graphics/src/polygon/RectangleCompound.scala
+++ Graphics/src/polygon/RectangleCompound.scala
@@ -3,7 +3,7 @@ package ostrat
 package geom
 import pWeb._
 
-case class RectangleCompound(shape: Rectangle, facets: Arr[GraphicFacet], children: Arr[GraphicElem] = Arr()) extends ShapeCompound with RectangleGraphic
+trait RectangleCompound extends ShapeCompound with RectangleGraphic
 {
   override def attribs: Arr[XmlAtt] = ???
 
@@ -50,4 +50,59 @@ case class RectangleCompound(shape: Rectangle, facets: Arr[GraphicFacet], childr
   override def yShear(operand: Double): PolygonCompound = ???
 
   override def reflect(line: LineSeg): RectangleCompound = ???
+}
+
+object RectangleCompound
+{
+  def apply(shape: Rectangle, facets: Arr[GraphicFacet], children: Arr[GraphicElem] = Arr()) : RectangleCompound =
+    new RectangleCompoundImp(shape, facets, children)
+
+  case class RectangleCompoundImp(shape: Rectangle, facets: Arr[GraphicFacet], children: Arr[GraphicElem] = Arr()) extends RectangleCompound
+  {
+    override def attribs: Arr[XmlAtt] = ???
+
+    override def svgStr: String = ???
+
+    override def svgElem(bounds: BoundingRect): SvgRect = SvgRect(shape.reflectX.slate(0, bounds.minY + bounds.maxY).
+      attribs ++ facets.flatMap(_.attribs))
+
+    /** Translate geometric transformation. */
+    override def slate(offset: Vec2): RectangleCompoundImp = RectangleCompoundImp(shape.slate(offset), facets, children.slate(offset))
+
+    /** Translate geometric transformation. */
+    override def slate(xOffset: Double, yOffset: Double): RectangleCompoundImp =
+      RectangleCompoundImp(shape.slate(xOffset, yOffset), facets, children.slate(xOffset, yOffset))
+
+    /** Uniform scaling transformation. The scale name was chosen for this operation as it is normally the desired operation and preserves Circles and
+     * Squares. Use the xyScale method for differential scaling. */
+    override def scale(operand: Double): RectangleCompoundImp = RectangleCompoundImp(shape.scale(operand), facets, children.scale(operand))
+
+    /** Mirror, reflection transformation across the X axis. This method has been left abstract in GeomElemNew to allow the return type to be narrowed
+     * in sub classes. */
+    override def reflectX: RectangleCompoundImp = RectangleCompoundImp(shape.reflectX, facets, children.reflectX)
+
+    /** Mirror, reflection transformation across the X axis. This method has been left abstract in GeomElemNew to allow the return type to be narrowed
+     * in sub classes. */
+    override def reflectY: RectangleCompoundImp = RectangleCompoundImp(shape.reflectY, facets, children.reflectY)
+
+    /** Mirror, reflection transformation across the line y = yOffset, which is parallel to the X axis. */
+    override def reflectXParallel(yOffset: Double): RectangleCompoundImp = RectangleCompoundImp(shape.reflectXParallel(yOffset), facets, children.reflectXOffset(yOffset))
+
+    /** Mirror, reflection transformation across the line x = xOffset, which is parallel to the X axis. */
+    override def reflectYParallel(xOffset: Double): RectangleCompoundImp = RectangleCompoundImp(shape.reflectYParallel(xOffset), facets, children.reflectYOffset(xOffset))
+
+    override def prolign(matrix: ProlignMatrix): RectangleCompoundImp = RectangleCompoundImp(shape.prolign(matrix), facets, children.prolign(matrix))
+
+    override def rotateRadians(radians: Double): RectangleCompoundImp = RectangleCompoundImp(shape.rotateRadians(radians), facets, children.rotateRadians(radians))
+
+    override def reflect(line: Line): RectangleCompoundImp = ???
+
+    override def xyScale(xOperand: Double, yOperand: Double): PolygonCompound = ???
+
+    override def xShear(operand: Double): PolygonCompound = ???
+
+    override def yShear(operand: Double): PolygonCompound = ???
+
+    override def reflect(line: LineSeg): RectangleCompoundImp = ???
+  }
 }
