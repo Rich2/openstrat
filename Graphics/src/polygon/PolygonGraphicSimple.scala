d67dc8152cd9b7b756ce3c0315755e0b289dde94
Rich2 <rr1@tutanota.com>
2020-10-03 09:56:04 +0100

    Minor.

--- Graphics/src/polygon/PolygonGraphicSimple.scala
+++ Graphics/src/polygon/PolygonGraphicSimple.scala
@@ -1,13 +1,11 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-import pCanv._, Colour.Black, pWeb._
+import Colour.Black, pWeb._
 
 /** This trait may be removed. */
 trait PolygonGraphicSimple extends PolygonGraphic with ShapeGraphicSimple
-{ //type ThisT <: PolygonGraphicSimple
-  //override def shape: Polygon
-  def xHead: Double = shape.x0
+{ def xHead: Double = shape.x0
   def yHead: Double = shape.y0
 
   /** The number of vertices. */
@@ -64,27 +62,6 @@ trait PolygonGraphicSimple extends PolygonGraphic with ShapeGraphicSimple
   override def canEqual(that: Any): Boolean = ???
 }
 
-/** Immutable Graphic element that defines and draws a Polygon. */
-trait PolygonDraw extends ShapeDraw with PolygonGraphicSimple
-{ //override def fTrans(f: Vec2 => Vec2): PolygonDraw = PolygonDraw(shape.fTrans(f), lineWidth, lineColour)
-  override def rendToCanvas(cp: CanvasPlatform): Unit = cp.polygonDraw(shape, lineWidth, lineColour)
-}
-
-object PolygonDraw
-{
-  def apply (shape: Polygon, lineWidth: Double = 2, lineColour: Colour = Black): PolygonDraw = PolygonDrawImp(shape, lineWidth, lineColour)
-  
-  /*implicit val persistImplicit: Persist3[Polygon, Double, Colour, PolygonDraw] =
-    Persist3("PolyFill", "poly", _.shape, "lineWidth", _.lineWidth, "colour", _.lineColour, apply)*/
-
-  /** Immutable Graphic element that defines and draws a Polygon. */
-  case class PolygonDrawImp(shape: Polygon, lineWidth: Double = 2, lineColour: Colour = Black) extends PolygonDraw with GraphicAffineElem with
-    GraphicBoundedAffine
-  { override type ThisT = PolygonDrawImp
-    override def fTrans(f: Vec2 => Vec2): PolygonDrawImp = PolygonDrawImp(shape.fTrans(f), lineWidth, lineColour)
-    override def rendToCanvas(cp: CanvasPlatform): Unit = cp.polygonDraw(shape, lineWidth, lineColour)
-  }
-}
 
 /** An active transparent pointable polygon */
 trait PolygonActiveOld extends GraphicActiveOld
--- Graphics/src/shapes/ShapeGraphicSimple.scala
+++ Graphics/src/shapes/ShapeGraphicSimple.scala
@@ -34,18 +34,3 @@ trait ShapeGraphicSimple extends ShapeGraphic with GraphicSimple
 
   override def xyScale(xOperand: Double, yOperand: Double): ShapeGraphicSimple
 }
-
-
-
-/** A simple no compound graphic that draws a shape. The line has a sinlge width and colour. */
-trait ShapeDraw extends ShapeGraphicSimple
-{ /** The line width of this draw graphic */
-  def lineWidth: Double
-
-  /** The line colour of this draw graphic. */
-  def lineColour: Colour
-
-  def strokeWidthAttrib: StrokeWidthAttrib = StrokeWidthAttrib(lineWidth)
-  def strokeAttrib: StrokeAttrib = StrokeAttrib(lineColour)
-  def nonShapeAttribs: Arr[XmlAtt] = Arr(strokeWidthAttrib, strokeAttrib)
-}
