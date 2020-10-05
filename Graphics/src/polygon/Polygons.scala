a41e13fe754ef52637ea91cd84559fa1e14d5f2a
Rich2 <rr1@tutanota.com>
2020-10-05 17:21:09 +0100

    Minor.

--- Graphics/src/polygon/Polygons.scala
+++ Graphics/src/polygon/Polygons.scala
@@ -1,21 +1,20 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-import collection.mutable.ArrayBuffer
+//import collection.mutable.ArrayBuffer
 
 /** Specialised Array based immutable collection class for [[Polygon]]s.  */
-/*
-final class Polygons(val array: Array[Array[Double]]) extends AnyVal with ArrArrayDbl[PolygonGen]
+/*final class Polygons(val array: Array[Array[Double]]) extends AnyVal with ArrArrayDbl[PolygonGen]
 { override type ThisT = Polygons
   override def typeStr: String = "Polygons"
   override def unsafeFromArrayArray(aad: Array[Array[Double]]): Polygons = new Polygons(aad)
   def apply(index: Int): PolygonGen = new PolygonGen(array(index))
   override def fElemStr: PolygonGen => String = _.str
-}
+}*/
 
 /** Companion object for the [[Polygons]] class. */
 object Polygons
-{
+/*{
   def apply(input: PolygonGen*): Polygons =
   {
     val array: Array[Array[Double]] = new Array[Array[Double]](input.length)
