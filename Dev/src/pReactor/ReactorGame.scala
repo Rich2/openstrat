be2941e2f553e3a88fa833b9826bbe19a5773547
w0d <35996314+w0d@users.noreply.github.com>
2020-10-07 01:28:28 +0100

    ReactorGame core functionality

--- Dev/src/pReactor/ReactorGame.scala
+++ Dev/src/pReactor/ReactorGame.scala
@@ -9,20 +9,23 @@ abstract class ReactorGame(rows: Int = 8, cols: Int = 10, thePlayers:Array[Colou
   var players:Array[Colour]
   var currentPlayer:Colour
   var cellCounts:Array[Int]
-  var cellColors = Array.fill[Colour](rows*cols)(Black)
-  val cellSites = new Array[Array[String]](rows*cols)
-  val cellNeighbours = new Array[Array[Int]](rows*cols)
-  var reactionQueue = Array[Array[Int]]()
+  var cellColors:Array[Colour]
+  val cellSites:Array[Array[String]]
+  val cellNeighbours:Array[Array[Int]]
+  var addBallQueue:Array[Int]
+  var currentAddBallQueue:Array[Int]
+  var winner:Colour
 
   newGame()
   
-  def newGame() : Unit =
+  def newGame(): Unit =
   { turn = 0
     players = thePlayers.clone()
     currentPlayer = players(0)
     cellCounts = Array.fill[Int](rows*cols)(0)
     cellColors = Array.fill[Colour](rows*cols)(Black)
-    reactionQueue = Array[Array[Int]]()
+    addBallQueue = Array[Int]()
+    winner = Black
     ijUntilForeach(0, rows)(0, cols)
     { (r, c) =>
       val index:Int = c + cols * r
@@ -46,25 +49,46 @@ abstract class ReactorGame(rows: Int = 8, cols: Int = 10, thePlayers:Array[Colou
       }
     }
   }
-   def isGameOver() : Boolean =
+   def isGameOver(): Boolean =
   { if (turn >= players.length) players = players.filter(cellColors.indexOf(_) != -1)
     if (players.length < 2) 
-    { reactionQueue.drop(reactionQueue.length)
+    { addBallQueue.drop(addBallQueue.length)
+      winner = currentPlayer
       true
     } else {
       false
     }
   }
-  def turnComplete() : Unit =
+  def turnComplete(): Unit =
   { turn += 1
     var currentPlayerIndex = players.indexOf(currentPlayer) + 1
     if (currentPlayerIndex >= players.length) currentPlayerIndex = 0
     currentPlayer = players(currentPlayerIndex)
   }
-  def addBallByIndex(cellIndex:Int) : Unit = 
-  { if (players.length > 1) 
-    { cellColors(cellIndex) = currentPlayer
-      cellCounts(cellIndex) += 1
+  def addBallByIndex(cellIndex:Int): Int =
+  { cellColors(cellIndex) = currentPlayer
+    cellCounts(cellIndex) += 1
+    cellIndex
+  }
+  def takeTurn(thisPlayer:Colour, thisCell:Int): Unit =
+  { if (players.length > 1 && thisPlayer == currentPlayer) 
+    { if (cellColors(thisCell) == Black || cellColors(thisCell) == thisPlayer) addBallQueue = Array(thisCell)
+    }
+  }
+  def processQueue():Unit = 
+  { //var newAddBallQueue:Array[Int] = Array[Int]()
+    for (i <- 0 to addBallQueue.length - 1) addBallByIndex(addBallQueue(i)) //{ newAddBallQueue = newAddBallQueue :+ addBallByIndex(addBallQueue(i)) }
+    addBallQueue = Array[Int]()  //  newAddBallQueue.clone
+  }
+  def isReadyToPop(thisCell:Int):Boolean = 
+  { if (cellCounts(thisCell) >= cellNeighbours(thisCell).length) true
+    else false
+  }
+  def doThePop(thisCell:Int): Unit = 
+  { if (isReadyToPop(thisCell))
+    { cellCounts(thisCell) -= cellNeighbours(thisCell).length
+      if (cellCounts(thisCell)==0) cellColors(thisCell) = Black
+      for ( x <- cellNeighbours(thisCell) ) addBallQueue = addBallQueue :+ x
     }
   }
 }
