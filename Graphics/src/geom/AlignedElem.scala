946ab5c6c7d5e47930be6f80261fe4bff9930f41
Rich2 <rr1@tutanota.com>
2020-10-09 10:36:12 +0100

    Minor.

---
+++ Graphics/src/geom/AlignedElem.scala
@@ -0,0 +1,26 @@
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+package geom
+
+/** This is either an alignedElem which inherits from BoundedElem or [[pCanv.CanvasPlatform]] or [[pCanv.Panel]]. */
+trait AlignedElem
+{
+  def xCen: Double
+  def yCen: Double
+  def cen: Vec2
+  def xTopRight: Double
+  def yTopRight: Double
+  def topRight: Vec2
+  def xBottomRight: Double
+  def yBottomRight: Double
+  def bottomRight: Vec2
+  def xBottomLeft: Double
+  def yBottomLeft: Double
+  def bottomLeft: Vec2
+  def xTopLeft: Double
+  def yTopLeft: Double
+  def topLeft: Vec2
+  /*def xTopCen: Double
+  def yTopCen: Double
+  def topCen: Vec2*/
+}
--- Graphics/src/geom/AlignedGeom.scala
+++ Graphics/src/geom/AlignedGeom.scala
@@ -2,29 +2,9 @@
 package ostrat
 package geom
 
-/** This is either an alignedElem which inherits from Geom or something like a Panel. */
-trait AlignedElem
-{
-  def xCen: Double
-  def yCen: Double
-  def cen: Vec2
-  def xTopRight: Double
-  def yTopRight: Double
-  def topRight: Vec2
-  def xBottomRight: Double
-  def yBottomRight: Double
-  def bottomRight: Vec2
-  def xBottomLeft: Double
-  def yBottomLeft: Double
-  def bottomLeft: Vec2
-  def xTopLeft: Double
-  def yTopLeft: Double
-  def topLeft: Vec2
-  /*def xTopCen: Double
-  def yTopCen: Double
-  def topCen: Vec2*/
-}
-
 trait AlignedGeom extends BoundedElem with AlignedElem
+{
+
+}
 
 trait ShapeAligned extends Shape with AlignedGeom
