20c937a8641b72e586f4f28bb0ab58240ab487cc
Rich2 <rr1@tutanota.com>
2020-10-12 11:11:15 +0100

    Minor.

---
+++ Dev/src/pReactor/package.scala
@@ -0,0 +1,9 @@
+/* Copyright 2018-20 W0d. Licensed under Apache Licence version 2.0. */
+package ostrat
+
+/** Describe the package here. */
+package object pReactor
+{
+  /** Method to make sure package object is not empty. */
+  def sillyMethod: String = "Silly"
+}
