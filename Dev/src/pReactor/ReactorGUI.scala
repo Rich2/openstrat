83c2837ecebe0a31d23a749690f090a7a7c72230
w0d <35996314+w0d@users.noreply.github.com>
2020-10-16 17:50:29 +0100

    Reactor Save Working (except Colors) Working on Load

--- Dev/src/pReactor/ReactorGUI.scala
+++ Dev/src/pReactor/ReactorGUI.scala
@@ -15,6 +15,8 @@ case class ReactorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Reactor")
   var animationStep = 0.0
   var animationType = "Scale"
 
+  var gameData = ""
+
   def gameBtn(str: String, cmd: MouseButton => Unit) =
     Rectangle.curvedCornersCentred(str.length.max(2) * 17, 25, 5, -100 vv -100).parentAll(MouseButtonCmd(cmd), White, 3, Black, 25, str)
 
@@ -61,7 +63,7 @@ case class ReactorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Reactor")
     }
   // popBall animation
     for (i <- 0 to aDefaultGame.popBallQueue.length - 1)
-    { var loc = size*(i % cols) vv size*(i / cols)
+    { val loc = size*(i % cols) vv size*(i / cols)
       var locy = 0 vv 0
       if (aDefaultGame.isReadyToPop(i) == true) 
       { //deb("POP_ANIM="+animationStep.toString)
@@ -107,7 +109,7 @@ case class ReactorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Reactor")
         { if (aDefaultGame.isGameOver() == true) declareWinner()
           turnComplete()
         } else canv.timeOut(() => doAnimation(), 25)
-      } else deb("***-- ITS A WRONG-UN! --**********************************************************")
+      } else deb("***-- ITS A WRONG-UN! --*********************************************************")
     } else canv.timeOut(() => doAnimation(), 25)
   }
   def getLocFromCellSite(whichCell: Int, whichOne: Int, whichPos: String = "") : Vec2 =
@@ -147,15 +149,25 @@ case class ReactorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Reactor")
       case (_, _, _) => deb("uncaptured click")
     }
 
+//--** NEED TO CHECK SAVE ISNT HAPPENING DURING A TURN**--//
   def saveGame() : Unit =
-  { var saveData = "\n"
-    saveData += aDefaultGame.turn.toString + "\n"
-    saveData += aDefaultGame.players.mkString(",") + "\n"
-    saveData += aDefaultGame.cellCounts.mkString(",") + "\n"
-    saveData += aDefaultGame.cellColors.mkString(",") + "\n"
-    saveData += aDefaultGame.currentPlayer.toString + "\n"
-    canv.saveFile("Reactor.data", saveData)
-    deb("Saved!")
+  { //var s2 = Sett("Rows", 5).ap("Cols", 6).str
+    //s2 = Sett("Game", aDefaultGame).str
+    //val c1 = s2.findSett("Cols")
+    //val c2 = s2.findSett("Rows")
+//******************************************    val c3 = s2.findType[ReactorGame]
+    //deb("***********************")
+    //deb("s2="+s2.toString)
+    //deb("rows="+c2.toString+" cols="+c1.toString)
+//******************************************    deb("2nd Int="+c3.toString)
+    deb("***********************")
+    if (aDefaultGame.gameState == "turn")
+    { val saveData = Sett("rows", aDefaultGame.rows).ap("cols", aDefaultGame.cols).ap("turn", aDefaultGame.turn).ap("gameState",
+         aDefaultGame.gameState).ap("currentPlayer", aDefaultGame.currentPlayer).ap("cellCounts", aDefaultGame.cellCounts).str//.ap("cellColors",  aDefaultGame.cellColors).str
+      canv.saveFile("Reactor.data", saveData)
+      deb("Saved! =>\n"+saveData)
+      gameData = saveData
+    }
   }
   def loadGame() : Unit = 
   {
@@ -176,177 +188,7 @@ case class ReactorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Reactor")
     //canv.textGraphic(turn.toString, 11, -3*size/4 vv -3*size/4, Black)
   }
 }
-// /* Copyright 2018-20 w0d. Licensed under Apache Licence version 2.0. */
-// package ostrat
-// package pReactor
-// import geom._, pCanv._, Colour._
 
-// /** A clone of the classic Atoms game */
-// case class ReactorGUI (canv: CanvasPlatform) extends CanvasNoPanels("Reactor")
-// {
-//   var stun_turn_roomname_playername = "stunId.turnId.roomId.playerId"
-//   val size = 40  //cell size in pixels
-//   val rows = 8
-//   val cols = 10
-//   var turn = 0
-//   var players = Array(Red, Green, Yellow, Blue)
-//   var currentPlayer = players(0)
-//   var cellCounts = Array.fill[Int](rows*cols)(0)
-//   var cellColors = Array.fill[Colour](rows*cols)(Black)
-//   val cellSites = new Array[Array[String]](rows*cols)
-//   val cellNeighbours = new Array[Array[Int]](rows*cols)
-//   var reactionQueue = Array[Int]()
-
-//   var animationStep = 0.0
-//   var animationIndexes = Array[Int]()
-//   var animationIndexesBall = Array[Int]()
-//   var animationType = "Scale"
-//   def gameBtn(str: String, cmd: MouseButton => Unit) =
-//     Rectangle.curvedCornersCentred(str.length.max(2) * 17, 25, 5, -100 vv -100).parentAll(MouseButtonCmd(cmd), White, 3, Black, 25, str)
-
-//   //newGame()
-
-//   val aDefaultGame = new ReactorGame
-
-  
-  
-//   def newGame() : Unit =
-//   { 
-//     repaints(
-//       Rect(width, height, 0 vv 0).fill(Colour(0xFF181818)),
-//       gameBtn("new | load | save", (mb: MouseButton) => { deb("3") })
-//     )
-//     animationIndexes = Array[Int]()
-//     animationIndexesBall = Array[Int]()
-//     animationStep = 0.0
-//     turn = 0
-//     players = Array(Red, Green, Yellow, Blue)
-//     currentPlayer = players(0)
-//     reactionQueue = Array[Int]()
-//     ijUntilForeach(0, rows)(0, cols){ (r, c) =>
-//       val index = c+cols*r
-//       cellCounts(index) = 0
-//       cellColors(index) = Black
-//       drawBalls(size*c vv size*r, cellColors(index), index)
-//       cellNeighbours(index) = Array[Int]()
-//       cellSites(index) = Array[String]()
-//       if (c>0) 
-//       { cellNeighbours(index) = (index-1) +: cellNeighbours(index)
-//         cellSites(index) = "W" +: cellSites(index)
-//       }
-//       if (r>0) 
-//       { cellNeighbours(index) = (index-cols) +: cellNeighbours(index)
-//         cellSites(index) = "S" +: cellSites(index)
-//       }
-//       if (c<(cols-1)) 
-//       { cellNeighbours(index) = (index+1) +: cellNeighbours(index)
-//         cellSites(index) = "E" +: cellSites(index)
-//       }
-//       if (r<(rows-1)) 
-//       { cellNeighbours(index) = (index+cols) +: cellNeighbours(index)
-//         cellSites(index) = "N" +: cellSites(index)
-//       }
-//     }
-//     canv.polygonFill(Rect.bl(size/2, size/2, -size vv -size), currentPlayer)
-//   }
-//   def drawBalls(loc:Vec2, color:Colour, cellIndex:Int, whichNewBall:Int = 1) : Unit =
-//   { val count = cellCounts(cellIndex)
-//     val isAnimation = animationIndexes.contains(cellIndex)
-//     canv.polygonFill(Rect.bl(size-1, size-1, loc), Black)
-//     if (isAnimation) canv.circleFill(Circle(size/(8/animationStep), loc+getLocFromCellSite(cellIndex, whichNewBall-1)), color)
-   
-//     if (count > 1) canv.circleFill(Circle(size/8, loc+getLocFromCellSite(cellIndex, 0)), color)
-//     if (count > 2) canv.circleFill(Circle(size/8, loc+getLocFromCellSite(cellIndex, 1)), color)
-//     if (count > 3) canv.circleFill(Circle(size/8, loc+getLocFromCellSite(cellIndex, 2)), color)
-//     if (count > 4) canv.circleFill(Circle(size/8, loc+getLocFromCellSite(cellIndex, 3)), color)
-//     if (count > 5) canv.circleFill(Circle(size/8, loc+getLocFromCellSite(cellIndex, 4)), color)
-//     if (count > 6) canv.polygonFill(Rect.bl(size-1, size-1, loc), Pink)
-//   }
-//   def getLocFromCellSite(whichCell: Int, whichOne: Int) : Vec2 =
-//   { deb("whichCell="+whichCell.toString)
-//     deb("whichOne="+whichOne.toString)
-//     val pos = cellSites(whichCell)(whichOne) 
-//     if ("N" == pos) size/2 vv 3*size/4
-//     else if ("E" == pos) 3*size/4 vv size/2
-//     else if ("S" == pos) size/2 vv size/4
-//     else if ("W" == pos) size/4 vv size/2
-//     else 0 vv 0
-//   }
-//   def processQueue() : Unit = 
-//   { if (animationIndexes.length > 0)
-//     { //deb("animateQueue")
-//       animationStep += 0.1
-//       for (animateThisIndex <- 0 to animationIndexes.length - 1)
-//       { drawBalls(size*(animationIndexes(animateThisIndex) % cols) vv size*(animationIndexes(animateThisIndex) / cols), 
-//         currentPlayer,
-//         animationIndexes(animateThisIndex),
-//         animationIndexesBall(animateThisIndex))
-//       }
-//       if (animationStep >= 1)
-//       { animationStep = 0.0
-//         reactionQueue = animationIndexes.clone()
-//         animationIndexes = Array[Int]()
-//         animationIndexesBall = Array[Int]()
-//       }
-//       canv.timeOut(() => ReactorGUI.this.processQueue(), 25)
-//     }
-//     else 
-//     { //deb("reactionQueue")
-//       if (reactionQueue.length > 0)
-//       { val thisOne = reactionQueue(0)
-//         reactionQueue = reactionQueue.tail
-//         if (cellCounts(thisOne) >= cellNeighbours(thisOne).length) {
-//           cellCounts(thisOne) -= cellNeighbours(thisOne).length
-//           if (cellCounts(thisOne)==0) cellColors(thisOne) = Black
-//           drawBalls(size*(thisOne % cols) vv size*(thisOne / cols), currentPlayer, thisOne)
-//           for ( x <- cellNeighbours(thisOne) ) addBallByIndex( x )
-//         }
-//         canv.timeOut(() => ReactorGUI.this.processQueue(), 25)
-//       } else 
-//       { turnComplete()
-//       }
-//       checkForGameOver()
-//     }
-//   }
-//   def checkForGameOver() : Unit =
-//   { if (turn >= players.length) players = players.filter(cellColors.indexOf(_) != -1)
-//     if (players.length < 2) 
-//     { canv.textGraphic(" Wins!", 16, 10 vv (-3*size/4), currentPlayer)
-//       reactionQueue.drop(reactionQueue.length)
-//       () // Explicitly return unit //NB this suppresses the compiler warning "discarded non-Unit value" generated by the above line 
-//     }
-//   }
-//   def turnComplete() : Unit =
-//   { turn += 1
-//     var currentPlayerIndex = players.indexOf(currentPlayer) + 1
-//     if (currentPlayerIndex >= players.length) currentPlayerIndex = 0
-//     currentPlayer = players(currentPlayerIndex)
-//     canv.polygonFill(Rect.bl(size/2, size/2, -size vv -size), currentPlayer)
-//     canv.textGraphic(turn.toString, 11, -3*size/4 vv -3*size/4, Black)
-//   }
-//   def addBallByIndex(cellIndex:Int) : Unit = 
-//   { if (players.length > 1) 
-//     { cellColors(cellIndex) = currentPlayer
-//       cellCounts(cellIndex) += 1
-//       animationIndexes = animationIndexes :+ cellIndex
-//       animationIndexesBall = animationIndexesBall :+ cellCounts(cellIndex)
-//       animationStep = 0.0
-//       drawBalls(size*(cellIndex % cols) vv size*(cellIndex / cols), currentPlayer, cellIndex, cellCounts(cellIndex))
-//     }
-//   }
-//   mouseUp =
-//     { case (LeftButton, cl, v) if((reactionQueue.length == 0 && animationIndexes.length == 0) && v._1 >= 0  &&  v._1 < (size*cols)  &&  v._2 >= 0  &&  v._2 < (size*rows)) =>
-//       { val clickedCellIndex = (v._1/size).toInt+cols*((v._2/size).toInt)
-//         if (currentPlayer == cellColors(clickedCellIndex) || Black  == cellColors(clickedCellIndex))
-//         { addBallByIndex(clickedCellIndex)
-//           canv.timeOut(() => ReactorGUI.this.processQueue(), 25)
-//         }
-//       }
-//       case (LeftButton, cl, v) if (cl.length > 0) => newGame()
-//       case (MiddleButton, cl, v) if (cl.length > 0) => loadGame()
-//       case (RightButton, cl, v) if (cl.length > 0) => saveGame()
-//       case (_, _, _) => deb("uncaptured click")
-//     }
 
 //   def saveGame() : Unit =
 //   { var saveData = "\n"
