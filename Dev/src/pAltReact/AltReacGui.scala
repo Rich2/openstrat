76c8fafe8f85f9b629e647f3a529e731ee603b20
Rich2 <rr1@tutanota.com>
2020-10-17 12:04:53 +0100

    Scala.js => 1.3.0.

--- Dev/src/pAltReact/AltReacGui.scala
+++ Dev/src/pAltReact/AltReacGui.scala
@@ -1,7 +1,10 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package pAltReact
+import pCanv._
 
-class AltReacGui {
-
+case class AltReacGui(canv: CanvasPlatform) extends CmdBarGui("Alternative Reactor")
+{
+  var statusText = "To be changed."
+  reTop(Arr(status))
 }
