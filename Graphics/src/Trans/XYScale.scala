a1ae7f6f7f505619e9d35fb813713c296a499c65
Rich2 <rr1@tutanota.com>
2020-10-18 00:47:48 +0100

    Minor.

--- Graphics/src/Trans/XYScale.scala
+++ Graphics/src/Trans/XYScale.scala
@@ -1,8 +1,7 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-import reflect.ClassTag
-import scala.annotation.unchecked.uncheckedVariance
+import reflect.ClassTag, annotation.unchecked.uncheckedVariance
 
 /** type class for scale transformation where the X and Y components can be scaled independently. */
 trait XYScale[T]
--- Graphics/src/rectangle/RectGraphic.scala
+++ Graphics/src/rectangle/RectGraphic.scala
@@ -9,11 +9,15 @@ trait RectGraphic extends RectangleGraphic with ShapeGraphicAligned
 
 trait RectGraphicSimple extends RectGraphic with RectangleGraphicSimple
 
+/** A rectangular Graphic aligned to the axes, filled with a single colour. */
 trait RectFill extends RectGraphicSimple with RectangleFill
 
-/** Companion object for the RectFill trait, contains a RectFillImp implementation class and an aplly method that delegates to it. */
+/** Companion object for the RectFill trait, contains a RectFillImp implementation class and an apply method that delegates to it. */
 object RectFill
 {
+  /** Factory method for RectFill. A rectangular Graphic aligned to the axes, filled with a single colour. it delegates to the RectfllImp i
+   * implementation class, but has a return type of RectFill. */
   def apply(rect: Rect, fillColour: Colour): RectFill = RectFillImp(rect, fillColour)
+
   case class RectFillImp(shape: Rect, colour: Colour) extends RectFill
 }
