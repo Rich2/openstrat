ded777a9b56c2f055a05653dfc6800e02fede43f
Rich2 <rr1@tutanota.com>
2020-10-18 16:58:54 +0100

    Minor.

---
+++ TileExp/src/HexGridRegSimple.scala
@@ -0,0 +1,7 @@
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+package pGrid
+
+class HexGridRegSimple(val yTileMin: Int, val yTileMax: Int, val cTileMin: Int, val cTileMax: Int)  {
+
+}
--- Tiling/srcExs/gOne/OneScen.scala
+++ Tiling/srcExs/gOne/OneScen.scala
@@ -39,7 +39,7 @@ object OneScen
 
 object OneScen1 extends OneScenStart
 { implicit val grid = new HexGridRegSimple(2, 6, 2, 10)
-  val oPlayers = grid.newTileArrOpt[Player]
+  val oPlayers: TilesArrOpt[Player] = grid.newTileArrOpt
   oPlayers.mutSetSome(4, 4, PlayerA)
   oPlayers.unsafeSetSomes((4, 8, PlayerB), (6, 10, PlayerC))
 }
