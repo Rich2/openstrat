5c1347495cc324115b22a51b344e9241105d6e70
Rich2 <rr1@tutanota.com>
2020-10-23 21:00:01 +0100

    Minor.

--- Graphics/src/polygon/PolygonNPlus.scala
+++ Graphics/src/polygon/PolygonNPlus.scala
@@ -2,25 +2,48 @@
 package ostrat
 package geom
 
+/** A [[Polygon]] with at least 3 vertices. */
 trait Polygon3Plus extends Polygon
 {
-  /** The X component of the 1st Vertex. */
+  /** The X component of the 1st Vertex. The default convention is for the vertices to be numbered in a clockwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def x1: Double
-  /** The Y component of the 1st Vertex. */
+  /** The Y component of the 1st Vertex. The default convention is for the vertices to be numbered in a clockwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def y1: Double
-  /** The 1st Vertex. */
+  /** The 1st Vertex. The default convention is for the vertices to be numbered in a clockwise direction with the 1st vertex immediately clockwise
+   *  from 12 o'clock. */
   def v1: Vec2
-  /** The X component of the 2nd Vertex. */
+  /** The X component of the 2nd Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def x2: Double
+  /** The Y component of the 2nd Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def y2: Double
+  /** The 2nd Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex immediately clockwise from
+   *  12 o'clock. */
   def v2: Vec2
+  /** The X component of the 3rd Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def x3: Double
+  /** The Y component of the 3rd Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def y3: Double
+  /** The 3rd Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex immediately clockwise from
+   *  12 o'clock. */
   def v3: Vec2
 }
 
+/** A [[Polygon]] with at least 4 vertices. */
 trait Polygon4Plus extends Polygon3Plus
-{ def x4: Double
+{
+  /** The X component of the 4th Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
+  def x4: Double
+  /** The Y component of the 4th Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex
+   *  immediately clockwise from 12 o'clock. */
   def y4: Double
+  /** The 4th Vertex. The default convention is for the vertices to be numbered in a clokwise direction with the 1st vertex immediately clockwise from
+   *  12 o'clock. */
   def v4: Vec2
 }
