425e16d314e9bf729488c3044318d5054f2a8e3f
Rich2 <rr1@tutanota.com>
2020-11-02 16:17:22 +0000

    Minor.

--- Graphics/src/geom/CArc3.scala
+++ Graphics/src/geom/CArc3.scala
@@ -3,7 +3,27 @@ package ostrat
 package geom
 
 /** Temporary name for new version of circular arc. */
-case class CArc3(xStart: Double, yStart: Double, xCircMid: Double, yCircMid: Double, xEnd: Double, yEnd: Double) extends CurveSeg
-{
+case class CArc3(xStart: Double, yStart: Double, xCiMid: Double, yCiMid: Double, xEnd: Double, yEnd: Double) extends CurveSeg
+{ /** The mid point (of the circumference) of the arc */
+  def ciMid: Vec2 = xCiMid vv yCiMid
 
+  /** The mid point of the chord of the arc. */
+  def chMid: Vec2 = pStart mid pEnd
+
+  def midMid: LineSeg = chMid.lineTo(ciMid)
+
+  def midMidLen: Double = midMid.length
+
+  /** The chord of this Arc */
+  def chord: LineSeg = pStart.lineTo(pEnd)
+
+  /** length of the chord of this arc. */
+  def chordLen: Double = chord.length
+
+  /** half of length of the chord of this arc. */
+  def chordHLen: Double = chord.length
+
+  def diameter: Double = chordHLen.squared / midMidLen + midMidLen
+
+  @inline def radius: Double = diameter / 2
 }
