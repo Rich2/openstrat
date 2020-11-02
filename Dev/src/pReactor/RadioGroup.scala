b5ba7206bc4da99819f2e8924317257a8c7cefb6
w0d <35996314+w0d@users.noreply.github.com>
2020-11-02 01:12:57 +0000

    Update RadioGroup.scala

--- Dev/src/pReactor/RadioGroup.scala
+++ Dev/src/pReactor/RadioGroup.scala
@@ -2,7 +2,7 @@ package ostrat
 package pReactor
 import geom._, pCanv._, Colour._
 
-/** Simple Checkbox */
+/** Controls a collection of RadioOptions */
 case class RadioGroup(selectedIndex:Int = 0, radioOptions:Array[RadioOption], loc:Vec2 = 0 vv 0)
 { def put(aSelected:Int = selectedIndex, aRadioOptions:Array[RadioOption] = radioOptions, loc:Vec2 = loc): Unit =
   { 
