7c9fcc12bfbea4530dc88c4bf6768a68598df3a4
w0d <35996314+w0d@users.noreply.github.com>
2020-11-02 01:13:47 +0000

    Update RadioOption.scala

--- Dev/src/pReactor/RadioOption.scala
+++ Dev/src/pReactor/RadioOption.scala
@@ -2,7 +2,7 @@ package ostrat
 package pReactor
 import geom._, pCanv._, Colour._
 
-/** Simple Checkbox */
+/** Simple Radion button with label */
 case class RadioOption(isSelected:Boolean = false, labelText:String = "", loc:Vec2 = 0 vv 0)
 { def put(canv: CanvasPlatform, aIsSelected:Boolean = isSelected, aLabelText:String = labelText, loc:Vec2 = loc): Unit =
   { if (aIsSelected == true)
