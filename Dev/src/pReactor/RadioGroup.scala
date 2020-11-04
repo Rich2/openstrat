eecd79c0d8844a934ca8f8a99b58ecfd733fbeae
w0d <35996314+w0d@users.noreply.github.com>
2020-11-04 16:06:40 +0000

    Update RadioGroup.scala

--- Dev/src/pReactor/RadioGroup.scala
+++ Dev/src/pReactor/RadioGroup.scala
@@ -1,10 +1,10 @@
-package ostrat
-package pReactor
-import geom._, pCanv._, Colour._
+// package ostrat
+// package pReactor
+// import geom._, pCanv._, Colour._
 
-/** Controls a collection of RadioOptions */
-case class RadioGroup(selectedIndex:Int = 0, radioOptions:Array[RadioOption], loc:Vec2 = 0 vv 0)
-{ def put(aSelected:Int = selectedIndex, aRadioOptions:Array[RadioOption] = radioOptions, loc:Vec2 = loc): Unit =
-  { 
-  }
-}
+// /** Controls a collection of RadioOptions */
+// case class RadioGroup(selectedIndex:Int = 0, radioOptions:Array[RadioOption], loc:Vec2 = 0 vv 0)
+// { def put(aSelected:Int = selectedIndex, aRadioOptions:Array[RadioOption] = radioOptions, loc:Vec2 = loc): Unit =
+//   { 
+//   }
+// }
