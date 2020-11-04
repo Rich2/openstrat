ee6e90d64327ba28316f28f745b6c9f7ddcbf7b5
w0d <35996314+w0d@users.noreply.github.com>
2020-11-04 16:06:53 +0000

    Update RadioOption.scala

--- Dev/src/pReactor/RadioOption.scala
+++ Dev/src/pReactor/RadioOption.scala
@@ -1,15 +1,15 @@
-package ostrat
-package pReactor
-import geom._, pCanv._, Colour._
+// package ostrat
+// package pReactor
+// import geom._, pCanv._, Colour._
 
-/** Simple Radion button with label */
-case class RadioOption(isSelected:Boolean = false, labelText:String = "", loc:Vec2 = 0 vv 0)
-{ def put(canv: CanvasPlatform, aIsSelected:Boolean = isSelected, aLabelText:String = labelText, loc:Vec2 = loc): Unit =
-  { if (aIsSelected == true)
-    { canv.circleFill(Circle(12, loc), Pink)
-    } else 
-    { canv.circleDraw(Circle(12, loc), 1, Pink)
-    }
-    canv.textGraphic(aLabelText, 12, loc, Black, LeftAlign)
-  }
-}
+// /** Simple Radion button with label */
+// case class RadioOption(isSelected:Boolean = false, labelText:String = "", loc:Vec2 = 0 vv 0)
+// { def put(canv: CanvasPlatform, aIsSelected:Boolean = isSelected, aLabelText:String = labelText, loc:Vec2 = loc): Unit =
+//   { if (aIsSelected == true)
+//     { canv.circleFill(Circle(12, loc), Pink)
+//     } else 
+//     { canv.circleDraw(Circle(12, loc), 1, Pink)
+//     }
+//     canv.textGraphic(aLabelText, 12, loc, Black, LeftAlign)
+//   }
+// }
