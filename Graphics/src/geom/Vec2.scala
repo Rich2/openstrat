feb9934d09f2936a409c2048815c1354d1c1b882
Rich2 <rr1@tutanota.com>
2020-11-07 20:28:15 +0000

    Various.

--- Graphics/src/geom/Vec2.scala
+++ Graphics/src/geom/Vec2.scala
@@ -1,6 +1,7 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
+import scala.math.{Pi, atan}
 
 /** A 2 dimensional vector, can be used with 2 dimensional points and translations of 2 dimensional points. Thanks to Rene Descarte for this great
  *  idea. */
@@ -57,6 +58,39 @@ class Vec2(val x: Double, val y: Double) extends Vec2Like with ProdDbl2
   }
 
   def vv(z: Double): Vec3 = Vec3(x, y, z)
+
+  /** Gives the angle of the vector with respect of the origin. */
+  def angle: Angle = Angle.radians(angleRadians)
+
+  /** Gives the angle of the vector with respect of the origin in radians. */
+  def angleRadians: Double =
+  { def at = atan(y / x)
+
+    x match
+    { case _ if x == 0 & y == 0 => 0
+    case _ if x == 0 & y > 0 => Pi / 2
+    case _ if x == 0 => -Pi / 2
+    case _ if x > 0 => at
+    case _ if y > 0 => Pi + at
+    case _ => at - Pi
+    }
+  }
+
+  /** Gives the angle of the vector with respect of the origin in radians. */
+  def angleRadiansPos: Double =
+  { val at = atan(y / x)
+
+    x match
+    { case x if x < - 0.000000010 && y < 0 => at + Pi
+    case x if x < - 0.00000001 => Pi + at
+    case x if x > 0.00000001 => at
+    case _ if y < 0 => at + Pi/2
+    case _ => at + Pi/2
+    }
+  }
+
+  /** The magnitude of this vector2Like. */
+  def magnitude: Double = math.sqrt(x * x + y * y)
 }
 
 object Vec2
--- Graphics/src/geom/Vec2Like.scala
+++ Graphics/src/geom/Vec2Like.scala
@@ -1,7 +1,6 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-import math._
 
 trait Vec2Like extends Any
 {
@@ -13,6 +12,7 @@ trait Vec2Like extends Any
   
   @inline def *(factor: Double): Vec2Like
   @inline def /(divisor: Double): Vec2Like
+
   /** rotates the vector 90 degrees or Pi/2 radians, anticlockwise. */
   @inline def rotate90: Vec2Like
 
@@ -27,39 +27,6 @@ trait Vec2Like extends Any
 
   /** The dot product of this and the operand vector. */
   @inline def dot(operand: Vec2Like): Double = x * operand.x + y * operand.y
-
-  /** The magnitude of this vector2Like. */
-  def magnitude: Double = math.sqrt(x * x + y * y)
-
-  /** Gives the angle of the vector with respect of the origin. */
-  def angle: Angle = Angle.radians(angleRadians)
-
-  /** Gives the angle of the vector with respect of the origin in radians. */
-  def angleRadians: Double =
-  { def at = atan(y / x)
-
-    x match
-    { case _ if x == 0 & y == 0 => 0
-      case _ if x == 0 & y > 0 => Pi / 2
-      case _ if x == 0 => -Pi / 2
-      case _ if x > 0 => at
-      case _ if y > 0 => Pi + at
-      case _ => at - Pi
-    }
-  }
-
-  /** Gives the angle of the vector with respect of the origin in radians. */
-  def angleRadiansPos: Double =
-  { val at = atan(y / x)
-
-    x match
-    { case x if x < - 0.000000010 && y < 0 => at + Pi
-      case x if x < - 0.00000001 => Pi + at
-      case x if x > 0.00000001 => at
-      case _ if y < 0 => at + Pi/2
-      case _ => at + Pi/2
-    }
-  }
 
   def doublesSeq = Seq(x, y)
   def toPair: (Double, Double) = (x, y)
