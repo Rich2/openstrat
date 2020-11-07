5fb400b1aea01ba6d24a60089fb29fa253cce876
Rich2 <rr1@tutanota.com>
2020-11-07 14:22:09 +0000

    Minor.

--- Tiling/src/pGrid/HexGridOld.scala
+++ Tiling/src/pGrid/HexGridOld.scala
@@ -84,17 +84,12 @@ object HexGridOld
   val adjTileCoodsOfTile00: Coods = sideCoodsOfTile00.pMap(_ * 2)
   def adjTileCoodsOfTile(tileCood: Cood): Coods = adjTileCoodsOfTile00.pMap(tileCood + _)
 
-  def sideCoodToLineRel(sideCood: Cood, scale: Double, relPosn: Pt2 = Vec2Z): LineSeg =
-    sideCoodToCoodLine(sideCood).toLine2(c => (coodToVec2(c) -*-relPosn) * scale)
-
   def sideCoodToLine(sideCood: Cood): LineSeg = sideCoodToCoodLine(sideCood).toLine2(coodToVec2)
   def sideCoodToCoodLine(sideCood: Cood): CoodLine = sideCoodToCoodLine(sideCood.xi, sideCood.yi)
   //override def sideCoodToCoodLine(sideCood: Cood): ostrat.pGrid.CoodLine = HexGrid.sideCoodToCoodLine(sideCood)
 
   def sideCoodToCoodLine(x: Int, y: Int): CoodLine = fOrientation(x, y, CoodLine(x - 1, y, x + 1, y), CoodLine(x, y + 1, x, y - 1),
     CoodLine(x + 1, y, x - 1, y))
-
-  def coodToVec2Rel(cood: Cood, relPosn: Pt2): Pt2 = coodToVec2(cood.xi, cood.yi) -*-relPosn
 
   /** Used for regular HexGrids and the regular aspect of irregular Hexgrids */
   def coodToVec2(cood: Cood): Pt2 = coodToVec2(cood.xi, cood.yi)
