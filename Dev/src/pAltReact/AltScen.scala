80364cc26ffa27e026f3dcebcc49bc5ec89a77b1
Rich2 <rr1@tutanota.com>
2020-11-09 20:01:57 +0000

    Minor.

---
+++ Dev/src/pAltReact/AltScen.scala
@@ -0,0 +1,16 @@
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+package pAltReact
+import prid._
+
+case class Player(str: String, colour: Colour)
+sealed trait Balls
+object NoBalls extends Balls
+case class SomeBalls(player: Player, num: Int)
+
+trait AltScen
+{ val turn: Int
+  def grid: SqGrid
+  //def terrs: TileBooleans
+  //def oPlayers: TilesArrOpt[Player]*/
+}
