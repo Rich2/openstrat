f603167d84230b1aa10b51da0b3694c48fad674c
w0d <35996314+w0d@users.noreply.github.com>
2020-11-10 07:10:46 +0000

    RadioOption.scala formatting

--- Dev/src/pReactor/RadioOption.scala
+++ Dev/src/pReactor/RadioOption.scala
@@ -3,13 +3,13 @@ package pReactor
 import geom.{GraphicElems, _}, Colour._
 
 /** Simple Radio button with label **/
-case class RadioOption(aIsSelected:Boolean = false, labelText:String = "", loc:Pt2 = 0 pp 0, aIsDisabled:Boolean = false, activeId: Any = "RadioOption") {
-  val defaultSize = 12
+case class RadioOption(aIsSelected:Boolean = false, labelText:String = "", loc:Pt2 = 0 pp 0, aIsDisabled:Boolean = false, activeId: Any = "RadioOption")
+{ val defaultSize = 12
   var isSelected = aIsSelected
   var isDisabled = aIsDisabled
 
-  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc:Pt2 = loc, aIsDisabled: Boolean = isDisabled, activeId: Any = activeId): GraphicElems = {
-    isSelected = aIsSelected
+  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc:Pt2 = loc, aIsDisabled: Boolean = isDisabled, activeId: Any = activeId): GraphicElems =
+  { isSelected = aIsSelected
     isDisabled = aIsDisabled
     val ink = if (!isDisabled) White else Grey
     var ret:GraphicElems = Arr(TextGraphic(labelText, loc + (defaultSize pp 0), defaultSize, ink, LeftAlign))
