3958fe42ae9e9bf5da2f674b627ef38d008bb576
w0d <35996314+w0d@users.noreply.github.com>
2020-11-10 03:48:55 +0000

    Update Checkbox.scala

--- Dev/src/pReactor/Checkbox.scala
+++ Dev/src/pReactor/Checkbox.scala
@@ -3,16 +3,16 @@ package pReactor
 import geom.{GraphicElems, _}, Colour._
 
 /** Simple Checkbox with label **/
-case class Checkbox(aIsSelected:Boolean = false, labelText:String = "", loc:Vec2 = 0 vv 0, aIsDisabled:Boolean = false, activeId:Any = "Checkbox") {
+case class Checkbox(aIsSelected:Boolean = false, labelText:String = "", loc:Pt2 = 0 pp 0, aIsDisabled:Boolean = false, activeId:Any = "Checkbox") {
   val defaultSize = 12
   var isSelected = aIsSelected
   var isDisabled = aIsDisabled
 
-  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc: Vec2 = loc, aIsDisabled: Boolean = isDisabled, activeId: Any = activeId): GraphicElems =
+  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc: Pt2 = loc, aIsDisabled: Boolean = isDisabled, activeId: Any = activeId): GraphicElems =
   { isSelected = aIsSelected
     isDisabled = aIsDisabled
     val ink = if (!isDisabled) White else Grey
-    var ret:GraphicElems = Arr(TextGraphic(labelText, loc + (defaultSize vv 0), defaultSize, ink, LeftAlign))
+    var ret:GraphicElems = Arr(TextGraphic(labelText, loc + (defaultSize pp 0), defaultSize, ink, LeftAlign))
     if (isSelected) ret = ret ++ Arr(Rect(defaultSize - 4, defaultSize - 4, loc).fill(ink))
     if (!isDisabled) ret ++ Arr(Rect(defaultSize, defaultSize, loc).drawActive(ink, 1, this))
     else ret ++ Arr(Rect(defaultSize, defaultSize, loc).draw(ink, 1))
