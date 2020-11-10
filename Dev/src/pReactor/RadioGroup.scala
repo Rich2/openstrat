ba5236cbc23f015b86d636fcf3bf728b17d305ac
w0d <35996314+w0d@users.noreply.github.com>
2020-11-10 07:12:32 +0000

    Reactor += RadioGroup working

--- Dev/src/pReactor/RadioGroup.scala
+++ Dev/src/pReactor/RadioGroup.scala
@@ -1,10 +1,17 @@
-// package ostrat
-// package pReactor
-// import geom._, pCanv._, Colour._
+package ostrat
+package pReactor
+import geom._, pCanv._, Colour._
 
-// /** Controls a collection of RadioOptions */
-// case class RadioGroup(selectedIndex:Int = 0, radioOptions:Array[RadioOption], loc:Vec2 = 0 vv 0)
-// { def put(aSelected:Int = selectedIndex, aRadioOptions:Array[RadioOption] = radioOptions, loc:Vec2 = loc): Unit =
-//   { 
-//   }
-// }
+/** Controls a collection of RadioOptions */
+case class RadioGroup(radioOptions:Arr[RadioOption], aSelected:RadioOption)
+{ var selected = aSelected
+
+ def clicked(targetReference:RadioOption): Unit =
+ { if (!targetReference.isSelected)
+   { if (selected != targetReference)
+     selected.isSelected = false
+     targetReference.isSelected = true
+     selected = targetReference
+   }
+ }
+}
