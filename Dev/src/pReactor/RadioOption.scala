444390db02a2b43ebbdc453626e60dd9c6a2724e
w0d <35996314+w0d@users.noreply.github.com>
2020-11-10 03:24:36 +0000

    Update RadioOption.scala

--- Dev/src/pReactor/RadioOption.scala
+++ Dev/src/pReactor/RadioOption.scala
@@ -1,15 +1,20 @@
-// package ostrat
-// package pReactor
-// import geom._, pCanv._, Colour._
+package ostrat
+package pReactor
+import geom.{GraphicElems, _}, Colour._
 
-// /** Simple Radion button with label */
-// case class RadioOption(isSelected:Boolean = false, labelText:String = "", loc:Vec2 = 0 vv 0)
-// { def put(canv: CanvasPlatform, aIsSelected:Boolean = isSelected, aLabelText:String = labelText, loc:Vec2 = loc): Unit =
-//   { if (aIsSelected == true)
-//     { canv.circleFill(Circle(12, loc), Pink)
-//     } else 
-//     { canv.circleDraw(Circle(12, loc), 1, Pink)
-//     }
-//     canv.textGraphic(aLabelText, 12, loc, Black, LeftAlign)
-//   }
-// }
+/** Simple Radio button with label **/
+case class RadioOption(aIsSelected:Boolean = false, labelText:String = "", loc:Vec2 = 0 vv 0, aIsDisabled:Boolean = false, activeId: Any = "RadioOption") {
+  val defaultSize = 12
+  var isSelected = aIsSelected
+  var isDisabled = aIsDisabled
+
+  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc: Vec2 = loc, aIsDisabled: Boolean = isDisabled, activeId: Any = activeId): GraphicElems = {
+    isSelected = aIsSelected
+    isDisabled = aIsDisabled
+    val ink = if (!isDisabled) White else Grey
+    var ret:GraphicElems = Arr(TextGraphic(labelText, loc + (defaultSize vv 0), defaultSize, ink, LeftAlign))
+    if (isSelected) ret = ret ++ Arr(Circle(defaultSize - 4, loc).fill(ink))
+    if (!isDisabled) ret ++ Arr(Rect(defaultSize, defaultSize, loc).drawActive(ink, 0.01, this), Circle(defaultSize, loc).draw(ink, 1))
+    else ret ++ Arr(Circle(defaultSize, loc).draw(ink, 1))
+  }
+}
