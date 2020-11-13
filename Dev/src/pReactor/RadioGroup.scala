0566b74812e828604f5b4f45414ebaf179296a93
w0d <35996314+w0d@users.noreply.github.com>
2020-11-13 01:45:28 +0000

    RadioGroup += setEnabled(newState) + put()

--- Dev/src/pReactor/RadioGroup.scala
+++ Dev/src/pReactor/RadioGroup.scala
@@ -6,12 +6,20 @@ import geom._, pCanv._, Colour._
 case class RadioGroup(radioOptions:Arr[RadioOption], aSelected:RadioOption)
 { var selected = aSelected
 
- def clicked(targetReference:RadioOption): Unit =
- { if (!targetReference.isSelected)
-   { if (selected != targetReference)
-     selected.isSelected = false
-     targetReference.isSelected = true
-     selected = targetReference
-   }
- }
+  def clicked(targetReference:RadioOption): Unit =
+  { if (!targetReference.isSelected)
+    { if (selected != targetReference)
+      { selected.isSelected = false
+        targetReference.isSelected = true
+        selected = targetReference
+      } else deb("h")
+    } else deb("i")
+  }
+  def setEnabled(newState:Boolean): Unit = radioOptions.foreach(radio => radio.isEnabled = newState)
+
+  def put(): GraphicElems =
+  { var ret:Arr[GraphicElem] = Arr()
+    radioOptions.foreach(radio => { ret = radio.put() ++ ret })
+    ret
+  }
 }
