6e93f84f6fb25d8218e65c38018d83fa9930bce2
w0d <35996314+w0d@users.noreply.github.com>
2020-11-16 16:40:13 +0000

    Update ComputerPlayer.scala

--- Dev/src/pReactor/ComputerPlayer.scala
+++ Dev/src/pReactor/ComputerPlayer.scala
@@ -1,27 +1,59 @@
+/* Copyright 2018-20 w0d. Licensed under Apache Licence version 2.0. */
 package ostrat
 package pReactor
-import geom.{GraphicElems, _}, Colour._
+import Colour._
 
-/** Simple Checkbox with label **/
-case class Checkbox(aIsSelected:Boolean = false, labelText:String = "", loc:Pt2 = 0 pp 0, aIsEnabled:Boolean = false, action: (Checkbox) => Unit = (Checkbox) => {}, activeId:Any = "Checkbox") {
-  val defaultSize = 12
-  var isSelected = aIsSelected
-  var isEnabled = aIsEnabled
+/** Computer Player for Reactor */
+case class ComputerPlayer(aGameData: ReactorGame) 
+{ var forThisColor = Black
+  val gameData = aGameData
 
-  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc: Pt2 = loc, aIsEnabled:Boolean = isEnabled, aAction: (Checkbox) => Unit = action, activeId: Any = activeId): GraphicElems =
-  { isSelected = aIsSelected
-    isEnabled = aIsEnabled
-    val ink = if (isEnabled) White else Grey
-    var ret:GraphicElems = Arr(TextGraphic(labelText, loc + (defaultSize pp 0), defaultSize, ink, LeftAlign))
-    if (isSelected) ret = ret ++ Arr(Rect(defaultSize - 4, defaultSize - 4, loc).fill(ink))
-    if (isEnabled) ret ++ Arr(Rect(defaultSize, defaultSize, loc).drawActive(ink, 1, this))
-    else ret ++ Arr(Rect(defaultSize, defaultSize, loc).draw(ink, 1))
-  }
+  def chooseTurnIndex(aForThisColor:Colour):Int = 
+  { forThisColor = aForThisColor
+    var validTurnIndexes = Array[Int]()
+    var primedIndexes = Array[Int]()
+    var myPrimedIndexes = Array[Int]()
+    var theirPrimedIndexes = Array[Int]()
+    var chooseFromTheseIndexes = Array[Int]()
 
-  def clicked() =
-  { if (isEnabled)
-    { isSelected = !isSelected
-      action(this)
+    //determin valid turns (all cells that are Black or myColor)
+    for (i <- 0 to gameData.cellColors.length - 1)
+    { val thisCellsColor = gameData.cellColors(i) 
+      if (thisCellsColor == Black || thisCellsColor == forThisColor) validTurnIndexes = i +: validTurnIndexes
     }
+
+    //determin cells that are primed (1 ball away from popping)
+    for (i <- 0 to gameData.cellColors.length - 1)
+    { if (isPrimed(i) == true)
+      { primedIndexes = i +: primedIndexes
+        if (isMyColor(i) == true) myPrimedIndexes = i +: myPrimedIndexes
+        else theirPrimedIndexes = i +: theirPrimedIndexes
+      }
+    }
+    //find my primed cells that are at risk of attack (adjacent cell is a different color and is primed itself)
+    for (i <- myPrimedIndexes)
+    { for (j <- 0 to gameData.cellNeighbours(i).length - 1)
+      { if (theirPrimedIndexes.indexOf(gameData.cellNeighbours(i)(j)) != -1) chooseFromTheseIndexes = chooseFromTheseIndexes :+ i
+      }
+
+    }
+    //
+
+    //search neighbours for primed (mine and others)
+    //search for islands (no neighbours)
+    //favour corners that arnt protected else sides
+    //am i vulnrable/can i be agressive
+
+    if (chooseFromTheseIndexes.length != 0) deb("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    if (chooseFromTheseIndexes.length == 0) chooseFromTheseIndexes = validTurnIndexes
+    chooseFromTheseIndexes( scala.util.Random.nextInt( chooseFromTheseIndexes.length))
+  }
+  def isMyColor(cellIndex:Int):Boolean =
+  { if (gameData.cellColors(cellIndex) == forThisColor) true
+    else false
+  }
+  def isPrimed(cellIndex:Int):Boolean =
+  { if (gameData.cellCounts(cellIndex) == gameData.cellNeighbours(cellIndex).length - 1) true
+    else false
   }
 }
