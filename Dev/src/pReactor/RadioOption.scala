8e768d249e0477d5bed3b7838f11f00ff0227501
w0d <35996314+w0d@users.noreply.github.com>
2020-11-16 11:34:29 +0000

    RadioOptions now working :)

--- Dev/src/pReactor/RadioOption.scala
+++ Dev/src/pReactor/RadioOption.scala
@@ -3,20 +3,29 @@ package pReactor
 import geom.{GraphicElems, _}, Colour._
 
 /** Simple Radio button with label **/
-case class RadioOption(aIsSelected:Boolean = false, labelText:String = "", loc:Pt2 = 0 pp 0, aIsEnabled:Boolean = false, activeId: Any = "RadioOption")
+case class RadioOption(aIsSelected:Boolean = false, labelText:String = "", loc:Pt2 = 0 pp 0, aIsEnabled:Boolean = true)
 { val defaultSize = 12
+  var parent:RadioGroup = null
   var isSelected = aIsSelected
   var isEnabled = aIsEnabled
 
-  def put(aIsSelected: Boolean = isSelected, labelText: String = labelText, loc:Pt2 = loc, aIsEnabled: Boolean = isEnabled, activeId: Any = activeId): GraphicElems =
+  def put(aParent:RadioGroup, aIsSelected: Boolean = isSelected, labelText: String = labelText, loc:Pt2 = loc, aIsEnabled: Boolean = isEnabled): GraphicElems =
   { isSelected = aIsSelected
     isEnabled = aIsEnabled
-    val ink = if (!isEnabled) White else Grey
+    parent = aParent
+
+    val ink = if (isEnabled) White else Grey
     var ret:GraphicElems = Arr(TextGraphic(labelText, loc + (defaultSize pp 0), defaultSize, ink, LeftAlign))
     if (isSelected) ret = ret ++ Arr(Circle(defaultSize - 4, loc).fill(ink))
 
     //drawActive with lineWidth 0.01 FUDGE :( todo: flesh out   circle.active(activeId) and circle.drawActive(activeId)
-    if (!isEnabled) ret ++ Arr(Rect(defaultSize, defaultSize, loc).drawActive(ink, 0.01, this), Circle(defaultSize, loc).draw(ink, 1))
+    if (isEnabled) ret ++ Arr(Rect(defaultSize, defaultSize, loc).drawActive(ink, 0.01, this), Circle(defaultSize, loc).draw(ink, 1))
     else ret ++ Arr(Circle(defaultSize, loc).draw(ink, 1))
   }
+
+  def clicked(): Unit =
+  { debvar(parent)
+    parent.clicked(this)
+  }
 }
+
