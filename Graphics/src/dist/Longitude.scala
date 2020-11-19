0f7bffe069946f14f2833081c86dfb36feeb9321
Rich2 <rr1@tutanota.com>
2020-11-19 13:12:37 +0000

    Minor.

--- Graphics/src/dist/Longitude.scala
+++ Graphics/src/dist/Longitude.scala
@@ -1,19 +1,19 @@
 /* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
-import math.Pi
+//import math.Pi
 
 /** The Longitude class is a compile time wrapper around a Double. The longitude value is stored in arc seconds,to allow precise storage of values
  * specified in the old Degrees, Minutes and Seconds system. Decimals of a degree can also be stored precisely. */
 final class Longitude private(val milliSecs: Double) extends AnyVal with AngleLike
 {
-  def addWithin(deltaAngle: Angle, maxLong: Longitude, minLong: Longitude): Longitude = (radians + deltaAngle.radians) match
+  /*def addWithin(deltaAngle: Angle, maxLong: Longitude, minLong: Longitude): Longitude = (radians + deltaAngle.radians) match
   { case r if r <= - Pi => Longitude.radians(-Pi)
     case r if r >= Pi => Longitude.radians(Pi)
 
     case _ if minLong.radians > maxLong.radians => excep("Latitude.addwithin minLat greater than maxLat")
     case _ if maxLong.radians < minLong.radians => excep("Latitude.addwithin maxLat less than minLat")
-  }
+  }*/
 }
 
 /** Companion object of the [[Longitude]] class. */
