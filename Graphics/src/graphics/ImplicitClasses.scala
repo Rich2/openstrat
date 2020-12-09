9a8e608b01075e4fcc3207db496d0486b6030b6a
Rich2 <rr1@tutanota.com>
2020-12-09 12:01:50 +0000

    Minor changes to a file name and ScalaDoc comments.

--- Graphics/src/graphics/ImplicitClasses.scala
+++ Graphics/src/graphics/ImplicitClasses.scala
@@ -1,12 +1,18 @@
-/* Copyright 2018 Richard Oliver. Licensed under Apache Licence version 2.0 */
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package geom
 
+/** Extension methods class for Int, for the geom package. */
 class IntGeomImplicit(thisInt: Int)
-{ /** Succinct syntax for creating 2 dimensional vectors, Vec2s from 2 numbers. Note the low precedence of this method relative to most numerical
-      operators. */
+{
+  /** Succinct syntax for creating 2 dimensional points [[Pt2]]s, from 2 numbers. Note the low precedence of this method relative to most numerical
+   * operators. A third number as example {{{3.1 pp 4 pp -7.25}}} can be used to create a [[Pt3]]. */
   @inline def pp(y: Double): Pt2 = Pt2(thisInt, y)
+
+  /** Succinct syntax for creating 2 dimensional vectors, [[Vec2]]s from 2 numbers. Note the low precedence of this method relative to most numerical
+      operators. A third number as example {{{3.1 vv 4 vv -7.25}}} can be used to create a [[Vec3]]. */
   @inline def vv(y: Double): Vec2 = Vec2(thisInt, y)
+
   def ° : Angle = Angle(thisInt)
   def km: Dist = Dist(thisInt * 1000)
   def metre: Dist = Dist(thisInt)
@@ -27,11 +33,17 @@ class IntGeomImplicit(thisInt: Int)
   def south: Latitude = Latitude.apply(-thisInt)
 }
 
+/** Extension methods class for [[Double]], for the geom package. */
 class DoubleImplicitGeom(thisDouble: Double)
-{ /** Succinct syntax for creating 2 dimensional vectors, Vec2s from 2 numbers. Note the low precedence of this method relative to most numerical
-      operators. */
+{
+  /** Succinct syntax for creating 2 dimensional points [[Pt2]]s, from 2 numbers. Note the low precedence of this method relative to most numerical
+   *  operators. A third number as example {{{3.1 pp 4 pp -7.25}}} can be used to create a [Pt3]. */
   @inline def pp(y: Double): Pt2 = Pt2(thisDouble, y)
+
+  /** Succinct syntax for creating 2 dimensional vectors, [[Vec2]]s from 2 numbers. Note the low precedence of this method relative to most numerical
+   *  operators. A third number as example {{{3.1 pp 4 pp -7.25}}} can be used to create a [Pt3]. */
   @inline def vv(y: Double): Vec2 = Vec2(thisDouble, y)
+
   def km: Dist = Dist(thisDouble * 1000)
   def metre: Dist = Dist(thisDouble)
   def * (operator: Dist): Dist = Dist(thisDouble * operator.metres)
@@ -45,7 +57,9 @@ class DoubleImplicitGeom(thisDouble: Double)
   /** Converts this Double into an [[AngleVec]] an angle of rotation from - infinity to + infinity. */
   def degs: AngleVec = AngleVec(thisDouble)
 
+  /** Creates a [[LatLong]], a position of Latitude and Longitude from 2 numbers. */
   def ll (longDegs: Double): LatLong = LatLong.degs(thisDouble, longDegs)
+
   def east: Longitude = Longitude.degs(thisDouble)
   def west: Longitude = Longitude.degs(-thisDouble)
   def north: Latitude = Latitude.apply(thisDouble)
