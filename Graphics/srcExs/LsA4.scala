c7be9e51644cefbc8b44258067eea8a8daf44243
Rich2 <rr1@tutanota.com>
2020-12-17 15:59:00 +0000

    Change to LSA4.

--- Graphics/srcExs/LsA4.scala
+++ Graphics/srcExs/LsA4.scala
@@ -13,6 +13,7 @@ import ostrat._, geom._, pCanv._, Colour._
 /** Lesson A4. */
 case class LsA4(canv: CanvasPlatform) extends CanvasNoPanels("Lesson A4")
 {
+  val c1St = TextGraphic("C1 and C2 Start", 10, 100 pp 200, Blue)
   val c1 = CArc.neg(100, 200, 100, 100, 200, 100).draw(Blue)
   val c2 = CArc.pos(100, 200, 100, 100, 200, 100).draw(DarkRed)
   val c3: CArc = CArc.neg(-100, 0, 0, 0, 0, 100)
@@ -30,7 +31,7 @@ case class LsA4(canv: CanvasPlatform) extends CanvasNoPanels("Lesson A4")
     LinePath(0 pp -50, 50 pp -100, -25 pp -75, 200 pp -60).draw(2, Orange),
 
     Bezier(200 pp -350, -500 pp -300, -600 pp -300, -450 pp -200).draw(Green, 2),
-    c1, c2,
+    c1, c2, c1St,
     c3d, c4d, e1.draw(Red), e2.draw(Green)
   )
   repaint(stuff)
