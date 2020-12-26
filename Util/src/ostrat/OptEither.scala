976a901c48d8738e93536a17d1d9706f06e992e4
Rich2 <rr1@tutanota.com>
2020-12-26 17:26:36 +0000

    Created OptEither trait.

---
+++ Util/src/ostrat/OptEither.scala
@@ -0,0 +1,14 @@
+/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+
+/** An Optional Either. The value can be an A a B or none. */
+sealed trait OptEither[+A, +B]
+
+/** An OptEither instantiation that has a value of type A, the first type parameter. */
+case class SomeA[+A, +B](value: A) extends OptEither[A, B]
+
+/** An OptEither instantiation that has a value of type B, the second type parameter. */
+case class SomeB[+A, +B](value: B) extends OptEither[A, B]
+
+/** An OptEither with a none value it has neither a value of type A or typeB,  */
+case object NoOptEither
