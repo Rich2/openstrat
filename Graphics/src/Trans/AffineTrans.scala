ac242f1294b2be0faa11d6b67597b4dbc5cde6e0
Rich2 <rr1@tutanota.com>
2020-12-26 20:22:37 +0000

    Removed method from TransSim.

--- Graphics/src/Trans/AffineTrans.scala
+++ Graphics/src/Trans/AffineTrans.scala
@@ -3,21 +3,17 @@ package ostrat
 package geom
 import reflect.ClassTag
 
-/** The typeclass trait for transforming an object in 2d geometry. Note overrides necessary to preserve type. */
+/** The type class trait for transforming an object in 2d geometry. Note overrides necessary to preserve type. */
 trait AffineTrans[T] extends TransSim[T]
 { def trans(obj: T, f: Pt2 => Pt2):  T
   override def slate(obj: T, offset: Vec2Like): T = trans(obj, _.slate(offset))
   override def scale(obj: T, operand: Double): T = trans(obj, _.scale(operand))
   def shear(obj: T, xScale: Double, yScale: Double): T = trans(obj, v => Pt2(v.x * yScale, v.y * xScale))
   override def rotate(obj: T, angle: AngleVec): T = trans(obj, _.rotate(angle))
-
-  def mirrorYOffset(obj: T, xOffset: Double): T = trans(obj, _.reflectYParallel(xOffset))
-  def mirrorXOffset(obj: T, yOffset: Double): T = trans(obj, _.reflectXParallel(yOffset))
-  override def reflectSegT(obj: T, line: LineSeg): T = trans(obj, _.reflect(line))
-  override def reflectT(obj: T, line: Line): T = trans(obj, _.reflect(line))
+  override def reflectT(obj: T, line: LineLike): T = trans(obj, _.reflect(line))
 }
 
-/** The companion object for the Trans[T] typeclass, containing instances for common classes. */
+/** The companion object for the Trans[T] type class, containing instances for common classes. */
 object AffineTrans
 {
   implicit def arrImplicit[A, AA <: ArrBase[A]](implicit build: ArrBuild[A, AA], ev: AffineTrans[A]): AffineTrans[AA] =
--- Graphics/src/Trans/TransSim.scala
+++ Graphics/src/Trans/TransSim.scala
@@ -5,8 +5,7 @@ import reflect.ClassTag
 
 /** A Similar Transformations type class */
 trait TransSim[T] extends TransAlign[T]
-{ def reflectT(obj: T, line: Line): T
-  def reflectSegT(obj: T, lineSeg: LineSeg): T
+{ def reflectT(obj: T, line: LineLike): T
   def rotate(obj: T, angle: AngleVec): T
 }
 
@@ -16,39 +15,35 @@ object TransSim
   implicit def transSimerImplicit[T <: SimilarPreserve]: TransSim[T] = new TransSim[T]
   { override def rotate(obj: T, angle: AngleVec): T = obj.rotate(angle).asInstanceOf[T]
     override def slate(obj: T, offset: Vec2Like): T = obj.slate(offset).asInstanceOf[T]
-    override def reflectSegT(obj: T, line: LineSeg): T = obj.reflect(line).asInstanceOf[T]
-    override def reflectT(obj: T, line: Line): T = obj.reflect(line).asInstanceOf[T]
+    override def reflectT(obj: T, line: LineLike): T = obj.reflect(line).asInstanceOf[T]
     override def scale(obj: T, operand: Double): T = obj.scale(operand).asInstanceOf[T]
   }
 
   implicit def arrImplicit[A, AA <: ArrBase[A]](implicit build: ArrBuild[A, AA], ev: TransSim[A]): TransSim[AA] = new TransSim[AA]
   { override def slate(obj: AA, offset: Vec2Like): AA = obj.map(ev.slate(_, offset))
     override def rotate(obj: AA, angle: AngleVec): AA = obj.map(ev.rotate(_, angle))
-    override def reflectSegT(obj: AA, line: LineSeg): AA = obj.map(ev.reflectSegT(_, line))
-    override def reflectT(obj: AA, line: Line): AA = obj.map(ev.reflectT(_, line))
+    override def reflectT(obj: AA, line: LineLike): AA = obj.map(ev.reflectT(_, line))
     override def scale(obj: AA, operand: Double): AA = obj.map(ev.scale(_, operand))
   }
 
   implicit def functorImplicit[A, F[_]](implicit evF: Functor[F], evA: TransSim[A]): TransSim[F[A]] = new TransSim[F[A]]
   { override def slate(obj: F[A], offset: Vec2Like): F[A] = evF.mapT(obj, ts => evA.slate(ts, offset))
     override def rotate(obj: F[A], angle: AngleVec): F[A] = evF.mapT(obj, ts => evA.rotate(ts, angle))
-    override def reflectSegT(obj: F[A], line: LineSeg): F[A] = evF.mapT(obj, evA.reflectSegT(_, line))
-    override def reflectT(obj: F[A], line: Line): F[A] = evF.mapT(obj, evA.reflectT(_, line))
+    override def reflectT(obj: F[A], line: LineLike): F[A] = evF.mapT(obj, evA.reflectT(_, line))
     override def scale(obj: F[A], operand: Double): F[A] = evF.mapT[A, A](obj, ts => evA.scale(ts, operand))
   }
 
   implicit def arrayImplicit[A](implicit ct: ClassTag[A], ev: TransSim[A]): TransSim[Array[A]] = new TransSim[Array[A]]
   { override def slate(obj: Array[A], offset: Vec2Like): Array[A] = obj.map(ev.slate(_, offset))
     override def rotate(obj: Array[A], angle: AngleVec): Array[A] = obj.map(ev.rotate(_, angle))
-    override def reflectSegT(obj: Array[A], line: LineSeg): Array[A] = obj.map(ev.reflectSegT(_, line))
-    override def reflectT(obj: Array[A], line: Line): Array[A] = obj.map(ev.reflectT(_, line))
+    override def reflectT(obj: Array[A], line: LineLike): Array[A] = obj.map(ev.reflectT(_, line))
     override def scale(obj: Array[A], operand: Double): Array[A] = obj.map(ev.scale(_, operand))
   }
 }
 
 class TransSimExtension[T](value: T, ev: TransSim[T])
 { def reflect(line: Line) = ev.reflectT(value, line)
-  def reflect(lineSeg: LineSeg): T = ev.reflectSegT(value, lineSeg)
+  def reflect(lineSeg: LineSeg): T = ev.reflectT(value, lineSeg)
    
   /** this.asInstanceOf[T] */
   def identity: T = this.asInstanceOf[T]
