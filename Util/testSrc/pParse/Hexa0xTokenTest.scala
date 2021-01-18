666ba09b0cdfcd05d241b5ad4c3c7ac1c5f5bf20
Rich2 <rr1@tutanota.com>
2021-01-18 22:41:10 +0000

    #137 Hexa0xToken tests.

--- Util/testSrc/pParse/Hexa0xTokenTest.scala
+++ Util/testSrc/pParse/Hexa0xTokenTest.scala
@@ -4,17 +4,16 @@ import utest._
 
 object Hexa0xTokenTest extends TestSuite
 {
-  val c1 = "0x1234".toChars
-  val o1 = c1.offsetter0
+  val s1 = "0x123"
   val p1 = StrPosn(1, 1)
-  val r1 = srcToETokens.str("0x1234")
-  println(r1)
-  val Ht1 = Nat0xToken(p1, "1234")
-  val rr1 = 0x1234
+  val r1 = srcToETokens.str(s1)
+  val Ht1 = Nat0xToken(p1, "123")
+  val rr1 = 0x123
 
-  val c2 = "0xC32AB34".toChars
+  val s2 = "0xC32AB34"
+  val c2 = s2.toChars
   val o2 = c2.offsetter0
-  val r2 = parseNatRawToken(o2, p1)(c2)
+  val r2 = srcToETokens.str(s2)
   val Ht2 = Nat0xToken(p1, "C32AB34")
   val rr2 = 0xC32AB34
 
@@ -22,13 +21,11 @@ object Hexa0xTokenTest extends TestSuite
   {
     "Parse" -
     {
-      assertMatch(r1){ case GoodArr1(Nat0xToken(p1, "1234")) => }
-      //assertMatch(r2){ case Good3(CharsOff(9), StrPosn(1, 10), Ht2 ) => }
-    }
-    "Values" -
-    {
+      assertMatch(r1){ case GoodArr1(Ht1) => }
+      Ht1.asHexaInt ==> 0x123
       Ht1.getInt ==> rr1
       Ht1.asHexaInt ==> rr1
+      assertMatch(r2){ case GoodArr1(Ht2) => }
       Ht2.getInt ==> rr2
     }
   }
