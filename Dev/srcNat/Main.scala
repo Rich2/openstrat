1d0cbad3baf3e9ea3e94e9db02c1a915caa63d7e
Rich2 <rr1@tutanota.com>
2021-01-20 23:24:03 +0000

    #138 Implemented more Xlib.

--- Dev/srcNat/Main.scala
+++ Dev/srcNat/Main.scala
@@ -1,17 +1,21 @@
 /* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 package pX11
-import geom._, scalanative.unsafe._
+import geom._, scalanative._, unsafe._, unsigned._
 
-@link("X11")
-@extern object Xlib {
+@link("X11") @extern object Xlib {
   def XOpenDisplay(display_name: CString): Ptr[CStruct2[CInt, CInt]] = extern
   def XCloseDisplay(disp: Display): Unit = extern
+  def XDefaultScreen(disp: Display): CInt = extern
   def XRootWindow(disp: Display, screen_number: CInt): Window = extern
+  def XBlackPixel(disp: Display, scren_number: CInt): CUnsignedLong = extern
+  def XWhitePixel(disp: Display, scren_number: CInt): CUnsignedLong = extern
+  def XCreateSimpleWindow(disp: Display, parent: Window, x: CInt, y: CInt, width: CUnsignedInt, height: CUnsignedInt, border_width: CUnsignedLong,
+    border: CUnsignedLong, background: CUnsignedLong): Window = extern
+  def XSelectInput(disp: Display, event_mask: CLong): Unit = extern
 }
 
-@extern
-object myapi {
+@extern object myapi {
   def add3(in: CLong): CLong = extern
 }
 
@@ -28,6 +32,12 @@ object myapi {
      if(disp == null) println("Cannot open display")
      else {
        println("Display opened")
+       val defualtScn = XDefaultScreen(disp)
+       val rootWin = XRootWindow(disp, defualtScn)
+       val bp = XBlackPixel(disp, defualtScn)
+       val wp = XWhitePixel(disp, defualtScn)
+       val w = XCreateSimpleWindow(disp, rootWin, 10, 10, 100.toUInt, 100.toUInt, 1.toUInt, bp, wp)
+       //XSelectInput(disp, w, ExposureMask | KeyPressMask)
        XCloseDisplay(disp)
        println("Display closed")
      }
