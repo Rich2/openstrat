171e10e679a496839941ac6fdb2985e21d16e2f0
Rich2 <rr1@tutanota.com>
2021-01-20 14:21:05 +0000

    #138 Started soem Xlib code.

--- Dev/srcNat/Main.scala
+++ Dev/srcNat/Main.scala
@@ -1,6 +1,9 @@
 package ostrat
-import geom._
-import scalanative.unsafe._
+import geom._, scalanative.unsafe._
+
+@extern object Xlib {
+  //def XOpenDisplay(display_name: CString): Ptr[Byte] = extern
+}
 
 @extern
 object myapi {
@@ -11,9 +14,10 @@ object myapi {
  {
    def main(args: Array[String]): Unit =
    {
-   	val i: Long = myapi.add3(-3L)
+   	 val i: Long = myapi.add3(-3L)
    	 println(i)
    	 val p1 = Pt2(5.6, 7)
      deb("Hello, native world!, p1 = " + p1.toString)
+    // Xlib.XOpenDisplay(null)
    }
 }
