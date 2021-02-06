97850d6c250c67a6ec85dd6a4b010a6b14d63515
Rich2 <rr1@tutanota.com>
2021-02-06 11:45:14 +0000

    #143 Documentation.

--- Util/src/pParse/Clause.scala
+++ Util/src/pParse/Clause.scala
@@ -2,6 +2,9 @@
 package ostrat
 package pParse
 
+/** Intended as a common trait for Clauses and Unclaused Statements. */
+trait Clauselike
+
 /** Statements in RCON can be unclaused or multi comma separated. The empty Clause just contains a comma. The comma at the end of the last Clause of a
  *  Statement is optional. */
 case class Clause(expr: Expr, optComma: OptRef[CommaToken]) extends TextSpan
@@ -9,7 +12,7 @@ case class Clause(expr: Expr, optComma: OptRef[CommaToken]) extends TextSpan
   def endPosn = optComma.fld(expr.endPosn, _.endPosn)
 }
 
-/** Empty Clause class, rperesentated by just a comma. */
+/** Empty Clause class, represented by just a comma. */
 class EmptyClause(ct: CommaToken) extends Clause(ct, OptRef(ct)) with TextSpanCompound
 { override def startMem = ct
   override def endMem = ct
