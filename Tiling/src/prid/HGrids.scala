56388ad2a05d3f15298449ed0231b8f809a4a927
Rich2 <rr1@tutanota.com>
2021-10-09 02:12:55 +0100

    #127 Created HGrids trait.

---
+++ Tiling/src/prid/HGrids.scala
@@ -0,0 +1,8 @@
+/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package prid
+
+trait HGrids
+
+class HGridsFlat(val grids: Arr[HGrid]) extends HGrids {
+
+}
