a6aab421dad742ed6b06186dd12d9fed46f306dd
Rich2 <rr1@tutanota.com>
2021-10-14 13:20:15 +0100

    Removed DotMain.

--- Util/src/ostrat/DotMain.scala
+++
@@ -1,9 +0,0 @@
-/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
-package ostrat
-
-object DotMain extends App
-{ val myPair = (5, "A String")
-  debvar(myPair)
-  deb("Janet")
-  debb()
-}
