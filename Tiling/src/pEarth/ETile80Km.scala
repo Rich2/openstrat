cb06f3849c90dee5970b34693e0f1b9b8cef098a
Rich2 <rr1@tutanota.com>
2021-10-17 11:36:48 +0100

    #127 More ETile80Km.

--- Tiling/src/pEarth/ETile80Km.scala
+++ Tiling/src/pEarth/ETile80Km.scala
@@ -6,12 +6,23 @@ object ETile80Km
 {
   val scale = 20000.metres * math.sqrt(3)
 
-  /** The key method to get the longitude delta for x based from 0 degs longitude. */
+  /** The key method to get the longitude delta for c based from 0 degs longitude. */
   def hCenToLatLong0(inp: HCen): LatLong =
-  { val adj: Pt2 = inp.toPt2// HexGridOld.roordToVec2(inp.subYC(300, 0))
-    val d2: PtMetre2 = adj.toMetres(scale)
-    val lat: Double = d2.y / EarthPolarRadius
-    val longDelta: Double =   d2.x / (EarthEquatorialRadius * math.cos(lat))
+  { val adj: Pt2 = inp.toPt2
+    val p2m: PtMetre2 = adj.toMetres(scale)
+    val lat: Double = p2m.y / EarthPolarRadius
+    val longDelta: Double =   p2m.x / (EarthEquatorialRadius * math.cos(lat))
     LatLong.radians(lat, longDelta)
   }
+
+  def hCenToLatLong0(r: Int, c: Int): LatLong =
+  { val x = c / Sqrt3
+    val xm = c * 20000.metres
+    val ym = r * 20000.metres * math.sqrt(3)
+    val lat: Double = ym / EarthPolarRadius
+    val longDelta: Double =   xm / (EarthEquatorialRadius * math.cos(lat))
+    LatLong.radians(lat, longDelta)
+  }
+
+  //def colToLong(latRadians: Double)
 }
