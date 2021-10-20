4b11b20c87422fca8530585c9135abd39f77bc3b
Rich2 <rr1@tutanota.com>
2021-10-20 13:36:00 +0100

    Moved PanelLike to own file.

--- Graphics/src/pCanv/Panel.scala
+++ Graphics/src/pCanv/Panel.scala
@@ -25,28 +25,3 @@ case class Panel(private val outerCanv: CanvasPanelled, clipPoly: Polygon, cover
 case class MouseButtonCmd(cmd: MouseButton => Unit)
 { @inline def apply(mb: MouseButton): Any = cmd(mb)
 }
-
-/** The purpose of this trait is to give common methods for Panels and Simple Canvases. A SimpleCanvas is like a Panel but not a Panel.*/ 
-trait PanelLike extends RectCenlign
-{var backColour: Colour = Colour.White
-
-  /** These are currently stored in reverse. I think this would be better in an Array */
-  var actives: Arr[GraphicActive] = Arr()
-
-  var canvObjs: Arr[GraphicElem] = Arr()
-
-  /** This method name is inconsistent with mouseup on the canvas class*/
-  var mouseUp: (MouseButton, List[Any], Pt2) => Unit = (_, _, _) => {}
-
-  /** This method name is inconsistent with mousedown on the canvas class */
-  var mouseDown: (Pt2, MouseButton, AnyRefs) => Unit = (v, b, s) => {}
-
-  var fMouseMoved: (Pt2, MouseButton, AnyRefs) => Unit = (v, b, s) => {}
-  var fMouseDragged: (Pt2, MouseButton, AnyRefs) => Unit = (v, b, s) => {}
-
-  /** Just responds to when the Panel is clicked, ignores the position of the mose click or any potential active objects on the screen. */
-  def setMouseSimplest(action: => Unit): Unit = mouseUp = (_, _, _) => action
-
-  /** Just responds to the position of the mose click on the Panel, ignores any potential active objects on the screen. */
-  def setMouseSimple(action: Pt2 => Unit): Unit = mouseUp = (_, _, v) => action(v)
-}
---
+++ Graphics/src/pCanv/PanelLike.scala
@@ -0,0 +1,28 @@
+/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package pCanv
+import geom._
+
+/** The purpose of this trait is to give common methods for Panels and Simple Canvases. A SimpleCanvas is like a Panel but not a Panel.*/
+trait PanelLike extends RectCenlign
+{var backColour: Colour = Colour.White
+
+  /** These are currently stored in reverse. I think this would be better in an Array */
+  var actives: Arr[GraphicActive] = Arr()
+
+  var canvObjs: Arr[GraphicElem] = Arr()
+
+  /** This method name is inconsistent with mouseUp on the canvas class*/
+  var mouseUp: (MouseButton, List[Any], Pt2) => Unit = (_, _, _) => {}
+
+  /** This method name is inconsistent with mousedown on the canvas class */
+  var mouseDown: (Pt2, MouseButton, AnyRefs) => Unit = (v, b, s) => {}
+
+  var fMouseMoved: (Pt2, MouseButton, AnyRefs) => Unit = (v, b, s) => {}
+  var fMouseDragged: (Pt2, MouseButton, AnyRefs) => Unit = (v, b, s) => {}
+
+  /** Just responds to when the Panel is clicked, ignores the position of the mose click or any potential active objects on the screen. */
+  def setMouseSimplest(action: => Unit): Unit = mouseUp = (_, _, _) => action
+
+  /** Just responds to the position of the mose click on the Panel, ignores any potential active objects on the screen. */
+  def setMouseSimple(action: Pt2 => Unit): Unit = mouseUp = (_, _, v) => action(v)
+}
