8d8bf8380943f908de11059d09a68916899198f1
Rich2 <rr1@tutanota.com>
2021-11-18 16:03:22 +0000

    Changed EarthBasicGui to LatLong focus.

--- Tiling/Exs/src/pEarth/EarthBasicGui.scala
+++ Tiling/Exs/src/pEarth/EarthBasicGui.scala
@@ -10,8 +10,8 @@ case class EarthBasicGui(canv: CanvasPlatform, startScale: Option[Metres] = None
 
   /** Scale in km / pixel */
   var scale: Length = startScale.getOrElse(12.kMetres)
-  var focus: LatLong = startFocus.sget// fld(LatLong0, ll => ll)
-  var long: Longitude = startFocus.dFold(_.long)
+  var focus: LatLong = startFocus.sget
+  def long: Longitude = focus.long// startFocus.dFold(_.long)
 
   def scaleStr = s"scale = ${scale.kMetresNum.str2} km/pixel"
 
@@ -42,7 +42,7 @@ case class EarthBasicGui(canv: CanvasPlatform, startScale: Option[Metres] = None
 
   def goEast: PolygonCompound = clickButton("\u2192"){b =>
     def delta = b.apply(1, 10, 90, 0)
-    long += Longitude.degs(delta)
+    focus = focus.addLongRadians(delta / 180.0) // long += Longitude.degs(delta)
     repaint()
     statusText = s"Longitude $long"
     thisTop()
@@ -50,7 +50,7 @@ case class EarthBasicGui(canv: CanvasPlatform, startScale: Option[Metres] = None
 
   def goWest: PolygonCompound = clickButton("\u2190"){b =>
     def delta = b.apply(1, 10, 90, 0)
-    long -= Longitude.degs(delta)
+    focus = focus.addLongRadians(-delta / 180.0) // long -= Longitude.degs(delta)
     repaint()
     statusText = s"Longitude $long"
     thisTop()
