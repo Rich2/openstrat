7b55bbed05250e7847fd0f7c7dcf8278c366defb
Rich2 <rr1@tutanota.com>
2021-12-02 09:56:19 +0000

    Correction to CssBody.

--- Geom/srcWeb/CssRule.scala
+++ Geom/srcWeb/CssRule.scala
@@ -17,7 +17,7 @@ case class CssBGColour(colour: Colour) extends CssDec
 trait CssRule
 { def selec: String
   def props: Arr[CssDec]
-  def out: String = selec + props.foldHeadTail("")(_.out, _ + "\n" + _)
+  def out: String = selec + props.foldHeadTail("")(_.out, _ + "\n" + _).enCurly
 }
 
 case class CssBody(props: Arr[CssDec]) extends CssRule
