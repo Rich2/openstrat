f91e31cf0dc160e1028a534d1124ba7d2142a7c5
Rich2 <rr1@tutanota.com>
2021-12-24 01:15:12 +0000

    Added millWrite to GtiProj.

--- Util/srcJvm/GitProj.scala
+++ Util/srcJvm/GitProj.scala
@@ -1,9 +1,12 @@
 /* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 
-object GitProj
+case class GitProj(path: String, projName: String)
 {
-  val ignoreStr =
+  def fullPath = path / projName
+  def scalaVersionStr: String = "3.1.0"
+
+  def ignoreStr =
   """project/project/
     |target/
     |DocProj/
@@ -13,26 +16,42 @@ object GitProj
     |SbtDir/
     |.*/""".stripMargin
 
-  def sbtStr(projName: String): String =
+  def ignoreWrite: Unit = fileWrite(fullPath,".gitignore", ignoreStr)
+
+  def sbtStr: String =
     s"""
       |name := "$projName"
-      |scalaVersion := "3.1.0"
+      |scalaVersion := $scalaVersionStr
       |Compile/scalaSource := baseDirectory.value / "src"
-      |scalacOptions ++= Seq("-feature", "-language:implicitConversions", "-noindent", "-deprecation", "-encoding", "UTF-8")
-      |""".stripMargin
+      |scalacOptions ++= Seq("-feature", "-language:implicitConversions", "-noindent", "-deprecation", "-encoding", "UTF-8")""".stripMargin
 
-  def mainStr(projName: String) =
+  def sbtWrite: Unit = fileWrite(fullPath,projName + ".sbt", sbtStr)
+
+  def mainStr =
   s"""
       |object ${projName}App extends App{
       |println("Hello from $projName")
+      |}""".stripMargin
+
+  def mainWrite: Unit = fileWrite(fullPath / "src",projName + "App.scala", mainStr)
+
+  def millStr: String =
+    """
+      |// build.sc
+      |import mill._, scalalib._
+      |
+      |object Common extends ScalaModule
+      |{ def scalaVersion = $scalaVersionStr
+      |  def sources = T.sources(millSourcePath / "../src")
       |}
       |""".stripMargin
 
-  def apply(path: String, projName: String): Unit =
-  {
-    val fullPath = path / projName
-    fileWrite(fullPath,".gitignore", ignoreStr)
-    fileWrite(fullPath,projName + ".sbt", sbtStr(projName))
-    fileWrite(fullPath / "src",projName + "App.scala", mainStr(projName))
+  def millWrite: Unit = fileWrite(fullPath,"build.sc", millStr)
+
+  def apply: Unit =
+  { ignoreWrite
+    sbtWrite
+    millWrite
+    mainWrite
   }
 }
