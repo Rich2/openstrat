95174f8c6f5050029caf99120996bcad21aa0846
Rich2 <rr1@tutanota.com>
2022-01-20 20:52:39 +0000

    #143 deciFracLoop.

--- Util/src/pParse/parseNatRawToken.scala
+++ Util/src/pParse/parseNatRawToken.scala
@@ -20,10 +20,14 @@ object parseNatRawToken
       case _ => Good3(rem, tp.addStr(str), RawNat32OnlyToken(tp, str))
     }
 
-    def deciFracLoop(seq1: String, seq2: String): EMon3[CharsOff, TextPosn, Token] = ???
+    def deciFracLoop(rem: CharsOff, tp: TextPosn, seq1: String, seq2: String): EMon3[CharsOff, TextPosn, Token] = rem match
+    { case CharsOff1Tail(d, tail) if d.isDigit => deciFracLoop(tail, tp, seq1, seq2 + d.toString)
+      case _ => Good3(rem, tp.addStr(seq1).addStr(seq2).right1, DeciFracToken(tp, seq1, seq2, ""))
+    }
 
     rem match
     { case CharsOff1Tail(d, tail) if d.isDigit => apply(tail, tp, str + d.toString)
+      case CharsOff2Tail('.', d, tail) if d.isDigit => deciFracLoop(tail, tp, str, d.toString)
       case CharsOff1Tail(HexaUpperChar(l), tail) => hexaLoop(tail, tp, str + l.toString)
       case CharsOff1Tail(l, tail) if (l <= 'G' && l >= 'G') | (l <= 'W' && l >= 'P') => base32Loop(tail, tp, l.toString)
       case CharsOffHead(LetterOrUnderscoreChar(l)) => tp.bad3("Badly formed number token.")
