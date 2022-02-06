06f640b717da89351bad0c3a540a6353763f4d58
Rich2 <rr1@tutanota.com>
2022-02-06 14:16:26 +0000

    Comments and formatting.

--- Geom/Exs/src/LsD1.scala
+++ Geom/Exs/src/LsD1.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package learn
 import ostrat._, geom._, pgui._
 
@@ -6,15 +6,19 @@ import ostrat._, geom._, pgui._
 case class LsD1(canv: CanvasPlatform) extends CanvasNoPanels("Lesson D1")
 {  
   val t1 = 5.str  
-  val t2 = 2.2.str  
-  val t3 = true.str//OK you're probably not noticing much advantage over toString yet except its shorter
+  val t2 = 2.2.str
+
+  /** OK you're probably not noticing much advantage over toString yet except its shorter. */
+  val t3 = true.str
   
   val v1 = Pt2(2.3, -9.8)
   val t4 = v1.str
   
   val v2: Pt2 = 4.6 pp 78.4
   val l1 = LineSeg(v1, v2)
-  val t5 = l1.str//So note how there is a semicolon between the two points but a comma between the x and y values of each point. 
+
+  /** So note how there is a semicolon between the two points but a comma between the x and y values of each point. */
+  val t5 = l1.str
   
   val topStrs = Strings(t1, t2, t3, t4, t5)
   val topBlock = MText(200, topStrs)//So note we've created a couple of useful Function objects below for use in this lesson series.  
--- Geom/Exs/src/LsD2.scala
+++ Geom/Exs/src/LsD2.scala
@@ -11,14 +11,23 @@ case class LsD2(canv: CanvasPlatform) extends CanvasNoPanels("Lesson D2")
       Vec2(2.3; 3.2);
       "Goodbye" """
   val t1 = tStr.intAtStsIndex(2)
+
   /** Gives the same result as t1 as the Int value ar index 2 is unique. */
-  val t2 = tStr.findType[Int]//Does the same as r1
+  val t2 = tStr.findType[Int]
+
   val t3 = tStr.findType[String]
-  val t4 = tStr.typeAtStsIndex[String](0)//Indexs start at 0
+
+  /** Indexes start at 0. */
+  val t4 = tStr.typeAtStsIndex[String](0)
+
   val t5 = tStr.typeAtStsIndex[String](1)
-  val t6 = tStr.typeAtStsIndex[String](2)//Because Indexs start at 0. There is no element 2 of type String.
+
+  /** Because Indexes start at 0. There is no element 2 of type String. */
+  val t6 = tStr.typeAtStsIndex[String](2)
+
   val t7 = tStr.findType[Pt2]
-  val t8 = tStr.dblAtStsIndex(0)// findDouble //This fails because 7 can be both an Int and a Double.
+  val t8 = tStr.dblAtStsIndex(0)
+
   val topStrs = Arr(t1, t2, t3, t4, t5, t6, t7, t8).map(_.toString)
   val topBlock = MText(200, topStrs)
   
