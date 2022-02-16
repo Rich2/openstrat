45841317d27bf9181fc46f4b165c09c05b69f54f
Rich2 <rr1@tutanota.com>
2022-02-16 19:52:59 +0000

    #177 Show5T no longer extends ShowDecT.

--- Util/src/Persist/Persist5.scala
+++ Util/src/Persist/Persist5.scala
@@ -14,8 +14,7 @@ trait TypeStr5[A1, A2, A3, A4, A5] extends Any with TypeStr4[A1, A2, A3, A4]
 class Show5T[A1, A2, A3, A4, A5, R](val typeStr: String, val name1: String, fArg1: R => A1, val name2: String, fArg2: R => A2,
   val name3: String, fArg3: R => A3, val name4: String, fArg4: R => A4, val name5: String, fArg5: R => A5, val opt5: Option[A5],
   opt4In: Option[A4] = None, opt3In: Option[A3] = None, opt2In: Option[A2] = None, opt1In: Option[A1] = None)(
-  implicit ev1: ShowDecT[A1], ev2: ShowDecT[A2], ev3: ShowDecT[A3], ev4: ShowDecT[A4], ev5: ShowDecT[A5]) extends
-  ShowProductDecT[R] with TypeStr5[A1, A2, A3, A4, A5]
+  implicit ev1: ShowT[A1], ev2: ShowT[A2], ev3: ShowT[A3], ev4: ShowT[A4], ev5: ShowT[A5]) extends ShowProductT[R] with TypeStr5[A1, A2, A3, A4, A5]
 {
   val opt4: Option[A4] = ife(opt5.nonEmpty, opt4In, None)
   val opt3: Option[A3] = ife(opt4.nonEmpty, opt3In, None)
@@ -25,16 +24,16 @@ class Show5T[A1, A2, A3, A4, A5, R](val typeStr: String, val name1: String, fArg
   final override def syntaxDepthT(obj: R): Int = ev1.syntaxDepthT(fArg1(obj)).max(ev2.syntaxDepthT(fArg2(obj))).max(ev3.syntaxDepthT(fArg3(obj))).
     max(ev4.syntaxDepthT(fArg4(obj))).max(ev5.syntaxDepthT(fArg5(obj))) + 1
 
-  override def strDecs(obj: R, way: ShowStyle, decimalPlaces: Int): Strings =
-    Strings(ev1.showDecT(fArg1(obj), way, decimalPlaces, 0), ev2.showDecT(fArg2(obj), way, decimalPlaces, 0), ev3.showDecT(fArg3(obj), way, decimalPlaces, 0),
-      ev4.showDecT(fArg4(obj), way, decimalPlaces, 0), ev5.showDecT(fArg5(obj), way, decimalPlaces, 0))
+  override def strs(obj: R, way: ShowStyle): Strings =
+    Strings(ev1.showT(fArg1(obj), way), ev2.showT(fArg2(obj), way), ev3.showT(fArg3(obj), way), ev4.showT(fArg4(obj), way),
+    ev5.showT(fArg5(obj), way))
 }
 
 object Show5T
 {
   def apply[A1, A2, A3, A4, A5, R](typeStr: String, name1: String, fArg1: R => A1, name2: String, fArg2: R => A2, name3: String, fArg3: R => A3,
     name4: String, fArg4: R => A4, name5: String, fArg5: R => A5, opt5: Option[A5] = None, opt4: Option[A4] = None, opt3: Option[A3] = None,
-    opt2: Option[A2] = None, opt1: Option[A1] = None)(implicit ev1: ShowDecT[A1], ev2: ShowDecT[A2], ev3: ShowDecT[A3], ev4: ShowDecT[A4], ev5: ShowDecT[A5]) =
+    opt2: Option[A2] = None, opt1: Option[A1] = None)(implicit ev1: ShowT[A1], ev2: ShowT[A2], ev3: ShowT[A3], ev4: ShowT[A4], ev5: ShowT[A5]) =
     new Show5T[A1, A2, A3, A4, A5, R](typeStr, name1, fArg1, name2, fArg2, name3, fArg3, name4, fArg4, name5, fArg5, opt5, opt4, opt3, opt2, opt1)(
       ev1, ev2, ev3, ev4, ev5)
 }
