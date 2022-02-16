4a361652c200faf1d4203978bcf0eb65bdf3f72e
Rich2 <rr1@tutanota.com>
2022-02-16 23:15:02 +0000

    Updated html link to Js file.

--- Dev/srcJsApps/GlApp/WebGlApp.scala
+++ Dev/srcJsApps/GlApp/WebGlApp.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pSJs
 import scalajs._,js.annotation._, org.scalajs.dom._, pGrid._
 
