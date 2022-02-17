abf888fe4fa62613a0df05a781e35c10e35892fb
Rich2 <rr1@tutanota.com>
2022-02-17 21:51:00 +0000

    #177 Created PersistN.

--- Util/src/Persist/ShowN.scala
+++ Util/src/Persist/ShowN.scala
@@ -54,3 +54,14 @@ trait ShowNT[R] extends ShowCompoundT[R]
     }
   }
 }
+
+trait ShowShowNT[R <: ShowN] extends ShowNT[R] with ShowShowT[R]
+{ override def strs(obj: R, way: ShowStyle): Strings = obj.showElemStrs(way)
+}
+
+/** The base trait for the persistence of algebraic product types, including case classes. Note the arity of the product, its size is based on the
+ *  number of logical parameters. For example, a LineSeg is a product 2, it has a start point and an end point, although its is stored as 4 parameters
+ *  xStart, yStart, xEnd, yEnd. */
+trait PersistN[R] extends Persist[R] with ShowNT[R]
+
+trait PersistShowN[R <: ShowN] extends PersistN[R] with ShowShowNT[R]
