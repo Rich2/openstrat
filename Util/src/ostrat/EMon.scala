e7905d9f385279ab6057044a3a215d4354ca7a19
Rich2 <rr1@tutanota.com>
2022-02-17 23:08:35 +0000

    #177 Yet More PersistDec implicits replaced with Unshows.

--- Util/src/ostrat/EMon.scala
+++ Util/src/ostrat/EMon.scala
@@ -69,8 +69,8 @@ object EMon
     def findSetting[A](settingStr: String)(implicit ev: Unshow[A]): EMon[A] =
       thisEMon.flatMap(str => pParse.stringToStatements(str).flatMap(_.findSetting[A](settingStr)))
     def findSettingElse[A: Unshow](settingStr: String, elseValue: => A): A = findSetting[A](settingStr).getElse(elseValue)
-    def findSomeSetting[A: PersistDec](settingStr: String, elseValue: => A): A = ??? //findSetting[Option[A]](settingStr)(implicit ev: Persist[A]): EMon[A]
-    def findSomeSettingElse[A: PersistDec](settingStr: String, elseValue: => A): A = ??? //findSetting[A](settingStr).getElse(elseValue)
+    def findSomeSetting[A: Unshow](settingStr: String, elseValue: => A): A = ??? //findSetting[Option[A]](settingStr)(implicit ev: Persist[A]): EMon[A]
+    def findSomeSettingElse[A: Unshow](settingStr: String, elseValue: => A): A = ??? //findSetting[A](settingStr).getElse(elseValue)
   }
 
   implicit def showImplicit[A](implicit ev: ShowDecT[A]): ShowDecT[EMon[A]] =
