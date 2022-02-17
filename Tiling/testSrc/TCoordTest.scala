5a512b4150fdf3ca44392f2602b5560f2546737d
Rich2 <rr1@tutanota.com>
2022-02-17 03:26:55 +0000

    Closes #182 Missing import added.

--- Tiling/testSrc/TCoordTest.scala
+++ Tiling/testSrc/TCoordTest.scala
@@ -1,6 +1,6 @@
-/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package prid
-import utest._
+import utest._, phex._
 
 object TCoordTest extends TestSuite
 {
