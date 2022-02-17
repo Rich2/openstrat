a4de581d18620aeb0b1ab22512b54968da6bd093
Rich2 <rr1@tutanota.com>
2022-02-17 19:58:55 +0000

    #177 Show4T is now a trait and no longer extends TypeStr4.

--- Util/src/Persist/Persist4.scala
+++ Util/src/Persist/Persist4.scala
@@ -12,19 +12,25 @@ trait TypeStr4[A1, A2, A3, A4] extends Any with TypeStr3[A1, A2, A3]
 }
 
 /** Show type class for 4 parameter case classes. */
-abstract class Show4T[A1, A2, A3, A4, R](val typeStr: String, name1: String, fArg1: R => A1, name2: String, fArg2: R => A2, name3: String,
-  fArg3: R => A3, name4: String, fArg4: R => A4, val opt4: Option[A4] = None, opt3In: Option[A3] = None, opt2In: Option[A2] = None,
-  opt1In: Option[A1] = None)(implicit ev1: ShowT[A1], ev2: ShowT[A2], ev3: ShowT[A3], ev4: ShowT[A4]) extends ShowProductT[R]
+trait Show4T[A1, A2, A3, A4, R] extends ShowProductT[R]
+
+object Show4T
 {
-  val opt3: Option[A3] = ife(opt4.nonEmpty, opt3In, None)
-  val opt2: Option[A2] = ife(opt3.nonEmpty, opt2In, None)
-  val opt1: Option[A1] = ife(opt2.nonEmpty, opt1In, None)
+  class Show4TImp[A1, A2, A3, A4, R](val typeStr: String, val name1: String, val fArg1: R => A1, val name2: String, val fArg2: R => A2,
+    val name3: String, val fArg3: R => A3, val name4: String, val fArg4: R => A4, val opt4: Option[A4] = None, opt3In: Option[A3] = None, opt2In: Option[A2] = None,
+    opt1In: Option[A1] = None)(implicit ev1: ShowT[A1], ev2: ShowT[A2], ev3: ShowT[A3], ev4: ShowT[A4]) extends Show4T[A1, A2, A3, A4, R] with
+    TypeStr4[A1, A2, A3, A4]
+  {
+    val opt3: Option[A3] = ife(opt4.nonEmpty, opt3In, None)
+    val opt2: Option[A2] = ife(opt3.nonEmpty, opt2In, None)
+    val opt1: Option[A1] = ife(opt2.nonEmpty, opt1In, None)
 
-  final override def syntaxDepthT(obj: R): Int = ev1.syntaxDepthT(fArg1(obj)).max(ev2.syntaxDepthT(fArg2(obj))).max(ev3.syntaxDepthT(fArg3(obj))).
-    max(ev4.syntaxDepthT(fArg4(obj))) + 1
+    final override def syntaxDepthT(obj: R): Int = ev1.syntaxDepthT(fArg1(obj)).max(ev2.syntaxDepthT(fArg2(obj))).max(ev3.syntaxDepthT(fArg3(obj))).
+      max(ev4.syntaxDepthT(fArg4(obj))) + 1
 
-  override def strs(obj: R, way: ShowStyle): Strings = Strings(ev1.showT(fArg1(obj), way), ev2.showT(fArg2(obj), way),
-    ev3.showT(fArg3(obj), way), ev4.showT(fArg4(obj), way))
+    override def strs(obj: R, way: ShowStyle): Strings = Strings(ev1.showT(fArg1(obj), way), ev2.showT(fArg2(obj), way),
+      ev3.showT(fArg3(obj), way), ev4.showT(fArg4(obj), way))
+  }
 }
 
 /** UnShow class for 3 logical parameter product types. */
