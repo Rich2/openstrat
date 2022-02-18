0d2777e7f9aed69452ce5936e3228e33673a9ce8
Rich2 <rr1@tutanota.com>
2022-02-18 21:31:48 +0000

    #185 Tidying.

--- TilingExs/src/gOne/Player.scala
+++ TilingExs/src/gOne/Player.scala
@@ -21,8 +21,8 @@ case class Player(char: Char, colour: Colour) extends ShowNoDec
 
 /** Companion object for Player case class contains implicit instance for Persist. */
 object Player
-{
-  implicit val showPlayer: Show2T[Char, Colour, Player] = Show2T[Char, Colour, Player]("Player", "char", _.char, "colour", _.colour)
+{ /* Implicit [[ShowT]] instance / evidence for [[Player]]. */
+  implicit val showTEv: Show2T[Char, Colour, Player] = Show2T[Char, Colour, Player]("Player", "char", _.char, "colour", _.colour)
 }
 object PlayerA extends Player('A', Red)
 object PlayerB extends Player('B', Orange)
