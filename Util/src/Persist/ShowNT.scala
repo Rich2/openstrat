227ae7a231e08cd9ec21482e8d5a30ccebef58b4
Rich2 <rr1@tutanota.com>
2022-02-27 02:22:06 +0000

    #185 Tidied up algorithm for fromExprSeq and added more tests to UnshowTest.

--- Util/src/Persist/ShowNT.scala
+++ Util/src/Persist/ShowNT.scala
@@ -59,22 +59,17 @@ trait UnshowN[R] extends Unshow[R] with TypeStrN
     if(exprs.length > numParams) Bad(Strings(exprs.length.toString + " parameters for 2 parameter constructor."))
     else
     {
-      def exprsLoop(i: Int, oldSeq: Ints, newSeq: Ints, usedNames: Strings): EMon[R] =
+      def exprsLoop(i: Int, usedNames: Strings): EMon[R] =
         if (i >= exprs.length)
-          if (i >= numParams) { val intSeq = paramNames.map(pn => usedNames.findIndex(_ == pn)) ;fromSortedExprs(exprs, intSeq) }// newSeq ++ oldSeq) }
-          else exprsLoop(i + 1, oldSeq, newSeq, usedNames :+ paramNames.find(u => !usedNames.exists(_ == u)).get)
+          if (i >= numParams) fromSortedExprs(exprs, paramNames.map(pn => usedNames.findIndex(_ == pn)))
+          else exprsLoop(i + 1, usedNames :+ paramNames.find(u => !usedNames.exists(_ == u)).get)
         else exprs(i) match
         {
           case AsignExprName(name) if !paramNames.contains(name) => bad1(exprs(i),"Unrecognised setting identifer name.")
           case AsignExprName(name) if usedNames.contains(name) => bad1(exprs(i), name + " Multiple parameters of the same name.")
-
-          case AsignExprName(name) => { val nameInd = paramNames.indexOf(name)
-            val oldSeqInd = oldSeq.indexOf(nameInd)
-            exprsLoop(i + 1,oldSeq.removeIndex(oldSeqInd), newSeq :+ oldSeq(oldSeqInd), usedNames :+ name)
-          }
-
-          case _ => exprsLoop(i + 1, oldSeq.drop1, newSeq :+ oldSeq(0), usedNames :+ paramNames.find(u => !usedNames.exists(_ == u)).get)
+          case AsignExprName(name) => exprsLoop(i + 1, usedNames :+ name)
+          case _ => exprsLoop(i + 1, usedNames :+ paramNames.find(u => !usedNames.exists(_ == u)).get)
         }
-      exprsLoop(0, Ints.until(0, numParams), Ints(), Strings())
+      exprsLoop(0, Strings())
   }
 }
--- Util/testSrc/ostrat/UnshowTest.scala
+++ Util/testSrc/ostrat/UnshowTest.scala
@@ -66,6 +66,10 @@ object UnshowTest extends TestSuite
       """ExUB(b: String = "AAA")""".asType[ExUB] ==> Good(ExUB(b = "AAA"))
       """ExUB(c = 5;)""".asType[ExUB] ==> Good(ExUB(c = 5))
       """ExUB(c = 5; a = ExUA(b = "Hi"))""".asType[ExUB] ==> Good(ExUB(ExUA(b = "Hi"), c = 5))
+      """ExUB(c = 5; a = 7, "Hi")""".asType[ExUB] ==> Good(ExUB(ExUA(7, "Hi"), c = 5))
+      """ExUB(c = 5; a = 7, "Hi",)""".asType[ExUB] ==> Good(ExUB(ExUA(7, "Hi"), c = 5))
+      """ExUB(c = 5; a = 7, "Hi";)""".asType[ExUB] ==> Good(ExUB(ExUA(7, "Hi"), c = 5))
+      """ExUB(c = 5; a = 7, "Hi", ;)""".asType[ExUB] ==> Good(ExUB(ExUA(7, "Hi"), c = 5))
     }
   }
 }
