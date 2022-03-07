fab2a34aae2a2484c5879902e3f969d070ceace9
Rich2 <rr1@tutanota.com>
2022-03-07 20:26:42 +0000

    #124 Removed methods from UnscaledPolygon.

--- Geom/src/polygon/UnScaled.scala
+++ Geom/src/polygon/UnScaled.scala
@@ -3,15 +3,8 @@ package ostrat; package geom
 
 /** This is not a [[Polygon]] but can apply or transformed to a Polygon. Not sure how useful this trait is. not sure if it is the correct approach to
  * preserving information. */
-trait UnScaledPolygon //extends  UnScaled
-{ type ThisT = PolygonGen
-  def apply(): PolygonGen
-  def fTrans(f: Pt2 => Pt2): ThisT = apply().vertsTrans(f)
-  def dist(width: Length, cen: PtM2 = Dist2Z): PolygonM  = apply().distScale(width)
-  def minX: Double = apply().minX
-  def maxX: Double = apply().maxX
-  def minY: Double = apply().minY
-  def maxY: Double = apply().maxY
+trait UnScaledPolygon
+{ def apply(): PolygonGen
 }
 
 /** An unscaled polygon that is symmetrical about the Y axis. */
@@ -19,11 +12,10 @@ trait UnScaledPolygonYMirror extends UnScaledPolygon
 {
   /* The right side of the Y Axis of this UnscaledPolygon, defined relative to a unit of 100 for convenience. So 0.35 is defined as 35. 0.222 is defined as 22.2  */
   def rtLine100: LinePath
-  final override def apply() = rtLine100.yMirrorClose.slateY(-50).scale(0.01)
+  final override def apply(): PolygonGen = rtLine100.yMirrorClose.slateY(-50).scale(0.01)
 }
 
 trait PolygonYMirror extends Polygon
 {
   def rtLine: LinePath
 }
-
