47218bc5a58bb3793188b9d2f969ea7a0dac62d4
Rich2 <rr1@tutanota.com>
2022-03-11 11:59:26 +0000

    #171 Tiling/srcOld/pEarth folder removed.

--- Tiling/srcOld/pEarth/EGridOld.scala
+++
@@ -1,8 +0,0 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0 */
-package ostrat; package pGrid
-import geom._, pglobe._
-
-trait EGridOld extends HexGridIrrOld
-{
-  def roodToLL(roord: Roord): LatLong
-}
