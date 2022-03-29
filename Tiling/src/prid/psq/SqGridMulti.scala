c9714cbaacf4899fdf180a82373bad71aa2aa3b5
Rich2 <rr1@tutanota.com>
2022-03-29 01:33:10 +0100

    #188 Created SqGrid2.

--- Tiling/src/prid/psq/SqGridMulti.scala
+++ Tiling/src/prid/psq/SqGridMulti.scala
@@ -1,7 +1,7 @@
 /* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package prid; package psq
 
-case class HGridMan(grid: SqGrid, arrIndex: Int)
+case class SqGridMan(grid: SqGrid, arrIndex: Int)
 { def numTiles: Int = grid.numTiles
   //final def outSteps(hCen: SqCen): Arr[(HStep, HCen)] = outSteps(hCen.r, hCen.c)
   //def outSteps(r: Int, c: Int): Arr[(HStep, HCen)] = Arr()
@@ -11,12 +11,18 @@ case class HGridMan(grid: SqGrid, arrIndex: Int)
 }
 
 trait SqGridMulti extends SqGrider {
-
+  def gridMans: Arr[SqGridMan]
+  def grids: Arr[SqGrid] = gridMans.map(_.grid)
 }
 
-/** A simple development class of 2 square grids. 1 directly above the ohter. */
-case class SqStory2(minCenR: Int, maxCenR: Int, minCenC: Int, maxCenC: Int) extends SqGridMulti
+trait SqGridMultiFlat extends SqGridMulti
+
+/** A simple development class of 2 square grids. Probably not a useful class for an application but a stage on the evolution of more exotic classes. */
+final class SqGrids2(val minCenR: Int, val maxCenR: Int, val minC1: Int, val maxC1: Int, val minC2: Int, maxC2: Int) extends SqGridMultiFlat
 {
+
+  override def gridMans: Arr[SqGridMan] = Arr()
+
   /** The total number of tile centres in this tile Grid system. */
   override def numTiles: Int = ???
 }
