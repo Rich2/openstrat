586589bd714f62904d3be7fc682755e88eba0fee
Rich2 <rr1@tutanota.com>
2022-03-31 16:46:16 +0100

    #168 More implicit HGrid => HGrider.

--- Tiling/src/prid/phex/HCenArrDGrid.scala
+++ Tiling/src/prid/phex/HCenArrDGrid.scala
@@ -2,7 +2,7 @@
 package ostrat; package prid; package phex
 import reflect.ClassTag
 
-/** A Hex grid data class of [[Arr]]s. */
+/** A [[HGrider]] data grid of [[Arr]]s of [[HCen]] data. */
 class HCenArrDGrid[A](val unsafeArray: Array[Array[A]])
 {
   def apply(hc: HCen)(implicit grider: HGrider): Arr[A] = new Arr(unsafeArray(grider.arrIndex(hc)))
@@ -16,24 +16,24 @@ class HCenArrDGrid[A](val unsafeArray: Array[Array[A]])
     unsafeArray(grider.arrIndex(hc)) = newElem
   }
 
-  def setSame(value: A, hcs: HCen*)(implicit grid: HGrid, ct: ClassTag[A]): Unit = hcs.foreach{ hc => set(hc, value) }
+  def setSame(value: A, hcs: HCen*)(implicit grider: HGrider, ct: ClassTag[A]): Unit = hcs.foreach{ hc => set(hc, value) }
 
-  def prepend(r: Int, c: Int, value: A)(implicit grid: HGrid, ct: ClassTag[A]): Unit = prepend(HCen(r, c), value)
+  def prepend(r: Int, c: Int, value: A)(implicit grider: HGrider, ct: ClassTag[A]): Unit = prepend(HCen(r, c), value)
 
-  def prepend(hc: HCen, value: A)(implicit grid: HGrid, ct: ClassTag[A]): Unit =
-  { val oldElem =  unsafeArray(grid.arrIndex(hc))
+  def prepend(hc: HCen, value: A)(implicit grider: HGrider, ct: ClassTag[A]): Unit =
+  { val oldElem =  unsafeArray(grider.arrIndex(hc))
     val newElem: Array[A] = new Array[A](oldElem.length + 1)
     newElem(0) = value
     oldElem.copyToArray(newElem, 1)
-    unsafeArray(grid.arrIndex(hc)) = newElem
+    unsafeArray(grider.arrIndex(hc)) = newElem
   }
   //    def prepends(value : A, roords: Roord*)(implicit grid: TileGridOld): Unit = roords.foreach{ r =>  thisRefs.unsafeArr(grid.arrIndex(r)) ::= value }
 
   /** flatMaps over the the first element of each tile's data Array. Ignores empty arrays and subsequent elements. */
-  def gridHeadsMap[B, BB <: SeqImut[B]](f: (HCen, A) => B)(implicit grid: HGrid, build: ArrBuilder[B, BB]): BB =
+  def gridHeadsMap[B, BB <: SeqImut[B]](f: (HCen, A) => B)(implicit grider: HGrider, build: ArrBuilder[B, BB]): BB =
   {
     val buff = build.newBuff()
-    grid.foreach { r =>
+    grider.foreach { r =>
       val el:Arr[A] = apply(r)
       if (el.dataLength >= 1) build. buffGrow(buff, f(r, el(0)))
     }
@@ -41,10 +41,10 @@ class HCenArrDGrid[A](val unsafeArray: Array[Array[A]])
   }
 
   /** flatMaps over the the first element of each tile's data Array. Ignores empty arrays and subsequent elements. */
-  def gridHeadsFlatMap[BB <: SeqImut[_]](f: (HCen, A) => BB)(implicit grid: HGrid, build: ArrFlatBuilder[BB]): BB =
+  def gridHeadsFlatMap[BB <: SeqImut[_]](f: (HCen, A) => BB)(implicit grider: HGrider, build: ArrFlatBuilder[BB]): BB =
   {
     val buff = build.newBuff()
-    grid.foreach { r =>
+    grider.foreach { r =>
       val el:Arr[A] = apply(r)
       if (el.dataLength >= 1) build.buffGrowArr(buff, f(r, el(0)))
     }
