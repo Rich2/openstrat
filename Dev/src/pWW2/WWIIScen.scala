d06b3caccd9774176f1d39da557e1acccf234bf2
Rich2 <rr1@tutanota.com>
2022-04-03 03:37:40 +0100

    Started to create Go.

--- Dev/src/pWW2/WWIIScen.scala
+++ Dev/src/pWW2/WWIIScen.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pWW2
 import geom._, pglobe._,pEarth._, pgui._, pParse._
 
---
+++ Dev/src/pgo/GoScen.scala
@@ -0,0 +1,21 @@
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package pgo
+import prid._, psq._, Colour._
+
+sealed trait Stone
+{ def colour: Colour
+}
+
+object BStone extends Stone
+{ override def colour: Colour = Black
+}
+
+object WStone extends Stone
+{ override def colour: Colour = Black
+}
+
+class GoScen
+{
+  def grid: SqGrid = SqGrid(2, 38, 2, 38)
+  def stones: SqCenOptDGrid[Stone] = grid.newTileArrOpt[Stone]
+}
