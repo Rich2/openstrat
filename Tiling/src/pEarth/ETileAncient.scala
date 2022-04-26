352fd6f2d1661d578188795b38076eaaff45a008
Rich2 <rr1@tutanota.com>
2022-04-26 11:28:30 +0100

    Formatting

--- Tiling/src/pEarth/ETileAncient.scala
+++ Tiling/src/pEarth/ETileAncient.scala
@@ -1,12 +1,9 @@
-/* Copyright 2018-21 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pEarth
-import geom._, pGrid._
+import pGrid._
 
 trait ETileAncient extends ColouredTileAncient
 { def terr: WTile
   def colour: Colour = terr.colour
   def str: String = terr.str
 }
-
-
-
