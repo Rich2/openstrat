d36113b2459da11b1239bc20a280e6f827f313ba
Rich2 <rr1@tutanota.com>
2022-07-24 20:07:02 +0100

    #194 Created HSysGui.

---
+++ Tiling/src/prid/phex/HSysGui.scala
@@ -0,0 +1,10 @@
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package prid; package phex
+import geom._
+
+trait HSysGui
+{
+  type GridT <: HGridSys
+  def gridSys: GridT
+  def polygons(inp: Arr[Polygon]): Arr[Polygon]
+}
