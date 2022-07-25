87d76ac32fb13c02d6841b4d57520b73eeb02aaf
Rich2 <rr1@tutanota.com>
2022-07-25 11:12:39 +0100

    Continued removing implicits.

--- Tiling/src/prid/phex/HGridSys.scala
+++ Tiling/src/prid/phex/HGridSys.scala
@@ -242,7 +242,7 @@ trait HGridSys extends Any with TGridSys
   final def outerSideLines(implicit grider: HGridSys): LineSegArr = outerSideLineSegHCs.map(_.lineSeg)
 
   /** This gives the all tile grid lines in a single colour and line width. */
-  final def sidesDraw(colour: Colour = Black, lineWidth: Double = 2.0)/*(implicit grider: HGridSys)*/: LinesDraw = sideLines.draw(lineWidth, colour)
+  final def sidesDraw(colour: Colour = Black, lineWidth: Double = 2.0): LinesDraw = sideLines.draw(lineWidth, colour)
 
   /** Draws the inner side lines in a single colour and line width. */
   final def innerSidesDraw(colour: Colour = Black, lineWidth: Double = 2.0)(implicit grider: HGridSys): LinesDraw =
