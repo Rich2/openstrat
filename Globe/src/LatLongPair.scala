e66f764881a6e227c4859500c634fd1bde8fa242
Rich2 <rr1@tutanota.com>
2022-10-18 00:16:17 +0100

    #196 Created LatLongPairArr

--- Globe/src/LatLongPair.scala
+++ Globe/src/LatLongPair.scala
@@ -7,3 +7,23 @@ class LatLongPair[A2](val latMilliSecs: Double, val longMilliSecs: Double, val a
   override def a1: LatLong = LatLong.milliSecs(latMilliSecs, longMilliSecs)
 }
 
+class LatLongPairArr[A2](val arrayDbl: Array[Double], val a2Array: Array[A2]) extends PointDbl2PairArr[LatLong, LatLongArr, A2, LatLongPair[A2]]
+{ override type ThisT = LatLongPairArr[A2]
+
+  override def a1Arr: LatLongArr = ???
+
+  /** Accesses the defining sequence element by a 0 based index. */
+  override def apply(index: Int): LatLongPair[A2] = ???
+
+  /** This method should rarely be needed to be used by end users, but returns a new uninitialised [[SeqSpec]] of the this [[Arr]]'s final type. */
+  override def unsafeSameSize(length: Int): LatLongPairArr[A2] = ???
+
+  /** Sets / mutates an element in the Arr. This method should rarely be needed by end users, but is used by the initialisation and factory
+   * methods. */
+  override def unsafeSetElem(i: Int, value: LatLongPair[A2]): Unit = ???
+
+  override def fElemStr: LatLongPair[A2] => String = ???
+
+  /** String specifying the type of this object. */
+  override def typeStr: String = ???
+}
