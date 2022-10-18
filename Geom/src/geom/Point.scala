431da893b344491e347bb198de635c00d6d58184
Rich2 <rr1@tutanota.com>
2022-10-18 09:37:25 +0100

    #196 More LatLongPair.

--- Geom/src/geom/Point.scala
+++ Geom/src/geom/Point.scala
@@ -14,9 +14,23 @@ trait PointDblN extends Any with Point with ElemDblN
 trait PointDblNPair[A1 <: PointDblN, A2] extends PointPair[A1, A2]
 
 trait PointDblNPairArr[A1 <: PointDblN, ArrA1 <: Arr[A1], A2, A <: PointDblNPair[A1, A2]] extends PointPairArr[A1, ArrA1, A2, A]
+{
+  def a1ArrayDbl: Array[Double]
+}
 
 trait PointDbl2 extends Any with PointDblN with ElemDbl2
 
-trait PointDbl2Pair[A1 <: PointDbl2, A2] extends PointPair[A1, A2]
+trait PointDbl2Pair[A1 <: PointDbl2, A2] extends PointDblNPair[A1, A2]
+{
+  def a1Dbl1: Double
+  def a1Dbl2: Double
+}
 
-trait PointDbl2PairArr[A1 <: PointDbl2, ArrA1 <: Arr[A1], A2, A <: PointDbl2Pair[A1, A2]] extends PointPairArr[A1, ArrA1, A2, A]
+trait PointDbl2PairArr[A1 <: PointDbl2, ArrA1 <: Arr[A1], A2, A <: PointDbl2Pair[A1, A2]] extends PointDblNPairArr[A1, ArrA1, A2, A]
+{ type ThisT <: PointDbl2PairArr[A1, ArrA1, A2, A]
+  def newPair(dbl1: Double, dbl2: Double, a2: A2): A
+  override final def apply(index: Int): A = newPair(a1ArrayDbl(index * 2), a1ArrayDbl(index * 2 + 1), a2Array(index))
+
+  override final def unsafeSetElem(i: Int, value: A): Unit = { a1ArrayDbl(i * 2) = value.a1Dbl1; a1ArrayDbl(i * 2 + 1) = value.a1Dbl2
+    a2Array(i) = value.a2 }
+}
--- Globe/src/LatLongPair.scala
+++ Globe/src/LatLongPair.scala
@@ -3,27 +3,20 @@ package ostrat; package geom; package pglobe
 import collection.mutable.ArrayBuffer, reflect.ClassTag
 
 class LatLongPair[A2](val latMilliSecs: Double, val longMilliSecs: Double, val a2: A2) extends PointDbl2Pair[LatLong, A2]
-{
+{ override def a1Dbl1: Double = latMilliSecs
+  override def a1Dbl2: Double = longMilliSecs
   override def a1: LatLong = LatLong.milliSecs(latMilliSecs, longMilliSecs)
 }
 
-class LatLongPairArr[A2](val arrayDbl: Array[Double], val a2Array: Array[A2]) extends PointDbl2PairArr[LatLong, LatLongArr, A2, LatLongPair[A2]]
+class LatLongPairArr[A2](val a1ArrayDbl: Array[Double], val a2Array: Array[A2]) extends PointDbl2PairArr[LatLong, LatLongArr, A2, LatLongPair[A2]]
 { override type ThisT = LatLongPairArr[A2]
+  override def typeStr: String = "LatLongPairArr"
+  override def a1Arr: LatLongArr = new LatLongArr(a1ArrayDbl)
 
-  override def a1Arr: LatLongArr = ???
+  override def newPair(dbl1: Double, dbl2: Double, a2: A2): LatLongPair[A2] = new LatLongPair[A2](dbl1, dbl2, a2)
 
-  /** Accesses the defining sequence element by a 0 based index. */
-  override def apply(index: Int): LatLongPair[A2] = ???
-
-  /** This method should rarely be needed to be used by end users, but returns a new uninitialised [[SeqSpec]] of the this [[Arr]]'s final type. */
+  /** needs restricting. */
   override def unsafeSameSize(length: Int): LatLongPairArr[A2] = ???
 
-  /** Sets / mutates an element in the Arr. This method should rarely be needed by end users, but is used by the initialisation and factory
-   * methods. */
-  override def unsafeSetElem(i: Int, value: LatLongPair[A2]): Unit = ???
-
-  override def fElemStr: LatLongPair[A2] => String = ???
-
-  /** String specifying the type of this object. */
-  override def typeStr: String = ???
+  override def fElemStr: LatLongPair[A2] => String = _.toString
 }
