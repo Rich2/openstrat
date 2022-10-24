48f4ed562d8f6f2c6a6c10db3edccd5416ab43e8
Rich2 <rr1@tutanota.com>
2022-10-24 16:20:43 +0100

    Fixed merge regression.

--- Util/src/Arr/ElemPair.scala
+++ Util/src/Arr/ElemPair.scala
@@ -1,6 +1,6 @@
 /* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
-import collection.mutable.ArrayBuffer
+import collection.mutable.ArrayBuffer, reflect.ClassTag
 
 /** These classes are for use in [[PairArr]]s. */
 trait ElemPair[A1, A2] extends Any
