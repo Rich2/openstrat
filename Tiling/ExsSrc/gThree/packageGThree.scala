ae38f87add67d9f0f4f34e218eb403effbb8adcd
Rich2 <rr1@tutanota.com>
2022-11-06 14:23:10 +0000

    Game descriptions.

---
+++ Tiling/ExsSrc/gThree/packageGThree.scala
@@ -0,0 +1,11 @@
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+
+/** This is the package for the third example of simple simultaneous turn tile based games. A hex based game like game One1, that introduces multi
+ *  turn directives. */
+package object gThree
+{
+  /**  This is just a dummy method as empty package objects can cause problems. */
+  def aboutStr: String = """This is the package for the third example of simple simultaneous turn tile based games. A hex based game like game One1,
+  that introduces multi turn directives.""".stripMargin
+}
--- Tiling/ExsSrc/gTwo/packageGTwo.scala
+++ Tiling/ExsSrc/gTwo/packageGTwo.scala
@@ -5,7 +5,7 @@ package ostrat
  * and adjacent moves take priority over diagonal tile steps. */
 package object gTwo
 {
-  /**  This is just a dummy method as empty package objects can cause problems. */
+  /** Package description [[String]]. */
   def aboutStr: String = """This is the package for the second example of simple simultaneous turn tile based games. It differs from the first in that it is on a square grid
   and adjacent moves take priority over diagonal tile steps.."""
 }
