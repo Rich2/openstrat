d6a572da022d8acd270da1c052ae023afe4bc803
Rich2 <rr1@tutanota.com>
2022-11-09 19:54:47 +0000

    Started creating HDirnPath.

---
+++ Tiling/src/prid/phex/HDirnPath.scala
@@ -0,0 +1,8 @@
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+//import collection.mutable.ArrayBuffer
+
+class HDirnPath(val unsafeArray: Array[Int]) extends ArrayIntBacked
+{
+  
+}
