a458a87052e8aa12a80c1d2620657499b4877ff0
Rich2 <rr1@tutanota.com>
2022-11-09 20:41:49 +0000

    More HDirnPath

--- Tiling/src/prid/phex/HDirnPath.scala
+++ Tiling/src/prid/phex/HDirnPath.scala
@@ -1,8 +1,37 @@
 /* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
-package ostrat
+package ostrat; package prid; package phex
 //import collection.mutable.ArrayBuffer
 
 class HDirnPath(val unsafeArray: Array[Int]) extends ArrayIntBacked
 {
-  
+  def startR: Int = unsafeArray(0)
+  def startC: Int = unsafeArray(1)
+  def startCen = HCen(unsafeArray(0), unsafeArray(1))
+  def segsNum: Int = unsafeArray.length - 2
+
+  def segHCsForeach(f: LineSegHC => Unit): Unit = {
+    var count = 0
+    var r1 = startR
+    var c1 = startC
+    var r2: Int = 0
+    var c2: Int = 0
+
+    while (count < segsNum) {
+      val step = HDirn.fromInt(unsafeArray(count + 2))
+      r2 = r1 + step.tr
+      c2 = c1 + step.tc
+      val hls = LineSegHC(r1, c1, r2, c2)
+      f(hls)
+      count += 1
+      r1 = r2
+      c1 = c2
+    }
+  }
+
+  def segHCsMap[B, ArrB <: Arr[B]](f: LineSegHC => B)(implicit build: ArrMapBuilder[B, ArrB], grider: HGridSys): ArrB =
+  { val res = build.uninitialised(segsNum)
+    var count = 0
+    segHCsForeach{ s => res.unsafeSetElem(count, f(s)); count += 1 }
+    res
+  }
 }
