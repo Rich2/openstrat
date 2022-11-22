394f0877864f12b9f2df0af2d84b7576b094ce39
Rich2 <rr1@tutanota.com>
2022-11-22 22:51:01 +0000

    #199 Created ArrNonParam.

--- Util/src/Arr/Arr.scala
+++ Util/src/Arr/Arr.scala
@@ -19,6 +19,14 @@ trait Arr[+A] extends Any with Sequ[A]
   def unsafeSetElemSeq(index: Int, elems: Iterable[A] @uncheckedVariance): Unit = elems.iForeach(index){(i, a) => unsafeSetElem(i, a) }
 }
 
+/** [[ShowT] type class for showing [[DataGen]][A] objects. */
+class ArrShowT[A, R <: Arr[A]](val evA: ShowT[A]) extends ShowTSeqLike[A, R]
+{
+  override def syntaxDepthT(obj: R): Int = obj.foldLeft(1)((acc, a) => acc.max(evA.syntaxDepthT(a)))
+  override def showDecT(obj: R, style: ShowStyle, maxPlaces: Int, minPlaces: Int): String =
+    typeStr + evA.typeStr.enSquare + obj.map(a => evA.showDecT(a, style, maxPlaces, minPlaces))
+}
+
 trait ArrSingle[+A] extends Any with Arr[A]
 {
   /** This method should rarely be needed to be used by end users, but returns a new uninitialised [[SeqSpec]] of the this [[Arr]]'s final type. */
@@ -77,10 +85,4 @@ trait ArrSingle[+A] extends Any with Arr[A]
   }
 }
 
-/** [[ShowT] type class for showing [[DataGen]][A] objects. */
-class ArrShowT[A, R <: Arr[A]](val evA: ShowT[A]) extends ShowTSeqLike[A, R]
-{
-  override def syntaxDepthT(obj: R): Int = obj.foldLeft(1)((acc, a) => acc.max(evA.syntaxDepthT(a)))
-  override def showDecT(obj: R, style: ShowStyle, maxPlaces: Int, minPlaces: Int): String =
-    typeStr + evA.typeStr.enSquare + obj.map(a => evA.showDecT(a, style, maxPlaces, minPlaces))
-}
+trait ArrNonParam[+A] extends Any with ArrSingle[A]
