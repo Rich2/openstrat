8c0c6426846d6a051233cc17a5764ff9e8c0752b
Rich2 <rr1@tutanota.com>
2022-11-22 16:05:19 +0000

    #196 Created HDirnPair.

--- Tiling/src/prid/phex/HDirnPair.scala
+++ Tiling/src/prid/phex/HDirnPair.scala
@@ -2,4 +2,16 @@
 package ostrat; package prid; package phex
 import geom._, collection.mutable.ArrayBuffer
 
-//class HDirnPair[A2](val dirnInt: Int, val a2: A2) extends Int1PairElem
+class HDirnPair[A2](val a1Int1: Int, val a2: A2) extends Int1PairElem[HDirn, A2]
+{ override def a1: HDirn = HDirn.fromInt(a1Int1)
+}
+
+class HDirnPairArr[A2](val a1ArrayInt: Array[Int], val a2Array: Array[A2]) extends Int1PairArr[HDirn, HDirnArr, A2, HDirnPair[A2]]
+{ override type ThisT = HDirnPairArr[A2]
+  override def typeStr: String = "HDirnPairArr"
+  override def newPair(int1: Int, a2: A2): HDirnPair[A2] = new HDirnPair[A2](int1, a2)
+  override def newA1(int1: Int): HDirn = HDirn.fromInt(int1)
+  override def newFromArrays(a1Array: Array[Int], a2Array: Array[A2]): HDirnPairArr[A2] = new HDirnPairArr[A2](a1ArrayInt, a2Array)
+  override def a1Arr: HDirnArr = new HDirnArr(a1ArrayInt)
+  override def fElemStr: HDirnPair[A2] => String = _.toString
+}
