220d9843fd0f1371aa741986f420e062c485217f
Rich2 <rr1@tutanota.com>
2022-12-13 03:38:53 +0000

    HGridDuo.

---
+++ Tiling/ExsSrc/pduo/GridMan1.scala
@@ -0,0 +1,39 @@
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package prid; package phex; package pduo
+import geom._
+
+class GridMan1(val sys: HGridsDuo, val grid: HGridReg) extends HGridMan
+{ //override  = This2
+ // override val grid: HGridReg = grid1
+  override def thisInd: Int = 0
+  override val indexStart: Int = 0
+
+  override def offset: Vec2 = Vec2(0, 0)
+
+  override def sidesForeach(f: HSide => Unit): Unit = grid.sidesForeach(f)
+
+  override def linksForeach(f: HSide => Unit): Unit = grid.linksForeach(f)
+
+  override def outerSidesForeach(f: HSide => Unit): Unit =
+  { grid.bottomRowForeachSide(f)
+  iToForeach(grid.bottomCenR, grid.topCenR){
+  case r if r.isEven => f(HSide(r, grid.rowLeftCenC(r) - 2))
+  case r => f(HSide(r, grid.leftCenC - 1))
+  }
+  grid.topRowForeachSide(f)
+  }
+
+  override def outSteps(r: Int, c: Int): HStepCenArr = (r, c) match
+  { case (r, c) if r == sys.maxCenR & c == sys.maxC1 => HStepCenArr((HexRt, r, c - sys.grid2OffsetC + 4), (HexDR, r - 2, c - sys.grid2OffsetC + 2))
+  case (r, c) if r == sys.maxCenR & c == sys.maxC1 - 2 => HStepCenArr((HexRt, r, c - sys.grid2OffsetC + 4))
+
+  case (r, c) if r == sys.minCenR & c == sys.maxC1 => HStepCenArr((HexUR, r + 2, c - sys.grid2OffsetC + 2), (HexRt, r, c - sys.grid2OffsetC + 4))
+  case (r, c) if r == sys.minCenR & c == sys.maxC1 - 2 => HStepCenArr((HexRt, r, c - sys.grid2OffsetC + 4))
+
+  case (r, c) if c == sys.maxC1 =>
+  HStepCenArr((HexUR, r + 2, c - sys.grid2OffsetC + 2), (HexRt, r, c - sys.grid2OffsetC + 4), (HexDR, r - 2, c - sys.grid2OffsetC + 2))
+
+  case (r, c) if c == sys.maxC1 - 2 => HStepCenArr((HexRt, r, c - sys.grid2OffsetC + 4))
+  case _ => HStepCenArr()
+  }
+}
--- Tiling/ExsSrc/pduo/HGridsDuo.scala
+++ Tiling/ExsSrc/pduo/HGridsDuo.scala
@@ -14,41 +14,7 @@ final class HGridsDuo(val minCenR: Int, val maxCenR: Int, val minC1: Int, val ma
 
   val grid2OffsetC: Int = maxC1 - minC2 + 2
 
-  val gridMan1: HGridMan = new HGridMan
-  { override val sys = This2
-    override val grid: HGridReg = grid1
-    override def thisInd: Int = 0
-    override val indexStart: Int = 0
-
-    override def offset: Vec2 = Vec2(0, 0)
-
-    override def sidesForeach(f: HSide => Unit): Unit = grid.sidesForeach(f)
-
-    override def linksForeach(f: HSide => Unit): Unit = grid.linksForeach(f)
-
-    override def outerSidesForeach(f: HSide => Unit): Unit =
-    { grid.bottomRowForeachSide(f)
-      iToForeach(grid.bottomCenR, grid.topCenR){
-        case r if r.isEven => f(HSide(r, grid.rowLeftCenC(r) - 2))
-        case r => f(HSide(r, grid.leftCenC - 1))
-      }
-      grid.topRowForeachSide(f)
-    }
-
-    override def outSteps(r: Int, c: Int): HStepCenArr = (r, c) match
-    { case (r, c) if r == maxCenR & c == maxC1 => HStepCenArr((HexRt, r, c - grid2OffsetC + 4), (HexDR, r - 2, c - grid2OffsetC + 2))
-      case (r, c) if r == maxCenR & c == maxC1 - 2 => HStepCenArr((HexRt, r, c - grid2OffsetC + 4))
-
-      case (r, c) if r == minCenR & c == maxC1 => HStepCenArr((HexUR, r + 2, c - grid2OffsetC + 2), (HexRt, r, c - grid2OffsetC + 4))
-      case (r, c) if r == minCenR & c == maxC1 - 2 => HStepCenArr((HexRt, r, c - grid2OffsetC + 4))
-
-      case (r, c) if c == maxC1 =>
-        HStepCenArr((HexUR, r + 2, c - grid2OffsetC + 2), (HexRt, r, c - grid2OffsetC + 4), (HexDR, r - 2, c - grid2OffsetC + 2))
-
-      case (r, c) if c == maxC1 - 2 => HStepCenArr((HexRt, r, c - grid2OffsetC + 4))
-      case _ => HStepCenArr()
-    }
-  }
+  val gridMan1: HGridMan = new GridMan1(this, grid1)
 
   val gridMan2: HGridMan = new HGridMan
   { override val sys = This2
