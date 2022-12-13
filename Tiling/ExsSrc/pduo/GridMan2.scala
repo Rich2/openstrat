b54f16afa2bc340a3373208546612ab36745be44
Rich2 <rr1@tutanota.com>
2022-12-13 15:38:40 +0000

    More GridMan2.

--- Tiling/ExsSrc/pduo/GridMan2.scala
+++ Tiling/ExsSrc/pduo/GridMan2.scala
@@ -19,16 +19,16 @@ class GridMan2(val sys: HGridsDuo, val grid: HGridReg) extends HGridMan
 
   override def linksForeach(f: HSide => Unit): Unit = grid.innerSideRowsForeach(r => innerRowForeachInnerSide(r)(f))
 
-  def innerRowForeachInnerSide(r: Int)(f: HSide => Unit): Unit = r match {
-    case r if r >= grid.topSideRow => excep(r.str + " is not an inner row.")
+  def innerRowForeachInnerSide(r: Int)(f: HSide => Unit): Unit = r match
+  { case r if r >= grid.topSideRow => excep(r.str + " is not an inner row.")
     case r if r <= grid.bottomSideR => excep(r.str + " is not an inner row.")
     case r if r.div4Rem2 => iToForeach(grid.leftrem2CenC - 2, grid.rightRem2CenC - 2, 4) { c => f(HSide(r, c)) }
     case r if r.div4Rem0 => iToForeach(grid.leftRem0CenC - 2, grid.rightRem0CenC - 2, 4) { c => f(HSide(r, c)) }
     case r => iToForeach(grid.leftCenC - 1, grid.rightCenC - 1, 2) { c => f(HSide(r, c)) }
   }
 
-  override def outerSidesForeach(f: HSide => Unit): Unit = {
-    grid.bottomRowForeachSide(f)
+  override def outerSidesForeach(f: HSide => Unit): Unit =
+  { grid.bottomRowForeachSide(f)
     iToForeach(grid.bottomCenR, grid.topCenR) {
       case r if r.isEven => f(HSide(r, grid.rowRightCenC(r) + 2))
       case r => f(HSide(r, grid.rightCenC + 1))
@@ -36,16 +36,16 @@ class GridMan2(val sys: HGridsDuo, val grid: HGridReg) extends HGridMan
     grid.topRowForeachSide(f)
   }
 
-  override def outSteps(r: Int, c: Int): HStepCenArr = (r, c) match {
-    case (r, c) if r == sys.maxCenR & c == sys.minC2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4), (HexDL, r - 2, c + sys.grid2OffsetC - 2))
-    case (r, c) if r == sys.maxCenR & c == sys.minC2 + 2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4))
+  override def outSteps(r: Int, c: Int): HStepCenArr = (r, c) match
+  { case (r, c) if r == grid.topCenR & c == grid.leftCenC => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4), (HexDL, r - 2, c + sys.grid2OffsetC - 2))
+    case (r, c) if r == grid.topCenR & c == grid.leftCenC + 2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4))
 
-    case (r, c) if r == sys.minCenR & c == sys.minC2 => HStepCenArr((HStepUL, r + 2, c + sys.grid2OffsetC - 2), (HexLt, r, c + sys.grid2OffsetC - 4))
-    case (r, c) if r == sys.minCenR & c == sys.minC2 + 2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4))
+    case (r, c) if r == grid.bottomCenR & c == grid.leftCenC => HStepCenArr((HStepUL, r + 2, c + sys.grid2OffsetC - 2), (HexLt, r, c + sys.grid2OffsetC - 4))
+    case (r, c) if r == grid.bottomCenR & c == grid.leftCenC + 2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4))
 
-    case (r, c) if c == sys.minC2 =>
+    case (r, c) if c == grid.leftCenC =>
       HStepCenArr((HStepUL, r + 2, c + sys.grid2OffsetC - 2), (HexLt, r, c + sys.grid2OffsetC - 4), (HexDL, r - 2, c + sys.grid2OffsetC - 2))
-    case (r, c) if c == sys.minC2 + 2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4))
+    case (r, c) if c == grid.leftCenC + 2 => HStepCenArr((HexLt, r, c + sys.grid2OffsetC - 4))
 
     case _ => HStepCenArr()
   }
