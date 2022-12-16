0c80f38c7cea514fa05cedecd39f60fa60fbd552
Rich2 <rr1@tutanota.com>
2022-12-16 15:02:50 +0000

    #171 More Bc305.

--- Dev/src/p305/Bc305Gui.scala
+++ Dev/src/p305/Bc305Gui.scala
@@ -6,12 +6,14 @@ case class Bc305Gui(canv: CanvasPlatform, scenIn: BcScen, viewIn: HGView, isFlat
 { var scen = scenIn
   override implicit val gridSys: HGridSys = scenIn.gridSys
   val terrs = scen.terrs
+  val sTerrs = scen.sTerrs
   focus = gridSys.cenVec
   cPScale = gridSys.fullDisplayScale(mainWidth, mainHeight)
   implicit val proj: HSysProjection = ife(isFlat, HSysProjectionFlat(gridSys, mainPanel), gridSys.projection(mainPanel))
   proj.setView(viewIn)
 
   def polyFills: RArr[PolygonFill] = terrs.projRowsCombinePolygons.map { pp => pp.a1.fill(pp.a2.colour) }
+  def sides1: GraphicElems = sTerrs.projTruesLineSegMap{ls => Rectangle.fromAxisRatio(ls, 0.3).fill(Colour.DarkBlue) }
   def lines: RArr[LineSegDraw] = terrs.projLinksLineOptMap{ (ls, t1, t2 ) => ife(t1 == t2, Some(ls.draw(t1.contrastBW)), None) }
 
   /** Creates the turn button and the action to commit on mouse click. */
@@ -25,7 +27,7 @@ case class Bc305Gui(canv: CanvasPlatform, scenIn: BcScen, viewIn: HGView, isFlat
   def thisTop(): Unit = reTop(bTurn %: proj.buttons)
 
   thisTop()
-  override def frame: GraphicElems = polyFills ++ lines
+  override def frame: GraphicElems = polyFills ++ sides1 ++ lines
 
   proj.getFrame = () => frame
   proj.setStatusText = { str =>
