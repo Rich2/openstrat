088dfe4e24ca94129d7aa35505ad639841eba3bf
Rich2 <rr1@tutanota.com>
2023-01-14 12:22:41 +0000

    160km E0.

--- Tiling/src/eg160/Terr160E0.scala
+++ Tiling/src/eg160/Terr160E0.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package eg160
 import pEarth._, prid._, phex._, WTile._, egrid._
 
@@ -9,18 +9,19 @@ object Terr160E0 extends LongTerrs
   override val terrs: HCenLayer[WTile] =
   {
     val res: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
+    def wr(r: Int, tileValues: Multiple[WTile]*): Unit = { res.completeRow(r, tileValues :_*); () }
     def gs(r: Int, cStart: Int, tileValues: Multiple[WTile]*): Unit = { res.toEndRow(r, cStart, tileValues :_*); () }
 //    gs(152, 460 + 60, taiga)
 //    gs(150, 460 + 58, taiga)
 //    gs(148, 460 + 56, taiga * 2)
 //    gs(146, 460 + 50, hills, sea * 2, plain)
 //    gs(144, 460 + 48, plain, sea * 2, plain)
-//    gs(142, 460 + 46, plain, plain, sea, plain * 2)
+    wr(296, sea * 6, hillTaiga * 2, taiga * 2)
     gs(294, 506, hills, sea * 3, hills, sea, taiga)
     gs(292, 504, hills * 2, sea * 5, plain)
     gs(290, 506, hills, sea * 4, plain, sea, plain)
     gs(288, 500, plain, hills, plain, sea * 4, plain, sea)
-    gs(286, 498, plain, sea * 2, plain, sea * 3, plain * 3)
+    wr(286, sea * 2, plain, sea * 2, plain, sea * 3, plain * 3)
     gs(284, 496, plain * 2, hills, plain * 2, sea * 2, plain * 4)
     gs(282, 506, hills, plain * 8)
     gs(280, 504, hills, sea * 2, plain * 6)
@@ -31,7 +32,7 @@ object Terr160E0 extends LongTerrs
 
   override val sTerrs: HSideBoolLayer =
   { val res = grid.newSideBools
-    //res.setTruesInts((142, 508), (143, 507), (144, 522), (145, 521))
+    res.setTruesInts(279,505,  281,515,  282,516,  284,502,  288,502)
     res
   }
 
