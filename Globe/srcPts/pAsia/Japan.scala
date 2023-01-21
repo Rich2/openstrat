8fe0052bc2db8536465262f08f578e3aef6b71d0
Rich2 <rr1@tutanota.com>
2023-01-21 13:50:32 +0000

    More 160Km Japan.

--- Globe/srcPts/pAsia/Japan.scala
+++ Globe/srcPts/pAsia/Japan.scala
@@ -27,9 +27,14 @@ object Hokkaido extends EArea2("Hokkaido", 43.50 ll 142.95, hills)
 object japan extends EArea2("Japan", degs(36.28, 138.71), hills)
 { val oma = 41.49 ll 140.94
   val p5 = 41.43 ll 141.46
+  val p10 = 39.55 ll 142.06
+
+  val p15 = 38.28 ll 141.52
+  val naruseMouth = 38.38 ll 141.18
   val sKyshu = 31.08 ll 130.75
   val capeKute = 33.44 ll 135.76
-  val p10 = 32.80 ll 131.97
+  val p25 = 32.80 ll 131.97
+
   val neKyushu = 33.34 ll 129.45
   val p60 = 35.42 ll 132.63
   val kashiwazaki = 37.37 ll 138.55
@@ -37,5 +42,5 @@ object japan extends EArea2("Japan", degs(36.28, 138.71), hills)
   val p90 = 40.62 ll 139.89
   val capeTappi = 41.26 ll 140.34
 
-  override val polygonLL = PolygonLL(oma, p5, choshi, capeKute, p10, sKyshu, neKyushu, p60, kashiwazaki, p90, capeTappi)
+  override val polygonLL = PolygonLL(oma, p5, p10, p15, naruseMouth, choshi, capeKute, p25, sKyshu, neKyushu, p60, kashiwazaki, p90, capeTappi)
 }
