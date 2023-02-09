51508f5401208c33c2894b7c44f5ce61b874edfb
Rich2 <rr1@tutanota.com>
2023-02-09 17:59:04 +0000

    Formatting British Isles.

--- Earth/srcPts/pEurope/Britain.scala
+++ Earth/srcPts/pEurope/Britain.scala
@@ -4,16 +4,15 @@ import geom._, pglobe._, WTile._
 
 /** [[polygonLL]] Graphical representation of Scotland. Depends on nothing. */
 object Scotland extends EArea2("Scotland", 56.82 ll -4.07, hills)
-{ val johnOGroats = 58.62 ll -3.08
-  val girnigoe = 58.47 ll -3.05
-  val inverness = 57.49 ll -4.22
-  val lossieMouth = 57.72 ll -3.33
-  val aberdeenshire = 57.58 ll -1.85
-   
-  val firthForth = 56.00 ll -3.39
-  val archerfield = 56.06 ll -2.81
-  val tantallion = 56.05 ll -2.65
-  val stAbbs = 55.91 ll -2.14
+{ val johnOGroats: LatLong = 58.62 ll -3.08
+  val girnigoe: LatLong = 58.47 ll -3.05
+  val inverness: LatLong = 57.49 ll -4.22
+  val lossieMouth: LatLong = 57.72 ll -3.33
+  val aberdeenshire: LatLong = 57.58 ll -1.85
+  val firthForth: LatLong = 56.00 ll -3.39
+  val archerfield: LatLong = 56.06 ll -2.81
+  val tantallion: LatLong = 56.05 ll -2.65
+  val stAbbs: LatLong = 55.91 ll -2.14
   val bamburgh: LatLong = 55.60 ll -1.68
   val tyneMouth: LatLong = 55.01 ll -1.41
 
@@ -25,16 +24,13 @@ object Scotland extends EArea2("Scotland", 56.82 ll -4.07, hills)
   val northCairn: LatLong = 55.00 ll -5.15
   val ayr: LatLong = 55.44 ll -4.64
   val sKintyre: LatLong = 55.29 ll -5.77
-
   val portahaven: LatLong = 55.69 ll -6.25
-
   val snaigmore: LatLong = 55.85 ll -6.45
   val wScarba: LatLong = 56.17 ll -5.75
   val kerrera: LatLong = 56.38 ll -5.58
   val wMull: LatLong = 56.28 ll -6.38
   val canna: LatLong = 57.05 ll -6.60
   val wRum: LatLong = 57.00 ll -6.45
-
   val wSkye: LatLong = 57.43 ll -6.78
   val nSkye: LatLong = 57.70 ll -6.29
   val nRona: LatLong = 57.58 ll -5.96
@@ -58,12 +54,35 @@ object Wales extends EArea2("Wales",  52.40 ll -3.50, hills)
   val mynydd: LatLong = 52.79 ll -4.77
   val anglesey: LatLong = 53.39 ll -4.54
 
-  val polygonLL: PolygonLL = PolygonLL(liverpool, chepstow, nashPoint, stDavids, aberaeron, criccieth, mynydd, anglesey)
+  override val polygonLL: PolygonLL = PolygonLL(liverpool, chepstow, nashPoint, stDavids, aberaeron, criccieth, mynydd, anglesey)
 }
 
 /** [[polygonLL]] graphical representation of England. Depends on [[Scotland]] and [[Wales]]. */
 object England extends EArea2("England",  52.73 ll -1.26, plain)
-{
+{ val scarborough: LatLong = 54.28 ll -0.39
+  val flamborough: LatLong = 54.11 ll -0.07
+  val holbeach: LatLong = 52.89 ll 0.08
+  val ouseMouth: LatLong = 52.80 ll 0.35
+  val hunstanton: LatLong = 52.97 ll 0.53
+  val cromer: LatLong = 52.93 ll 1.30
+  val horsey: LatLong = 52.75 ll 1.66
+  val lowestoft: LatLong = 52.48 ll 1.76
+  val foulness: LatLong = 51.61 ll 0.95
+  val nwGrain: LatLong = 51.48 ll 0.48
+  val nekent: LatLong = 51.38 ll 1.43
+  val dover: LatLong = 51.15 ll 1.38
+
+  val dungeness: LatLong = 50.91 ll 0.98
+  val beachyHead: LatLong = 50.73 ll 0.24
+  val ventnor: LatLong = 50.57 ll -1.29
+  val bournemouth: LatLong = 50.71 ll -1.89
+  val swanage: LatLong = 50.59 ll -1.95
+  val ePortland: LatLong = 50.54 ll -2.41
+  val charmouth: LatLong = 50.73 ll -2.91
+  val exeMouth: LatLong = 50.61 ll -3.42
+  val startPeninsular: LatLong = 50.22 ll -3.64
+  val stAustell: LatLong = 50.33 ll -4.75
+  val lizard: LatLong = 49.95 ll -5.20
   val penzance: LatLong = 50.06 ll -5.68
   val trevoseHead: LatLong = 50.55 ll -5.03
   val nwDevon: LatLong = 51.18 ll -4.19
@@ -73,36 +92,11 @@ object England extends EArea2("England",  52.73 ll -1.26, plain)
   val sBarrow: LatLong = 54.04 ll -3.20
   val stBeesHead: LatLong = 54.51 ll -3.63
 
-  val scarborough: LatLong = 54.28 ll -0.39
-  val flamborough: LatLong = 54.11 ll -0.07
-   
-  val holbeach: LatLong = 52.89 ll 0.08
-  val ouseMouth: LatLong = 52.80 ll 0.35
-   
-  val hunstanton: LatLong = 52.97 ll 0.53
-  val cromer: LatLong = 52.93 ll 1.30
-  val horsey: LatLong = 52.75 ll 1.66
-  val lowestoft: LatLong = 52.48 ll 1.76
-  val foulness: LatLong = 51.61 ll 0.95
-  val nwGrain: LatLong = 51.48 ll 0.48
-  val nekent: LatLong = 51.38 ll 1.43
-  val dover: LatLong = 51.15 ll 1.38
-  val dungeness = 50.91 ll 0.98
-  val beachyHead: LatLong = 50.73 ll 0.24
-  val ventnor: LatLong = 50.57 ll -1.29
-  val bournemouth: LatLong = 50.71 ll -1.89
-  val swanage: LatLong = 50.59 ll -1.95
-  val ePortland: LatLong = 50.54 ll -2.41
-  val charmouth: LatLong = 50.73 ll -2.91
-  val exeMouth: LatLong = 50.61 ll -3.42
-  
-  val startPeninsular: LatLong = 50.22 ll -3.64
-  val stAustell: LatLong = 50.33 ll -4.75
-  val lizard: LatLong = 49.95 ll -5.20
-
-  val polygonLL: PolygonLL = PolygonLL(penzance, trevoseHead, nwDevon, parrettMouth, Wales.chepstow,  Wales.liverpool,  kentMouth, sBarrow,
-     stBeesHead, Scotland.solwayMouth, Scotland.tyneMouth, scarborough, flamborough, holbeach, ouseMouth, hunstanton, cromer, horsey, lowestoft, foulness, nwGrain,
-     nekent, dover, dungeness, beachyHead, ventnor, bournemouth, swanage, ePortland, charmouth, exeMouth, startPeninsular, stAustell, lizard)
+  override val polygonLL: PolygonLL = PolygonLL(Scotland.tyneMouth, scarborough, flamborough, holbeach, ouseMouth, hunstanton, cromer, horsey,
+    lowestoft, foulness, nwGrain, nekent, dover,
+    dungeness, beachyHead, ventnor, bournemouth, swanage, ePortland, charmouth, exeMouth, startPeninsular, stAustell, lizard, penzance, trevoseHead,
+    nwDevon, parrettMouth, Wales.chepstow,  Wales.liverpool,  kentMouth, sBarrow, stBeesHead, Scotland.solwayMouth,
+  )
 
    val london: LocationLL = LocationLL("London", 51.51, - 0.13, 1)
    override val places: LocationLLArr = LocationLLArr(london)
--- Earth/srcPts/pEurope/BritshIslands.scala
+++ Earth/srcPts/pEurope/BritshIslands.scala
@@ -2,56 +2,56 @@
 package ostrat; package pEarth; package pEurope
 import geom._, pglobe._, WTile._
 
-/** 2D Graphical representation of the Orkney's */
+/** [[PolygonLL]] graphical representation of the Orkney's */
 object Orkneys extends EArea2("Orkneys", 59.06 ll -3.15, plain)
-{
-   val north = 59.38 ll -2.88
-   val stronsayE = 59.09 ll -2.53
-   val south = 58.73 ll -2.96
-   val hoyS = 58.77 ll -3.29
-   val marwick = 59.10 ll -3.36
-   override val polygonLL = PolygonLL(north, stronsayE, south, hoyS, marwick)
+{ val north: LatLong = 59.38 ll -2.88
+  val stronsayE: LatLong = 59.09 ll -2.53
+  val south: LatLong = 58.73 ll -2.96
+  val hoyS: LatLong = 58.77 ll -3.29
+  val marwick: LatLong = 59.10 ll -3.36
+
+  override val polygonLL = PolygonLL(north, stronsayE, south, hoyS, marwick)
 }
 
+/** [[polygonLL]] graphical representation of Outer Hebrides. Depends on nothing. */
 object OuterHebrides extends EArea2("OuterHebrides", 57.83 ll -6.09, plain)
-{  val nLewis = 58.51 ll -6.26
-   val swLewis = 57.94 ll -6.47
-   val sHarris = 57.73 ll -6.97
-   val sandray = 56.88 ll -7.50
-   val wUist = 57.60 ll -7.53
-   val wLewis = 58.12 ll -7.13
+{ val nLewis: LatLong = 58.51 ll -6.26
+  val swLewis: LatLong = 57.94 ll -6.47
+  val sHarris: LatLong = 57.73 ll -6.97
+  val sandray: LatLong = 56.88 ll -7.50
+  val wUist: LatLong = 57.60 ll -7.53
+  val wLewis: LatLong = 58.12 ll -7.13
    
-   override val polygonLL = PolygonLL(nLewis, swLewis, sHarris, sandray, wUist, wLewis)
+  override val polygonLL: PolygonLL = PolygonLL(nLewis, swLewis, sHarris, sandray, wUist, wLewis)
 }
 
+/** [[polygonLL]] Graphical representation of Ireland. Depends on nothing. */
 object Ireland extends EArea2("Ireland", 53.36 ll -7.63, plain)
-{
-   val irelandSE = 52.17 ll -6.36
-   val baltimore = 51.47 ll -9.37
-   val dunquin = 52.11 ll -10.45
-   val oranmore = 53.27 ll -8.93
-   val carrowteige = 53.32 ll -9.85
-   val ardoone = 54.30 ll -10.00
-   val derkmorePoint = 54.27 ll -8.65
-   val malinBeg = 54.66 ll -8.79
-   val irelandN = 55.38 ll -7.37
-   val torHead = 55.19 ll -6.06
-   val nIrelandE = 54.48 ll -5.43
-   val dundalk = 54.01 ll -6.34
-   
-   val polygonLL = PolygonLL(irelandSE, baltimore, dunquin, oranmore, carrowteige, ardoone, derkmorePoint, malinBeg,
-         irelandN, torHead, nIrelandE, dundalk)         
+{ val north: LatLong = 55.38 ll -7.37
+  val torHead: LatLong = 55.19 ll -6.06
+  val nIrelandE: LatLong = 54.48 ll -5.43
+  val dundalk: LatLong = 54.01 ll -6.34
+  val irelandSE: LatLong = 52.17 ll -6.36
+  val baltimore: LatLong = 51.47 ll -9.37
+  val dunquin: LatLong = 52.11 ll -10.45
+  val oranmore: LatLong = 53.27 ll -8.93
+  val carrowteige: LatLong = 53.32 ll -9.85
+  val ardoone: LatLong = 54.30 ll -10.00
+  val derkmorePoint: LatLong = 54.27 ll -8.65
+  val malinBeg: LatLong = 54.66 ll -8.79
+
+  val polygonLL = PolygonLL(north, torHead, nIrelandE, dundalk,irelandSE, baltimore, dunquin, oranmore, carrowteige, ardoone, derkmorePoint, malinBeg)
 }
 
+/** [[polygonLL]] Graphical representation of Shetland. Depends on nothing. */
 object Shetland extends EArea2("Shetland", 60.34 ll -1.23, plain)
-{
-   val south = 59.85 ll -1.27
-   val sSoundsound = 60.20 ll -1.34
-   val sSilwick = 60.14 ll -1.46
-   val papaStour = 60.33 ll -1.73
-   val eshaness = 60.48 ll -1.63
-   val nUnst = 60.84 ll -0.87
-   val eWhalsay = 60.38 ll -0.90
+{ val south = 59.85 ll -1.27
+  val sSoundsound = 60.20 ll -1.34
+  val sSilwick = 60.14 ll -1.46
+  val papaStour = 60.33 ll -1.73
+  val eshaness = 60.48 ll -1.63
+  val nUnst = 60.84 ll -0.87
+  val eWhalsay = 60.38 ll -0.90
    
-   val polygonLL = PolygonLL(south, sSoundsound, sSilwick, papaStour, eshaness, nUnst, eWhalsay)
+  override val polygonLL = PolygonLL(south, sSoundsound, sSilwick, papaStour, eshaness, nUnst, eWhalsay)
 }
