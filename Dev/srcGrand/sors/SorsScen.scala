815606aa587f7897ce12f2aaa049d72ade002a4b
Rich2 <rr1@tutanota.com>
2023-02-15 22:18:14 +0000

    #211 Started creating Sors Imperiorum

---
+++ Dev/srcGrand/sors/SorsScen.scala
@@ -0,0 +1,32 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package sors
+import prid._, phex._, egrid._, eg320._, pEarth._
+
+trait SorsScen extends HSysTurnScen
+{ def title: String = "DLessScen"
+  val terrs: HCenLayer[WTile]
+  val sTerrs: HSideOptLayer[WSide]
+  val corners: HCornerLayer
+}
+
+/** Scenario 1  for SorsScen. */
+object SorsScen1 extends SorsScen
+ {
+  override def turn: Int = 0
+
+  override implicit val gridSys: EGrid320LongMulti = new EGrid320LongMulti { ThisSys =>
+    override val grids: RArr[EGridLongFull] = EGrid320.grids(2, 0, 124)
+
+    override def headGridInt: Int = 0
+
+    override def gridsXSpacing: Double = 40
+
+    override val gridMans: RArr[EGridLongMan] = iToMap(1)(EGridLongMan(_, ThisSys))
+
+    override def adjTilesOfTile(tile: HCen): HCenArr = ???
+  }
+
+  override val terrs: HCenLayer[WTile] = fullTerrsHCenLayerSpawn
+  override val sTerrs: HSideOptLayer[WSide] = fullTerrsSideOptLayerSpawn
+  override val corners: HCornerLayer = fullTerrsCornerLayerSpawn
+}
---
+++ Dev/srcGrand/sors/packageSors.scala
@@ -0,0 +1,8 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat;
+
+/** Sors Imperiorum a game at a 320km scale. */
+package object sors
+{
+  def packageDescrip: String = "A game where empires appear at set times accoridng to history."
+}
