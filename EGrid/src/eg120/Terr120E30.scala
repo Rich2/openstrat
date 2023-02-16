27c77ccefbbeb7e2d730ef4407b5211dc5230400
Rich2 <rr1@tutanota.com>
2023-02-16 19:33:46 +0000

    120km Greece terrain.

--- EGrid/src/eg120/Terr120E30.scala
+++ EGrid/src/eg120/Terr120E30.scala
@@ -4,7 +4,7 @@ import pEarth._, prid._, phex._, WTile._
 
 object Terr120E30 extends Long120Terrs
 {
-  override implicit val grid: EGrid120LongFull = EGrid120.e30(348)
+  override implicit val grid: EGrid120LongFull = EGrid120.e30(274)
 
   override val terrs: HCenLayer[WTile] =
   { val res: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
@@ -13,6 +13,15 @@ object Terr120E30 extends Long120Terrs
 
     wr(350, sea, mtain, hillTundra * 2, sea * 5)
     wr(348, mtain * 2, tundra * 4, sea * 3)
+
+    wr(286, hills, plain, hills * 3, sea, hills * 2, hills * 13)
+    wr(284, hills, sea * 2, hills * 3, sea * 2, hills * 13)
+    wr(282, hills, sea * 2, hills * 3, sea * 2, hills * 13)
+    wr(280, sea * 4, hills * 2, sea * 2, hills * 14)
+    wr(278, sea * 5, hills, sea * 9, hills * 7)
+    wr(276, sea * 6, hills * 2, sea * 7, hills * 7)
+    wr(274, sea * 8, sea * 7, hills * 7)
+
     res
   }
 
