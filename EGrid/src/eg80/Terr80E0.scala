d8144c4f6a89eb465369b15d925ce2008eeddddb
Rich2 <rr1@tutanota.com>
2023-02-20 14:12:15 +0000

    #195 Finished E0 terrain down to Rome.

--- EGrid/src/eg80/Terr80E0.scala
+++ EGrid/src/eg80/Terr80E0.scala
@@ -6,7 +6,7 @@ import pEarth._,prid._, phex._, WTile._, egrid._
  *  in base 32. The c offset for North East Europe will be 1536 or 1G0 in base 32. Current y offset is 300 for the equator. The Old c offset was 200 so a diff of 312 */
 object Terr80E0 extends Long80Terrs
 {
-  implicit val grid: EGrid80LongFull = EGrid80.e0(438)
+  implicit val grid: EGrid80LongFull = EGrid80.e0(434)
 
   override val terrs: HCenLayer[WTile] =
   {
@@ -55,7 +55,8 @@ object Terr80E0 extends Long80Terrs
     wr(442, sea * 12, plain * 2, hills * 3, mtain, hills, mtain * 2, hills, mtain * 3, hills, plain, sea * 2)
     wr(440, sea * 13, plain * 3, hills, mtain, hills * 3, mtain * 2, sea * 2, hills, mtain, hills, sea * 2)
     wr(438, sea * 5, hills * 3, mtain * 3, hills, mtain * 2, hills * 4, sea * 2, hills * 2, sea * 4, hills * 3, sea)
-
+    wr(436, sea * 5, hills * 3, plain * 3, desert * 2, hills, mtain * 4, sea * 5, hills, sea * 2, hills * 2, mtain, hills)
+    wr(434, sea * 6, hills * 2, plain, desert, hillDesert, plain, hills, plain * 2, hills * 3, sea * 6, hills, sea * 2, hills * 2, mtain, hills)
     res
   }
 
