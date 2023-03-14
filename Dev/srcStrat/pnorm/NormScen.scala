0e249333212f76e1d0929be558f5653197f5ddb3
Rich2 <rr1@tutanota.com>
2023-03-14 17:53:21 +0000

    #226 Initial creation.

---
+++ Dev/srcStrat/pnorm/NormScen.scala
@@ -0,0 +1,8 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package pnorm
+import prid.phex._
+
+trait NormScen
+{
+  val grid: HGrid
+}
---
+++ Dev/srcStrat/pnorm/packageNorm.scala
@@ -0,0 +1,8 @@
+ /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat;
+
+/** A game set in Normandy 1944, at a 1km scale. Origin will be 49 north 1 degree West. */
+package object pnorm
+{
+  def packageDescrip: String = "An operational strategy for Normandy 1944, at a 1km scale."
+}
