398a6fc4ab4494e6502621e39cf5980c13c536ef
Rich2 <rr1@tutanota.com>
2023-03-15 11:18:29 +0000

    #226.

--- Dev/srcStrat/pnorm/Terr.scala
+++ Dev/srcStrat/pnorm/Terr.scala
@@ -1,6 +1,6 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pnorm
-import Colour._
+import prid.phex._, Colour._
 
 trait Terr extends Coloured
 {
@@ -13,4 +13,11 @@ object Sea extends Terr
 
 object Plain extends Terr
 { override def colour: Colour = LightGreen
+}
+
+object Island extends Terr with HIslandColoured[Sea.type, Plain.type]
+{
+  override def outer: Sea.type = Sea
+
+  override def inner: Plain.type = Plain
 }
---
+++ Tiling/srcHex/HIsland.scala
@@ -0,0 +1,12 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package prid; package phex
+
+/** Exploratory trait. Will not pursue further for now. */
+trait HIsland[OT, IT]
+{ def outer: OT
+  def inner: IT
+}
+
+trait HIslandColoured[OT <: Coloured, IT] extends HIsland[OT, IT] with Coloured
+{ override def colour: Colour = outer.colour
+}
