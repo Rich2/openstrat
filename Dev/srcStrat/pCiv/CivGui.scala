55bb6f3e904ee1cb555a23222fbb11f0b2b06703
Rich2 <rr1@tutanota.com>
2023-03-18 01:11:08 +0000

    #227 Started on Sides.

--- Dev/srcStrat/pCiv/CivGui.scala
+++ Dev/srcStrat/pCiv/CivGui.scala
@@ -7,6 +7,7 @@ case class CivGui(canv: CanvasPlatform, scen: CivScen) extends HGridSysGui("Civ 
 { statusText = "Welcome to Civ Rise."
   implicit val gridSys: HGridSys = scen.gridSys
   val terrs: HCenLayer[VTile] = scen.terrs
+  val sTerrs: HSideOptLayer[VSide] = scen.sTerrs
   val corners: HCornerLayer = scen.corners
   val lunits: HCenArrLayer[Warrior] = scen.lunits
 
@@ -20,7 +21,7 @@ case class CivGui(canv: CanvasPlatform, scen: CivScen) extends HGridSysGui("Civ 
 
     val sls = proj.sidesDraw()
 
-    def unitFills = lunits.gridHeadsMap { (hc, lu) =>
+    def unitFills: RArr[PolyCurveParentFull] = lunits.gridHeadsMap { (hc, lu) =>
       Rectangle.curvedCornersCentred(120, 80, 3, hc.toPt2).parentAll(lu, lu.colour, 2, lu.colour.contrast, 16, 4.toString)
     }
 
@@ -43,7 +44,6 @@ case class CivGui(canv: CanvasPlatform, scen: CivScen) extends HGridSysGui("Civ 
     case (_, _, h) => deb("Other; " + h.toString)
   }
 
-
   def thisTop(): Unit = reTop(proj.buttons)
 
   proj.getFrame = () => frame
@@ -53,6 +53,5 @@ case class CivGui(canv: CanvasPlatform, scen: CivScen) extends HGridSysGui("Civ 
   }
 
   thisTop()
-
   repaint()
 }
--- Dev/srcStrat/pCiv/CivScen.scala
+++ Dev/srcStrat/pCiv/CivScen.scala
@@ -1,13 +1,14 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pCiv
-import prid._, phex._, pgui._
+import prid._, phex._
 
 /** A Civ scenario turn state. */
 trait CivScen  extends HSysTurnScen
 { override def title: String = "Civ Scenario"
 
   /** tile terrain. */
-  def terrs: HCenLayer[VTile]
+  val terrs: HCenLayer[VTile]
+  val sTerrs: HSideOptLayer[VSide]
   val corners: HCornerLayer
   val lunits: HCenArrLayer[Warrior]
 }
@@ -17,14 +18,13 @@ trait CivScenStart extends CivScen
 { override def turn: Int = 0
 }
 
-
 /** Civ scenario 1. */
 object Civ1 extends CivScenStart
-{
-  override implicit val gridSys: HGrid = HGridReg(2, 12, 4, 40)
+{ override implicit val gridSys: HGrid = HGridReg(2, 12, 4, 40)
   val terrs: HCenLayer[VTile] = gridSys.newHCenLayer[VTile](Land())
   terrs.setRowEnd(12, 20, Land(Hill), Land(Mountain) * 2, Land() * 3)
   terrs.setRowEnd(4, 4, Land(Hill) * 3, Land(Plain) * 7)
+  override val sTerrs: HSideOptLayer[VSide] = gridSys.newSideOpts[VSide]
   val lunits: HCenArrLayer[Warrior] = gridSys.newHCenArrLayer[Warrior]
   lunits.set(10, 18, Warrior(Uruk))
   lunits.set(6, 10, Warrior(Eridu))
@@ -37,6 +37,7 @@ object Civ2 extends CivScenStart
 { override val title: String = "CivRise Scen 2"
   override implicit val gridSys: HGrid = HGridReg(2, 12, 4, 40)
   val terrs: HCenLayer[VTile] = gridSys.newHCenLayer[VTile](Sea)
+  override val sTerrs: HSideOptLayer[VSide] = gridSys.newSideOpts[VSide]
   terrs.setRowEndUnchecked(4, Land(Mountain) * 3, Land(Plain) * 2)
   terrs.setRow(10, Land(), Head3Land(2), Sea * 4, Island() * 2, Sea)
   terrs.setRow(8, Land(Plain) * 4, Head1Land(4), Sea * 3, Land(), Sea)
