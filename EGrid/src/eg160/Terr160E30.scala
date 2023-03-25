aa48d9268254b647e3ee638f1c12179c64888407
Rich2 <rr1@tutanota.com>
2023-03-25 16:01:40 +0000

    ##228 Converting 160km E30 to helper.

--- EGrid/src/eg160/Terr160E30.scala
+++ EGrid/src/eg160/Terr160E30.scala
@@ -5,55 +5,54 @@ import prid._, phex._, egrid._, WTile._
 /** Terrain at 160km for 30E. Land and sea should be correct, but elevation has not been checked. */
 object Terr160E30 extends Long160Terrs
 { override implicit val grid: EGrid160LongFull = EGrid160.e30(256)
-
-  override val terrs: HCenLayer[WTile] =
-  { val res: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
-    def gs(r: Int, cStart: Int, tileValues: Multiple[WTile]*): Unit = { res.setRowEnd(r, cStart, tileValues :_*); () }
-    def wr(r: Int, tileValues: Multiple[WTile]*): Unit = { res.setRow(r, tileValues: _*); () }
-
-    wr(320, sea * 5)
-    wr(318, sea * 5)
-    wr(316, sea * 6)
-    wr(314, sea * 6)
-    wr(312, sea, tundraHills * 3, sea * 3)
-    wr(310, taigaHills * 2, taiga * 3, sea, taiga)
-    wr(308, taigaHills, taiga * 7)
-    wr(306, taiga * 5, sea, taiga * 2)
-    wr(304, taiga, sea, taiga * 6)
-    wr(302, taiga, sea, taiga * 7)
-    wr(300, taiga, sea, taiga * 7)
-    wr(298, taiga, sea, taiga * 3, lake, taiga * 4)
-    wr(296, taiga, sea * 3, taiga * 6)
-    wr(294, plain, sea, plain * 7, taiga)
-    wr(292, plain, sea, plain * 9)
-    wr(290, sea, plain * 10)
-    wr(288, sea * 2, plain * 9)
-    wr(286, plain * 12)
-    wr(284, plain * 3, forest * 2, plain * 7)
-    wr(282, plain * 4, forest * 2, plain * 7)
-    wr(280, hills, plain * 12)
-    wr(278, hills * 3, plain * 10)
-    wr(276, plain, hills, plain * 12)
-    wr(274, hills * 2, plain, hills * 3, plain * 4, sea, plain * 3)
-    wr(272, hills, plain, hills * 2, plain * 2, plain, sea, plain * 2, plain * 4)
-    wr(270, hills * 3, plain * 4, sea * 5, mtain, hills, plain)
-    wr(268, sea, mtain, hills * 3, plain * 2, sea * 6, plain, mtain)
-    wr(266, sea, mtain, hills * 3, plain * 2, hills * 8)
-
-    wr(264, hills * 2, hills * 2, sea, hills * 10)
-    wr(262, hills, sea * 2, hills * 2, sea, hills * 10)
-    wr(260, sea * 3, hills, sea * 2, hills * 10)
-    gs(258, 1550, hills * 5)
-    wr(256, sea * 5, hills * 2, sea * 4, hills * 6)
-
-    res
-  }
+  override val terrs: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
 
   override val sTerrs: HSideLayer[WSide] =
   { val res: HSideLayer[WSide] = grid.newSideLayer[WSide](WSideNone)
-    res.setSomeInts(WSideMid(), 293,1525,  293,1527,  303,1543,  304,1542,  304,1546,  305,1545,  305,1547,  306,1548,  307,1549,  308,1550,  309,1549)
+    //res.setSomeInts(WSideMid(), 293,1525,  293,1527,  303,1543,  304,1542,  304,1546,  305,1545,  305,1547,  306,1548,  307,1549,  308,1550,  309,1549)
     res
   }
 
   override val corners: HCornerLayer = grid.newHVertOffsetLayer
+
+
+  val help = new WTerrSetter(grid, terrs, sTerrs, corners) {
+    override val rowDatas: RArr[RowBase] = RArr(
+      TRow(320, sea * 5),
+      TRow(318, sea * 5),
+      TRow(316, sea * 6),
+      TRow(314, sea * 6),
+      TRow(312, sea, tundraHills * 3, sea * 3),
+      TRow(310, taigaHills * 2, taiga * 3, sea, taiga),
+      TRow(308, taigaHills, taiga * 7),
+      TRow(306, taiga * 5, sea, taiga * 2),
+      TRow(304, taiga, sea, taiga * 6),
+      TRow(302, taiga, sea, taiga * 7),
+      TRow(300, taiga, sea, taiga * 7),
+      TRow(298, taiga, sea, taiga * 3, lake, taiga * 4),
+      TRow(296, taiga, sea * 3, taiga * 6),
+      TRow(294, plain, sea, plain * 7, taiga),
+      TRow(292, plain, sea, plain * 9),
+      TRow(290, sea, plain * 10),
+      TRow(288, sea * 2, plain * 9),
+      TRow(286, plain * 12),
+      TRow(284, plain * 3, forest * 2, plain * 7),
+      TRow(282, plain * 4, forest * 2, plain * 7),
+      TRow(280, hills, plain * 12),
+      TRow(278, hills * 3, plain * 10),
+      TRow(276, plain, hills, plain * 12),
+      TRow(274, hills * 2, plain, hills * 3, plain * 4, sea, plain * 3),
+      TRow(272, hills, plain, hills * 2, plain * 2, plain, sea, plain * 2, plain * 4),
+      TRow(270, hills * 3, plain * 4, sea * 5, mtain, hills, plain),
+      TRow(268, sea, Head2Land(3, Mountains), hills * 3, plain * 2, sea * 6, plain, mtain),
+      TRow(266, Head2Land(0), Head2Land(4, Mountains), hills * 3, plain * 2, hills * 8),
+      VRow(265, MouthUp(1510)),
+      TRow(264, hills, Head4Land(1), hills * 2, sea, Head2Land(4, Hilly),  hills * 9),
+      TRow(262, hills, sea * 2, hills * 2, sea, hills, hills * 4, hills * 5),
+      TRow(260, sea * 3, hills, sea * 2, hills * 10),
+      TRow(258, sea * 11, hills * 5),
+      TRow(256, sea * 5, hills * 2, sea * 3, Island(Hilly), hills * 6),
+    )
+  }
+  help.run
 }
