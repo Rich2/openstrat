6f771b517b63d9a0db10ca3b0bbb367f87d9170f
Rich2 <rr1@tutanota.com>
2023-04-01 10:54:23 +0100

    #229 Convering more 160km grids.

--- EGrid/src/eg160/Terr160E120.scala
+++ EGrid/src/eg160/Terr160E120.scala
@@ -4,38 +4,25 @@ import prid._, phex._, egrid._, WTile._
 
 /** Terrain for 160km 120 west. The terrain here is only very rough first approximation. */
 object Terr160E120 extends Long160Terrs
-{
-  override implicit val grid: EGrid160LongFull = EGrid160.e120(252, 272)
-
-  override val terrs: HCenLayer[WTile] =
-  { val res: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
-    def wr(r: Int, tileValues: Multiple[WTile]*): Unit = { res.setRow(r, tileValues :_*); () }
-
-    wr(272, desert * 3, desertHills * 3, hills, desert, plain * 2, hills * 2, plain, forestHills)
-    wr(270, plain * 14, sea)
-    wr(268, plain * 13, sea * 2)
-    wr(266, plain * 12, sea * 3)
-    wr(264, plain * 12, sea * 3)
-    wr(262, plain * 7, sea * 4, hills * 2, sea * 3)
-    wr(260, plain * 9, sea, hills * 3, sea * 3)
-    wr(258, plain * 9, sea * 2, hills * 2, sea * 3)
-    wr(256, plain * 8, sea * 4, hills * 2, sea, hills * 2)
-    wr(254, plain * 8, sea * 6, hills * 3)
-    wr(252, plain * 9, sea * 5, hills * 2, sea)
-    res
-  }
-
-  override val sTerrs: HSideLayer[WSide] =
-  { val res: HSideLayer[WSide] = grid.newSideLayer[WSide](WSideNone)
-    res.setSomeInts(WSideMid(), 264,4610)
-    res
-  }
-
+{ override implicit val grid: EGrid160LongFull = EGrid160.e120(252, 272)
+  override val terrs: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
+  override val sTerrs: HSideLayer[WSide] = grid.newSideLayer[WSide](WSideNone)
   override val corners: HCornerLayer = grid.newHVertOffsetLayer
+
   val help = new WTerrSetter(grid, terrs, sTerrs, corners)
   {
     override val rowDatas: RArr[RowBase] = RArr(
-
+      TRow(272, desert * 3, desertHills * 3, hills, desert, plain * 2, hills * 2, plain, forestHills),
+      TRow(270, plain * 14, sea),
+      TRow(268, plain * 13, sea * 2),
+      TRow(266, plain * 12, sea * 3),
+      TRow(264, plain * 12, sea * 3),
+      TRow(262, plain * 7, sea * 4, hills * 2, sea * 3),
+      TRow(260, plain * 9, sea, hills * 3, sea * 3),
+      TRow(258, plain * 9, sea * 2, hills * 2, sea * 3),
+      TRow(256, plain * 8, sea * 4, hills * 2, sea, hills * 2),
+      TRow(254, plain * 8, sea * 6, hills * 3),
+      TRow(252, plain * 9, sea * 5, hills * 2, sea),
     )
   }
   help.run
--- EGrid/src/eg160/Terr160W90.scala
+++ EGrid/src/eg160/Terr160W90.scala
@@ -4,34 +4,27 @@ import prid._, phex._, egrid._, WTile._
 
 /** Terrain for 160km 90 west. */
 object Terr160W90 extends Long160Terrs
-{
-  override implicit val grid: EGrid160LongFull = EGrid160.w90(314)
-
-  override val terrs: HCenLayer[WTile] =
-  { val res: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
-    def wr(r: Int, tileValues: Multiple[WTile]*): Unit = { res.setRow(r, tileValues :_*); () }
-    def gs(r: Int, cStart: Int, tileValues: Multiple[WTile]*): Unit = { res.setRowEnd(r, cStart, tileValues :_*); () }
-
-    wr(320, sea * 2, tundraHills * 2, sea)
-    wr(318, tundraHills * 4, sea)
-    wr(316, tundraHills * 2, sea, tundraHills * 3)
-    wr(314, sea, tundraHills, sea, tundraHills, tundra, tundraHills)
-//    gs(312, 11764, ice * 5, sea * 2)
-//    gs(310, 11766, ice * 4, sea * 3)
-//    gs(308, 11764, ice * 3, sea * 5)
-//    gs(306, 11762, ice * 2, sea * 4, hills * 2)
-//    gs(304, 11764, ice, sea * 4, tundra * 3)
-//    gs(302, 11762, ice, sea * 5, tundra * 3)
-//    gs(300, 11760, tundra, sea * 8)
-//    gs(298, 11758, tundra, sea * 9)
-    res
-  }
-
-  override val sTerrs: HSideLayer[WSide] =
-  { val res: HSideLayer[WSide] = grid.newSideLayer[WSide](WSideNone)
-    res.setSomeInts(WSideMid(), 315,9721,  316,9722, 318,9724,  318,9728,  318,9732,  319,9727,  319,9729,  319,9731,  319,9733)
-    res
-  }
-
+{ override implicit val grid: EGrid160LongFull = EGrid160.w90(314)
+  override val terrs: HCenLayer[WTile] = grid.newHCenLayer[WTile](sea)
+  override val sTerrs: HSideLayer[WSide] = grid.newSideLayer[WSide](WSideNone)
   override val corners: HCornerLayer = grid.newHVertOffsetLayer
+
+  val help = new WTerrSetter(grid, terrs, sTerrs, corners)
+  {
+    override val rowDatas: RArr[RowBase] = RArr(
+      TRow(320, sea * 2, tundraHills * 2, sea),
+      TRow(318, tundraHills * 4, sea),
+      TRow(316, tundraHills * 2, sea, tundraHills * 3),
+      TRow(314, sea, tundraHills, sea, tundraHills, tundra, tundraHills),
+      //    TRow(312, 11764, ice * 5, sea * 2),
+      //    TRow(310, 11766, ice * 4, sea * 3),
+      //    TRow(308, 11764, ice * 3, sea * 5),
+      //    TRow(306, 11762, ice * 2, sea * 4, hills * 2),
+      //    TRow(304, 11764, ice, sea * 4, tundra * 3),
+      //    TRow(302, 11762, ice, sea * 5, tundra * 3),
+      //    TRow(300, 11760, tundra, sea * 8),
+      //    TRow(298, 11758, tundra, sea * 9),
+    )
+  }
+  help.run
 }
