fa94e56f4ee57451884380cbb6a50dba24e05689
Rich2 <rr1@tutanota.com>
2023-04-12 21:45:31 +0100

    Red Sea.

--- Earth/srcPts/middleEast/MiddleEastSouth.scala
+++ Earth/srcPts/middleEast/MiddleEastSouth.scala
@@ -4,7 +4,7 @@ import geom._, pglobe._, LatLong._
 
 /** [[PolygonLL]] graphic for the Levant depends on [[Kurdistan]] and [[Sinai]]. */
 object Levant extends EArea2("Levant", 33 ll 35.5, Hilly)
-{ val damascus = 33.51 ll 36.82
+{ val damascus: LatLong = 33.51 ll 36.82
   val ashkelon = 31.68 ll 34.56
   val haifa = 32.83 ll 34.98
   val p50 = 35.58 ll 35.72
@@ -18,20 +18,22 @@ object Arabia extends EArea2("Arabia", degs (25, 45.0), Desert)
 { val salwa = 24.71 ll 50.77
   val nQatar = 26.15 ll 51.26
   val doha = 25.25 ll 51.61
-  val alGharbia = degs(23.97, 51.94)
-  val icad = 24.30 ll 54.44
-  val kumzar = 26.36 ll 56.38
+  val alGharbia: LatLong = 23.97 ll 51.94
+  val icad: LatLong = 24.30 ll 54.44
+  val kumzar: LatLong = 26.36 ll 56.38
   val alKhaburah = 23.98 ll 57.10
-  val eOman = degs(22.48, 59.72)
+  val eOman = 22.48 ll 59.72
   val mirbat = 16.94 ll 54.80
   val dhahawnMouth = 16.20 ll 52.23
   val haswayn = 15.63 ll 52.22
-  val sYemen = degs(12.65, 43.98)
-  val sharmas = degs(28.03, 35.23)
-  val alFaw = degs(29.93, 48.47)
+  val sYemen = 12.65 ll 43.98
+  val p75 = 16.42 ll 42.77
+  val p80 = 21.31 ll 39.10
+  val sharmas = 28.03 ll 35.23
+  val alFaw = 29.93 ll 48.47
 
   override val polygonLL: PolygonLL = PolygonLL(alFaw, salwa, nQatar, doha, alGharbia, icad, kumzar, alKhaburah, eOman, mirbat,
-    dhahawnMouth, haswayn, sYemen, sharmas, pMed.Sinai.eilat)
+    dhahawnMouth, haswayn, sYemen, p75, p80, sharmas, pMed.Sinai.eilat)
 }
 
 /** [[PolygonLL]] graphic for Persia. Depends on [[Caspian]] and [[pAsia.India]]. */
