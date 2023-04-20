d2ab5e6dca2ab59d7ce0a975b972e96aa1483f7c
Rich2 <rr1@tutanota.com>
2023-04-20 11:28:15 +0100

    #239 Converted to 320km.

--- Dev/srcStrat/pww2/WW2Scen.scala
+++ Dev/srcStrat/pww2/WW2Scen.scala
@@ -1,6 +1,6 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pww2
-import prid._, phex._, egrid._, eg220._, pEarth._
+import prid._, phex._, egrid._, eg320._
 
 /** Scenario for World War 2 grand strategy game. */
 trait WW2Scen extends HSysTurnScen
@@ -24,8 +24,8 @@ trait WW2Scen extends HSysTurnScen
     val armiesNew: HCenOptLayer[Army] = armies.clone
     targets.foreach { (hc2, buff) => buff.foreachLen1(stCenStep => if (armies.tileNone(hc2)) armiesNew.moveMut(stCenStep.startHC, hc2)) }
 
-    new WW2Scen {
-      override implicit val gridSys: EGridSys = ThisScen.gridSys
+    new WW2Scen
+    { override implicit val gridSys: EGridSys = ThisScen.gridSys
       override val terrs: HCenLayer[WTile] = ThisScen.terrs
       override val sTerrs: HSideOptLayer[WSide, WSideSome] = ThisScen.sTerrs
       override val corners: HCornerLayer = ThisScen.corners
@@ -39,35 +39,37 @@ trait WW2Scen extends HSysTurnScen
 object WW2Scen
 {
   def sa0(layer: HCenOptLayer[Army])(implicit sys: HGridSys): Unit =
-  { val polities: RArr[Polity] = RArr(Britain, France, Germany)
-    implicit val counters: ArrCounters[Polity] = ArrCounters(polities)
-    layer.setFSomesMut(Germany.armyNext, 162,522,  160,520,  158,518,  156,520)
-    layer.setFSomesMut(France.armyNext, 156,518, 154,518)
-    layer.setFSomesMut(Britain.armyNext, 160,512,  158,514)
+  {
   }
 }
 
 object WW2Scen1 extends WW2Scen
 { override def turn: Int = 0
 
-  override implicit val gridSys: EGrid220LongFull = Terr220E0.grid
-  override val terrs: HCenLayer[WTile] = Terr220E0.terrs
-  override val sTerrs: HSideOptLayer[WSide, WSideSome] = Terr220E0.sTerrs
-  override val corners: HCornerLayer = Terr220E0.corners
+  override implicit val gridSys: EGrid320LongMulti =  Scen320All.gridSys
+  override val terrs: HCenLayer[WTile] = Scen320All.terrs
+  override val sTerrs: HSideOptLayer[WSide, WSideSome] = Scen320All.sTerrs
+  override val corners: HCornerLayer = Scen320All.corners
 
   val armies: HCenOptLayer[Army] = gridSys.newHCenOptLayer
-  WW2Scen.sa0(armies)
+  val polities: RArr[Polity] = RArr(Britain, France, Germany)
+  implicit val counters: ArrCounters[Polity] = ArrCounters(polities)
+  armies.setFSomesMut(Germany.armyNext, 146,1534,  144,1532,  142,1534,  140,1536)
+  //    layer.setFSomesMut(France.armyNext, 156,518, 154,518)
+  armies.setFSomesMut(Britain.armyNext, 142,514)
+
+  //WW2Scen.sa0(armies)
 }
 
 object WW2Scen2 extends WW2Scen
 { override def turn: Int = 0
 
-  override implicit val gridSys: Grids220S0E1.type = Grids220S0E1
-  override val terrs: HCenLayer[WTile] = fullTerrsHCenLayerSpawn
-  override val sTerrs: HSideOptLayer[WSide, WSideSome] = fullTerrsSideLayerSpawn
-  override val corners: HCornerLayer = fullTerrsCornerLayerSpawn
+  override implicit val gridSys: EGrid320LongFull = Terr320E0.grid
+  override val terrs: HCenLayer[WTile] = Terr320E0.terrs
+  override val sTerrs: HSideOptLayer[WSide, WSideSome] = Terr320E0.sTerrs
+  override val corners: HCornerLayer = Terr320E0.corners
 
   val armies: HCenOptLayer[Army] = gridSys.newHCenOptLayer
-  WW2Scen.sa0(armies)
+  //WW2Scen.sa0(armies)
   //armies.unsafeSetSames(Japan.ar, 136,5624)
 }
