a4adbe6cb006263ae7a242ad0b146f093819db49
Rich2 <rr1@tutanota.com>
2023-04-26 21:57:58 +0100

    More conversion of Game 2Hex to PlayerState.

--- Tiling/ExsSrc/gTwo/h2p/G2HExamples.scala
+++ Tiling/ExsSrc/gTwo/h2p/G2HExamples.scala
@@ -7,9 +7,9 @@ object G2HScen1 extends G2HScen
 { override def turn: Int = 0
   implicit val gridSys: HGridReg = GSys.g1
   val playerStates: HCenOptLayer[PlayerState] = HCenOptLayer()
- // playerStates.setSomeMut(6, 2, PlayerA)
+  playerStates.setSomeMut(6, 2, PlayerState(PlayerA, HexDR))
 //  playerStates.setSomeMut(4, 4, PlayerA)
-//  playerStates.setSomesMut((4, 8, PlayerB), (6, 10, PlayerC))
+  playerStates.setSomesMut((4, 8, PlayerState(PlayerB)), (6, 10, PlayerState(PlayerC)))
 
 //  override def playerOrders: HDirnPathPairArr[Player] = HDirnPathPairArr[Player](PlayerA.hPath(6, 2, HexDR, HexUL), PlayerA.hPath(4, 4, HexDR, HexLt),
 //    PlayerB.hPath(4, 8, HexDL, HexRt), PlayerC.hPath(6, 10, HexLt, HexDR, HexDL))
@@ -20,7 +20,7 @@ object G2HScen2 extends G2HScen
 { override def turn: Int = 0
   implicit val gridSys: HGridReg = HGridReg(2, 10, 4, 8)
   val playerStates: HCenOptLayer[PlayerState] = HCenOptLayer()
- // playerStates.setSomesMut((4, 4, PlayerA), (8, 4, PlayerB), (6, 6, PlayerC))
+  playerStates.setSomesMut((4, 4, PlayerState(PlayerA)), (8, 4, PlayerState(PlayerB)), (6, 6, PlayerState(PlayerC)))
 }
 
 /** 3rd example Turn 0 scenario state for Game Three. */
@@ -28,13 +28,13 @@ object G2HScen3 extends G2HScen
 { override def turn: Int = 0
   implicit val gridSys: HGrid = HGridIrr(10, (1, 6), (2, 4), (3, 2), (2, 4), (1, 6))
   val playerStates: HCenOptLayer[PlayerState] = HCenOptLayer()
- // playerStates.setSomesMut((4, 4, PlayerA), (10, 6, PlayerB), (8, 8, PlayerC))
+  playerStates.setSomesMut((4, 4, PlayerState(PlayerA)), (10, 6, PlayerState(PlayerB)), (8, 8, PlayerState(PlayerC)))
 }
 
 object G2HScen4 extends G2HScen
 { override def turn: Int = 0
   implicit val gridSys: HGridReg = HGridReg(2, 12, 2, 60)
   val playerStates: HCenOptLayer[PlayerState] = HCenOptLayer()
- // playerStates.setSomeMut(4, 4, PlayerA)
-//  playerStates.setSomesMut((4, 8, PlayerB), (6, 10, PlayerC))
+  playerStates.setSomeMut(4, 4, PlayerState(PlayerA))
+  playerStates.setSomesMut((4, 8, PlayerState(PlayerB)), (6, 10, PlayerState(PlayerC)), (8, 32, PlayerState(PlayerD)))
 }
