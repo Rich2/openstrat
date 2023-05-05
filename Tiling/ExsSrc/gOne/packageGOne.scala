ab6b8abb685d471c1fd846a15ad5408dd901b666
Rich2 <rr1@tutanota.com>
2023-05-05 15:54:26 +0100

    ScalaDocs

--- Tiling/ExsSrc/gOne/packageGOne.scala
+++ Tiling/ExsSrc/gOne/packageGOne.scala
@@ -1,17 +1,15 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 
 /** This is the package for the first example of an extremely simple simultaneous turn tile based game. The game is created purely to illustrate and
  * test fundamental algorithm and software patterns. It is not intended as an interesting game to play. Each player can move one hex tile step. Any
- * move to a tile already containing a player or that one more than one player is attempting to move to fails.his is the package for the second
- * example of simple simultaneous turn tile based games. It differs from the first in that it is on a square grid
- * and adjacent moves take priority over diagonal tile steps. */
+ * move to a tile already containing a player or that one more than one player is attempting to move to fails. */
 package object gOne
 {
   /** Package description [[String]]. */
   def aboutStr: String = """This is the package for the first example of an extremely simple simultaneous turn tile based game. The game is created
   purely to illustrate and test fundamental algorithm and software patterns. It is not intended as an interesting game to play. Each player can move
   one hex tile step. Any move to a tile already containing a player or that one more than one player is attempting to move to fails.This is the
-   package for the second example of simple simultaneous turn tile based games. It differs from the first in that it is on a square grid
-    and adjacent moves take priority over diagonal tile steps.."""
+  package for the second example of simple simultaneous turn tile based games. It differs from the first in that it is on a square grid and adjacent
+  moves take priority over diagonal tile steps."""
 }
--- Tiling/ExsSrc/gTwo/packageGTwo.scala
+++ Tiling/ExsSrc/gTwo/packageGTwo.scala
@@ -1,8 +1,9 @@
 /* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat
 
-/** This is the package for the second example of simple simultaneous turn tile based games. A hex
- *  based game like game One1, that introduces multi turn directives. */
+/** This is the package for the second example of simple simultaneous turn tile based games. It introduces multi segment / turn intentions and state
+ * for the objects that populate the game universe. That state is simply the intentions which are updated when an intention is completed. The
+ * resolution rules for Game Two are the same as Game One.  */
 package object gTwo
 {
   /**  This is just a dummy method as empty package objects can cause problems. */
