1011ef8f7f10cb38f1038342636aafa5e4ba7df5
Rich2 <rr1@tutanota.com>
2023-05-10 11:16:07 +0100

    More G2SqScen.

--- Tiling/ExsSrc/gTwo/s2p/G2SqScen.scala
+++ Tiling/ExsSrc/gTwo/s2p/G2SqScen.scala
@@ -3,6 +3,9 @@ package ostrat; package gTwo; package s2p
 import prid._, psq._, gPlay._
 
 case class PlayerState(player: Player, steps: SqStepArr)
+{ /** Returns the PlayerState with the tail of the steps. */
+  def takeStep: PlayerState = PlayerState(player, steps.tail)
+}
 
 object PlayerState
 {
@@ -10,18 +13,18 @@ object PlayerState
 }
 
 /** A scenario turn or state for Game Three. Adds in multiple turn orders which are now part of the game state. */
-/*trait G2SqScen extends SqGridScen
+trait G2SqScen extends SqGridScen
 { /** An optional player can occupy each tile. This is the only tile data in the game. */
-  def oPlayers: HCenOptLayer[Player]
+  def players: SqCenOptLayer[Player]
 
-  def playerOrders: HDirnPathPairArr[Player] = HDirnPathPairArr()
-  lazy val playersKey: HCenPairArr[Player] = oPlayers.somePairArr
+ // def playerOrders: HDirnPathPairArr[Player] = HDirnPathPairArr()
+//  lazy val playersKey: HCenPairArr[Player] = oPlayers.somePairArr
 
   /** Resolves turn. Takes a list [[RArr]] of commands consisting in this simple case of (Player, HStep) pairs. The command is passed in as a relative
    * move. This is in accordance with the principle in more complex games that the entity issueing the command may not know its real location. */
-  def endTurn(orderList: HDirnPathPairArr[Player]): G2SqScen =
+  /*def endTurn(orderList: HDirnPathPairArr[Player]): G2SqScen =
   {
-    val targets: HCenBuffLayer[HDirnPathPair[Player]] = gridSys.newHCenArrOfBuff
+    //val targets: HCenBuffLayer[HDirnPathPair[Player]] = gridSys.newHCenArrOfBuff
 
     orderList.foreach { pr =>
       val path = pr.path
@@ -46,17 +49,16 @@ object PlayerState
     }
 
     G2SqScen(turn + 1, gridSys, oPlayersNew, newOrders)
-  }
-}*/
+  }*/
+}
 
-/** Companion object for OneScen trait, contains factory apply method. */
-/*
+/** Companion object for [[G2SqScen]] trait, contains factory apply method. */
 object G2SqScen
 { /** Factory apply method for [[G2SqScen]] trait. */
-  def apply(turnIn: Int, gridIn: HGridSys, opIn: HCenOptLayer[Player], newData: HDirnPathPairArr[Player]): G2SqScen = new G2SqScen
+  /*def apply(turnIn: Int, gridIn: SqGridSys, opIn: SqCenOptLayer[Player], newData: HDirnPathPairArr[Player]): G2SqScen = new G2SqScen
   { override val turn = turnIn
     override implicit val gridSys: HGridSys = gridIn
     override def oPlayers: HCenOptLayer[Player] = opIn
     override def playerOrders: HDirnPathPairArr[Player] = newData
-  }
-}*/
+  }*/
+}
