b1b41a95ce6ea66499be52c1db03050d6e6ca43c
Rich2 <rr1@tutanota.com>
2023-05-13 20:11:43 +0100

    Created G1HGame.

---
+++ Tiling/ExsSrc/gOne/hp1/G1HGame.scala
@@ -0,0 +1,19 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package gOne; package hp1
+import prid._, phex._, gPlay._
+
+/** Game manager for G1HScen. */
+class G1HGame(startScen: G1HScen)
+{
+  var scen: G1HScen = startScen
+  implicit val gridSys: HGridSys = scen.gridSys
+
+  /** Resolves turn. Takes a list [[RArr]] of commands consisting in this simple case of (Player, HStep) pairs. The command is passed in as a relative
+   * move. This is in accordance with the principle in more complex games that the entity issuing the command may not know its real location. */
+  def endTurn(orders: HCenStepPairArr[Player]): G1HScen =
+  { val res1 = HCenOptStepLayer[Player]()
+    orders.pairForeach { (hcst, pl) => res1.setSome(hcst.startHC, hcst.step, pl) }
+    val playersNew = scen.resolve(res1)
+    G1HScen(scen.turn + 1, scen.gridSys, playersNew)
+  }
+}
--- Tiling/ExsSrc/gOne/hp1/G1HScen.scala
+++ Tiling/ExsSrc/gOne/hp1/G1HScen.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package gOne; package hp1
 import prid._, phex._, gPlay._
 
