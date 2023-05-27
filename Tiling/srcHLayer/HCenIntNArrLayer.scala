370434883004a320c71c37a15bb7b815871c7633
Rich2 <rr1@tutanota.com>
2023-05-27 08:34:00 +0100

    More builder.

--- Tiling/srcHLayer/HCenIntNArrLayer.scala
+++ Tiling/srcHLayer/HCenIntNArrLayer.scala
@@ -7,13 +7,20 @@ trait HCenArrLayerBase[A, ArrA <: Arr[A]]
 trait HCenArrBuilder[A, ArrT <: Arr[A], LayerT <: HCenArrLayerBase[A, ArrT]]
 {
   def uninitialised(length: Int): LayerT
+  def iSet(i: Int, arr: ArrT): Unit
 }
 
 object HCenArrLayerBase{
   implicit def intNEv[A <: IntNElem, ArrT <: IntNArr[A]](implicit intNArrMapBuilder: IntNArrMapBuilder[A, ArrT]): HCenArrBuilder[A, ArrT, HCenIntNArrLayer[A, ArrT]] =
     new HCenArrBuilder[A, ArrT, HCenIntNArrLayer[A, ArrT]]
   {
+    val mapBuilder: IntNArrMapBuilder[A, ArrT] = intNArrMapBuilder
     override def uninitialised(length: Int): HCenIntNArrLayer[A, ArrT] = new HCenIntNArrLayer[A, ArrT](new Array[Array[Int]](length))
+
+    override def iSet(i: Int, arr: ArrT): Unit = {
+      var j = i * mapBuilder.elemProdSize
+      ???
+    }
   }
 }
 
@@ -22,4 +29,3 @@ extends HCenArrLayerBase[A, ArrT]
 {
 
 }
-
