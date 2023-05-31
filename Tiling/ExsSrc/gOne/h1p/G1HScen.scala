f5e5e5e28544f86f662594c9479717a9fe445737
Rich2 <rr1@tutanota.com>
2023-05-31 08:46:15 +0100

    Naming and ScalaDocs for G1HScen.

--- Tiling/ExsSrc/gOne/h1p/G1HScen.scala
+++ Tiling/ExsSrc/gOne/h1p/G1HScen.scala
@@ -6,24 +6,25 @@ import prid._, phex._, gPlay._
 trait G1HScen extends HSysTurnScen
 { override def title: String = "Game 1 hex Scen"
 
-  /** An optional player can occupy each tile. This is the only tile data in the game. */
+  /** An optional counter can occupy each tile. This is the only tile data in the game. */
   def counters: HCenOptLayer[Counter]
 
+  /** The [[Counter]]s in the scenario. The data is folded into an [[Arr]]. */
   def counterSet: RArr[Counter] = counters.somesMap(c => c)
 
-    /** Contains the resolution logic. The actions are presumed to be correct. Combining and checking of actions should be done before calling this
-   *  method. */
-  def resolve(actions: HCenOptStepLayer[Counter]): HCenOptLayer[Counter] =
+    /** Contains the resolution logic. The intensions are presumed to be correct. Combining and checking of intensions should be done before calling
+     *  this method. */
+  def resolve(intensions: HCenOptStepLayer[Counter]): HCenOptLayer[Counter] =
   { val playersNew: HCenOptLayer[Counter] = counters.copy
-    val acc: HCenAccLayer[Counter] = actions.mapAcc
+    val acc: HCenAccLayer[Counter] = intensions.mapAcc
     acc.foreach{ (target, arr) => if(arr.length == 1 & counters.emptyTile(target)) playersNew.moveUnsafe(arr.headHCen, target) }
     playersNew
   }
 }
 
-/** Companion object for OneScen trait, contains factory apply method. */
+/** Companion object for [[G1HScen]], contains factory apply method. */
 object G1HScen
-{ /** Factory apply method for OneScen trait. */
+{ /** Factory apply method for [[G1HScen]] trait. */
   def apply(turnIn: Int, gridIn: HGridSys, opIn: HCenOptLayer[Counter]): G1HScen = new G1HScen
   { override val turn = turnIn
     override implicit val gridSys: HGridSys = gridIn
