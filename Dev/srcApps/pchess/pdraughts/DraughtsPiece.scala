e5c8672c8edfd0887e3200334df54b9c82894924
Rich2 <rr1@tutanota.com>
2023-06-10 18:05:59 +0100

    Draught now extends ShowSimple.

--- Dev/srcApps/pchess/pdraughts/DraughtsPiece.scala
+++ Dev/srcApps/pchess/pdraughts/DraughtsPiece.scala
@@ -2,13 +2,34 @@
 package ostrat; package pchess; package pdraughts
 import Colour._
 
-sealed trait Draught
-{ def colour: Colour}
-sealed trait WhiteDraught extends Draught { override def colour = White }
-sealed trait BlackDraught extends Draught { override def colour = Black }
+sealed trait Draught extends ShowSimple
+{ def colour: Colour
+  override def typeStr: String = "Draught"
+}
+
+sealed trait LightDraught extends Draught
+{ override def colour = White
+}
+
+sealed trait DarkDraught extends Draught
+{ override def colour = Black
+}
+
 sealed trait Man extends Draught
 sealed trait King extends Draught
-object WhiteMan extends Man with WhiteDraught
-object BlackMan extends Man with BlackDraught
-object WhiteKing extends King with WhiteDraught
-object BlackKing extends King with BlackDraught
+
+object LightMan extends Man with LightDraught
+{ override def str: String = "LightMan"
+}
+
+object DarkMan extends Man with DarkDraught
+{ override def str: String = "DarkMan"
+}
+
+object LightKing extends King with LightDraught
+{ override def str: String = "LightKing"
+}
+
+object DarkKing extends King with DarkDraught
+{ override def str: String = "DarkKing"
+}
