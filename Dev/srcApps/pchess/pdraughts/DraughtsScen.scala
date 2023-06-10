b20717d49d274690017e675b04e2f2c6c54c50a1
Rich2 <rr1@tutanota.com>
2023-06-11 00:16:50 +0100

    Started implementation of Draughts resolve method.

--- Dev/srcApps/pchess/pdraughts/DraughtsScen.scala
+++ Dev/srcApps/pchess/pdraughts/DraughtsScen.scala
@@ -1,13 +1,20 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pchess; package pdraughts
 import prid._, psq._
 
 trait DraughtsScen extends ChessLikeScen
-{ def draughts: SqCenOptLayer[Draught]
+{ def segNum: Int
+  def turn: Int = segNum / 2
+  def draughts: SqCenOptLayer[Draught]
+  def resolve(move: SqCenArr): EMon[DraughtsScen] = move match
+  { case _ if move.empty => badNone("Empty Command")
+    case _ if move.length == 1 => badNone("No move given.")
+    case _ => badNone("Default error.")
+  }
 }
 
 object DraughtsStart extends DraughtsScen
-{ val turn = 0
+{ val segNum:Int = 0
   val draughts: SqCenOptLayer[Draught] = gridSys.newSCenOptDGrider[Draught]
   def rowCens(y: Int): SqCenArr = iToMap(ife(y.div4Rem2, 2, 4), 16, 4){c => SqCen(y, c) }
   iToForeach(2, 6, 2){y => rowCens(y).foreach{r => draughts.unsafeSetSome(r, LightMan) } }
