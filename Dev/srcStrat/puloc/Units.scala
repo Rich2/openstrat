e3dec63d674c250833d5972ebc7f267dbd1fc7ff
Rich2 <rr1@tutanota.com>
2023-06-20 06:47:34 +0100

    Created 1st French Corps.

--- Dev/srcStrat/puloc/Units.scala
+++ Dev/srcStrat/puloc/Units.scala
@@ -14,7 +14,7 @@ trait Lunit
 {
   def nation: Nation
 
-  def endDate: Date = Date(1945, 5, 8)
+  def endDate: Date// = Date(1945, 5, 8)
 
   def locStarts: RArr[LocStart]
 
@@ -25,12 +25,22 @@ object DeuCp1 extends Lunit
 {
   override val nation: Nation = Germany
 
-
   override def locStarts: RArr[LocStart] = RArr(LocStart(Some(pEurope.Baltland.konigsberg), Date(1934, 10)), LocStart(None, Date(1939, 9)))
+
+  override def endDate: Date = Date(1945, 5, 8)
 }
 
 object DeuCp2 extends Lunit
 {
   override def nation: Nation = Germany
   override def locStarts: RArr[LocStart] = RArr(LocStart(None, Date(1935, 4)))
+  override def endDate: Date = Date(1945, 5, 8)
+}
+
+object FraCp1 extends Lunit
+{
+  override def nation: Nation = France
+
+  override def locStarts: RArr[LocStart] = RArr(LocStart(Some(pEurope.Frankia.calais), Date(1939, 8, 27)), LocStart(None, Date(1939, 11, 15)))
+  override def endDate: Date = Date(1945, 5, 8)
 }
--- Earth/srcPts/pEurope/EuropeNW.scala
+++ Earth/srcPts/pEurope/EuropeNW.scala
@@ -5,36 +5,34 @@ import geom._, pglobe._
 /** Alsace Lorraine, no map dependencies. */
 object Alsace extends EArea2("Alsace", 48.75 ll 7.42, Hilly)
 {
-  val northWest = 49.50 ll 5.89
-  val luxSE = 49.46 ll 6.37
-  val east = 48.97 ll 8.23
-  val basel = 47.56 ll 7.58
-  val southWest = 47.50 ll 7.13
-  val hautRhinNW = 48.31 ll 7.19
-  val moselleSW = 48.54 ll 7.07
+  val northWest: LatLong = 49.50 ll 5.89
+  val luxSE: LatLong = 49.46 ll 6.37
+  val east: LatLong = 48.97 ll 8.23
+  val basel: LatLong = 47.56 ll 7.58
+  val southWest: LatLong = 47.50 ll 7.13
+  val hautRhinNW: LatLong = 48.31 ll 7.19
+  val moselleSW: LatLong = 48.54 ll 7.07
 
-  val p10 = 49.258 ll 6.03
-  override val polygonLL = PolygonLL(northWest, luxSE, east, basel, southWest, hautRhinNW, moselleSW, p10)
+  val p10: LatLong = 49.258 ll 6.03
+  override val polygonLL: PolygonLL = PolygonLL(northWest, luxSE, east, basel, southWest, hautRhinNW, moselleSW, p10)
 }
 
-/** Blegium andLuxembourg. Depends on Alsace. */
+/** Belgium and Luxembourg. Depends on Alsace. */
 object BelgLux extends EArea2("BelgLux", 50.6 ll 4.78, Plain)
-{ val northWest = 51.09 ll 2.54
-  val coastEast = 51.36 ll 3.37
-  val north = 51.49 ll 5.04
-  val aachen = 50.78 ll 6.08
-  val p10 = 49.50 ll 5.47
+{ val northWest: LatLong = 51.09 ll 2.54
+  val coastEast: LatLong = 51.36 ll 3.37
+  val north: LatLong = 51.49 ll 5.04
+  val aachen: LatLong = 50.78 ll 6.08
+  val p10: LatLong = 49.50 ll 5.47
 
-  override val polygonLL = PolygonLL(northWest, coastEast, north, aachen, Alsace.luxSE, Alsace.northWest, p10)
+  override val polygonLL: PolygonLL = PolygonLL(northWest, coastEast, north, aachen, Alsace.luxSE, Alsace.northWest, p10)
 }
 
 /** Displays most of France. Depends on [[Alsace]] and [[BelgLux]]. */
 object Frankia extends EArea2("Frankia", 47.28 ll 1.93, Plain)
-{
-  val southWest = divN45 ll -1.29
-  val southEast = divN45 ll 5.53
-
-  val sLAmelie = 45.47 ll -1.15
+{ val southWest: LatLong = divN45 ll -1.29
+  val southEast: LatLong = divN45 ll 5.53
+  val sLAmelie: LatLong = 45.47 ll -1.15
   val royan = 45.61 ll -1.04
   val laCoubre = 45.69 ll -1.23
   val laRochelle = 46.15 ll -1.22
@@ -59,18 +57,19 @@ object Frankia extends EArea2("Frankia", 47.28 ll 1.93, Plain)
   val seineMouth = 49.43 ll 0.23
   val wLeHavre = 49.51 ll 0.06
   val capAntifer = 49.69 ll 0.16
-  val cayeux = 50.18 ll 1.49
+  val cayeux: LatLong = 50.18 ll 1.49
   val capGrisNez = 50.87 ll 1.58
   val calais = 50.93 ll 1.74
 
-  val bourgeEnBresse = 46.20 ll 5.22
+  val bourgeEnBresse: LatLong = 46.20 ll 5.22
 
-  override val polygonLL = PolygonLL(southEast, southWest, sLAmelie, royan, laCoubre, laRochelle, niortaise, sablesdOlonne, vilaineMouth, penmarch,
+  override val polygonLL: PolygonLL = PolygonLL(southEast, southWest, sLAmelie, royan, laCoubre, laRochelle, niortaise, sablesdOlonne, vilaineMouth, penmarch,
     brest, landunvez, pleubian, yffiniac, capFrehel, pointeDuGrouin, vildeLaMarine, avranches,  cabaneVauban, auderville, gatteville, carentan,
     cabourg,villierville, seineMouth, wLeHavre, capAntifer, cayeux, capGrisNez, calais, BelgLux.northWest, BelgLux.p10, Alsace.northWest, Alsace.p10,
     Alsace.moselleSW, Alsace.hautRhinNW, Alsace.southWest, bourgeEnBresse)
 
   val paris: LocationLL = LocationLL("Paris", 48.86, 2.35, 1)
+  val lille: LocationLL = LocationLL("Lille", 50.63, 3.06, 2)
 
   override val places: LocationLLArr = LocationLLArr(paris)
 }
