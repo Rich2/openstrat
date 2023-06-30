e706187947c0b561cd81ac8a8788ae4a5784683b
Rich2 <rr1@tutanota.com>
2023-06-30 10:26:59 +0100

    ScalaDocs.

--- Dev/srcStrat/puloc/Lunit.scala
+++ Dev/srcStrat/puloc/Lunit.scala
@@ -6,15 +6,18 @@ trait Lunit extends Coloured
 { /** The nation / state to which this unit belongs. */
   def polity: Polity
 
+  /** The date this unt comes into existence. */
   val startDate: MTime
 
   /** The end date for this incarnation of this Unit identity. */
   val endDate: MTime
 
+  /** An implicit value for the start and end of the unit to be used in building time series.  */
   implicit def startEnd: MTime2 = new MTime2(startDate.int1, endDate.int1)
 
   override def colour: Colour = polity.colour
 
+  /** Optional location of the unit throughout its existence. */
   def locPosns: MTimeSeries[LatLongOpt]
 
   /** Finds all the [[Lunit]]s that have a defined location at the given time. */
@@ -22,7 +25,7 @@ trait Lunit extends Coloured
 }
 
 trait CorpsNumbered extends Lunit
-{ /** The umber of the Corps 1st, 2nd 3rd, etc. */
+{ /** The number of the Corps 1st, 2nd 3rd, etc. */
   def corpsNum: Int
 
   override def toString: String = polity.name -- corpsNum.adjective -- "Corps"
