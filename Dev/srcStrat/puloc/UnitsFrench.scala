1764c8b8c63fe667e0033ab9ac2adce2a283a09b
Rich2 <rr1@tutanota.com>
2023-07-05 14:19:33 +0100

    Created FraNumberedCorps class.

---
+++ Dev/srcStrat/puloc/UnitsFrench.scala
@@ -0,0 +1,16 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package puloc
+import geom._, pglobe._, pEarth._, pEurope._
+
+abstract class FraNumberedCorps(startDate: MTime, endDate: MTime, val corpsNum: Int) extends Lunit(startDate, endDate)
+{ override def desig: String = corpsNum.adjective
+  override def levelName: String = "Corps"
+  override val polity: MTimeSeries[Polity] = MTimeSeries(France)
+}
+object FraCp1 extends FraNumberedCorps(MTime(1939, 8, 27), MTime(1945, 5, 8), 1)
+{ override val locPosns: MTimeSeries[LatLong] = MTimeSeries(Frankia.calais, (MTime(1939, 11, 15), Frankia.calais))
+}
+
+object FraCp2 extends FraNumberedCorps(MTime(1939, 8, 23), MTime(1940, 5, 26), 2)
+{ override val locPosns: MTimeSeries[LatLong] = MTimeSeries(Frankia.calais)
+}
--- Dev/srcStrat/puloc/UnitsModern.scala
+++ Dev/srcStrat/puloc/UnitsModern.scala
@@ -2,18 +2,3 @@
 package ostrat; package puloc
 import geom._, pglobe._, pEarth._, pEurope._
 
-object FraCp1 extends Lunit(MTime(1939, 8, 27), MTime(1945, 5, 8)) with CorpsNumbered
-{ override val corpsNum: Int = 1
-  //override val startDate: MTime =
-  //override val endDate: MTime =
-  override val polity: MTimeSeries[Polity] = MTimeSeries(France)
-  override val locPosns: MTimeSeries[LatLong] = MTimeSeries(Frankia.calais, (MTime(1939, 11, 15), Frankia.calais))
-}
-
-object FraCp2 extends Lunit(MTime(1939, 8, 23), MTime(1940, 5, 26)) with CorpsNumbered
-{ override val corpsNum: Int = 2
-  //override val startDate =
- // override val endDate: MTime =
-  override val polity: MTimeSeries[Polity] = MTimeSeries(France)
-  override val locPosns: MTimeSeries[LatLong] = MTimeSeries(Frankia.calais)
-}
