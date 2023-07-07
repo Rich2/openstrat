1e890698febb81300a536ac2814f20a5ce049cc8
Rich2 <rr1@tutanota.com>
2023-07-07 23:38:10 +0100

    Updated EarthApp.html.

--- Dev/JsAppsSrc/BC305App/BC305JsApp.scala
+++ Dev/JsAppsSrc/BC305App/BC305JsApp.scala
@@ -4,6 +4,5 @@ import scalajs.js.annotation._, p305._, prid.phex._
 
 @JSExportTopLevel("BC305JsApp")
 object BC305JsApp
-{   
-  @JSExport def main(): Unit = BC305Gui(CanvasJs, BCScen1, HGView(432, 1488, 20))
+{ @JSExport def main(): Unit = BC305Gui(CanvasJs, BCScen1, HGView(432, 1488, 20))
 } 
