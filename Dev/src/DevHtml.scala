af8b9689d2087482a17f36735b24ef5f19138993
Rich2 <rr1@tutanota.com>
2023-07-09 18:06:57 +0100

    More DevHtmlApp.

--- Dev/src/DevHtml.scala
+++ Dev/src/DevHtml.scala
@@ -15,21 +15,24 @@ object DevHtmlApp extends App
   }
 
   val sett = findDevSettingT[DirPathAbs]("projPath")
+  val subPages = RArr(SubPage("UnitLoc", "unitloc", "Unit Locator"), SubPage("Diceless"), SubPage("BC305"), SubPage("Planets"),
+    SubPage("Zug","zug", "ZugFuhrer"))
 
-  def make(title: String): Unit = sett.forGoodForBad { path =>
-    val head = HtmlHead.titleCss(title, "only")
-    val subPages = RArr(SubPage("Diceless"), SubPage("BC305"), SubPage("Planets"), SubPage("Zug","zug", "ZugFuhrer"))
+  def make(page: SubPage): Unit = sett.forGoodForBad { path =>
+    val head = HtmlHead.titleCss(page.linkText, "only")
+
     val pairs: StrPairArr[String] = StrStrPairArr("index", "Home", "diceless", "Diceless", "bc305", "BC305", "planets", "Planets", "zug", "Zugfuhrer")
-    val pairs2 = pairs.filterNotOnA2(_ == title)
+    val pairs2 = pairs.filterNotOnA2(_ == page.appStemName)
+
     val list = HtmlUl(pairs2.pairMap{ (s1, s2) => HtmlLi.a(s1 + ".html", s2) })
     val body = HtmlBody.elems(list, HtmlCanvas.id("scanv"))
     val content = HtmlPage(head, body)
 
-    val res = fileWrite(path.str -/- "Dev/SbtDir", title.toLowerCase() + "app.html", content.out)
+    val res = fileWrite(path.str -/- "Dev/SbtDir", page.fileName + "app.html", content.out)
     debvar(res)
   }{ errs =>
      deb("")
      errs.foreach(println)
   }
-  make("Diceless")
+  subPages.foreach(make)
 }
