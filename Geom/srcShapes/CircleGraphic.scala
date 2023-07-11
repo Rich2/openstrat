ab19b93dd89644acf9362d778aaa08db0618e1c9
Rich2 <rr1@tutanota.com>
2023-07-12 00:51:57 +0100

    Removed old web code.

--- Geom/srcShapes/CircleGraphic.scala
+++ Geom/srcShapes/CircleGraphic.scala
@@ -1,6 +1,5 @@
-/* Copyright 2018-20 Richard Oliver. Licensed under Apache Licence version 2.0. */
-package ostrat
-package geom
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package geom
 import pWeb._
 
 /** A circle based Graphic, may be simple or compound. */
