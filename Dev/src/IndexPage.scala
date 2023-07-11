e04f56fe9866170982504c20498d9ba857f1c15a
Rich2 <rr1@tutanota.com>
2023-07-11 01:39:57 +0100

    Created Apps.html.

--- Dev/src/IndexPage.scala
+++ Dev/src/IndexPage.scala
@@ -86,7 +86,7 @@ object IndexPage
   |  will be organised according to a number of levels, which are likely to change over increasingly shorter historical time frames.
   |</li>
   |
-  |<li>Apps Module</li>
+  |<li><a href="Documentation/apps.html"><b>Apps Module</b></a></li>
   |
   |<li><a href="Documentation/Dev.html"><b>Dev Module</b></a> The Module as a whole Depends on all the other modules, although its constiurent parts may
   |  not. This module is for the use of developer tools and settings and for end-user applications, that may eventually end up in their own repositaries.
@@ -101,32 +101,6 @@ object IndexPage
   |     Scala, complete beginners to programming and beginners in geometry, using the graphical API. These lessons are published separately as the
   |     LearnScala project.
   |    </li>
-  |
-  |    <li>A number of rudimentary games and applications depending on some or all of the above packages. The intention is to factor out common
-  |      functionality and classes.
-  |      <ul>
-  |        <li>ostrat.pWW2 A grand strategy world War 2 game, using the hex tiled world terrain map.</li>
-  |        <li>ostrat.p1783 A grand strategy game, also using the world map starting in 1783.</li>
-  |        <li>ostrat.p305 A grand strategy game set in 305BC, using part of the world map.</li>
-  |        <li>ostrat.pZug A 20th century squad based strategy game using hex tiles.</li>
-  |        <li>ostrat.pGames.pCiv A human history 4x development game using hex tiles.</li>
-  |        <li>ostrat.pGames.pDung A Square tile based dungeon game.</li>
-  |      </ul>
-  |    </li>
-  |
-  |      <li>ostrat.pStrat depends on geom, pCanv and pGrid and pEarth.
-  |        <ul>
-  |          <li>Flags</li>
-  |          <li>DemoCanvas for graphics elements.</li>
-  |          <li>Odds and ends.</li>
-  |        </ul>
-  |      </li>
-  |
-  |      <li>ostrat.pCloseOrder. Pre modern close order formation based battles, not using tiles.</li>
-  |
-  |      <li>ostrat.pSpace A solar system app.</li>
-  |
-  |      <li>ostratpChess. A search for an elegant implementation of Draughts and Chess.</li>
   |
   |  </ul>
   | </li>
