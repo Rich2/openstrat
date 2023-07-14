1f92ce6b3ce7d47c52b67b4a0df94682b2f17f79
Rich2 <rr1@tutanota.com>
2023-07-14 01:05:19 +0100

    Created TilingPage

---
+++ Tiling/ExsSrc/prid/TilingPage.scala
@@ -0,0 +1,9 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package prid
+import pWeb._
+
+object TilingPage extends HtmlPage {
+  override def head: HtmlHead = HtmlHead.titleCss("Util Module", "https://richstrat.com/Documentation/documentation")
+
+  override def body: HtmlBody = ???
+}
