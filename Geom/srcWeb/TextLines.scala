678c181118de76bcddd7a78e61afcb83dda4f565
Rich2 <rr1@tutanota.com>
2023-07-19 14:44:45 +0100

    Created XmlInLineable trait.

--- Geom/srcWeb/TextLines.scala
+++ Geom/srcWeb/TextLines.scala
@@ -5,16 +5,22 @@ sealed trait TextLines
 { def text: String
 }
 
+trait TextInLines extends TextLines
+{ /** The length of the last line of this text. The purpose of this field is to to dertimine if there is space to add further tokens on the same
+   * line. */
+  def lastLineLen: Int
+}
+
 /** Contains text from a structure of a human / programming language that continues a line and finishes on the same line. */
-case class TextIn1Line(text: String, length: Int) extends TextLines
+case class TextIn1Line(text: String, lastLineLen: Int) extends TextInLines
 
 /** Contains text from a structure of a human / programming language that continues a line and finishes on the 2nd line same line. The second line is
  *  indented. */
-case class TextIn2Line(text: String, length2: Int) extends TextLines
+case class TextIn2Line(text: String, lastLineLen: Int) extends TextInLines
 
 /** Contains text from a structure of a human / programming languageProduces text that continues a line and finishes on the 3rd line same line. The
  *  second and third lines are indented. */
-case class TextInMultiLines(text: String, lastLineLen: Int) extends TextLines
+case class TextInMultiLines(text: String, lastLineLen: Int) extends TextInLines
 
 /** Contains text from a structure of a human / programming languageProduces text that starts on its own line. It is intended that any following text
  *  should continue on its own line. All lines are indented. */
--- Geom/srcWeb/XCon.scala
+++ Geom/srcWeb/XCon.scala
@@ -10,33 +10,32 @@ trait XCon
   def outEither(indent: Int, maxLineLen: Int = 150): (Boolean, String) = (false, out(indent, maxLineLen))
 }
 
-/** XML / HTML text that can have its line breaks changed. */
-case class XConText(value: String) extends XCon
-{ override def out(indent: Int, line1Delta: Int = 0, maxLineLen: Int = 150): String = value
-  override def outEither(indent: Int, maxLineLen: Int): (Boolean, String) = (true, out(indent, maxLineLen))
-
+/** XML / HTML content that can be inlined. */
+trait XConInLineable extends XCon
+{
+  def value: String
   def outLines(indent: Int, line1Delta: Int, maxLineLen: Int = lineLenDefault): TextLines =
   {
     implicit val charArr: CharArr = new CharArr(value.toCharArray)
 
     def line1Len: Int = indent + line1Delta
 
-    def in1Loop(rem: CharsOff, currStr: String, lineLen: Int): TextLines = rem match
-    { case CharsOff0() => TextIn1Line(currStr, lineLen)
+    def in1Loop(rem: CharsOff, currStr: String, lineLen: Int): TextLines = rem match {
+      case CharsOff0() => TextIn1Line(currStr, lineLen)
       case CharsOff1Tail(c, tail) if c.isWhitespace => in1Loop(tail, currStr, lineLen)
-      case s =>
-      { val (newRem, newWord) = getWord(s)
+      case s => {
+        val (newRem, newWord) = getWord(s)
         val newLen = lineLen + newWord.length + 1
         if (newLen > maxLineLen) in2Loop(newRem, currStr + "\n" + indent.spaces + newWord, indent + newWord.length)
         else in1Loop(newRem, currStr -- newWord, newLen)
       }
     }
 
-    def in2Loop(rem: CharsOff, currStr:String, lineLen: Int): TextLines = rem match
+    def in2Loop(rem: CharsOff, currStr: String, lineLen: Int): TextLines = rem match
     { case CharsOff0() => TextIn2Line(currStr, lineLen)
       case CharsOff1Tail(c, tail) if c.isWhitespace => in2Loop(tail, currStr, lineLen)
-      case rem =>
-      { val (newRem, newWord) = getWord(rem)
+      case rem => {
+        val (newRem, newWord) = getWord(rem)
         val newLen = lineLen + newWord.length + 1
         if (newLen > maxLineLen) multiLoop(newRem, currStr, "\n" + indent.spaces + newWord)
         else in2Loop(newRem, currStr -- newWord, newLen)
@@ -46,8 +45,8 @@ case class XConText(value: String) extends XCon
     def multiLoop(rem: CharsOff, lines: String, currLine: String): TextInMultiLines = rem match
     { case CharsOff0() => TextInMultiLines(lines + currLine, currLine.length)
       case CharsOff1Tail(c, tail) if c.isWhitespace => multiLoop(tail, lines, currLine)
-      case s =>
-      { val (newRem, newWord) = getWord(rem)
+      case s => {
+        val (newRem, newWord) = getWord(rem)
         val newLen = currLine.length + newWord.length + 1
         if (newLen > maxLineLen) multiLoop(newRem, lines + currLine, "\n" + indent.spaces + newWord)
         else multiLoop(newRem, lines, currLine -- newWord)
@@ -57,16 +56,24 @@ case class XConText(value: String) extends XCon
     def getWord(rem: CharsOff): (CharsOff, String) =
     {
       def loop(rem: CharsOff, newWord: String): (CharsOff, String) = rem match
-      { case CharsOff0()  => (rem, newWord)
+      { case CharsOff0() => (rem, newWord)
         case CharsOffHead(c) if c.isWhitespace => (rem, newWord)
         case CharsOff1Tail(c, tail) => loop(tail, newWord + c)
       }
+
       loop(rem, "")
     }
 
     val (newRem, firstWord) = getWord(charArr.offsetter0)
     in1Loop(newRem, firstWord, line1Len + firstWord.length)
   }
+}
+
+/** XML / HTML text that can have its line breaks changed. */
+case class XConText(value: String) extends XConInLineable
+{ override def out(indent: Int, line1Delta: Int = 0, maxLineLen: Int = 150): String = value
+  override def outEither(indent: Int, maxLineLen: Int): (Boolean, String) = (true, out(indent, maxLineLen))
+
 }
 
 /** Not sure about this trait. Its intended for short pieces of text that should be kept on the same line. */
