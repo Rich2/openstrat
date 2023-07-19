f9871dcab002f0a5adaa5f2b6a56e35424002c22
Rich2 <rr1@tutanota.com>
2023-07-19 21:48:44 +0100

    More conversion of UtilPage to code.

--- Util/ExsSrc/UtilPage.scala
+++ Util/ExsSrc/UtilPage.scala
@@ -8,7 +8,7 @@ object UtilPage extends HtmlPage
   override def head: HtmlHead = HtmlHead.titleCss("Util Module", "https://richstrat.com/Documentation/documentation")
 
   override def body: HtmlBody = HtmlBody(HtmlH1("Util Module"), central)
-  def central: HtmlDiv = HtmlDiv.classAtt("central", list, HtmlH2("Tokeniser"), centralStr.xCon)
+  def central: HtmlDiv = HtmlDiv.classAtt("central", list, HtmlH2("Tokeniser"), tokList, centralStr.xCon)
   def list: HtmlOlWithLH = HtmlOlWithLH(HtmlH2("The Util module contains"), debug, gen, coll, errs, parse, persist)
 
   def debug: HtmlLi = HtmlLi.str("Some simple debug macros")
@@ -24,25 +24,16 @@ object UtilPage extends HtmlPage
 
   def persist: HtmlLi = HtmlLi.str("Persistence system for Show and UnShow, uses the previously mentioned RSON syntax.")
 
-  def tokList: HtmlUlWithLH = HtmlUlWithLH("The Tokeniser will create the following tokens")
+  def tokList: HtmlUlWithLH = HtmlUlWithLH("The Tokeniser will create the following tokens",
+    HtmlLi.str("""Keytokens <span class= lexical>_ ? ?? ???</ span >"""),
+    HtmlLi.str("Identifiers alphanumeric tokens starting with a letter or underscore character."), HtmlLi.str("Operators"),
+    HtmlLi.str("Numeric literals"), HtmlLi.str("Seperators , . .. ... {} etc."), HtmlLi.str("String literals"), HtmlLi.str("Character literals"),
+    HtmlLi.str("Comments"))
 
   val centralStr: String = """
-      |  <p>
-      |    <ul>
-      |      <li>Keytokens <span class=lexical>_ ? ?? ???</span></li>
-      |      <li>Identifiers alphanumeric tokens starting with a letter or underscore character.</li>
-      |      <li>Operators</li>
-      |      <li>numeric literals</li>
-      |      <li>Seperators , . .. ... { } etc.</li>
-      |      <li>String literals</li>
-      |      <li>Character literals</li>
-      |      <li>Comments</li>
-      |    </ul>
-      |  </p>
-      |
-      |  <p>KeyTokens, Identifiers, and literals are all expressions. Operators, seperators and comments are not. Identifers includes lexemes such as
+      |  <p>KeyTokens, Identifiers, and literals are all expressions. Operators, separators and comments are not. Identifiers includes lexemes such as
       |   <span class=lexical>if, IF true and TRUE</span>. There are no alphabetic keywords in RSON syntax. Consumers of RSON syntax can of course treat
-      |    what ever identifers they want as keywords appropriate to their use case. Identifers are categorised into 3 types.
+      |    what ever identifiers they want as keywords appropriate to their use case. Identifiers are categorised into 3 types.
       |    <ul>
       |      <li>IdentUnder An identifer beginning with an underscore character</li>
       |      <li>IdentLow And identifer beginning with a lower case alphabetic character.</li>
