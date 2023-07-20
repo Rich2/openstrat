1d60b2ec1b04adf5083cf7fb07bbde7ae056066b
Rich2 <rr1@tutanota.com>
2023-07-20 14:24:48 +0100

    More conversion of UtilPAge to code.

--- Util/ExsSrc/UtilPage.scala
+++ Util/ExsSrc/UtilPage.scala
@@ -30,7 +30,7 @@ object UtilPage extends HtmlPage
     HtmlLi("Numeric literals"), HtmlLi("Separators , . .. ... {} etc."), HtmlLi("String literals"), HtmlLi("Character literals"), HtmlLi("Comments"))
 
   def gen2 = HtmlP("KeyTokens, Identifiers, and literals are all expressions. Operators, separators and comments are not. Identifiers" --
-    "includes lexemes such as <span class= lexical>if, IF true and TRUE </ span >.There are no alphabetic keywords in RSON syntax. Consumers of" --
+    "includes lexemes such as <span class= lexical>if, IF true and TRUE </span>.There are no alphabetic keywords in RSON syntax. Consumers of" --
     "RSON syntax can of course treat what ever identifiers they want as keywords appropriate to their use case.")
 
   def identList = HtmlUlWithLH("Identifiers are categorised into 3 types.", HtmlLi("IdentUnder An identifer beginning with an underscore character."),
@@ -39,38 +39,44 @@ object UtilPage extends HtmlPage
       "tokens, and a subset of these will also be considered valid raw Hexadecimals, however all the alphabetic characters must be lower case.")
     )
 
+  def lits: Any = HtmlOlWithLH("Numerical literals come in 4 types.",
+    HtmlLi("Floating point numbers <span class= lexical>6.02e23 6.02e-23 6.02e-23d</span>.Note this is the only case where a negative or dash" --
+      "character is included as part of a token. Can have optional trailing lower case alphabetic characters at the end of the token."),
+    HtmlLi("Explicit hexadecimals <span class= lexical>0x433A 0x2222 0xff000bc</span> Alphabetic characters must be all lower or all upper case."),
+    HtmlLi("Explicit Base32 tokens <span class= lexical>0y433G 0x222C 0yWW000MP</span> Alphabetic characters must all be upper case.</li>"),
+    digToks, negToks)
 
+  def digToks = HtmlLi("""DigitCode tokens. These are a sequence of one or more sequences of digits separated by decimal points, as well as integer
+    and fractional
+    decimal numbers they can be used for version numbers, IP addresses and other codes. These can themsleves be further divided into
+    <ul>
+      <li>Valid natural integers
+        <span class= lexical>0 21 567</span>
+        These are valid raw hexademimal and raw Base32 tokens.</li>
+      <li>Valid natural integers with trailing lower case alphabetic characters at the end of the token
+        <span class= lexical>0f 21snap 567d</span>
+        .
+        These may be valid raw hexadecimal or Base32 tokens if the alphabetic letter all fall within the correct set of letters.</li>
+      <li>Raw Hexadecimals that start with a digit, the alphabetic characters must be all lower or all upper case.
+        <span class= lexical>3A 2d1e 567D</span>
+        . These are also valid raw Base32 tokens.</li>
+      <li>Raw Base32 tokens that start with a digit that are not valid raw Hexadecimals, the alphabetic characters must be all lower or all upper
+        case.
+        <span class= lexical>3G 2d1s 567R</span>
+      </li>
+      <li>Valid fractional decimal numbers, which may have trailing lower case alphabetic characters at the end of the token
+        <span class= lexical>0.0f 2084.4 3.1rc</span>
+      </li>
+      <li>DigitCode Tokens with 3 or greater digit sequence parts
+        <span class= lexical>2.13.4 0.0.3snap 192.168.1.1</span>
+      </li>
+    </ul>""")
+
+    def negToks = HtmlLi("There will be tokens for negative numbers. <span class= lexical>-5 -5.32 -5.87e7 -0xA434</span> will all be lexically" --
+      "processed as single tokens. This means that raw hexidecimals and raw base32s can be processed as 1 or 2 tokens depending on whether they" --
+      "start with a digit. This should not cause a problem as long as they are not combined with dot operators in dot expressions.")
 
   val centralStr: String = """
-      |  <p>
-      |    Numerical literals come in 4 types.
-      |    <ul>
-      |      <li>Floating point numbers <span class=lexical>6.02e23 6.02e-23 6.02e-23d</span>.Note this is the only case where a negative or dash
-      |       character is included as part of a token. Can have optional trailing lower case alphabetic characters at the end of the token.
-      |      </li>
-      |      <li>Explicit hexadecimals <span class=lexical>0x433A 0x2222 0xff000bc</span> Alphabetic characters must be all lower or all upper case.</li>
-      |      <li>Explicit Base32 tokens <span class=lexical>0y433G 0x222C 0yWW000MP</span> Alphabetic characters must all be upper case.</li>
-      |      <li>DigitCode tokens. These are a sequence of one or more sequences of digits separated by decimal points, as well as integer and fractional
-      |       decimal numbers they can be used for version numbers, IP addresses and other codes. These can themsleves be further divided into
-      |       <ul>
-      |        <li>Valid natural integers <span class=lexical>0 21 567</span> These are valid raw hexademimal and raw Base32 tokens.</li>
-      |        <li>Valid natural integers with trailing lower case alphabetic characters at the end of the token <span class=lexical>0f 21snap 567d</span>.
-      |         These may be valid raw hexadecimal or Base32 tokens if the alphabetic letter all fall within the correct set of letters.</li>
-      |        <li>Raw Hexadecimals that start with a digit, the alphabetic characters must be all lower or all upper case.
-      |         <span class=lexical>3A 2d1e 567D</span>. These are also valid raw Base32 tokens.</li>
-      |        <li>Raw Base32 tokens that start with a digit that are not valid raw Hexadecimals, the alphabetic characters must be all lower or all upper
-      |         case. <span class=lexical>3G 2d1s 567R</span></li>
-      |        <li>Valid fractional decimal numbers, which may have trailing lower case alphabetic characters at the end of the token
-      |          <span class=lexical>0.0f 2084.4 3.1rc</span></li>
-      |        <li>DigitCode Tokens with 3 or greater digit sequence parts <span class=lexical>2.13.4 0.0.3snap 192.168.1.1</span></li>
-      |       </ul>
-      |       There will be tokens for negative numbers. <span class=lexical>-5 -5.32 -5.87e7 -0xA434</span> will all be lexically processed as single
-      |       tokens. This means that raw hexidecimals and raw base32s can be processed as 1 or 2 tokens depending on whether they start with a digit. This
-      |       should not cause a problem as long as they are not combined with dot operators in dot expressions.
-      |      </li>
-      |    </ul>
-      |  </p>
-      |
       |  <table>
       |    <tr><td>LetterChar</td>                  <td>= unicode_letter | '_'</td></tr>
       |    <tr><td>NonZeroDigit</td>                <td>= '1' ... '9'</td></tr>
@@ -91,8 +97,6 @@ object UtilPage extends HtmlPage
       |    <tr><td>IdentifierToken</td>             <td>= letter | UnderscoreThenLetterOrDigit, { LetterOrDigitChar | UnderscoreThenLetterOrDigit }</td></tr>
       |    <tr><td>DeciLitToken</td>                <td>= '0' | (NonZeroDigit { DigitChar })</td></tr>
       |  </table>
-      |</div>
-      |<div class="main">
       |  <h2>Abstract Syntax Tree</h2>
       |  <p>So after the source has been tokenised it is parsed into an Abstract Syntax tree. the basic idea is that an RSON file can be three things.
       |  <ol>
@@ -133,7 +137,6 @@ object UtilPage extends HtmlPage
       |  <p>Hexadecimal is written with Uppercase letters. Base32 is written with the digits followed by the upper case letters A to W, with the letter 'O'
       |    unused.</p>
       |  <p>A 10, B 11, C 12, D 13, E 14, F 15, G 16, H 17, I 18, J 19, K 20, L 21, M 22, N 23, P 24, Q 25, R 26, S 27, T 28, U 29 V 30, W 31</p>
-      |</div>
       |  <h2>Miscellaneous</h2>
       |  <p>A Lower case letter will be use after numerals in names.</p>
       |""".stripMargin
