70c066429ae2bb9af226dacb4aa044137b4406f6
Rich2 <rr1@tutanota.com>
2023-07-25 17:10:33 +0100

    More Convertion of DevPage to code.

--- Dev/src/DevPage.scala
+++ Dev/src/DevPage.scala
@@ -9,7 +9,7 @@ object DevPage extends HtmlPage
 
   override def body: HtmlBody = HtmlBody(HtmlH1("Dev Module"), central)
 
-  def central: HtmlDiv = HtmlDiv.classAtt("central", list, p1, cenStr1.xCon, cenStr2.xCon, miscTitle, miscStr.xCon)
+  def central: HtmlDiv = HtmlDiv.classAtt("central", list, p1, p2, p3, cenStr2.xCon, miscTitle, miscStr.xCon)
 
   def list: HtmlOlWithLH = HtmlOlWithLH(HtmlH2("The Dev module contains"), appSel, siteGen)
 
@@ -19,20 +19,18 @@ object DevPage extends HtmlPage
 
   def miscTitle = HtmlH2("Place to put various notes, so as stuff doesn't get lost. It can be sorted into proper documentation later.")
 
-  def p1 = HtmlP("""It currently works on JavaFx and web page. Using canvas on both platforms. See <a href=" api / index.html">Scala Docs</a>""" --
-    """and See <a href="apiJs/index.html">Scala Docs for JavaScript target.</a>")""")
+  def p1: HtmlP = HtmlP("It currently works on JavaFx and web page. Using canvas on both platforms. See" --
+    """<a href=" api / index.html">Scala Docs</a> and See <a href="apiJs/index.html">Scala Docs for JavaScript target.</a>")""")
 
-  def cenStr1 ="""
-  |<p>The Strategy games was the original motivation for the project, but the geometry and graphics library have far wider applicability. The geometry
-  |  and graphics are far more developed, while the tiling and strategy games are still in a far more experimental stage. This is in accordance with the
-  |  original vision, part of which was to explore the possibilities of an Algebra of Tiling.
-  |
-  |<p>I would welcome input from developers with greater experience and knowledge than my own. One of the goals of the project
-  |  is to explore, where it is best to compose with trait / class inheritance and where to use functions. When to use mutation and when to use
-  |  immutability. When to use smart, garbage collected heap based objects and when to use dumb data values. Balancing the competing priorities of
-  |  elegance, succinctness, readability, run-time performance, compile time performance and accessibility for inexperienced programmers. I feel Scala
-  |  is, and in particular Scala 3 will be the ideal language to explore these questions.</p>
-  |""".stripMargin
+  def p2: HtmlP = HtmlP("The Strategy games was the original motivation for the project, but the geometry and graphics library have far wider" --
+    "applicability. The geometry and graphics are far more developed, while the tiling and strategy games are still in a far more experimental" --
+    "stage. This is in accordance with the original vision, part of which was to explore the possibilities of an Algebra of Tiling.")
+
+  def p3: HtmlP = HtmlP("I would welcome input from developers with greater experience and knowledge than my own. One of the goals of the project" --
+    "is to explore, where it is best to compose with trait / class inheritance and where to use functions. When to use mutation and when to use" --
+    "immutability. When to use smart, garbage collected heap based objects and when to use dumb data values. Balancing the competing priorities of" --
+    "elegance, succinctness, readability, run-time performance, compile time performance and accessibility for inexperienced programmers. I feel" --
+    "Scala is, and in particular Scala 3 will be the ideal language to explore these questions.")
 
   def cenStr2 ="""
   |<p>Scala currently set to 3.3.0. Jdk 11+, 11 preferred. Scala.Js set to 1.13.2. Scala native set to 0.4.14. Sbt currently set to 1.9.3 (uses the
