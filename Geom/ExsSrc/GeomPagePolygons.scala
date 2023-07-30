a52bd093ca34dbdb27be19edcc839969d8ca5168
Rich2 <rr1@tutanota.com>
2023-07-30 19:26:12 +0100

    Reorganised graphics code on GeomPage.

--- Geom/ExsSrc/GeomPagePolygons.scala
+++ Geom/ExsSrc/GeomPagePolygons.scala
@@ -15,47 +15,40 @@ object GeomPagePolygons extends HtmlSection
   val width: Int = 250
   val polyColour: Colour = DarkGreen
   val dodec1: DoDeclign = DoDeclign(width)
-  val dodec2: SvgElem = dodec1.draw(polyColour).svgElem
-  val circ: SvgElem = Circle(width * 2).draw().svgElem
-  val verts: RArr[SvgElem] = dodec1.vertsIFlatMap { (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str).map(_.svgElem) }
-  val sides: RArr[SvgElem] = dodec1.sidesIFlatMap { (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour).map(_.svgElem) }
-  val cen: SvgElem = Pt2Z.textAt("Centre").svgElem
-  val svg1: HtmlSvg = HtmlSvg.bounds(dodec1.boundingRect.addMargin(svgMargin), RArr(dodec2, circ, cen) ++ verts ++ sides, RArr(CentreBlockAtt))
+  val dodec2 = dodec1.draw(polyColour)
+  val circ = Circle(width * 2).draw()
+  val verts = dodec1.vertsIFlatMap { (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str) }
+  val sides = dodec1.sidesIFlatMap { (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour) }
+  val cen = Pt2Z.textAt("Centre")
+  val clock = RArr(dodec2, circ, cen) ++ verts ++ sides
+  val svg1: HtmlSvg = HtmlSvg(dodec1.boundingRect.addMargin(svgMargin), clock, RArr(CentreBlockAtt))
 
   val rect1: Rect = Rect(400, 250)
-  val rect2: SvgElem = rect1.draw(polyColour).svgElem
-  val verts2: RArr[SvgElem] = rect1.vertsIFlatMap { (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str).map(_.svgElem) }
-  val sides2: RArr[SvgElem] = rect1.sidesIFlatMap { (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour).map(_.svgElem) }
-  val svg2: HtmlSvg = HtmlSvg.bounds(rect1.boundingRect.addMargin(svgMargin), RArr(rect2, cen) ++ verts2 ++ sides2, RArr(CentreBlockAtt))
+  val rect2 = rect1.draw(polyColour)
+  val verts2 = rect1.vertsIFlatMap { (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str) }
+  val sides2 = rect1.sidesIFlatMap { (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour) }
+  val svg2: HtmlSvg = HtmlSvg(rect1.boundingRect.addMargin(svgMargin), RArr(rect2, cen) ++ verts2 ++ sides2, RArr(CentreBlockAtt))
 
   def p2: HtmlP = HtmlP(
-  """I've included the Scala code below both for the above diagram and for the rectangle diagram below. If you check the html source code for this web
-  | page you will see that it is pretty succinct compared with the generated SVG code.""".stripMargin)
+  """I've included the Scala code below both for the above diagram. If you check the html source code for this web page you will see that it is pretty
+  | succinct compared with the generated SVG code. The code is in small font and is not type annotated so it is not intended as a tutorial, but just
+  | to give an idea of possibilities. It is only the last line that creates the SVG. The rest of the code could be used in an HTML or a JavaFx
+  | canvas.""".stripMargin)
 
   def code1: HtmlScala = HtmlScala(
     "val width: Int = 250<br>" ---
     "val polyColour: Colour = DarkGreen<br>" ---
     "val dodec1: DoDeclign = DoDeclign(width)<br>" ---
-    "val dodec2: SvgElem = dodec1.draw(polyColour).svgElem<br>" ---
-    "val circ: SvgElem = Circle(width * 2).draw().svgElem<br>" ---
+    "val dodec2 = dodec1.draw(polyColour)<br>" ---
+    "val circ = Circle(width * 2).draw()<br>" ---
 
-    "val verts: RArr[SvgElem] =" ---
-    """  dodec1.vertsIFlatMap{ (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str).map(_.svgElem) }<br>""" ---
+    """val verts = dodec1.vertsIFlatMap{ (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str) }<br>""" ---
 
-    "val sides: RArr[SvgElem] =" ---
-    """dodec1.sidesIFlatMap{ (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour).map(_.svgElem) }<br>""" ---
-    """val cen: SvgElem = Pt2Z.textAt("Centre").svgElem<br>""" ---
-      
-    "val svg1: SvgSvgElem = SvgSvgElem.bounds(dodec1.boundingRect.addMargin(svgMargin), RArr(dodec2, circ, cen) ++ verts ++ sides, RArr(CentreBlockAtt))<br>" ---
-
-    "val rect1: Rect = Rect(400, 250)<br>" ---
-    "val rect2: SvgElem = rect1.draw(polyColour).svgElem<br>" ---
-    """val verts2: RArr[SvgElem] = rect1.vertsIFlatMap { (pt, i) => pt.textArrowToward(Pt2Z, "V" + i.str).map(_.svgElem) }<br>""" ---
-    """val sides2: RArr[SvgElem] = rect1.sidesIFlatMap { (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour).map(_.svgElem) }<br>""" ---
-
-    "val svg2: SvgSvgElem = SvgSvgElem.bounds(rect1.boundingRect.addMargin(svgMargin), RArr(rect2, cen) ++ verts2 ++ sides2, RArr(CentreBlockAtt))"
+    """val sides = dodec1.sidesIFlatMap{ (sd, i) => sd.midPt.textArrowAwayFrom(Pt2Z, "Sd" + i.str, colour = polyColour) }<br>""" ---
+    """val cen = Pt2Z.textAt("Centre")<br>""" ---
+    "val clock = RArr(dodec2, circ, cen) ++ verts ++ sides<br>" ---
+    "val svg1 = HtmlSvg(dodec1.boundingRect.addMargin(svgMargin), clock, RArr(CentreBlockAtt))<br>"
   )
-
 
   def p3: HtmlP = HtmlP(
     """If there is no vertex at the 12 o'clock / 00 hundred hours postion as in the rectangle below vertex 0 is the first vertex clockwise of 12
