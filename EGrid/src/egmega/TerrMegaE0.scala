9cc6ecff5217e379fdb5c6bb81fd075ef8270d95
Rich2 <rr1@tutanota.com>
2023-08-11 17:45:29 +0100

    Initial completion of TerrMegaE0.

--- EGrid/src/egmega/TerrMegaE0.scala
+++ EGrid/src/egmega/TerrMegaE0.scala
@@ -8,7 +8,7 @@ import prid._, phex._, egrid._, WTile._
  *  area it must be assigned to France  */
 object TerrMegaE0 extends LongMegaTerrs
 {
-  override implicit val grid: EGridMegaLongFull = EGridMega.e0(102)
+  override implicit val grid: EGridMegaLongFull = EGridMega.e0(82)
   override val terrs: HCenLayer[WTile] = HCenLayer[WTile](sea)
   override val sTerrs: HSideOptLayer[WSide, WSideSome] = HSideOptLayer[WSide, WSideSome]()
   override val corners: HCornerLayer = HCornerLayer()
@@ -16,11 +16,8 @@ object TerrMegaE0 extends LongMegaTerrs
   val help = new WTerrSetter(grid, terrs, sTerrs, corners)
   {
     override val rowDatas: RArr[RowBase] = RArr(
-//      TRow(124, sea, taiga),
-//      TRow(122, hills, plain),
-//      TRow(120, sea, plain * 2),
-//      TRow(118, sea, plain, hills),
-//      TRow(116, hills * 2, sea),
+      TRow(118, sea),
+      TRow(116, sea),
       TRow(114, plain),
       TRow(112, plain, hills),
       TRow(110, hills, sea),
@@ -28,24 +25,16 @@ object TerrMegaE0 extends LongMegaTerrs
       TRow(106, desert * 3),
       TRow(104, desert * 3),
       TRow(102, jungle * 3),
-//      VRow(141, Mouth(504, HVUL)),
-//      TRow(140, sea, Hland(4, 2, Hilly()), Hland(2, 5), plain * 3),
-//      TRow(138, sea * 2, plain * 2, hills, mtain * 2),
-//      VRow(137, Mouth(526, HVUp)),
-//      TRow(136, sea * 3, plain, hills, mtain, plain),
-//      VRow(135, VertIn(526, HVUR)),
-//      TRow(134, sea, hills * 3, sea, Isle(Hilly()), Hland(1, 1, Hilly())),
-//      TRow(132, sea, hills, plain * 2, sea, Isle(Hilly()), sea),
-//      VRow(131, MouthSpec(526, HVUL, HVRt, HVLt)),
-//      TRow(130, sea, Hland(3, 3), Hland(1, 3, Hilly()), Hland(2, 2, Hilly()), sea * 2, Hland(3, 5, Hilly()), Hland(2, 3, Hilly())),
-//      TRow(128, sea, Hland(2, 5, Hilly()), Hland(1, 0, Hilly()), hills * 3, Hland(1, 1, Hilly()), sea),
-//      TRow(126, sea, Hland(1, 5), mtain, hillyDesert, desert * 4),
-//      TRow(124, sea, hillyDesert, hills, desert * 6),
-//      VRow(123, SetSide(495)),
-//      TRow(122, SideB(), desert * 9),
-//      VRow(121, SetSide(495)),
-//      TRow(120, desert * 5, hillyDesert * 2, desert * 2),
-//      TRow(118, desert * 9),
+      TRow(100, sea * 2, jungle),
+      TRow(98, sea * 2, jungle),
+      TRow(96, sea * 3),
+      TRow(94, sea * 2, desert),
+      TRow(92, sea * 2),
+      TRow(90, sea * 2),
+      TRow(88, sea * 2),
+      TRow(86, sea),
+      TRow(84, sea),
+      TRow(82, ice),
     )
   }
   help.run
