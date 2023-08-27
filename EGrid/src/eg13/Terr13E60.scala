73db93522209aebdbb6ff19a1c2ee294a7b41a4f
Rich2 <rr1@tutanota.com>
2023-08-27 13:35:17 +0100

    Formatting.

--- EGrid/src/eg13/Terr13E60.scala
+++ EGrid/src/eg13/Terr13E60.scala
@@ -4,8 +4,7 @@ import prid._, phex._, egrid._, WTile._
 
 /** [[WTile]] terrain for 45° East to 75°, centred on 60° east. Hex tile scale 1300km. */
 object Terr13E60 extends Long13Terrs
-{
-  override implicit val grid: EGrid13LongFull = EGrid13.e60(86)
+{ override implicit val grid: EGrid13LongFull = EGrid13.e60(86)
   override val terrs: HCenLayer[WTile] = HCenLayer[WTile](sea)
   override val sTerrs: HSideOptLayer[WSide, WSideSome] = HSideOptLayer[WSide, WSideSome]()
   override val corners: HCornerLayer = HCornerLayer()
@@ -37,19 +36,3 @@ object Terr13E60 extends Long13Terrs
   }
   help.run
 }
-
-/*
-object BritReg
-{ def britGrid: EGrid14Long = EGrid14Long.reg(138, 148, 0, 504, 520)
-  def britTerrs: HCenLayer[WTile] = Terr14E0.terrs.spawn(Terr14E0.grid, britGrid)
-  def britSTerrs: HSideOptLayer[WSide, WSideSome] =Terr14E0.sTerrs.spawn(Terr14E0.grid, britGrid)
-  def britCorners: HCornerLayer =Terr14E0.corners.spawn(Terr14E0.grid, britGrid)
-
-  def regScen: EScenBasic = new EScenBasic
-  {  override def title: String = "Regular Britain"
-    override implicit val gridSys: EGrid14Long = britGrid
-    override val terrs: HCenLayer[WTile] = britTerrs
-    override val sTerrs: HSideOptLayer[WSide, WSideSome] = britSTerrs
-    override val corners: HCornerLayer = britCorners
-  }
-}*/
