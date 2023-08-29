27b42d6162a02925792fd05b6b86d55111cd53d4
Rich2 <rr1@tutanota.com>
2023-08-29 23:19:16 +0100

    WW2 Lunits.

--- Apps/srcStrat/pww2/Army.scala
+++ Apps/srcStrat/pww2/Army.scala
@@ -1,16 +1,24 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pww2
+import pStrat._
 
 trait Lunit extends Coloured
 {
   def polity: Polity
+  def level: LunitLevel
+  def levelStr: String
   override def colour = polity.colour
+
+  override def toString: String = levelStr + polity.toString.enParenth
 }
 
 case class Army(polity: Polity, num: Int) extends Lunit
 {
 
-  override def toString = "Army" + polity.toString.enParenth
+  override def level: LunitLevel = FieldArmy
+
+  override def levelStr: String = "Army"
+
 
   /*override def equals(other: Any): Boolean = other match
   { case that: Army => polity == that.polity
