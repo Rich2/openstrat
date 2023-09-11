980cc964a699b150f2a219355d64918ba819e36c
Rich2 <rr1@tutanota.com>
2023-09-11 14:41:57 +0100

    Added points to South America.

--- EGrid/srcPts/pAmericas/AmericasSouth.scala
+++ EGrid/srcPts/pAmericas/AmericasSouth.scala
@@ -36,13 +36,13 @@ object SouthAmericaMiddle extends EArea2("South America\nmiddle", -27.0 ll -56.0
   val curamilla: LatLong = -33.10 ll -71.74
   val lenguaDeVaca : LatLong= -30.24 ll -71.63
 
-  val sSAmericaNW: LatLong = north * 70.16.west
+  val northWest: LatLong = north * 70.16.west
 
   override val polygonLL: PolygonLL = PolygonLL(nwSAmericaES, sSAmericaNE, grutaAzul, taquari, barra, puntaDelEste, buenosAires, sBuenos,
-    SouthAmericaSouth.northEast, SouthAmericaSouth.northWest, puntaLavapie, curamilla, lenguaDeVaca, sSAmericaNW)
+    SouthAmericaSouth.northEast, SouthAmericaSouth.northWest, puntaLavapie, curamilla, lenguaDeVaca, northWest)
 }
 
-/** [[polygonLL]] graphical representation for the west of South America. Dependant on [[SouthAmericaMiddle]] [[AmericasCentral]]. */
+/** [[polygonLL]] graphical representation for the north west of South America. Dependant on [[SouthAmericaMiddle]] [[AmericasCentral]]. */
 object SouthAmericaWest extends EArea2("South America\nwest", -5.0 ll -70.0, jungle)
 { val nColumbia: LatLong = 12.19 ll -71.27
   val caracas: LatLong = 11 ll -71
@@ -56,22 +56,27 @@ object SouthAmericaWest extends EArea2("South America\nwest", -5.0 ll -70.0, jun
   val nPeru: LatLong = -5 ll -81
   val west: LatLong = -4.68 ll -81.33
   val sanLorenzo: LatLong = -1.06 ll -80.91
+  val p90: LatLong = -2.187 ll -81.008
+  val puntaTortuga = 0.775 ll -80.089
+  val p95 = 4.258 ll -77.524
   val bahiaSolano10: LatLong = 6.55 ll -77.32
 
   override val polygonLL: PolygonLL = PolygonLL(nColumbia, caracas, caicara, margaritaE, trinidadNE, nwSAmericaEN, SouthAmericaMiddle.nwSAmericaES,
-    SouthAmericaMiddle.sSAmericaNW, nChile, p60, nPeru, west, sanLorenzo, bahiaSolano10,AmericasCentral.sePanama, AmericasCentral.nePanama,
-  )
+    SouthAmericaMiddle.northWest, nChile, p60, nPeru, west, sanLorenzo, p90, puntaTortuga, p95, bahiaSolano10,AmericasCentral.sePanama,
+    AmericasCentral.nePanama)
 }
 
 /** [[polygonLL]] graphical representation for the east of South America. Dependant on [[AmericasCentral]]. */
 object SouthAmericaEast extends EArea2("South America\neast", -10.04 ll -45.81, jungle)
 { val nAmapa: LatLong = 4.39 ll -51.51
   val amazonMouthS: LatLong = -0.18 ll -49.3
+  val marajoMouth: LatLong = -0.430 ll -48.136
   val p10: LatLong = -2.34 ll -43.32
+  val pedraFurada: LatLong = -2.784 ll -40.500
   val paraiba: LatLong = -7.15 ll -34.82
   val p70: LatLong = -13.23 ll -38.94
   val jequitinhonhaMouth: LatLong = -15.84 ll -38.85
 
-  override val polygonLL: PolygonLL = PolygonLL(SouthAmericaWest.nwSAmericaEN, nAmapa, amazonMouthS, p10, paraiba, p70, jequitinhonhaMouth,
+  override val polygonLL: PolygonLL = PolygonLL(SouthAmericaWest.nwSAmericaEN, nAmapa, amazonMouthS, marajoMouth, p10, pedraFurada, paraiba, p70, jequitinhonhaMouth,
     SouthAmericaMiddle.sSAmericaNE, SouthAmericaMiddle.nwSAmericaES)
 }
