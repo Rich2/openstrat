59efc7b782db5f53cdf4ce2f6829fb8b3f4b33f2
Rich2 <rr1@tutanota.com>
2023-09-16 14:18:18 +0100

    Started PeriGame class.

---
+++ Apps/srcStrat/peri/PeriGame.scala
@@ -0,0 +1,7 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package peri;
+import prid.phex._, egrid._
+
+case class PeriGame(var scen: PeriScen, guiNations: RArr[Nation]) {
+
+}
