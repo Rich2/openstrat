a62b796afa2ab7a76a250acda5479415d5ca4d9a
Rich2 <rr1@tutanota.com>
2023-09-21 13:02:10 +0100

    PeriServe now using pWeb classes.

--- Dev/JvmSrc/PeriServe.scala
+++ Dev/JvmSrc/PeriServe.scala
@@ -1,11 +1,15 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pDev
-import jakarta.servlet.http._
+import jakarta.servlet.http._, pWeb._
 
 class PeriServe extends HttpServlet
 {
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit =
   {
-    resp.getWriter().println("<h1>This is the start for Periculo</h1>")
+    val head = HtmlHead.title("Periculo")
+    val p1 = HtmlP("This is the first paragraph, using pWeb classes.")
+    val body = HtmlBody(HtmlH1("This is the start for Periculo"), p1)
+    val page = HtmlPage(head, body)
+    resp.getWriter().println(page.out)
   }
 }
