72675b0a87e9559697e19a7ab0a28e7b5f6fa789
Rich2 <rr1@tutanota.com>
2023-09-22 16:57:11 +0100

    Using

--- Apps/JsAppsSrc/Peri2App/Peri2JsApp.scala
+++ Apps/JsAppsSrc/Peri2App/Peri2JsApp.scala
@@ -1,9 +1,15 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pSJs
-import scalajs.js.annotation._, peri._, prid.phex._
+import scalajs.js.annotation._, peri._, prid.phex._, org.scalajs.dom._
 
 @JSExportTopLevel("Peri2JsApp")
 object Peri2JsApp
 {
-  @JSExport def main(): Unit = { Peri2Gui(CanvasJs, PeriScen.init(PeriScen1), HGView(102, 1536, 24)); () }
+  @JSExport def main(): Unit =
+  { val xhr = new XMLHttpRequest()
+    xhr.open("GET", "https://37.221.93.158/")
+    xhr.onload = { (e: Event) => debvar(xhr.status) }
+    xhr.send()
+    Peri2Gui(CanvasJs, PeriScen.init(PeriScen1), HGView(102, 1536, 24)); ()
+  }
 } 
