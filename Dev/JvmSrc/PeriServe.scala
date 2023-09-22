cc36fcdc72d9426ca7246dcfa0ac34f242dd14ce
Rich2 <rr1@tutanota.com>
2023-09-22 02:08:52 +0100

    Responding with file.

--- Dev/JvmSrc/PeriServe.scala
+++ Dev/JvmSrc/PeriServe.scala
@@ -1,19 +1,27 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pDev
-import jakarta.servlet.http._, pWeb._
+import jakarta.servlet.http._, pWeb._, java.io._
 
 class PeriServe extends HttpServlet
 {
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit =
   {
     val path = req.getServletPath()
-    if (path == "/") {
-      val head = HtmlHead.title("Periculo")
-      val p1 = HtmlP("This is the first paragraph, using pWeb classes.")
-      val body = HtmlBody(HtmlCanvas.id("scanv"), HtmlScript.jsSrc("../peri2.js"), HtmlScript.main("Peri2JsApp"))
-      val page = HtmlPage(head, body)
-      resp.getWriter().println(page.out)
+    path match
+    { case "/" =>
+      { val head = HtmlHead.title("Periculo")
+        val p1 = HtmlP("This is the first paragraph, using pWeb classes.")
+        val body = HtmlBody(HtmlCanvas.id("scanv"), HtmlScript.jsSrc("../peri2.js"), HtmlScript.main("Peri2JsApp"))
+        val page = HtmlPage(head, body)
+        resp.getWriter().println(page.out)
+      }
+      case "/car" =>{
+        val file = File(getServletContext().getRealPath(File.separator) + "car.html")
+        val inpStream = new FileInputStream(file)
+        val outStream = resp.getOutputStream()
+        inpStream.transferTo(outStream)
+      }
+      case _ => resp.getWriter().println("Your path was " + path + "<br>\nServletContext: " + getServletContext().getRealPath(File.separator))
     }
-    else resp.getWriter().println("Your path was " + path)
   }
 }
