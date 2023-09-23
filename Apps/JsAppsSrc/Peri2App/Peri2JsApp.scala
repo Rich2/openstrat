029f36f89afe1448ea004fb8924dcc24b78dd21d
Rich2 <rr1@tutanota.com>
2023-09-23 01:19:23 +0100

    Added Promiss to scalaJs App.

--- Apps/JsAppsSrc/Peri2App/Peri2JsApp.scala
+++ Apps/JsAppsSrc/Peri2App/Peri2JsApp.scala
@@ -1,6 +1,6 @@
 /* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pSJs
-import scalajs.js.annotation._, peri._, prid.phex._, org.scalajs.dom._
+import scalajs.js._, annotation._, peri._, prid.phex._, org.scalajs.dom._
 
 @JSExportTopLevel("Peri2JsApp")
 object Peri2JsApp
@@ -10,6 +10,7 @@ object Peri2JsApp
     xhr.open("GET", "http ://37.221.93.158/index.html")
     xhr.onload = { (e: Event) => debvar(xhr.status) }
     xhr.send()
+    val oFile: Promise[Response] = fetch("http ://37.221.93.158/index.html")
     Peri2Gui(CanvasJs, PeriScen.init(PeriScen1), HGView(102, 1536, 24)); ()
   }
 } 
