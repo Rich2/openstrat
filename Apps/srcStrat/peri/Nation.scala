f34912455fe1e6aeea26c90ff1eab087f7555aba
Rich2 <rr1@tutanota.com>
2023-09-23 16:09:32 +0100

    Peri Nation now extends Show.

--- Apps/srcStrat/peri/Nation.scala
+++ Apps/srcStrat/peri/Nation.scala
@@ -2,13 +2,21 @@
 package ostrat; package peri
 import Colour._
 
-class Nation(val colour: Colour) extends Coloured
+class Nation(val str: String, val colour: Colour) extends Coloured with ShowSimple
+{ override def typeStr: String = "Nation"
+}
 
-object NoNation extends Nation(White)
-object NRed extends Nation(Red)
-object NViolet extends Nation(Violet)
-object NBlue extends Nation(Blue)
-object NOrange extends Nation(Orange)
+object Nation{
+  implicit val persistEv: PersistSingletons[Nation] = new PersistSingletons[Nation]("Nation") {
+    override def singletonList: List[Nation] = ???
+  }
+}
+
+object NoNation extends Nation("NoNation", White)
+object NRed extends Nation("NRed", Red)
+object NViolet extends Nation("NViolet", Violet)
+object NBlue extends Nation("NBlue", Blue)
+object NOrange extends Nation("NOrange", Orange)
 
 case class Army(nation: Nation, num: Int) extends Coloured
 {  override def colour: Colour = nation.colour
