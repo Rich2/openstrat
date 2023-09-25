046c68c5856355b2ad0d170ab5a5d7d171d20c19
Rich2 <rr1@tutanota.com>
2023-09-25 13:53:14 +0100

    Added some ZIO types.

---
+++ Dev/src/packageDev.scala
@@ -0,0 +1,18 @@
+/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat
+import zio._
+
+package object pDev
+{ def packStr: String = "This is pDev"
+  object NoneExc extends RuntimeException
+  type ETask[A] = ZIO[Any, Exception, A]
+  type IOTask[A] = ZIO[Any, java.io.IOException | NoneExc.type , A]
+  type ZOpt[A] = ZIO[Any, NoneExc.type, A]
+
+  implicit class OptionDevExtensions[A](thisOption: Option[A])
+  { def toZio: ZOpt[A] = thisOption match
+    { case Some(a) => ZIO.succeed(a)
+      case None => ZIO.fail(NoneExc)
+    }
+  }
+}
