d4c319b6d71aef393bbaf76947444e783e94a484
Rich2 <rr1.tutanota.com>
2023-10-01 14:26:44 +0100

    More ShowNing

--- Util/srcPersist/Persist4.scala
+++ Util/srcPersist/Persist4.scala
@@ -56,6 +56,12 @@ trait ShowInt4Ed extends Any with Show4ed[Int, Int, Int, Int]
 
 /** Show type class for 4 parameter case classes. */
 trait Show4ing[A1, A2, A3, A4, R] extends PersistBase4[A1,A2, A3, A4] with ShowNing[R]
+{
+
+  override def strDecs(obj: R, way: ShowStyle, maxPlaces: Int): StrArr = ???
+  //StrArr(persist1.showDecT(fArg1(obj), way, maxPlaces), persist2.showDecT(fArg2(obj), way, maxPlaces),
+    //persist3.showDecT(fArg3(obj), way, maxPlaces), persist4.showDecT(fArg4(obj), way, maxPlaces))
+}
 
 object Show4ing
 { /** Implementation class for the [[Show4ing]] trait. */
@@ -70,8 +76,6 @@ object Show4ing
     final override def syntaxDepthT(obj: R): Int = persist1.syntaxDepthT(fArg1(obj)).max(persist2.syntaxDepthT(fArg2(obj))).max(persist3.syntaxDepthT(fArg3(obj))).
       max(persist4.syntaxDepthT(fArg4(obj))) + 1
 
-    override def strDecs(obj: R, way: ShowStyle, maxPlaces: Int): StrArr = StrArr(persist1.showDecT(fArg1(obj), way, maxPlaces), persist2.showDecT(fArg2(obj), way, maxPlaces),
-      persist3.showDecT(fArg3(obj), way, maxPlaces), persist4.showDecT(fArg4(obj), way, maxPlaces))
   }
 }
 
@@ -80,20 +84,22 @@ trait ShowInt4ing[R] extends Show4ing[Int, Int, Int, Int, R]
   override def persist2: Persist[Int] = Showing.intPersistEv
   override def persist3: Persist[Int] = Showing.intPersistEv
   override def persist4: Persist[Int] = Showing.intPersistEv
+  override def syntaxDepthT(obj: R): Int = 2
 }
 
-object ShowInt4ing{
+object ShowInt4ing
+{
   /** Factory apply method for creating quick ShowDecT instances for products of 4 Ints. */
   /*def apply[R](typeStr: String, name1: String, name2: String, name3: String, name4: String, opt4: Option[Int] = None,
     opt3: Option[Int] = None, opt2: Option[Int] = None, opt1: Option[Int] = None):
   ShowInt4ingImp[R] = new ShowInt4ingImp[R](typeStr, name1, name2, name3, name4, opt4, opt3, opt2, opt1)
-
+*/
   class ShowInt4ingImp[R](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
     val opt4: Option[Int], opt3In: Option[Int] = None, opt2In: Option[Int] = None, opt1In: Option[Int] = None) extends ShowInt4ing[R] {
     val opt3: Option[Int] = ife(opt4.nonEmpty, opt3In, None)
     val opt2: Option[Int] = ife(opt3.nonEmpty, opt2In, None)
     val opt1: Option[Int] = ife(opt2.nonEmpty, opt1In, None)
-  }*/
+  }
 }
 
 /** Produces [[Show4ing]] instances for types that extend [[Show4ed]]. */
