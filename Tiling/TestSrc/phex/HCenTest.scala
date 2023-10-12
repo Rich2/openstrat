bea384f6f0d742762c965e19e21e11c982eaf994
Rich2 <rr1.tutanota.com>
2023-10-13 00:19:40 +0100

    HCenArr persist.

---
+++ Tiling/TestSrc/phex/HCenTest.scala
@@ -0,0 +1,23 @@
+/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package prid; package phex
+import utest._
+
+object HCenTest extends TestSuite
+{
+  val hc1 = HCen(6, 2)
+  val hcs1 = "HCen(6; 2)"
+  val hc2 = HCen(6, 10)
+  val hc3 = HCen(2, 14)
+  val arr1 = HCenArr(hc1, hc2, hc3)
+  val arrStr1 = "HCenArr(6, 2; 6. 10; 2, 14)"
+
+  val tests = Tests {
+    test("test1")
+    { hc1.str ==> hcs1
+      hcs1.asType[HCen] ==> Good(hc1)
+      //arr1.str ==> "6, 10"
+      arrStr1.asType[HCenArr] ==> Good(arrStr1)
+    }
+  }
+
+}
