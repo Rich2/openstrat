91ebe4b89cd7ab71660b118741b619a60c262d36
Rich2 <rr1.tutanota.com>
2023-10-27 17:43:57 +0100

    Changes to HRow.

--- Tiling/srcHex/HCenRow.scala
+++ Tiling/srcHex/HCenRow.scala
@@ -2,9 +2,10 @@
 package ostrat; package prid; package phex
 
 /** A hex tile row. Has a row number, a row starting coordinate number and the number of tiles. */
-final class HCenRow(val r: Int, val c: Int, val tNum: Int)
+final class HCenRow(val r: Int, val cStart: Int, val tNum: Int)
 {
-  def cLen: Int = tNum / 4
+  def cLen: Int = tNum * 4
+  def cEnd: Int = cStart + cLen
   def verts: HVertArr = new HVertArr(setHVertArray)
 
   /** The polygon of this tile, specified in [[HVert]] coordinates. */
@@ -13,20 +14,20 @@ final class HCenRow(val r: Int, val c: Int, val tNum: Int)
   /** Creates the backing Array[Int] of [[HVert]]s for this HCenRow. This same array can be used inside an [[HVertArr]] or a [[PolygonHC]] class. */
   def setHVertArray: Array[Int] =
   { val res = new Array[Int]((tNum * 4 + 2) * 2)
-    res.setIndex2(0, r + 1, c + 2)
+    res.setIndex2(0, r + 1, cStart + 2)
     iToForeach(2, tNum ){ i =>
-      res.setIndex2(i * 2 - 3, r + 1, c + i * 4 - 4)
-      res.setIndex2(i * 2 - 2, r + 1, c + i * 4 - 2)
+      res.setIndex2(i * 2 - 3, r + 1, cStart + i * 4 - 4)
+      res.setIndex2(i * 2 - 2, r + 1, cStart + i * 4 - 2)
     }
     iToForeach(tNum, 2, - 1) { i =>
-      res.setIndex2(tNum * 4 - i * 2 - 1, r - 1, c + i * 4 - 2)
-      res.setIndex2(tNum * 4 - i * 2,     r - 1, c + i * 4 - 4)
+      res.setIndex2(tNum * 4 - i * 2 - 1, r - 1, cStart + i * 4 - 2)
+      res.setIndex2(tNum * 4 - i * 2,     r - 1, cStart + i * 4 - 4)
     }
-    res.setIndex2(tNum * 4 - 3, r - 1, c + 2)
-    res.setIndex2(tNum * 4 - 2, r - 1, c)
-    res.setIndex2(tNum * 4 - 1, r - 1, c - 2)
-    res.setIndex2(tNum * 4,     r + 1, c - 2)
-    res.setIndex2(tNum * 4 + 1, r + 1, c)
+    res.setIndex2(tNum * 4 - 3, r - 1, cStart + 2)
+    res.setIndex2(tNum * 4 - 2, r - 1, cStart)
+    res.setIndex2(tNum * 4 - 1, r - 1, cStart - 2)
+    res.setIndex2(tNum * 4,     r + 1, cStart - 2)
+    res.setIndex2(tNum * 4 + 1, r + 1, cStart)
     res
   }
 }
