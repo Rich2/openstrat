8b23fd6a1ab495fdcc1e4538c9bd5034bcd9c75f
Rich2 <rr1@tutanota.com>
2023-10-31 01:00:54 +0000

    New stack safe version of srcLex.

--- Util/srcParse/plex/lexSrc.scala
+++ Util/srcParse/plex/lexSrc.scala
@@ -2,8 +2,8 @@
 package ostrat; package pParse; package plex
 import collection.mutable.ArrayBuffer
 
-/** Function object for creating an [[EMon]] of Refs of Token from a source. This internally uses a mutable ArrayBuffer, but the mutability is fully
- *  encapsulated. */
+/** More imperative version of Function object for creating an [[EMon]] of Refs of Token from a source. This internally uses a mutable ArrayBuffer,
+ *  but the mutability is fully encapsulated. */
 object lexSrc
 {
   def str(inp: String): EArr[Token] = apply(inp.toArray, "String")
@@ -12,47 +12,56 @@ object lexSrc
   def apply(charsIn: Array[Char], fileName: String): EArr[Token] =
   { implicit val charArr: CharArr = new CharArr(charsIn)
     val acc: ArrayBuffer[Token] = Buffer[Token]()
+    var acc2: EMon[ArrayBuffer[Token]] = Good(acc)
+    var rem: CharsOff = charArr.offsetter0
+    var posn: TextPosn = new TextPosn(fileName, 1, 1)
 
     implicit class E3Implicit (e3: EMon3[CharsOff, TextPosn, Token])
     {
-      def appendLoop: EArr[Token] = e3.flatMap { (cOff, tp, token) =>
-        acc.append(token)
-        mainLoop(cOff, tp)
+      def append3Loop: Unit = e3 match {
+        case Good3(newChars, newPosn, token) =>
+        { acc.append(token)
+          rem = newChars
+          posn = newPosn
+        }
+        case Bad3(errs) => acc2 = new Bad[ArrayBuffer[Token]](StrArr(errs))
       }
     }
 
     /** Before this was inlined getting stack overflows. */
-    inline def appendLoop(newToken: Token, charsOff: CharsOff, tp: TextPosn): EArr[Token] =
+    def appendLoop(newToken: Token, charsOff: CharsOff, tpNew: TextPosn): Unit =
     { acc.append(newToken)
-      mainLoop(charsOff, tp)
+      rem = charsOff
+      posn = tpNew
     }
 
-    def mainLoop(rem: CharsOff, tp: TextPosn): EArr[Token] = rem match
+    while(rem.length > 0 && acc2.isGood)  rem match
     { case CharsOff0() => acc.goodRefs
-      case CharsOff1Tail(';', tail) => appendLoop(SemicolonToken(tp), tail, tp.right1)
-      case CharsOff1Tail(',', tail) => appendLoop(CommaToken(tp), tail, tp.right1)
+      case CharsOff1Tail(';', tail) => appendLoop(SemicolonToken(posn), tail, posn.right1)
+      case CharsOff1Tail(',', tail) => appendLoop(CommaToken(posn), tail, posn.right1)
 
-      case CharsOff1Tail('(', tail) => appendLoop(ParenthOpenToken(tp), tail, tp.right1)
-      case CharsOff1Tail(')', tail) => appendLoop(ParenthCloseToken(tp), tail, tp.right1)
-      case CharsOff1Tail('[', tail) => appendLoop(SquareOpenToken(tp), tail, tp.right1)
-      case CharsOff1Tail(']', tail) => appendLoop(SquareCloseToken(tp), tail, tp.right1)
-      case CharsOff1Tail('{', tail) => appendLoop(CurlyOpenToken(tp), tail, tp.right1)
-      case CharsOff1Tail('}', tail) => appendLoop(CurlyCloseToken(tp), tail, tp.right1)
+      case CharsOff1Tail('(', tail) => appendLoop(ParenthOpenToken(posn), tail, posn.right1)
+      case CharsOff1Tail(')', tail) => appendLoop(ParenthCloseToken(posn), tail, posn.right1)
+      case CharsOff1Tail('[', tail) => appendLoop(SquareOpenToken(posn), tail, posn.right1)
+      case CharsOff1Tail(']', tail) => appendLoop(SquareCloseToken(posn), tail, posn.right1)
+      case CharsOff1Tail('{', tail) => appendLoop(CurlyOpenToken(posn), tail, posn.right1)
+      case CharsOff1Tail('}', tail) => appendLoop(CurlyCloseToken(posn), tail, posn.right1)
 
-      case CharsOffHead4('.', '.', '.', '.') => tp.right(4).bad(".... is not an allowed character sequence.")
-      case CharsOff3Tail('.', '.', '.', tail) => appendLoop(Dot3Token(tp), tail, tp.right3)
-      case CharsOff2Tail('.', '.', tail) => appendLoop(Dot2Token(tp), tail, tp.right2)
-      case CharsOff1Tail('.', tail) => appendLoop(DotToken(tp), tail, tp.right1)
-      case CharsOff1Tail('\n', tail) => mainLoop(tail, tp.newLine)
-      case CharsOff1Tail(h, tail) if h.isWhitespace => mainLoop(tail, tp.right1)
+      case CharsOffHead4('.', '.', '.', '.') => posn.right(4).bad(".... is not an allowed character sequence.")
+      case CharsOff3Tail('.', '.', '.', tail) => appendLoop(Dot3Token(posn), tail, posn.right3)
+      case CharsOff2Tail('.', '.', tail) => appendLoop(Dot2Token(posn), tail, posn.right2)
+      case CharsOff1Tail('.', tail) => appendLoop(DotToken(posn), tail, posn.right1)
+      case CharsOff1Tail('\n', tail) => { rem = tail; posn = posn.newLine }
+      case CharsOff1Tail(h, tail) if h.isWhitespace =>  { rem = tail; posn = posn.right1 }
 
       case CharsOff2Tail('/', '/', tail) =>
       { val len = tail.notPredicateLength(_ == '\n')
-        mainLoop(tail.drop(len), tp.right(len + 2))
+        rem = tail.drop(len)
+        posn = posn.right(len + 2)
       }
 
-      case CharsOff3Tail('\'', c1, '\'', tail) => appendLoop(CharToken(tp, c1), tail, tp.right3)
-      case CharsOff1Tail('\'', _) => tp.bad("Unclosed Character literal.")
+      case CharsOff3Tail('\'', c1, '\'', tail) => appendLoop(CharToken(posn, c1), tail, posn.right3)
+      case CharsOff1Tail('\'', _) => posn.bad("Unclosed Character literal.")
 
       case CharsOff2Tail('/', '*', tail) =>
       {
@@ -61,21 +70,22 @@ object lexSrc
           case CharsOff2Tail('*', '/', tail) => (tail, tp.right(2))
           case CharsOff1Tail(_, rem) => loop(rem, tp.right1)
         }
-        loop(tail, tp.right2).f2(mainLoop)
+        val pair = loop(tail, posn.right2)
+        rem = pair._1
+        posn = pair._2
       }
 
-      case CharsOffHead('\"') => lexStringToken(rem, tp).appendLoop
-      case CharsOffHead(LetterOrUnderscoreChar(_)) => lexIdentifierToken(rem, tp).appendLoop
+      case CharsOffHead('\"') => lexStringToken(rem, posn).append3Loop
+      case CharsOffHead(LetterOrUnderscoreChar(_)) => lexIdentifierToken(rem, posn).append3Loop
 
-      case CharsOffHead2('0', 'x') => Nat0xToken.parse(rem, tp).appendLoop
-      case CharsOffHead2('0', 'y') => Nat0yToken.parse(rem, tp).appendLoop
-      case CharsOff1Tail(DigitChar(d), tail) => lexRawNumberToken(tail, tp, d.toString, false).appendLoop
-      case CharsOff2Tail('-', DigitChar(d), tail) => lexRawNumberToken(tail, tp, d.toString, true).appendLoop
-      case CharsOffHead2('/', LetterOrUnderscoreChar(_) ) => lexPathToken(rem, tp).appendLoop
-      case CharsOffHead(c) if isOperator(c) => lexOperatorToken(rem, tp).appendLoop
-      case CharsOffHead(c) => tp.bad("Unimplemented character in main loop: " + c.toString)
+      case CharsOffHead2('0', 'x') => Nat0xToken.parse(rem, posn).append3Loop
+      case CharsOffHead2('0', 'y') => Nat0yToken.parse(rem, posn).append3Loop
+      case CharsOff1Tail(DigitChar(d), tail) => lexRawNumberToken(tail, posn, d.toString, false).append3Loop
+      case CharsOff2Tail('-', DigitChar(d), tail) => lexRawNumberToken(tail, posn, d.toString, true).append3Loop
+      case CharsOffHead2('/', LetterOrUnderscoreChar(_) ) => lexPathToken(rem, posn).append3Loop
+      case CharsOffHead(c) if isOperator(c) => lexOperatorToken(rem, posn).append3Loop
+      case CharsOffHead(c) => posn.bad("Unimplemented character in main loop: " + c.toString)
     }
-
-    mainLoop(charArr.offsetter0, new TextPosn(fileName, 1, 1))
+    acc2.map(_.toArr)
   }
 }
