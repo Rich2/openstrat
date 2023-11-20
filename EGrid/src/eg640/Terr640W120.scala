144a0519b253c2d26b276c609d7e6c363bfeb65e
Rich2 <rr1@tutanota.com>
2023-11-20 17:46:30 +0000

    More change over of parameter order on Cape.

--- EGrid/src/eg640/Terr640W120.scala
+++ EGrid/src/eg640/Terr640W120.scala
@@ -19,7 +19,7 @@ object Terr640W120 extends Long640Terrs
       TRow(124, taiga * 2),
       TRow(122, hillyTaiga, taiga),
       TRow(120, sea, hillyTaiga, savannah),
-      TRow(118, Cape(2, 4, mtain), mtain, hillySahel),
+      TRow(118, Cape.a(4, 2, mtain), mtain, hillySahel),
       TRow(116, sea, hillySavannah, hillyDesert),
       TRow(114, sea * 2, hillySahel, hillyDesert),
       TRow(112, sea * 2, hillySahel, hillySahel),
