92965d6a766e7708150df95e3b389c55d1309342
Rich2 <rr1@tutanota.com>
2023-12-02 13:42:33 +0000

    Renaming type parmeters.

--- Util/srcPersist/Persist4.scala
+++ Util/srcPersist/Persist4.scala
@@ -97,26 +97,26 @@ object ShowInt4
 
 /** Produces [[Show]] type class instances for types with 4 [[Double]] components. Note a LineSeg does not use this class although it is held in
  * memory as 4 [[Double]]s. As its logical components are 2 points. */
-abstract class ShowDbl4[R] extends Show4[Double, Double, Double, Double, R]
+abstract class ShowDbl4[A] extends Show4[Double, Double, Double, Double, A]
 { override def showEv1: Show[Double] = Show.doublePersistEv
   override def showEv2: Show[Double] = Show.doublePersistEv
   override def showEv3: Show[Double] = Show.doublePersistEv
   override def showEv4: Show[Double] = Show.doublePersistEv
-  override def syntaxDepth(obj: R): Int = 2
+  override def syntaxDepth(obj: A): Int = 2
 }
 
 /** common trait for [[Unshow]] type class instances for sum types with 4 or more components. */
-trait Unshow4Plus[A1, A2, A3, A4, R] extends Unshow3Plus[A1, A2, A3, R] with Persist4Plus[A1, A2, A3, A4]
+trait Unshow4Plus[A1, A2, A3, A4, A] extends Unshow3Plus[A1, A2, A3, A] with Persist4Plus[A1, A2, A3, A4]
 { /** The [[Unshow]] type class instance for type A4. */
   def unshow4: Unshow[A4]
 }
 
 /** UnShow class for 4 logical parameter product types. */
-trait Unshow4[A1, A2, A3, A4, R] extends Unshow4Plus[A1,A2, A3, A4, R] with Persist4[A1, A2, A3, A4]
+trait Unshow4[A1, A2, A3, A4, A] extends Unshow4Plus[A1,A2, A3, A4, A] with Persist4[A1, A2, A3, A4]
 { /** Allows this [[Unshow]] instance to create object from it's 4 components. */
-  def newT: (A1, A2, A3, A4) => R
+  def newT: (A1, A2, A3, A4) => A
 
-  protected def fromSortedExprs(sortedExprs: RArr[Expr], pSeq: IntArr): EMon[R] =
+  protected def fromSortedExprs(sortedExprs: RArr[Expr], pSeq: IntArr): EMon[A] =
   { val len: Int = sortedExprs.length
     val e1: EMon[A1] = ife(len > pSeq(0), unshow1.fromSettingOrExpr(name1, sortedExprs(pSeq(0))), opt1.toEMon)
     def e2: EMon[A2] = ife(len > pSeq(1), unshow2.fromSettingOrExpr(name2, sortedExprs(pSeq(1))), opt2.toEMon)
@@ -128,15 +128,15 @@ trait Unshow4[A1, A2, A3, A4, R] extends Unshow4Plus[A1,A2, A3, A4, R] with Pers
 
 object Unshow4
 {
-  def apply[A1, A2, A3, A4, R](typeStr: String, name1: String, name2: String, name3: String, name4: String, newT: (A1, A2, A3, A4) => R,
+  def apply[A1, A2, A3, A4, A](typeStr: String, name1: String, name2: String, name3: String, name4: String, newT: (A1, A2, A3, A4) => A,
     opt4: Option[A4] = None, opt3: Option[A3] = None, opt2: Option[A2] = None,  opt1: Option[A1] = None)(implicit unshow1: Unshow[A1],
-    unshow2: Unshow[A2], unshow3: Unshow[A3], unshow4: Unshow[A4], ct: ClassTag[R]): Unshow4[A1, A2, A3, A4, R] =
-    new Unshow4Imp(typeStr, name1, name2, name3, name4, newT, ArrPairStr[R](), opt4, opt3, opt2, opt1)(unshow1, unshow2, unshow3, unshow4)
+    unshow2: Unshow[A2], unshow3: Unshow[A3], unshow4: Unshow[A4], ct: ClassTag[A]): Unshow4[A1, A2, A3, A4, A] =
+    new Unshow4Imp(typeStr, name1, name2, name3, name4, newT, ArrPairStr[A](), opt4, opt3, opt2, opt1)(unshow1, unshow2, unshow3, unshow4)
 
-  class Unshow4Imp[A1, A2, A3, A4, R](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
-    val newT: (A1, A2, A3, A4) => R, val shortKeys: ArrPairStr[R], override val opt4: Option[A4] = None, val opt3In: Option[A3] = None,
+  class Unshow4Imp[A1, A2, A3, A4, A](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
+    val newT: (A1, A2, A3, A4) => A, val shortKeys: ArrPairStr[A], override val opt4: Option[A4] = None, val opt3In: Option[A3] = None,
     opt2In: Option[A2] = None, opt1In: Option[A1] = None)(implicit val unshow1: Unshow[A1], val unshow2: Unshow[A2], val unshow3: Unshow[A3],
-    val unshow4: Unshow[A4]) extends Unshow4[A1, A2, A3, A4, R]
+    val unshow4: Unshow[A4]) extends Unshow4[A1, A2, A3, A4, A]
   { override val opt3: Option[A3] = ife(opt4.nonEmpty, opt3In, None)
     override val opt2: Option[A2] = ife(opt3.nonEmpty, opt2In, None)
     override val opt1: Option[A1] = ife(opt2.nonEmpty, opt1In, None)
@@ -150,7 +150,7 @@ object Unshow4
   }
 }
 
-trait UnshowInt4[R] extends Unshow4[Int, Int, Int, Int, R]
+trait UnshowInt4[A] extends Unshow4[Int, Int, Int, Int, A]
 { override def unshow1: Unshow[Int] = Unshow.intEv
   override def unshow2: Unshow[Int] = Unshow.intEv
   override def unshow3: Unshow[Int] = Unshow.intEv
@@ -159,19 +159,19 @@ trait UnshowInt4[R] extends Unshow4[Int, Int, Int, Int, R]
 
 object UnshowInt4
 {
-  def apply[R](typeStr: String, name1: String, name2: String, name3: String, name4: String, newT: (Int, Int, Int, Int) => R, opt4: Option[Int] = None,
-    opt3: Option[Int] = None, opt2: Option[Int] = None, opt1: Option[Int] = None)(implicit ct: ClassTag[R]): UnshowInt4[R] =
-    new UnshowInt4Imp(typeStr, name1, name2, name3, name4, newT, ArrPairStr[R](), opt4, opt3, opt2, opt1)
+  def apply[A](typeStr: String, name1: String, name2: String, name3: String, name4: String, newT: (Int, Int, Int, Int) => A, opt4: Option[Int] = None,
+    opt3: Option[Int] = None, opt2: Option[Int] = None, opt1: Option[Int] = None)(implicit ct: ClassTag[A]): UnshowInt4[A] =
+    new UnshowInt4Imp(typeStr, name1, name2, name3, name4, newT, ArrPairStr[A](), opt4, opt3, opt2, opt1)
 
-  class UnshowInt4Imp[R](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
-    val newT: (Int, Int, Int, Int) => R, val shortKeys: ArrPairStr[R], override val opt4: Option[Int] = None, opt3In: Option[Int] = None,
-    opt2In: Option[Int] = None, opt1In: Option[Int] = None) extends UnshowInt4[R]
+  class UnshowInt4Imp[A](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
+    val newT: (Int, Int, Int, Int) => A, val shortKeys: ArrPairStr[A], override val opt4: Option[Int] = None, opt3In: Option[Int] = None,
+    opt2In: Option[Int] = None, opt1In: Option[Int] = None) extends UnshowInt4[A]
   { override val opt3: Option[Int] = ife(opt4.nonEmpty, opt3In, None)
     override val opt2: Option[Int] = ife(opt3.nonEmpty, opt2In, None)
     override val opt1: Option[Int] = ife(opt2.nonEmpty, opt1In, None)
 
-    val defaultNum: Int = None match {
-      case _ if opt3.isEmpty => 0
+    val defaultNum: Int = None match
+    { case _ if opt3.isEmpty => 0
       case _ if opt2.isEmpty => 1
       case _ if opt1.isEmpty => 2
       case _ => 3
@@ -179,7 +179,7 @@ object UnshowInt4
   }
 }
 
-trait UnshowDbl4[R] extends Unshow4[Double, Double, Double, Double, R]
+trait UnshowDbl4[A] extends Unshow4[Double, Double, Double, Double, A]
 { override def unshow1: Unshow[Double] = Unshow.doubleEv
   override def unshow2: Unshow[Double] = Unshow.doubleEv
   override def unshow3: Unshow[Double] = Unshow.doubleEv
@@ -188,14 +188,14 @@ trait UnshowDbl4[R] extends Unshow4[Double, Double, Double, Double, R]
 
 object UnshowDbl4
 {
-  def apply[R](typeStr: String, name1: String, name2: String, name3: String, name4: String, newT: (Double, Double, Double, Double) => R,
-    opt4: Option[Double] = None, opt3: Option[Double] = None, opt2: Option[Double] = None, opt1: Option[Double] = None)(implicit ct: ClassTag[R]):
-    UnshowDbl4[R] = new UnshowDbl4Imp[R](typeStr, name1, name2, name3, name4, newT, ArrPairStr[R](), opt4, opt3, opt2, opt1)
+  def apply[A](typeStr: String, name1: String, name2: String, name3: String, name4: String, newT: (Double, Double, Double, Double) => A,
+    opt4: Option[Double] = None, opt3: Option[Double] = None, opt2: Option[Double] = None, opt1: Option[Double] = None)(implicit ct: ClassTag[A]):
+    UnshowDbl4[A] = new UnshowDbl4Imp[A](typeStr, name1, name2, name3, name4, newT, ArrPairStr[A](), opt4, opt3, opt2, opt1)
 
   /** Implementation class for [[UnshowDbl4]]. */
-  class UnshowDbl4Imp[R](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
-    val newT: (Double, Double, Double, Double) => R, val shortKeys: ArrPairStr[R], val opt4: Option[Double] = None, val opt3In: Option[Double] = None,
-    opt2In: Option[Double] = None, opt1In: Option[Double] = None) extends UnshowDbl4[R]
+  class UnshowDbl4Imp[A](val typeStr: String, val name1: String, val name2: String, val name3: String, val name4: String,
+    val newT: (Double, Double, Double, Double) => A, val shortKeys: ArrPairStr[A], val opt4: Option[Double] = None, val opt3In: Option[Double] = None,
+    opt2In: Option[Double] = None, opt1In: Option[Double] = None) extends UnshowDbl4[A]
   { override val opt3: Option[Double] = ife(opt4.nonEmpty, opt3In, None)
     override val opt2: Option[Double] = ife(opt3.nonEmpty, opt2In, None)
     override val opt1: Option[Double] = ife(opt2.nonEmpty, opt1In, None)
