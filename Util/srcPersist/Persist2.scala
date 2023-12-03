edc2414e71f0bf03df104288ad5d20b496f58ffe
Rich2 <rr1@tutanota.com>
2023-12-03 01:23:29 +0000

    Removed unnecessary Unshow2 classes.

--- Util/srcPersist/Persist2.scala
+++ Util/srcPersist/Persist2.scala
@@ -166,45 +166,40 @@ object Unshow2
     opt2: Option[A2] = None, opt1: Option[A1] = None)(implicit  classTag: ClassTag[A]): Unshow2[A1, A2, A] =
     new Unshow2Imp[A1, A2, A](typeStr, name1, name2, newT, ArrPairStr[A](), opt2, opt1)(unshow1, unshow2)
 
+  /** Implementation class for the general case of [[Unshow2]]. */
   case class Unshow2Imp[A1, A2, A](typeStr: String, name1: String, name2: String, newT: (A1, A2) => A, val shortKeys: ArrPairStr[A],
     override val opt2: Option[A2], opt1In: Option[A1])(implicit val unshow1: Unshow[A1], val unshow2: Unshow[A2]) extends Unshow2[A1, A2, A]
   { override val opt1: Option[A1] = ife(opt2.nonEmpty, opt1In, None)
   }
 }
 
-trait UnshowInt2[A] extends Unshow2[Int, Int, A]
-{ override implicit def unshow1: Unshow[Int] = Unshow.intEv
+/** [[Unshow2]] with 2 [[Int]] components type class instances. */
+class UnshowInt2[A](val typeStr: String, val name1: String, val name2: String, val newT: (Int, Int) => A, val shortKeys: ArrPairStr[A],
+  override val opt2: Option[Int] = None, opt1In: Option[Int] = None) extends Unshow2[Int, Int, A]
+{ override val opt1: Option[Int] = ife(opt2.nonEmpty, opt1In, None)
+  override implicit def unshow1: Unshow[Int] = Unshow.intEv
   override implicit def unshow2: Unshow[Int] = Unshow.intEv
 }
 
 object UnshowInt2
-{
+{ /** Factory apply method for creating [[Unshow2]] with 2 [[Int]] component type class instances. */
   def apply[A](typeStr: String, name1: String, name2: String, newT: (Int, Int) => A, opt2: Option[Int] = None, opt1In: Option[Int] = None)(implicit
-    ct: ClassTag[A]): UnshowInt2[A] = new UnshowInt2Imp[A](typeStr, name1, name2, newT, ArrPairStr[A](), opt2, opt1In)
-
-  /** Implementation class for the general cases of [[UnshowDbl2]] trait. */
-  class UnshowInt2Imp[A](val typeStr: String, val name1: String, val name2: String, val newT: (Int, Int) => A, val shortKeys: ArrPairStr[A],
-    override val opt2: Option[Int] = None, opt1In: Option[Int] = None) extends UnshowInt2[A]
-  { override val opt1: Option[Int] = ife(opt2.nonEmpty, opt1In, None)
-  }
+    ct: ClassTag[A]): UnshowInt2[A] = new UnshowInt2[A](typeStr, name1, name2, newT, ArrPairStr[A](), opt2, opt1In)
 }
 
-trait UnshowDbl2[A] extends Unshow2[Double, Double, A]
-{ override implicit def unshow1: Unshow[Double] = Unshow.doubleEv
+/** [[Unshow2]] with 2 [[Double]] components type class instances. */
+class UnshowDbl2[A](val typeStr: String, val name1: String, val name2: String, val newT: (Double, Double) => A, val shortKeys: ArrPairStr[A],
+  override val opt2: Option[Double] = None, opt1In: Option[Double] = None) extends Unshow2[Double, Double, A]
+{ override val opt1: Option[Double] = ife(opt2.nonEmpty, opt1In, None)
+  override implicit def unshow1: Unshow[Double] = Unshow.doubleEv
   override implicit def unshow2: Unshow[Double] = Unshow.doubleEv
 }
 
 object UnshowDbl2
-{
+{ /** Factory apply method for creating [[Unshow2]] with 2 [[IDouble]] component type class instances. */
   def apply[A](typeStr: String, name1: String, name2: String, newT: (Double, Double) => A, opt2: Option[Double] = None,
     opt1In: Option[Double] = None)(implicit classTag: ClassTag[A]): UnshowDbl2[A] =
-    new UnshowDbl2Imp[A](typeStr, name1, name2, newT, ArrPairStr[A](), opt2, opt1In)
-
-  /** Implementation class for the general cases of [[UnshowDbl2]] trait. */
-  class UnshowDbl2Imp[A](val typeStr: String, val name1: String, val name2: String, val newT: (Double, Double) => A, val shortKeys: ArrPairStr[A],
-    override val opt2: Option[Double] = None, opt1In: Option[Double] = None) extends UnshowDbl2[A]
-  { override val opt1: Option[Double] = ife(opt2.nonEmpty, opt1In, None)
-  }
+    new UnshowDbl2[A](typeStr, name1, name2, newT, ArrPairStr[A](), opt2, opt1In)
 }
 
 class Unshow2Repeat[A1, A2, A](val typeStr: String, f: (A1, Seq[A2]) => A)(implicit val unshowA1: Unshow[A1], val unshowA2: Unshow[A2]) extends Unshow[A]
