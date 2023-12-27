73f27ae7fd24c549bcd2b28eaa55f02485d0abad
Rich2 <rr1@tutanota.com>
2023-12-27 21:53:35 +0000

    640km Island areas.

--- EGrid/src/eg640/package640.scala
+++ EGrid/src/eg640/package640.scala
@@ -2,9 +2,10 @@
 package ostrat
 import prid.phex._, egrid._
 
-/** Hex tile grids for Earth with a hex scale of 640km, a C scale of 160km. A hex tile area of 354724.005km² . A minimum island area of 59120.667km²,
- *  which includes Ireland, Hokkaidō, Hispaniola, Sakhalin, Banks Island, Sri Lanka and Tasmania but not Devon Island Alexander Island, Isla Grande de
- *  Tierra del Fuego, Severny Island North, Hawaii.  */
+/** Hex tile grids for Earth with a hex scale of 640km, a C scale of 160km. A hex tile area of 354724.005km².
+ *  Isle 234173.269km² <= 112236.892km². Honshu, Victoria Island, Great Britain, Ellesmere Island, Sulawesi, South Island(NZ), Java.
+ *  Isle8 112236.892km² <= 67896.391km².
+ *  Isle6 67896.391km² <= 41915.629km². */
 package object eg640
 {
   val fullTerrs: RArr[Long640Terrs] = RArr(Terr640E0, Terr640E30, Terr640E60, Terr640E90, Terr640E120, Terr640E150,Terr640E180, Terr640W150,
