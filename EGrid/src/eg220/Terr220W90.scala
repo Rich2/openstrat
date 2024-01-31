821af3177d3c865445ef63062721756eaacb3893
Rich2 <rr1@tutanota.com>
2024-01-31 13:52:22 +0000

    220km Canada terrain.

--- EGrid/src/eg220/Terr220W90.scala
+++ EGrid/src/eg220/Terr220W90.scala
@@ -18,8 +18,8 @@ object Terr220W90 extends Long220Terrs
       VRow(163, MouthRt(9720, HVUp, 7, lake), BendInOut(9738, HVUR, 13, 6), BendInOut(9740, HVDL, 13, 4)),
       TRow(162, taiga, wetTaiga, taiga, wetTaiga * 3, taiga, wetTaiga * 2),
       VRow(161, BendIn(9718, HVDR, 6, lake), BendIn(9720, HVUL, 6, lake), BendInOut(9740, HVUR, 11, 7), MouthLt(9742, HVDR, 7)),
-      TRow(160, land, wetTaiga, taiga, wetTaiga * 3, taiga * 2, wetTaiga),//Unchecked
-      TRow(158, land * 2, taiga * 7),//Unchecked
+      TRow(160, savannah, wetTaiga, taiga, wetTaiga * 3, taiga * 2, wetTaiga),
+      TRow(158, savannah * 2, wetTaiga, sea  * 6),
       TRow(156, land * 2, taiga * 3, lake, taiga * 4),//Unchecked
       TRow(154, land * 3, taiga * 7),//Unchecked
     )
