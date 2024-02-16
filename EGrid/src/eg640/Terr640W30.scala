7959209790dbb25dd914a3581a1d264ee21def73
Rich2 <rr1@tutanota.com>
2024-02-16 02:05:11 +0000

    Replacing Capes in 640km terrain.

--- EGrid/src/eg640/Terr640W30.scala
+++ EGrid/src/eg640/Terr640W30.scala
@@ -12,10 +12,15 @@ object Terr640W30 extends Long640Terrs
   val help = new WTerrSetter(grid, terrs, sTerrs, corners)
   {
     override val rowDatas: RArr[RowBase] = RArr(
+      VRow(131, MouthRt(11780, HVUp)),
       TRow(130, ice),
-      TRow(128, Cape(2, 1, ice)),
-      TRow(126, ice, Cape(3, 4, hillyTundra)),
-      VRow(121, Mouth(11780, HVDn)),
+      VRow(129, BendOut(11778, HVDR), BendIn(11780, HVUL, 13)),
+      TRow(128, ice),
+      VRow(127, BendInOut(11776, HVDR, 6, 4), BendIn(11778, HVUL, 9)),
+      TRow(126, ice, hillyTundra),
+      VRow(125, BendOut(11774, HVDR), ThreeUp(11776, 11, 0, 13), BendIn(11778, HVUp, 8)),
+      VRow(123, BendIn(11772, HVUp, 13), BendIn(11774, HVUL, 9)),
+      //VRow(121, Mouth(11780, HVDn)),
       VRow(109, Mouth(11784, HVUR)),
       TRow(108, sea * 3, Cape(4, 2, desert)),
       VRow(107, BendOut(11784, HVDL)),
