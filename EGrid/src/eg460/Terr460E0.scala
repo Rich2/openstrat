a6f08cadfc91bbb56080f4bfbe4532a7d40cd063
Rich2 <rr1@tutanota.com>
2024-02-20 01:29:52 +0000

    460km terrain.

--- EGrid/src/eg460/Terr460E0.scala
+++ EGrid/src/eg460/Terr460E0.scala
@@ -23,7 +23,7 @@ object Terr460E0 extends Long460Terrs
       VRow(141, BendIn(512, HVUL, 13, SeaIceWinter)),
       VRow(137, MouthLt(514, HVUp)),
       TRow(136, sea * 2, hillyTaiga),
-      VRow(135, MouthRt(506, HVUR), BendIn(512, HVDR, 13), BendOut(514, HVUL), BendAllOld(520, HVDR)),
+      VRow(135, MouthRt(506, HVUR), BendIn(512, HVDR, 13), BendOut(514, HVUL), Bend(520, HVDR, 4, 5, SeaIceWinter, sea)),
       TRow(134, sea, mtain, land),
       VRow(133, BendIn(506, HVDR, 13), MouthLt(508, HVUR), BendIn(512, HVUR, 13), BendOut(514, HVDL)),
       TRow(132, hilly, sea, forest),
