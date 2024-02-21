c975bee02e21ee64747b3774e15d07df0547bdfe
Rich2 <rr1@tutanota.com>
2024-02-21 19:24:35 +0000

    Added parameter to TileExs main.

--- Tiling/ExsSrc/pZero/TilingTryApp.scala
+++ Tiling/ExsSrc/pZero/TilingTryApp.scala
@@ -2,15 +2,18 @@
 package ostrat; package pZero
 import prid.phex._
 /** Scrap pad console app for the Tiling module. */
-object TilingTryApp  extends App
+object TilingTryApp
 {
-  deb("Hello TilingTryApp")
-  val scs = DblArr(1300, 1000, 640, 460, 320, 220, 160, 120, 80)
+  @main def run(inp: Int): Unit =
+  {
+    deb("Hello TilingTryApp")
+    val scs = DblArr(1300, 1000, 640, 460, 320, 220, 160, 120, 80)
+    val sc: Double = scs.find(_ == inp).getOrElse(1000)
+    val isl = DblArr(16, 13, 9.5, 8.5, 7.5, 6.5, 5.5, 4.5, 3.5, 2.5)
 
-  val isl = DblArr(16, 13, 9.5, 8.5, 7.5, 6.5, 5.5, 4.5, 3.5, 2.5)
-  val sc: Int = 1000
-  val a460 = tileScaleToArea(sc)
-  println(sc + "km")
-  val isl1 = isl.map{ i =>(i,  i.squared * a460 / 256) }.map (p => p._1 + ", " + p._2)
-  isl1.foreach(println)
+    val a460 = tileScaleToArea(sc)
+    println(sc + "km")
+    val isl1 = isl.map{ i => (i, i.squared * a460 / 256) }.map(p => p._1 + ", " + p._2)
+    isl1.foreach(println)
+  }
 }
