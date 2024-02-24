330ff3cddeb223139dab33560bffbbefa1b13f10
Rich2 <rr1@tutanota.com>
2024-02-24 12:59:40 +0000

    Created Steppe Climate.

--- EGrid/src/egrid/Climate.scala
+++ EGrid/src/egrid/Climate.scala
@@ -2,7 +2,7 @@
 package ostrat; package egrid
 import Colour._
 
-/** Describes climate and biome. */
+/** Describes climate with broad brush strokes for strategy games. Can be used as a crude proxy for biome. Uses both classical Köppen and Köppen–Trewartha. */
 trait Climate extends TellSimple
 { override def typeStr: String = "Climate"
   def colour: Colour
@@ -20,61 +20,68 @@ object Climate
 
 /** Trewartha Fi Ice cap classification. All year round snow. */
 case object IceCap extends Climate
-{ override def str = "IceCap"
-  override def colour = White
+{ override def str: String = "IceCap"
+  override def colour: Colour = White
 }
 
 
 /** Trewartha Ft Tundra climate classification. Too cold and dry for [[Forest]] */
 case object Tundra extends Climate
-{ override def str = "Tundra"
-  override def colour = Plum.average(Thistle)
+{ override def str: String = "Tundra"
+  override def colour: Colour = Plum.average(Thistle)
 }
 
-/** Trewartha E Boreal climate classification. Subartic or Boreal Taiga climate. Normally has [[Forest]]. */
+/** Trewartha E Boreal climate classification. Subartic or Boreal Taiga climate. Normally has [[Forest]]. Assumes a northern latitude with a significantly
+ *  colder northern winter. */
 case object Boreal extends Climate
-{ override def str = "Boreal"
-  override def colour = DarkCyan
+{ override def str: String = "Boreal"
+  override def colour: Colour = DarkCyan
 }
 
 /** Trewartha Dc Temperate Continental classification. No intense dry season. The coldest monthly mean temperature reaches below 0 °C. */
 case object Continental extends Climate
 { def colour: Colour = LightGreen
-  override def str = "Continental"
+  override def str: String = "Continental"
 }
 
 /** Trewartha Do Temperate oceanic climate classification. No intense dry season. The coldest monthly mean temperature reaches is not below 0 °C. */
 case object Temperate extends Climate
 { def colour: Colour = LightGreen
-  override def str = "Temperate"
+  override def str: String = "Temperate"
 }
 
 /** Köppen BWk cold desert climate classification. Average annual temperature less than 18°C. */
 case object DesertCold extends Climate
-{ override def str = "DesertCold"
-  override def colour = LemonChiffon
+{ override def str: String = "DesertCold"
+  override def colour: Colour = LemonChiffon
 }
 
 /** Köppen BWh hot desert climate classification. Average annual temperature above 18°C. */
 case object Desert extends Climate
-{ override def str = "Desert"
-  override def colour = LemonChiffon
+{ override def str: String = "Desert"
+  override def colour: Colour = LemonChiffon
 }
 
 /** Semi Desert climate and biome. */
 case object Sahel extends Climate
-{ override def str = "Sahel"
-  override def colour = LemonChiffon.average(YellowGreen)
+{ override def str: String = "Sahel"
+  override def colour: Colour = LemonChiffon.average(YellowGreen)
 }
 
-/** Savannah, steppe and prairie biome. */
+/** Köppen BSk Dry Semi-Arid Cold climate classification. Assumes a northern latitude with a significantly colder northern winter. */
+case object Steppe extends Climate
+{ override def str: String = "Steppe"
+  override def colour: Colour = YellowGreen
+}
+
+/** Köppen BSh Dry Semi-Arid hot climate classification. */
 case object Savannah extends Climate
-{ override def str = "Savannah"
-  override def colour = YellowGreen
+{ override def str: String = "Savannah"
+  override def colour: Colour = YellowGreen
 }
 
 /** Tropical climate. High precipitation through out year. */
 case object Tropical extends Climate
-{ override def str = "Tropical"
-  override def colour = YellowGreen
+{ override def str: String = "Tropical"
+  override def colour: Colour = YellowGreen
 }
