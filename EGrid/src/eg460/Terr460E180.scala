8b8a4d9950097bc1f93a7b3cf462378fc11d6f40
Rich2 <rr1@tutanota.com>
2024-03-15 00:38:38 +0000

    #268, #269, #270. 460km terrain.

--- EGrid/src/eg460/Terr460E180.scala
+++ EGrid/src/eg460/Terr460E180.scala
@@ -11,21 +11,20 @@ object Terr460E180 extends Long460Terrs
   override val hexNames: LayerHcRefGrid[String] = LayerHcRefGrid[String]()
 
   val help = new WTerrSetter(grid, terrs, sTerrs, corners)
-  {
-    override val rowDatas: RArr[RowBase] = RArr(
-      TRow(146, SeaIcePerm),
-      TRow(144, SeaIcePerm),
-      TRow(142, SeaIceWinter),
-      TRow(140, SeaIceWinter * 2),
-      VRow(139, Source(6656, HVUR, 2, 4, siceWin), BendIn(6658, HVDn, 13, siceWin), BendIn(6660, HVDL, 13, siceWin)),
-      TRow(138, mtainTundra * 2),
-      VRow(137, BendOut(6660, HVUR, 7, siceWin), BendIn(6662, HVDL, 13, siceWin)),
-      TRow(136, mtainTundra, hillyTundra * 2),
-      VRow(135, BendOut(6656, HVDR), ThreeDown(6660, 6, 13, 0, siceWin), Bend(6662, HVUL, 13, 2, siceWin)),
-      TRow(134, mtainTundra, sea, hillyTundra),
-      VRow(133, BendOut(6650, HVDR), BendIn(6660, HVUR, 13, siceWin), BendOut(6662, HVDL, 6, siceWin)),
-      VRow(131, SetSep(6649)),
-      TRow(130, SepB()),
+  { override val rowDatas: RArr[RowBase] = RArr(
+    TRow(146, SeaIcePerm),
+    TRow(144, SeaIcePerm),
+    TRow(142, SeaIceWinter),
+    TRow(140, SeaIceWinter * 2),
+    VRow(139, Source(6656, HVUR, 2, 4, siceWin), BendIn(6658, HVDn, 13, siceWin), BendIn(6660, HVDL, 13, siceWin)),
+    TRow(138, mtainTundra * 2),
+    VRow(137, BendOut(6660, HVUR, 7, siceWin), BendIn(6662, HVDL, 13, siceWin)),
+    TRow(136, mtainTundra, hillyTundra * 2),
+    VRow(135, BendOut(6656, HVDR), ThreeDown(6660, 6, 13, 0, siceWin), Bend(6662, HVUL, 13, 2, siceWin)),
+    TRow(134, mtainTundra, sea, hillyTundra),
+    VRow(133, BendOut(6650, HVDR), BendIn(6660, HVUR, 13, siceWin), BendOut(6662, HVDL, 6, siceWin)),
+    VRow(131, SetSep(6649)),
+    TRow(130, SepB()),
     )
   }
   help.run
--- EGrid/src/eg460/package460.scala
+++ EGrid/src/eg460/package460.scala
@@ -3,15 +3,18 @@ package ostrat
 import prid.phex._, egrid._
 
 /** Hex tile grids for Earth with a hex scale of 460km, a C scale of 115km. A hex tile area of 183250.975km².
- *  A maximum Isle size of 120974.276km² excludes South Island(NZ), Java
- *  Isle10 64603.127km² => 120974.276km².
- *  Isle9 51718.292km² => 64603.127km²
- *  Isle8 40265.106km² => 51718.292km².
- *  Isle7 30243.569km² => 40265.106km²
- *  Isle6 21653.679km² => 30243.569km².
- *  Isle5 14495.438km² => 21653.679km².
- *  Isle4 8768.845km² => 14495.438km².
- *  Isle3 4473.900km² => 8768.845km². */
+ *  A maximum Isle size of 130458.946km² excludes Java 128297km², South Island(NZ) 150437km²,=.
+ *  [[Isle13]] 111847.519km² => 130458.946km².
+ *  [[Isle12]] 94667.740km² => 111847.519km².
+ *  [[Isle11]] 78919.609km² => 94667.740km².
+ *  [[Isle10]] 64603.127km² => 78919.609km².
+ *  [[Isle9]] 51718.292km² => 64603.127km².
+ *  [[Isle8]] 40265.106km² => 51718.292km².
+ *  [[Isle7]] 30243.569km² => 40265.106km².
+ *  [[Isle6]] 21653.679km² => 30243.569km².
+ *  [[Isle5]] 14495.438km² => 21653.679km².
+ *  [[Isle4]] 8768.845km² => 14495.438km².
+ *  [[Isle3]] 4473.900km² => 8768.845km². */
 package object eg460
 {
   val fullTerrs: RArr[Long460Terrs] = RArr(Terr460E0, Terr460E30, Terr460E60, Terr460E90, Terr460E120, Terr460E150,Terr460E180, Terr460W150,
