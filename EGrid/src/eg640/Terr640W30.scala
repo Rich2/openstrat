558cfbcd986e00dc25cdcb8f80409a6da141f691
Rich2 <rr1@tutanota.com>
2024-03-24 14:08:00 +0000

    460km terrain.

--- EGrid/src/eg640/Terr640W30.scala
+++ EGrid/src/eg640/Terr640W30.scala
@@ -14,27 +14,26 @@ object Terr640W30 extends Long640Terrs
   override val hexNames: LayerHcRefGrid[String] = LayerHcRefGrid[String]()
 
   val help = new WTerrSetter(grid, terrs, sTerrs, corners)
-  {
-    override val rows: RArr[RowBase] = RArr(
-      VRow(131, MouthRt(11780, HVUp)),
-      TRow(130, ice),
-      VRow(129, BendOut(11778, HVDR), BendIn(11780, HVUL, 13)),
-      TRow(128, ice),
-      VRow(127, Bend(11776, HVDR, 10, 4), ThreeUp(11778, 0, 6, 9)),
-      TRow(126, ice, hillyTundra),
-      VRow(125, BendOut(11774, HVDR), ThreeUp(11776, 11, 0, 13), BendIn(11778, HVUp, 6), SourceRt(11780, HVDL)),
-      VRow(123, BendIn(11772, HVUp, 13), ThreeDown(11774, 13, 0, 13)),
-      TRow(122, SepB(siceWin)),
-      VRow(121, SetSep(11771)),
-      VRow(119, BendIn(11770, HVUL, 13)),
-      VRow(109, MouthLt(11782, HVUp, 7)),
-      TRow(108, sea * 3, deshot),
-      VRow(107, BendIn(11782, HVUR, 13), BendMin(11784, HVDL)),
-      TRow(106, sea * 4, sahel),
-      VRow(101, MouthLt(11766, HVDR, 7), MouthLt(11788, HVUL, 7)),
-      VRow(99, MouthRt(11770, HVUL, 7), MouthLt(11772, HVDR, 7)),
-      TRow(98, hillySahel),
-      TRow(96, hillySavannah, hillySavannah),
+  { override val rows: RArr[RowBase] = RArr(
+    VRow(131, MouthRt(11780, HVUp)),
+    TRow(130, ice),
+    VRow(129, BendOut(11778, HVDR), BendIn(11780, HVUL, 13)),
+    TRow(128, ice),
+    VRow(127, Bend(11776, HVDR, 10, 4), ThreeUp(11778, 0, 6, 9)),
+    TRow(126, ice, hillyTundra),
+    VRow(125, BendOut(11774, HVDR), ThreeUp(11776, 11, 0, 13), BendIn(11778, HVUp, 6), SourceRt(11780, HVDL)),
+    VRow(123, BendIn(11772, HVUp, 13), ThreeDown(11774, 13, 0, 13)),
+    TRow(122, SepB(siceWin)),
+    VRow(121, SetSep(11771)),
+    VRow(119, BendIn(11770, HVUL, 13)),
+    VRow(109, MouthLt(11782, HVUp, 7)),
+    TRow(108, sea * 3, deshot),
+    VRow(107, BendIn(11782, HVUR, 13), BendMin(11784, HVDL)),
+    TRow(106, sea * 4, sahel),
+    VRow(101, MouthLt(11766, HVDR, 7), MouthLt(11788, HVUL, 7)),
+    VRow(99, MouthRt(11770, HVUL, 7), MouthLt(11772, HVDR, 7)),
+    TRow(98, hillySahel),
+    TRow(96, hillySavannah, hillySavannah),
     )
   }
   help.run
