63d25e1fdb6c87dfe43099a564173511869a084d
Rich2 <rr1@tutanota.com>
2024-03-30 16:09:18 +0000

    160km terrain.

--- EGrid/src/eg160/package160.scala
+++ EGrid/src/eg160/package160.scala
@@ -3,12 +3,17 @@ package ostrat
 import prid.phex._, egrid._
 
 /** Hex tile grids for Earth with a hex scale of 160km, a C scale of 40km. A tile area of 22170.250km.
- * km² => km²
- * km² => km²
- * km² => km²
- * km² => km²
- * 1060.881km² => km²
- * 541.265km² =>1060.881km² South Uist 320.3km² North Uist 303km² Benbcuala 82.03km² Berneray 10.1km² Grimsay 8.33km² = 723.76km² */
+ * [[Isle13]] km² => km².
+ * [[Isle12]] km² => km².
+ * [[Isle11]] km² => km².
+ * [[Isle10]] km² => km².
+ * [[Isle9]] 6257.033km² => km².
+ * [[Isle8]] 4871.392km² => 6257.033km².
+ * [[Isle7]] 3658.957km² => 4871.392km².
+ * [[Isle6]] 2619.726km² => 3658.957km².
+ * [[Isle5]] 1753.701km² => 2619.726km².
+ * [[Isle4]] 1060.881km² => 1753.701km².
+ * [[Isle3]] 541.265km² => 1060.881km². */
 package object eg160
 {
   val fullTerrs: RArr[Long160Terrs] = RArr(Terr160E0, Terr160E30, null, null, Terr160E120, Terr160E150, null, null, Terr160W120, Terr160W90, Terr160W60, Terr160W30)
