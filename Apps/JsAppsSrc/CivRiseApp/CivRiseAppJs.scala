cf859a2c28287402e5606542beb8e406c183e9ca
Rich2 <rr1@tutanota.com>
2024-05-01 10:38:56 +0100

    Changes for Civrise Js deployment.

---
+++ Apps/JsAppsSrc/CivRiseApp/CivRiseAppJs.scala
@@ -0,0 +1,8 @@
+/* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package pSJs
+import scalajs.js.annotation._, pCiv._
+
+@JSExportTopLevel("CivRiseAppJs")
+object CivRiseAppJs
+{ @JSExport def main(): Unit = CivGui(CanvasJs, Civ2)
+}
