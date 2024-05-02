2dcfb76970c85e15e2c3cba3cbec43da5d6c3f22
Rich2 <rr1@tutanota.com>
2024-05-02 23:03:03 +0100

    Created PomDep.

--- Geom/srcWeb/PomProject.scala
+++ Geom/srcWeb/PomProject.scala
@@ -1,18 +1,28 @@
 /* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pWeb
 
-class PomProject(val artifactSTr: String, versionStr: String, val groupStr: String, val modelVersionStr: String = "4.0.0") extends XmlElem
-{
-  val artifactId: XmlElem = XmlElemSimple("artifactId", artifactSTr)
-  val groudId: XmlElem = XmlElemSimple("groupId", groupStr)
-  val version: XmlVersion = XmlVersion(versionStr)
-  val modelVersion = XmlElemSimple("modelVersion", modelVersionStr)
+class PomProject(val groupStr: String, val artifactStr: String, versionStr: String, val modelVersionStr: String = "4.0.0") extends XmlMulti
+{ def artifactId: ArtifactId = ArtifactId(artifactStr)
+  val groudId: GroupId = GroupId(groupStr)
+  def version: XmlVersion = XmlVersion(versionStr)
+  def modelVersion = XmlElemSimple("modelVersion", modelVersionStr)
   override def tag: String = "project"
   override def attribs: RArr[XmlAtt] = RArr()
 
-  /** The content of this XML / HTML element. */
-  override def contents: RArr[XCon] = RArr(modelVersion, artifactId, groudId, version)
-
-  /** Returns the XML / HTML source code, formatted according to the input. This allows the XML to be indented according to its context. */
-  override def out(indent: Int, line1Delta: Int, maxLineLen: Int): String = ???
+  override def contents: RArr[XCon] = RArr(modelVersion, groudId, artifactId, version)
 }
+
+class PomDep(val groupStr: String, val artifactStr: String, val versionStr: String) extends XmlMulti
+{ override def tag: String = "dependency"
+  override def attribs: RArr[XmlAtt] = RArr()
+  def artifactId: ArtifactId = ArtifactId(artifactStr)
+  val groudId: GroupId = GroupId(groupStr)
+  def version: XmlVersion = XmlVersion(versionStr)
+  override def contents: RArr[XCon] = RArr(groudId, artifactId, version)
+}
+
+class ArtifactId(artifactStr: String) extends XmlElemSimple("artifactId", artifactStr)
+object ArtifactId { def apply(artifactStr: String): ArtifactId = new ArtifactId(artifactStr) }
+
+class GroupId(groupStr: String) extends XmlElemSimple("groupId", groupStr)
+object GroupId { def apply(groupStr: String): GroupId = new GroupId(groupStr) }
