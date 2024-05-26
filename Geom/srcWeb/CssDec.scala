7fa821032cee2f233875d92975883c81ff5a248f
Rich2 <rr1@tutanota.com>
2024-05-26 16:44:06 +0100

    Renaming and ScalaDoc of CssDecs.

--- Geom/srcWeb/CssDec.scala
+++ Geom/srcWeb/CssDec.scala
@@ -4,18 +4,26 @@ import geom._
 
 /** CSS declaration */
 trait CssDec
-{ def prop: String
+{ /** The CSS property */
+  def prop: String
+
+  /** The CSS value output. */
   def valueStr: String
+
+  /** The CSS code output. */
   def out: String = prop + ": " + valueStr + ";"
 }
 
+/** Standard CSS deccalaration which takes a [[CssVal]] to produce the CSS value output. */
 trait CssDecStd extends CssDec
-{ def value: CssVal
+{ /** The CSS declaration value */
+  def value: CssVal
+
   final override def valueStr: String = value.str
 }
 
-/** CSS background-color property. */
-case class CssColour(colour: Colour) extends CssDec
+/** CSS color declaration. */
+case class DecColour(colour: Colour) extends CssDec
 { override def prop: String = "color"
   override def valueStr: String = colour.webStr
 }
@@ -26,33 +34,31 @@ case class CssBGColour(colour: Colour) extends CssDec
   override def valueStr: String = colour.webStr
 }
 
-object CssBGColour
-{
-
-}
-
-case class CssTextAlign(align: TextAlign) extends CssDec
+/** CSS text-align declaration. */
+case class DecTextAlign(align: TextAlign) extends CssDec
 { override def prop: String = "text-align"
   override def valueStr: String = align.jsStr
 }
 
-object CssTextCentre extends CssTextAlign(CenAlign)
+/** CSS text-align declaration set to center. */
+object DecTextCentre extends DecTextAlign(CenAlign)
 
-case class CssFontSize(value: CssVal) extends CssDecStd
+/** CSS font-size declaration. */
+case class DecFontSize(value: CssVal) extends CssDecStd
 { override def prop: String = "font-size"
 }
 
-
-case class CssSpaces(value: CssVal) extends CssDecStd
+/** CSS white-space declaration. */
+case class DecSpaces(value: CssVal) extends CssDecStd
 { override def prop: String = "white-space"
 }
 
-object CssSpaces
-{
-  def apply(str: String): CssSpaces = new CssSpaces(CssVal(str))
+object DecSpaces
+{ def apply(str: String): DecSpaces = new DecSpaces(CssVal(str))
 }
 
-object CssNoWrap extends CssSpaces(CssVal("nowrap"))
+/** CSS white-space declaration set to nowrap. */
+object DecNoWrap extends DecSpaces(CssVal("nowrap"))
 
 /** Css max-width declaration. */
 case class DecMaxWidth(value: CssVal) extends CssDecStd
@@ -64,13 +70,22 @@ case class DecDisplay(value: CssVal) extends CssDecStd
 { override def prop: String = "display"
 }
 
+/** Css Display declaration set to inline-block. */
 object DispInBlock extends DecDisplay(CssInBlock)
+
+/** Css Display declaration set to block. */
 object DispBlock extends DecDisplay(CssBlock)
+
+/** Css Display declaration set to none. */
 object DispNone extends DecDisplay(CssNone)
 
+/** Css text-align declaration. */
 case class DecAlign(value: CssVal) extends CssDecStd
 { override def prop: String = "text-align"
 }
 
+/** Css text-align declaration set to center. */
 object DecAlignCen extends DecAlign(CssCentre)
+
+/** Css text-align declaration set to start. */
 object DecAlignStart extends DecAlign(CssStart)
--- Util/ExsSrc/CssOpenstrat.scala
+++ Util/ExsSrc/CssOpenstrat.scala
@@ -6,19 +6,19 @@ import Colour._
 object CssOpenstrat extends CssSpec
 {
   override def rules: RArr[CssRuleLike] = RArr(
-    CssBody(CssBGColour(Ivory), CssFontSize(18.px)), CssH1(CssTextCentre, CssFontSize(44.px)),
+    CssBody(CssBGColour(Ivory), DecFontSize(18.px)), CssH1(DecTextCentre, DecFontSize(44.px)),
     CssClassesRule("central", DecMaxWidth(68.em) %: DecMargLeftRight(CssAuto)),
     CssClassesRule("main", DecMaxWidth(68.em) %: DecMargLeftRight(CssAuto)),
     CssOl(DecPadLeft(1.em)), CssRule("ol li", CssMargTopBot(2.em)), CssRule("ul li", DecMarg(0.25.em)), CssRule("ol > li", CssMargTopBot(1.em)),
-    CssClassesRule("lexical", CssColour(DarkBlue)),
-    CssCode(CssColour(DarkRed)), CssClassesRule("sbt", CssColour(DarkGreen)), CssClassesRule("folder", CssColour(DarkBlue)),
-    CssClassesRule("path", CssColour(DarkBlue), CssNoWrap), CssClassesRule("bash", CssColour(DarkRed), CssNoWrap),
-    CssClassesRule("scala", CssColour(Black), CssNoWrap, CssFontSize(10.px)), CssObjectRule("centreBlock", DispBlock %: DecMargLeftRightAuto),
+    CssClassesRule("lexical", DecColour(DarkBlue)),
+    CssCode(DecColour(DarkRed)), CssClassesRule("sbt", DecColour(DarkGreen)), CssClassesRule("folder", DecColour(DarkBlue)),
+    CssClassesRule("path", DecColour(DarkBlue), DecNoWrap), CssClassesRule("bash", DecColour(DarkRed), DecNoWrap),
+    CssClassesRule("scala", DecColour(Black), DecNoWrap, DecFontSize(10.px)), CssObjectRule("centreBlock", DispBlock %: DecMargLeftRightAuto),
     CssRule("td th", DecPadRight(2.em), DecAlignStart),
     minMed
   )
 
-  def minMed: CssMedia = new CssMedia("min-width:50em")
+  def minMed: CssMedia = new CssMedia("min-width: 50em")
   {
     override def rules: RArr[CssRule] = RArr(
       CssObjectRule("topmenu li", DispInBlock, CssBGColour(Colour(0xFFDDDDDD)), DecPad(0.2.em), DecBorder(CssSolid(Yellow))),
@@ -26,23 +26,4 @@ object CssOpenstrat extends CssSpec
       CssObjectRule("bottommenu", DispNone)
     )
   }
-
-  override def endStr: String =
-  """
-  @media (min-width:50em)
-  {
-    #topmenu li
-    { display: inline-block;
-      background-color: #dddddd;
-      padding: 0.2em;
-      border: 0.2em solid yellow;
-    }
-
-    #topmenu { text-align: center; max-width: 100em;}
-    #bottommenu {display: none;}
-  }
-
-  td, th {padding-right: 2.0em;
-  text-align: start;
-}"""
 }
