a918c483655a7faa935b09072c6a9a5de53cfb9b
Rich2 <rr1@tutanota.com>
2024-06-13 14:38:12 +0100

    More ServRaw.

--- Dev/JvmSrc/ServRaw.scala
+++ Dev/JvmSrc/ServRaw.scala
@@ -1,18 +1,18 @@
 /* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pDev
-import java.net.ServerSocket
+import java.net.*, java.io.{ BufferedReader, InputStreamReader }
 
 object ServRaw extends App
 { deb("Starting")
-  val sock = new ServerSocket(8080)
-  val cl = sock.accept()
-  val st1 = new java.io.BufferedReader(new java.io.InputStreamReader(cl.getInputStream()))
+  val sock: ServerSocket = new ServerSocket(8080)
+  val cl: Socket = sock.accept()
+  val readbuf: BufferedReader = new java.io.BufferedReader(new java.io.InputStreamReader(cl.getInputStream()))
   var line: String = null
-  while ( { line = st1.readLine; line != null && line != "" }) {
+  while ( { line = readbuf.readLine; line != null && line != "" }) {
     println(line)
   }
   cl.getOutputStream.write("HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, Server with Http!".getBytes)
-  st1.close
+  readbuf.close
   sock.close()
   deb("Finishing")
 }
