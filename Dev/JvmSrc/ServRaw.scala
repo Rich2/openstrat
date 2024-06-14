f2fc340005543a687ce7164f0e955f29e60e311f
Rich2 <rr1@tutanota.com>
2024-06-14 07:34:46 +0100

    ServRaw now multi threaded.

--- Dev/JvmSrc/ServRaw.scala
+++ Dev/JvmSrc/ServRaw.scala
@@ -4,15 +4,27 @@ import java.net.*, java.io.{ BufferedReader, InputStreamReader }
 
 object ServRaw extends App
 { deb("Starting")
-  val sock: ServerSocket = new ServerSocket(8080)
-  val cl: Socket = sock.accept()
-  val readbuf: BufferedReader = new java.io.BufferedReader(new java.io.InputStreamReader(cl.getInputStream()))
-  var line: String = null
-  while ( { line = readbuf.readLine; line != null && line != "" }) {
-    println(line)
+  val servSock: ServerSocket = new ServerSocket(8080)
+  var numConns: Int = 0
+  while(true)
+  { val sock: Socket = servSock.accept()
+    val conn = new ConnSesh(numConns, sock)
+    conn.run()
+    numConns += 1
   }
-  cl.getOutputStream.write("HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, Server with Http!".getBytes)
-  readbuf.close
-  sock.close()
+  servSock.close()
   deb("Finishing")
+}
+
+class ConnSesh(val cNum: Int, val sock: Socket) extends Runnable
+{
+  override def run(): Unit =
+  { val readbuf: BufferedReader = new java.io.BufferedReader(new java.io.InputStreamReader(sock.getInputStream()))
+    var line: String = null
+    while ( { line = readbuf.readLine; line != null && line != "" }) {
+      println(line)
+    }
+    sock.getOutputStream.write(s"HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, Server with Http! Connection: $cNum".getBytes)
+    readbuf.close
+  }
 }
