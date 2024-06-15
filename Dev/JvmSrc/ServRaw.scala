ea91dfbb939eef3b65477c16a1afb3d6b28a65a6
Rich2 <rr1@tutanota.com>
2024-06-15 16:13:08 +0100

    Output now flushing on ServRaw.

--- Dev/JvmSrc/ServRaw.scala
+++ Dev/JvmSrc/ServRaw.scala
@@ -1,6 +1,6 @@
 /* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pDev
-import pWeb.*, java.net.*, java.io.{ BufferedReader, InputStreamReader }
+import pWeb.*, java.net.*, java.io.*//{ BufferedReader, InputStreamReader }
 
 object ServRaw extends App
 { deb("Starting")
@@ -19,8 +19,8 @@ object ServRaw extends App
 class ConnSesh(val cNum: Int, val sock: Socket) extends Runnable
 {
   override def run(): Unit =
-  { val readbuf: BufferedReader = new BufferedReader(new java.io.InputStreamReader(sock.getInputStream()))
-    val outSt = sock.getOutputStream
+  { val readbuf: BufferedReader = new BufferedReader(new InputStreamReader(sock.getInputStream()))
+    val outPS = new PrintStream(new BufferedOutputStream(sock.getOutputStream))
     var reqNum = 0
     while(true)
     { println(s"Req $cNum, $reqNum start")
@@ -47,26 +47,21 @@ class ConnSesh(val cNum: Int, val sock: Socket) extends Runnable
       { case Good(hrg: HttpReqGet) => hrg.uri match
         { case "/" =>
           { val repStr: String = IndexPage.httpResp("localhost").out
-            deb("About to send Response Index page")
-            outSt.write(repStr.getBytes)
-            outSt.write("\n".getBytes())
-            outSt.flush()
+            outPS.print(repStr)
+            outPS.flush()
             deb("Sent Response Index page")
           }
 
           case "/Documentation/documentation.css" =>
           { val repStr: String = CssDocmentation.httpResp("localhost").out
-            deb("About to send Response doc css")
-            outSt.write(repStr.getBytes)
-            outSt.write("\n".getBytes())
-            outSt.flush()
+            outPS.print(repStr)
+            outPS.flush()
             deb("Response doc css")
           }
           case id => deb(s"Other URI |$id|")
         }
         case _ => deb("Other match")
       }
-      deb("Response sent.")
       reqNum += 1
     }
     readbuf.close
