492ad86216253be8b54a8941556685cdee9a74b1
Rich2 <rr1@tutanota.com>
2024-07-27 21:37:16 +0100

    Created fastOpt and fullOpt Files for DevHtmls.

--- Dev/JvmSrc/DevHtmls.scala
+++ Dev/JvmSrc/DevHtmls.scala
@@ -11,17 +11,22 @@ object DevHtmls extends App
 
   sett.forGoodForBad { path =>
     deb(path.str)
-    writeFile(path)
+    writeFile(path, true)
+    writeFile(path, false)
   }{
     strArr => deb(strArr.mkStr(","))
   }
 
-  def writeFile(path: DirPathAbs): Unit =
-  { val noCacheScript = s"""
+  def writeFile(path: DirPathAbs, isFast: Boolean): Unit =
+  {
+    val jsStr = ife(isFast, "fast", "")
+    val htmlStr = ife(isFast, "Fast", "Full")
+
+    val noCacheScript = s"""
     |  // aid local development in ensuring script not cached during a simple refresh
     |  var script = document.createElement("script");
     |  script.setAttribute("type", "text/javascript");
-    |  script.setAttribute("src", "../../GenAppJs/target/scala-$scalaVersionStr/genappjs-fastopt.js?"+Date.now().toString());
+    |  script.setAttribute("src", "../../GenAppJs/target/scala-$scalaVersionStr/genappjs-${jsStr}opt.js?"+Date.now().toString());
     |  document.getElementsByTagName("head")[0].appendChild(script);
     |  script.addEventListener('load', function(e) { EG460AppJs.main(); });
     |""".stripMargin
@@ -31,8 +36,7 @@ object DevHtmls extends App
     val script = HtmlScript.inlineJsStr(noCacheScript)
     val body = HtmlBody(HtmlCanvas.id("scanv"), HtmlNoScript(), script)
     val page = HtmlPage(head, body)
-    val p2: DirPathAbs = path
-    val res = fileWrite(path / "Dev" / "target", "EG460SbtFast.html", page.out)
+    val res = fileWrite(path / "Dev" / "target", s"EG460Sbt${htmlStr}.html", page.out)
     deb(res.toString)
   }
 }
