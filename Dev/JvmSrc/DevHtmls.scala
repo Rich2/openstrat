9faa9daab444657649e7e7925307a443d90dbef1
Rich2 <rr1@tutanota.com>
2024-07-28 11:20:18 +0100

    Added 1300km js file to Dev/res.

--- Dev/JvmSrc/DevHtmls.scala
+++ Dev/JvmSrc/DevHtmls.scala
@@ -11,13 +11,17 @@ object DevHtmls extends App
 
   sett.forGoodForBad { path =>
     deb(path.str)
-    writeFile(path, true)
-    writeFile(path, false)
+    writeFastFull(path, "1300")
   }{
     strArr => deb(strArr.mkStr(","))
   }
 
-  def writeFile(path: DirPathAbs, isFast: Boolean): Unit =
+  def writeFastFull(path: DirPathAbs, scale: String): Unit = {
+    writeFile(path, true, scale)
+    writeFile(path, false, scale)
+  }
+
+  def writeFile(path: DirPathAbs, isFast: Boolean, scale: String): Unit =
   {
     val jsStr = ife(isFast, "fast", "")
     val htmlStr = ife(isFast, "Fast", "Full")
@@ -28,7 +32,7 @@ object DevHtmls extends App
     |  script.setAttribute("type", "text/javascript");
     |  script.setAttribute("src", "../../GenAppJs/target/scala-$scalaVersionStr/genappjs-${jsStr}opt.js?"+Date.now().toString());
     |  document.getElementsByTagName("head")[0].appendChild(script);
-    |  script.addEventListener('load', function(e) { EG460AppJs.main(); });
+    |  script.addEventListener('load', function(e) { EG${scale}AppJs.main(); });
     |""".stripMargin
 
     val style = HtmlStyle(CssBody(DecMarg(0.px), DecOverflowHidden))
@@ -36,7 +40,7 @@ object DevHtmls extends App
     val script = HtmlScript.inlineJsStr(noCacheScript)
     val body = HtmlBody(HtmlCanvas.id("scanv"), HtmlNoScript(), script)
     val page = HtmlPage(head, body)
-    val res = fileWrite(path / "Dev" / "target", s"EG460Sbt${htmlStr}.html", page.out)
+    val res = fileWrite(path / "Dev" / "target", s"EG${scale}Sbt${htmlStr}.html", page.out)
     deb(res.toString)
   }
 }
