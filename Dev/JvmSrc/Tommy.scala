8aac7f9ccbdf90a73b7df369af051d8dc0009d4f
Rich2 <rr1@tutanota.com>
2024-07-29 13:00:05 +0100

    Tommy Servlet now using Geam jar.

--- Dev/JvmSrc/Tommy.scala
+++ Dev/JvmSrc/Tommy.scala
@@ -1,12 +1,16 @@
 /* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pDev
-import jakarta.servlet.http.{Cookie, HttpServlet, HttpServletRequest as HSReq, HttpServletResponse as HSResp}
+import jakarta.servlet.http.{Cookie, HttpServlet, HttpServletRequest as HSReq, HttpServletResponse as HSResp }, pWeb._
 
+/** First openstrat Servlet fro Tomcat. */
 class Tommy extends HttpServlet
 {
   override def doGet(req: HSReq, resp: HSResp): Unit =
   {
     val ints = IntArr(2, 4, 6)
-    resp.getWriter().println(s"Hello from Servlet: ${ints.str}")
+    val head = HtmlHead.title("Tommy")
+    val body = HtmlBody(s"Hello from Servlet: ${ints.str}".xCon)
+    val page = HtmlPage(head, body)
+    resp.getWriter().println(page.out)
   }
 }
