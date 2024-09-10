ec202cd4e7e0637284bcbc66facbea305c8b8273
Rich2 <rr1@tutanota.com>
2024-09-10 12:00:50 +0100

    Minor changes to Graphics lessons.

--- GeomExs/src/GraphicsA.scala
+++ GeomExs/src/GraphicsA.scala
@@ -1,9 +1,6 @@
 /* Copyright 2018-24 Licensed under Apache Licence version 2.0. */
 package learn
-import ostrat._
-import geom._
-import ostrat.pgui.{CanvasNoPanels, CanvasPlatform}
-import pWeb._
+import ostrat.*, geom.*, pgui.{CanvasNoPanels, CanvasPlatform }, pWeb.*
 
 trait LessonPage
 { def page: HtmlPage
