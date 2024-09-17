31d743c77745ba5d4ae4319a287cc9cdc64fbcde
Rich2 <rr1@tutanota.com>
2024-09-17 18:08:11 +0100

    Lesson Text.

--- GeomExs/src/LsB2.scala
+++ GeomExs/src/LsB2.scala
@@ -2,17 +2,18 @@
 package learn
 import ostrat._, geom._, pgui._, Colour._
 
-object LsB2 extends LessonGraphics {
+object LsB2 extends LessonGraphics
+{
   override def title: String = "Timer Lesson"
 
-  override def bodyStr: String = """"""
+  override def bodyStr: String = """This again uses the simplest timer method. The screen is repainted every 15 milliseconds, that forty frames a second. The %
+     | operator divides, and returns the remainder. This means every 5000 milliseconds or 5 seconds the rectangle goes back to the start.""".stripMargin
 
   override def canv: CanvasPlatform => Any = LsB2Canv(_)
 
   case class LsB2Canv(canv: CanvasPlatform) extends CanvasNoPanels("Lesson B2")
   {
-    /* This again uses the simplest timer method. The screen is repainted every 15 milliseconds, that forty frames a second. The % operator divides, and returns
-     the remainder. This means every 5000 milliseconds or 5 seconds the rectangle goes back to the start. */
+    /* T */
     timedRepaint1 { e =>
       val e2 = e % 5000
       Rect(200, 100).slateX(e2 / 4 - 600).fill(Red)
