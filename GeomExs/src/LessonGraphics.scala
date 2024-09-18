9fba8e5c2cbe0265670402dc20a417ded9ebb2f8
Rich2 <rr1@tutanota.com>
2024-09-19 00:11:28 +0100

    Removed LessonPage.

--- GeomExs/src/LessonGraphics.scala
+++ GeomExs/src/LessonGraphics.scala
@@ -2,17 +2,13 @@
 package learn
 import ostrat.*, geom.*, pgui.{CanvasNoPanels, CanvasPlatform }, pWeb.*
 
-trait LessonPage
-{ def page: HtmlPage
-}
-
-trait LessonGraphics extends LessonPage
+trait LessonGraphics
 { def title: String
-
+  
   def canv: CanvasPlatform => Any
   def bodyStr: String
 
-  override def page: HtmlPage = HtmlPage(head, HtmlBody(HtmlH1(title), HtmlP(bodyStr)))
+  def page: HtmlPage = HtmlPage(head, HtmlBody(HtmlH1(title), HtmlP(bodyStr)))
   def head: HtmlHead = HtmlHead(RArr(HtmlTitle(title)))
 }
 
