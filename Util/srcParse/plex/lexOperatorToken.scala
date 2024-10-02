490bd220464efeee3ecf39153b84a4e909b409e7
Rich2 <rr1@tutanota.com>
2024-10-03 00:55:01 +0100

    #276 Removed old EMon3 from lexRawNumberToken.

--- Util/srcParse/plex/lexOperatorToken.scala
+++ Util/srcParse/plex/lexOperatorToken.scala
@@ -4,15 +4,16 @@ package ostrat; package pParse; package plex
 /** Function object to parse operator token. */
 object lexOperatorToken
 {
-  /** Not sure if this is fully fixed. Parses an operator. Operators can have multiple characters in RSON. */
-  /*def apply(remOff: CharsOff, tp: TextPosn)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, Token] =
-  {
-    var acc: String = ""
-    def loop(remOff: CharsOff, tp: TextPosn): EMon3[CharsOff, TextPosn, Token] = remOff match
-    {
-      case CharsOffHead2('/', '/' | '*') => Good3(remOff, tp, sort)
-      case CharsOff1Tail(OperatorChar(c), tail) => { acc :+= c; loop(tail, tp.right1) }
-      case _ => Good3(remOff, tp, sort)
+  def apply(remOff: CharsOff, tp: TextPosn)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] =
+  { var acc: String = ""
+
+    def loop(remOff: CharsOff, tp: TextPosn): EEMon3[CharsOff, TextPosn, Token] = remOff match {
+      case CharsOffHead2('/', '/' | '*') => Succ3(remOff, tp, sort)
+      case CharsOff1Tail(OperatorChar(c), tail) =>
+      { acc :+= c;
+        loop(tail, tp.right1)
+      }
+      case _ => Succ3(remOff, tp, sort)
     }
 
     def sort: Token = acc.last match
@@ -20,31 +21,7 @@ object lexOperatorToken
       case ':' => ColonToken(tp)
       case _ if acc == "/" => SlashToken(tp)
       case _ => acc.head match
-      { case '|' | '^' | '&' | '=' | '!' | '<' | '>' | ':' | '+' | '-' | '*'  | '/' | '%' => OperatorPrec1Token(tp, acc)
-        case _ => OperatorPrec0Token(tp, acc)
-      }
-    }
-    loop(remOff, tp)
-  }*/
-
-  def apply(remOff: CharsOff, tp: TextPosn)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] =
-  { var acc: String = ""
-
-    def loop(remOff: CharsOff, tp: TextPosn): EEMon3[CharsOff, TextPosn, Token] = remOff match {
-      case CharsOffHead2('/', '/' | '*') => Succ3(remOff, tp, sort)
-      case CharsOff1Tail(OperatorChar(c), tail) => {
-        acc :+= c;
-        loop(tail, tp.right1)
-      }
-      case _ => Succ3(remOff, tp, sort)
-    }
-
-    def sort: Token = acc.last match {
-      case '=' => AsignToken(tp)
-      case ':' => ColonToken(tp)
-      case _ if acc == "/" => SlashToken(tp)
-      case _ => acc.head match {
-        case '|' | '^' | '&' | '=' | '!' | '<' | '>' | ':' | '+' | '-' | '*' | '/' | '%' => OperatorPrec1Token(tp, acc)
+      { case '|' | '^' | '&' | '=' | '!' | '<' | '>' | ':' | '+' | '-' | '*' | '/' | '%' => OperatorPrec1Token(tp, acc)
         case _ => OperatorPrec0Token(tp, acc)
       }
     }
--- Util/srcParse/plex/lexRawNumberToken.scala
+++ Util/srcParse/plex/lexRawNumberToken.scala
@@ -2,64 +2,48 @@
 package ostrat; package pParse; package plex
 
 /** Function object for parsing a raw number, could be a normal decimal, hexadecimal or trigdual number. Not all natural numbers are
- * parsed with this function object. Raw hex and trigdual numbers can be encoded as alpha numeric identity tokens. */
+ * parsed with this function object. Raw hex and trigdual numbers can be encoded as alphanumeric identity tokens. */
 object lexRawNumberToken
 {
-  def apply(rem: CharsOff, tp: TextPosn, str:String, isNeg: Boolean)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, Token] =  rem match
+  def apply(rem: CharsOff, tp: TextPosn, str: String, isNeg: Boolean)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] = rem match
   { case CharsOff1Tail(d, tail) if d.isDigit => apply(tail, tp, str + d.toString, isNeg)
     case CharsOff2Tail('.', d, tail) if d.isDigit => parseDeciFrac(tail, tp, str, d.toString, isNeg)
     case CharsOff1Tail(HexaUpperChar(l), tail) => parseHexaToken(tail, tp, str + l.toString, isNeg)
     case CharsOff1Tail(l, tail) if (l <= 'N' && l >= 'G') | (l <= 'W' && l >= 'P') => parseBase32(tail, tp, str + l.toString, isNeg)
-    case CharsOff1Tail(LetterOrUnderscoreChar(l), tail) => lexDigitHeadToken(tail, tp, str, isNeg, l.toString)// tp.bad3("lexRawnumberToken: Badly formed number token.")
-    case _ if isNeg => Good3(rem, tp.addStr(str).right1, NegBase10Token(tp, str))
-    case _ => Good3(rem, tp.addStr(str), NatBase10Token(tp, str))
+    case CharsOff1Tail(LetterOrUnderscoreChar(l), tail) => lexDigitHeadToken(tail, tp, str, isNeg, l.toString)
+    case _ if isNeg => Succ3(rem, tp.addStr(str).right1, NegBase10Token(tp, str))
+    case _ => Succ3(rem, tp.addStr(str), NatBase10Token(tp, str))
   }
 }
 
 object parseHexaToken
 {
-  def apply(rem: CharsOff, tp: TextPosn, str: String, isNeg: Boolean)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, Token] = rem match
+  def apply(rem: CharsOff, tp: TextPosn, str: String, isNeg: Boolean)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] = rem match
   { case CharsOff1Tail(d, tail) if d.isDigit | (d <= 'F' && d >= 'A') => parseHexaToken(tail, tp, str + d.toString, isNeg)
     case CharsOff1Tail(l, tail) if (l <= 'G' && l >= 'G') | (l <= 'W' && l >= 'P') => parseBase32(tail, tp, l.toString, isNeg)
-    case CharsOffHead(LetterOrUnderscoreChar(l)) => tp.bad3("Badly formed raw hexadecimal token.")
-    case _ if isNeg => Good3(rem, tp.addStr(str), RawHexaNegToken(tp, str))
-    case _ => Good3(rem, tp.addStr(str), RawHexaNatToken(tp, str))
+    case CharsOffHead(LetterOrUnderscoreChar(l)) => FailE("Badly formed raw hexadecimal token.")
+    case _ if isNeg => Succ3(rem, tp.addStr(str), RawHexaNegToken(tp, str))
+    case _ => Succ3(rem, tp.addStr(str), RawHexaNatToken(tp, str))
   }
 }
 
 object parseBase32
 {
-  def apply(rem: CharsOff, tp: TextPosn, str: String, isNeg: Boolean)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, ValidRawBase32IntToken]= rem match
+  def apply(rem: CharsOff, tp: TextPosn, str: String, isNeg: Boolean)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, ValidRawBase32IntToken] = rem match
   { case CharsOff1Tail(l, tail) if l.isDigit | (l <= 'A' && l >= 'G') | (l <= 'W' && l >= 'P') => parseBase32(tail, tp, l.toString, isNeg)
-    case CharsOffHead(LetterOrUnderscoreChar(l)) => tp.bad3("Badly formed raw Base 32 token.")
-    case _ if isNeg => Good3(rem, tp.addStr(str), RawBase32NegToken(tp, str))
-    case _ => Good3(rem, tp.addStr(str), RawBase32NatToken(tp, str))
-  }
-
-  def alt(rem: CharsOff, tp: TextPosn, str: String, isNeg: Boolean)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, ValidRawBase32IntToken] = //???
-  rem match {
-    case CharsOff1Tail(l, tail) if l.isDigit | (l <= 'A' && l >= 'G') | (l <= 'W' && l >= 'P') => parseBase32.alt(tail, tp, l.toString, isNeg)
     case CharsOffHead(LetterOrUnderscoreChar(l)) => tp.failE("Badly formed raw Base 32 token.")
     case _ if isNeg => Succ3(rem, tp.addStr(str), RawBase32NegToken(tp, str))
-//    case _ => Good3(rem, tp.addStr(str), RawBase32NatToken(tp, str))
+    case _ => Succ3(rem, tp.addStr(str), RawBase32NatToken(tp, str))
   }
 }
 
 object parseDeciFrac
 {
-  def apply (rem: CharsOff, tp: TextPosn, seq1: String, seq2: String, isNeg: Boolean)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, Token] = rem match
-  { case CharsOff1Tail(d, tail) if d.isDigit => apply(tail, tp, seq1, seq2 + d.toString, isNeg)
-    case _ => {
-      val token = ife(isNeg, DeciFracNegToken(tp, seq1, seq2, ""), DeciFracPosToken(tp, seq1, seq2, ""))
-      Good3(rem, tp.addStr(seq1).addStr(seq2).right1, token)
-    }
-  }
-
-  def alt(rem: CharsOff, tp: TextPosn, seq1: String, seq2: String, isNeg: Boolean)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] = rem match
+  def apply(rem: CharsOff, tp: TextPosn, seq1: String, seq2: String, isNeg: Boolean)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] = rem match
   {
-    case CharsOff1Tail(d, tail) if d.isDigit => alt(tail, tp, seq1, seq2 + d.toString, isNeg)
-    case _ => {
-      val token = ife(isNeg, DeciFracNegToken(tp, seq1, seq2, ""), DeciFracPosToken(tp, seq1, seq2, ""))
+    case CharsOff1Tail(d, tail) if d.isDigit => apply(tail, tp, seq1, seq2 + d.toString, isNeg)
+    case _ =>
+    { val token = ife(isNeg, DeciFracNegToken(tp, seq1, seq2, ""), DeciFracPosToken(tp, seq1, seq2, ""))
       Succ3(rem, tp.addStr(seq1).addStr(seq2).right1, token)
     }
   }
@@ -67,15 +51,9 @@ object parseDeciFrac
 
 object lexDigitHeadToken
 {
-  def apply(rem: CharsOff, tp: TextPosn, digitsStr: String, isNeg: Boolean, alphaStr: String)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, Token] =
-    rem match {
-      case CharsOff1Tail(LetterOrUnderscoreChar(l), tail) => apply(tail, tp, digitsStr, isNeg, alphaStr + l)
-      case _ => Good3(rem, tp.addStr(digitsStr).addStr(alphaStr), DigitHeadAlphaTokenGen(tp, digitsStr, alphaStr))
-  }
-
-  def alt(rem: CharsOff, tp: TextPosn, digitsStr: String, isNeg: Boolean, alphaStr: String)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] =
-    rem match {
-      case CharsOff1Tail(LetterOrUnderscoreChar(l), tail) => alt(tail, tp, digitsStr, isNeg, alphaStr + l)
+  def apply(rem: CharsOff, tp: TextPosn, digitsStr: String, isNeg: Boolean, alphaStr: String)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, Token] =
+    rem match
+    { case CharsOff1Tail(LetterOrUnderscoreChar(l), tail) => apply(tail, tp, digitsStr, isNeg, alphaStr + l)
       case _ => Succ3(rem, tp.addStr(digitsStr).addStr(alphaStr), DigitHeadAlphaTokenGen(tp, digitsStr, alphaStr))
     }
 }
