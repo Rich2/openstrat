83f5a85cb0f0283df70ecb7b848a93509f58401a
Rich2 <rr1@tutanota.com>
2024-10-03 19:37:12 +0100

    Removed EEMon3 type.

--- Util/src/ErrBi.scala
+++ Util/src/ErrBi.scala
@@ -61,7 +61,7 @@ type EEArr[A <: AnyRef] = EEMon[RArr[A]]
 
 /** Extractor function object for a successful Arr Sequence of length 1. */
 object SuccEArr1
-{ /** Extractor method for a Good [[Arr]] Sequence of length 1. */
+{ /** Extractor method for a successful [[Arr]] Sequence of length 1. */
   def unapply[A](eArr: EEMon[Arr[A]]): Option[A] = eArr match
   { case Succ(Arr1(head)) => Some(head)
     case _ => None
@@ -71,16 +71,15 @@ object SuccEArr1
 /** [[Exception]] bifunctor for [[Tuple3]]. */
 type ExcBi3[E <: Exception, A1, A2, A3] = ErrBi[E, (A1, A2, A3)]
 
-type EEMon3[A1, A2, A3] = EEMon[(A1, A2, A3)]
-
-implicit class EEMon3Extensions[A1, A2, A3](val thisEE3: EEMon3[A1, A2, A3])
+/** Extensiion class for [[Exception]] bifunctor for [[Tuple3]]s. */
+implicit class ExcBi3Extensions[E <: Exception, A1, A2, A3](val thisEE3: ExcBi3[E, A1, A2, A3])
 {
   /*def toEMon3: EMon3[A1, A2, A3] = thisEE3 match {
     case Succ3(a1, a2, a3) => Good3(a1, a2, a3)
     case Fail(err) => Bad3(StrArr(err.toString))
   }*/
 
-  def flatMap3[B1, B2, B3](f: (A1, A2, A3) => EEMon3[B1, B2, B3]): EEMon3[B1, B2, B3] = thisEE3 match
+  def flatMap3[B1, B2, B3](f: (A1, A2, A3) => ExcBi3[E, B1, B2, B3]): ExcBi3[E, B1, B2, B3] = thisEE3 match
   { case Succ3(a1, a2, a3) => f(a1, a2, a3)
     case Fail(err) => Fail(err)
   }
@@ -94,7 +93,7 @@ object Succ3
   def apply[E <: Throwable, A1, A2, A3](a1: A1, a2: A2, a3: A3): Succ3[E, A1, A2, A3] = new Succ[E, (A1, A2, A3)]((a1, a2, a3))
 
   /** unapply extractor for success on an [[ErrBi]] with a [[Tuple3]] value type. */
-  def unapply[E <: Throwable, A1, A2, A3](inp: EEMon3[A1, A2, A3]): Option[(A1, A2, A3)] = inp match{
+  def unapply[E <: Exception, A1, A2, A3](inp: ExcBi3[E, A1, A2, A3]): Option[(A1, A2, A3)] = inp match{
     case succ: Succ3[E, A1, A2, A3] => Some(succ.value._1, succ.value._2, succ.value._3)
     case _ => None
   }
