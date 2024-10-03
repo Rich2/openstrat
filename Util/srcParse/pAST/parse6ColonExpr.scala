355dbe9f3065d3296659c0a8a9b5db9a881e84f3
Rich2 <rr1@tutanota.com>
2024-10-03 23:35:13 +0100

    #276 Converting Ast parsing.

--- Util/srcParse/pAST/parse6ColonExpr.scala
+++ Util/srcParse/pAST/parse6ColonExpr.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pParse; package pAST
 import collection.mutable.ArrayBuffer
 
@@ -10,15 +10,15 @@ object parse6ColonExpr
     val leftAcc: ArrayBuffer[ColonOpMem] = Buffer()
     val rightAcc: ArrayBuffer[ColonOpMem] = Buffer()
 
-    def rightLoop(rem: ArrOff[AssignMem], leftExpr: ColonMemExpr, ct: ColonToken): EMon[ColonExpr] = rem match {
-      case ArrOff0() => parse7Clauses(rightAcc.toArr).map{ r => ColonExpr(leftExpr, ct, r) }
+    def rightLoop(rem: ArrOff[AssignMem], leftExpr: ColonMemExpr, ct: ColonToken): EMon[ColonExpr] = rem match
+    { case ArrOff0() => parse7Clauses(rightAcc.toArr).map{ r => ColonExpr(leftExpr, ct, r) }.toEMon
       case ArrOff1Tail(ct2: ColonToken, tail) => bad1(ct2, "More than 1 Colon in expression.")
       case ArrOff1Tail(cm: ColonOpMem, tail) => { rightAcc.append(cm); rightLoop(tail, leftExpr, ct) }
     }
 
     def leftLoop(rem: ArrOff[AssignMem]): EMon[AssignMemExpr] = rem match {
-      case ArrOff0() => parse7Clauses(leftAcc.toArr)
-      case ArrOff1Tail(ct: ColonToken, tail) => parse7Clauses(leftAcc.toArr).flatMap{ leftExpr => rightLoop(tail, leftExpr, ct) }
+      case ArrOff0() => parse7Clauses(leftAcc.toArr).toEMon
+      case ArrOff1Tail(ct: ColonToken, tail) => parse7Clauses(leftAcc.toArr).toEMon.flatMap{ leftExpr => rightLoop(tail, leftExpr, ct) }
       case ArrOff1Tail(cm: ColonOpMem, tail) => { leftAcc.append(cm); leftLoop(tail) }
     }
 
--- Util/srcParse/pAST/parse7Clauses.scala
+++ Util/srcParse/pAST/parse7Clauses.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pParse; package pAST
 import collection.mutable.ArrayBuffer
 
@@ -6,20 +6,20 @@ import collection.mutable.ArrayBuffer
 object parse7Clauses
 {
   /** Function apply method parses [[Clause]]s. Assumes input [[RArr]] is not empty. */
-  def apply (implicit seg: RArr[ColonOpMem]): EMon[ColonMemExpr] = fromOffset(seg.offset0)
+  def apply (implicit seg: RArr[ColonOpMem]): ErrBi[ExcAst, ColonMemExpr] = fromOffset(seg.offset0)
 
-  def fromOffset(inp: ArrOff[ColonOpMem])(implicit seg: RArr[ColonOpMem]): EMon[ColonMemExpr] =
+  def fromOffset(inp: ArrOff[ColonOpMem])(implicit seg: RArr[ColonOpMem]): ErrBi[ExcAst, ColonMemExpr] =
   {
     var subAcc: ArrayBuffer[ClauseMem] = Buffer()
     val acc: ArrayBuffer[Clause] = Buffer()
 
-    def loop(rem: ArrOff[ColonOpMem]): EMon[ColonMemExpr] = rem match
-    { case ArrOff0() if acc.isEmpty => parse8ClauseMem(subAcc.toArr).toEMon
-      case ArrOff0() if subAcc.isEmpty => Good(ClausesExpr(acc.toArr))
-      case ArrOff0() => parse8ClauseMem(subAcc.toArr).map{ e => ClausesExpr(acc.append(Clause(e, None)).toArr) }.toEMon
+    def loop(rem: ArrOff[ColonOpMem]): ErrBi[ExcAst, ColonMemExpr] = rem match
+    { case ArrOff0() if acc.isEmpty => parse8ClauseMem(subAcc.toArr)
+      case ArrOff0() if subAcc.isEmpty => Succ(ClausesExpr(acc.toArr))
+      case ArrOff0() => parse8ClauseMem(subAcc.toArr).map{ e => ClausesExpr(acc.append(Clause(e, None)).toArr) }
       case ArrOff1Tail(ct: CommaToken, tail) if subAcc.isEmpty => { acc.append(EmptyClause(ct)); loop(tail) }
 
-      case ArrOff1Tail(ct: CommaToken, tail) => parse8ClauseMem(subAcc.toArr).toEMon.flatMap{ expr =>
+      case ArrOff1Tail(ct: CommaToken, tail) => parse8ClauseMem(subAcc.toArr).flatMap{ expr =>
         acc.append(Clause(expr, Some(ct)))
         subAcc = Buffer()
         loop(tail)
