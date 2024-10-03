de3966cb250c53a8b958b35ddd397b5f3dd09860
Rich2 <rr1@tutanota.com>
2024-10-03 08:59:48 +0100

    #276 Converted lexStrToken.

--- Util/TestSrc/pParse/plex/lexStringTokenTest.scala
+++ Util/TestSrc/pParse/plex/lexStringTokenTest.scala
@@ -1,4 +1,4 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pParse; package plex
 import utest._
 
@@ -11,7 +11,7 @@ object lexStringTokenTest extends TestSuite
   val p1 = StrPosn(1, 1)
   val s1 = "\"Hello\""
   val chs1 = s1.toChars
-  val r1: EMon3[CharsOff, TextPosn, StringToken] = lexStringToken(chs1.offsetter0, StrPosn())(chs1)
+  val r1: EEMon3[CharsOff, TextPosn, StringToken] = lexStringToken(chs1.offsetter0, StrPosn())(chs1)
   val s2 = s1 + " AnIdentifier"
   val chs2 = s2.toChars
   val r2 = lexStringToken(chs2.offsetter0, StrPosn())(chs2)
@@ -21,10 +21,10 @@ object lexStringTokenTest extends TestSuite
 
   val tests = Tests {
     test("Test1")
-    { assertMatch(r0){case Good3(CharsOff(2), StrPosn(1, 3), StringToken(p1, s0)) => }
-      assertMatch(r1){case Good3(CharsOff(7), StrPosn(1, 8), StringToken(p1, s1)) => }
-      assertMatch(r2){case Good3(CharsOff(7), StrPosn(1, 8), StringToken(p1, s1)) => }
-      assertMatch(r3){case Good3(CharsOff(13), StrPosn(1, 14), StringToken(StrPosn(1, 5), s3)) => }
+    { assertMatch(r0){case Succ3(CharsOff(2), StrPosn(1, 3), StringToken(p1, s0)) => }
+      assertMatch(r1){case Succ3(CharsOff(7), StrPosn(1, 8), StringToken(p1, s1)) => }
+      assertMatch(r2){case Succ3(CharsOff(7), StrPosn(1, 8), StringToken(p1, s1)) => }
+      assertMatch(r3){case Succ3(CharsOff(13), StrPosn(1, 14), StringToken(StrPosn(1, 5), s3)) => }
     }
 
     //val s4 = "\"Hello"
--- Util/srcParse/plex/lexStringToken.scala
+++ Util/srcParse/plex/lexStringToken.scala
@@ -1,25 +1,25 @@
-/* Copyright 2018-22 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-24 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pParse; package plex
 
 /** Function object that parses String (with RSON syntax) searching for the String terminator. Returns error if end of file found first. */
 object lexStringToken
 { /** Parses String (with RSON syntax) searching for the String terminator. Returns error if end of file found first. */
-  def apply(rem: CharsOff, tp: TextPosn)(implicit charArr: CharArr): EMon3[CharsOff, TextPosn, StringToken] =
+  def apply(rem: CharsOff, tp: TextPosn)(implicit charArr: CharArr): EEMon3[CharsOff, TextPosn, StringToken] =
   {
     val strAcc: StringBuilder = new StringBuilder()
 
-    def loop(rem: CharsOff): EMon3[CharsOff, TextPosn, StringToken] = rem match
-    { case CharsOff0() => tp.bad3("Unclosed String")
-      case CharsOff1Tail('\"', tail2) => Good3(tail2, tp.right(strAcc.length + 2),  StringToken(tp, strAcc.mkString))
-      case CharsOff1('\\') =>  tp.bad3("Unclosed String ending with unclosed escape Sequence")
+    def loop(rem: CharsOff): EEMon3[CharsOff, TextPosn, StringToken] = rem match
+    { case CharsOff0() => tp.fail("Unclosed String")
+      case CharsOff1Tail('\"', tail2) => Succ3(tail2, tp.right(strAcc.length + 2),  StringToken(tp, strAcc.mkString))
+      case CharsOff1('\\') =>  tp.fail("Unclosed String ending with unclosed escape Sequence")
       case CharsOff2Tail('\\', c2, tail) if Array('\"', '\n', '\b', '\t', '\f', '\r', '\'', '\\').contains(c2) => { strAcc.append(c2); loop(tail) }
-      case CharsOffHead2('\\', c2) => tp.bad3("Unrecognised escape Sequence \\" + c2.toString)
+      case CharsOffHead2('\\', c2) => tp.fail("Unrecognised escape Sequence \\" + c2.toString)
       case CharsOff1Tail(h, tail2) => { strAcc.append(h); loop(tail2) }
     }
+    
     rem match
-    {
-      case CharsOff1Tail('\"', tail) => loop(tail)
-      case _ => tp.bad3("These characters do not begin with a String opening delimitter.")
+    { case CharsOff1Tail('\"', tail) => loop(tail)
+      case _ => tp.fail("These characters do not begin with a String opening delimitter.")
     }
 
   }
