deae2a1041f7520442317d45387cba9ea9c9b307
Rich2 <rr1@tutanota.com>
2024-10-13 21:44:24 +0100

    #276 Converting example games.

--- Tiling/TilingExs/src/gTwo/s2p/G2SLaunch.scala
+++ Tiling/TilingExs/src/gTwo/s2p/G2SLaunch.scala
@@ -12,7 +12,7 @@ object G2SLaunch extends GuiLaunchMore
     (G2SGui(_, G2SGame(G2SScen1, G2SScen1.counterSet), G2SGuiSettings(G2SScen1.defaultView(), G2SScen1.counterSet)), "JavaFx Game Two Square")
 
   override def fromStatements(sts: RArr[Statement]): (CanvasPlatform => Any, String) =
-  { val oScen: EMonOld[Int] = sts.findSettingOld[Int]("scen")
+  { val oScen = sts.findSetting[Int]("scen")
     val num: Int = oScen.getElse(1)
     val scen: G2SqScen = num match
     { case 1 => G2SScen1
@@ -21,9 +21,9 @@ object G2SLaunch extends GuiLaunchMore
       case _ => G2SScen1
     }
 
-    val oSetts: EMonOld[AssignMemExpr] = sts.findIntSettingExprOld(num)
-    val sts2: EMonOld[RArr[Statement]] = oSetts.map(_.toStatements)
-    val pls1 = sts2.findSettingIdentifierArrOld("counters")
+    val oSetts: ErrBi[Exception, AssignMemExpr] = sts.findIntSettingExpr(num)
+    val sts2 = oSetts.map(_.toStatements)
+    val pls1 = sts2.findSettingIdentifierArr("counters")
     val plAll = scen.counterSet
     val pls2 = pls1.map { arrA => arrA.optMap(st => plAll.find(_.charStr == st)) }
     val pls3 = pls2.getElse(plAll)
