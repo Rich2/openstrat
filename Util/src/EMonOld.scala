f4f4b7f8528e1e21da869c05778c0d45bd769a5c
Rich2 <rr1@tutanota.com>
2024-10-16 11:55:28 +0100

    #276 Removed EMonN and started deleting EMon methods.

--- Util/src/EMonOld.scala
+++ Util/src/EMonOld.scala
@@ -12,22 +12,6 @@ sealed trait EMonOld[+A]
   def map[B](f: A => B): EMonOld[B]
 
   def flatMap[B](f: A => EMonOld[B]): EMonOld[B]
-  //def toEMon2[B1, B2](f: A => EMon2[B1, B2]): EMon2[B1, B2]
-
-  /** 2 type parameters, maps the Good case of this [[EMonOld]], with the [[Good]] case of an additional [[EMonOld]] of a different type. */
-  def map2[A2, R](e2: EMonOld[A2])(f: (A, A2) => R): EMonOld[R]
-
-  /** 3 type parameters, maps the Good case of this [[EMonOld]], with the [[Good]] cases of an additional 3 [[EMonOld]]s of a different types. */
-  def map3[A2, A3, R](e2: EMonOld[A2], e3: EMonOld[A3])(f: (A, A2, A3) => R): EMonOld[R]
-
-  /** 4 type parameters, maps the Good case of this [[EMonOld]], with the [[Good]] cases of an additional 3 [[EMonOld]]s of a different types. */
-  def map4[A2, A3, A4, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4])(f: (A, A2, A3, A4) => R): EMonOld[R]
-
-  /** 5 type parameters, maps the Good case of this [[EMonOld]], with the [[Good]] cases of an additional 4 [[EMonOld]]s of a different types. */
-  def map5[A2, A3, A4, A5, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4], e5: EMonOld[A5])(f: (A, A2, A3, A4, A5) => R): EMonOld[R]
-
-  /** 6 type parameters, maps the Good case of this [[EMonOld]], with the [[Good]] cases of an additional 5 [[EMonOld]]s of a different types. */
-  def map6[A2, A3, A4, A5, A6, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4], e5: EMonOld[A5], e6: EMonOld[A6])(f: (A, A2, A3, A4, A5, A6) => R): EMonOld[R]
 
   /** Gets the value of Good or returns the elseValue parameter if Bad. Both Good and Bad should be implemented in the leaf classes to avoid unnecessary boxing
    *  of primitive values. */
@@ -70,8 +54,6 @@ sealed trait EMonOld[+A]
   def flatMap2ToOption[A2, B](o2: EMonOld[A2], f: (A, A2) => B): Option[B]
 
   def goodOrOther[A1 >: A](otherEMon: => EMonOld[A1] @uncheckedVariance): EMonOld[A1]
-
-  def badMap(f: Bad[A] => Bad[A] @uncheckedVariance): EMonOld[A]
 }
 
 /** The Good sub class of EMon[+A]. This corresponds, but is not functionally equivalent to an Either[List[String], +A] based
@@ -85,17 +67,6 @@ final case class Good[+A](val value: A) extends EMonOld[A]
   override def fold[B](noneValue: => B)(fGood: A => B): B = fGood(value)
   override def fld[B](noneValue: => B, fGood: A => B): B = fGood(value)
 
-  override def map2[A2, R](e2: EMonOld[A2])(f: (A, A2) => R): EMonOld[R] = e2.map{ a2 => f(value, a2) }
-  override def map3[A2, A3, R](e2: EMonOld[A2], e3: EMonOld[A3])(f: (A, A2, A3) => R): EMonOld[R] = e2.map2(e3)({ (a2, a3) => f(value, a2, a3) })
-
-  override def map4[A2, A3, A4, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4])(f: (A, A2, A3, A4) => R): EMonOld[R] =
-    e2.map3(e3, e4){(a2, a3, a4) => f(value, a2, a3, a4) }
-
-  override def map5[A2, A3, A4, A5, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4], e5: EMonOld[A5])(f: (A, A2, A3, A4, A5) => R): EMonOld[R] =
-    e2.map4(e3, e4, e5){ (a2, a3, a4, a5) => f(value, a2, a3, a4, a5) }
-
-  override def map6[A2, A3, A4, A5, A6, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4], e5: EMonOld[A5], e6: EMonOld[A6])(f: (A, A2, A3, A4, A5, A6) => R): EMonOld[R] =
-    e2.map5(e3, e4, e5, e6){ (a2, a3, a4, a5, a6) => f(value, a2, a3, a4, a5, a6) }
 
   override def forGoodForBad(fGood: A => Unit)(fBad: StrArr => Unit): Unit = fGood(value)
   override def forGood(f: A => Unit): Unit = f(value)
@@ -112,12 +83,7 @@ final case class Good[+A](val value: A) extends EMonOld[A]
   override def biMap[L2, R2](fLeft: StrArr => L2, fRight: A => R2): Either[L2, R2] = Right(fRight(value))
   override def mapToOption[B](f: A => B): Option[B] = Some[B](f(value))
   override def flatMap2ToOption[A2, B](e2: EMonOld[A2], f: (A, A2) => B): Option[B] = e2.fld(None, a2 => Some(f(value, a2)))
-
-  /** Returns this [[EMonOld]][A] if this is Good, else returns the operand EMon[A]. */
   override def goodOrOther[A1 >: A](otherEMon: => EMonOld[A1] @uncheckedVariance): Good[A] = this
-
-  /** Maps over the [[Bad]] errors side of this bifunctor. */
-  override def badMap(f: Bad[A] => Bad[A] @uncheckedVariance): EMonOld[A] = this
 }
 
 /** The errors case of EMon[+A]. This corresponds, but is not functionally equivalent to an Either[List[String], +A] based Left[List[String], +A]. */
@@ -128,17 +94,6 @@ class Bad[+A](val errs: StrArr) extends EMonOld[A]
   override def fold[B](noneValue: => B)(fGood: A => B): B = noneValue
   override def fld[B](noneValue: => B, fGood: A => B): B = noneValue
   @inline override def foldErrs[B](fGood: A => B)(fBad: StrArr => B): B = fBad(errs)
-
-  override def map2[A2, R](e2: EMonOld[A2])(f: (A, A2) => R): EMonOld[R] = Bad[R](errs ++ e2.errs)
-  override def map3[A2, A3, R](e2: EMonOld[A2], e3: EMonOld[A3])(f: (A, A2, A3) => R): EMonOld[R] = Bad[R](errs ++ e2.errs ++ e3.errs)
-  override def map4[A2, A3, A4, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4])(f: (A, A2, A3, A4) => R): EMonOld[R] =
-    Bad[R](errs ++ e2.errs ++ e3.errs ++ e4.errs)
-
-  override def map5[A2, A3, A4, A5, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4], e5: EMonOld[A5])(f: (A, A2, A3, A4, A5) => R): EMonOld[R] =
-    Bad[R](errs ++ e2.errs ++ e3.errs ++ e4.errs ++ e5.errs)
-
-  override def map6[A2, A3, A4, A5, A6, R](e2: EMonOld[A2], e3: EMonOld[A3], e4: EMonOld[A4], e5: EMonOld[A5], e6: EMonOld[A6])(f: (A, A2, A3, A4, A5, A6) => R):
-    EMonOld[R] = Bad[R](errs ++ e2.errs ++ e3.errs ++ e4.errs ++ e5.errs)
 
   override def getElse(elseValue: A @uncheckedVariance): A = elseValue
   override def forGood(f: A => Unit): Unit = {}
@@ -154,14 +109,9 @@ class Bad[+A](val errs: StrArr) extends EMonOld[A]
   override def mapToOption[B](f: A => B): Option[B] = None
   override def flatMap2ToOption[A2, B](e2: EMonOld[A2], f: (A, A2) => B): Option[B] = None
   override def goodOrOther[A1 >: A](otherEMon: => EMonOld[A1] @uncheckedVariance): EMonOld[A1] = otherEMon
-  override def badMap(f: Bad[A] => Bad[A] @uncheckedVariance): EMonOld[A] = f(this)
 }
 
 object Bad
 {
   def apply[A](errs: StrArr): Bad[A] = new Bad[A](errs)
-  def unapplySeq(inp: Any): Option[Seq[String]] = inp match
-  { case b: Bad[_] => Some(b.errs.toList)
-    case _ => None
-  }
 }
