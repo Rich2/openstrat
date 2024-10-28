b1d4498f236c9fb35b8b7098c15fcfe0157b0fe7
Rich2 <rr1@tutanota.com>
2024-10-28 23:44:08 +0000

    120km terrain.

--- EGrid/src/eg120/Terr120E30.scala
+++ EGrid/src/eg120/Terr120E30.scala
@@ -16,11 +16,11 @@ object Terr120E30 extends Long120Terrs
 
   val help = new WTerrSetter(grid, terrs, sTerrs, corners)
   { override val rows: RArr[RowBase] = RArr(
-    TRow(292, mtainSub * 2, mtainCont * 3, hillyCont * 3),
+    TRow(292, mtainSub * 2, mtainCont * 3, hillyCont * 3, sea * 8, mtainSavannah * 3, hillySavannah),
     VRow(291, OrigRt(1496, HVUR, 7), ThreeUp(1498, 7, 13, 0), BendInLt(1500, HVDL, 13, 7)),
-    TRow(290, mtainSub, sea, mtainSavannah * 4, hillySavannah * 2, mtainSub),
+    TRow(290, mtainSub, sea, mtainSavannah * 4, hillySavannah * 2, mtainSub, sea * 3, mtainSub, hillySub, sea * 4, hillySub, mtainSavannah),
     VRow(289, BendOut(1500, HVUR, 7), BendIn(1502, HVDL, 13)),
-    TRow(288, hillySub, sea, mtainSub, hillySavannah * 4, savannah, hillySavannah, mtainSub),
+    TRow(288, hillySub, sea, mtainSub, hillySavannah * 4, savannah, hillySavannah, mtainSub * 2, mtainSavannah * 7, hillySavannah * 2),
     VRow(287, ThreeDown(1504, 0, 13, 11), OrigRt(1528, HVUR, 7), Bend(1530, HVDn, 13, 3),  BendIn(1532, HVUp, 12), OrigMin(1534, HVDL)),
 
     TRow(286, hillySub, subtrop, mtainSub, mtainSubForest, mtainSavannah, mtainSavannah, sea, mtainSavannah, hillySavannah, mtainSavannah * 3, mtainSahel,
@@ -41,11 +41,18 @@ object Terr120E30 extends Long120Terrs
     TRow(280, sea * 4, mtainSub, mtainSavannah, Isle5(mtainSavannah), sea, hillySavannah, mtainSavannah * 3, savannah, hillySahel * 2, mtainSavannah,
       hillySavannah, savannah, mtainDeshot, mtainSahel * 3),
 
-    TRow(278, sea * 5, mtainSavannah, sea, Isle5(mtainSavannah)),
-    VRow(277, OrigRt(1518, HVUR, 7), BendIn(1520, HVDn, 13), Bend(1522, HVUp, 11, 7), BendIn(1524, HVDn, 13), BendIn(1526, HVDL, 13)),
-    TRow(276, sea * 6, mtainSavannah * 2),
+    VRow(279, BendOut(1528, HVDL)),
+
+    TRow(278, sea * 5, mtainSavannah, sea, Isle5(mtainSavannah), sea, mtainSavannah * 4, mtainSahel, hillySavannah, hillySavannah, sahel * 3, deshot, sahel,
+      mtainSahel),
+
+    VRow(277, OrigRt(1518, HVUR, 7), BendIn(1520, HVDn, 13), Bend(1522, HVUp, 11, 7), BendIn(1524, HVDn, 13), BendIn(1526, HVDL, 13), OrigRt(1528, HVUp, 7),
+      OrigLt(1532, HVDR, 7), OrigRt(1534, HVUL, 7), OrigLt(1538, HVUR, 7), BendOut(1540, HVDn, 7), ThreeDown(1542, 13, 13, 0), BendIn(1544, HVDn, 13),
+      Bend(1546, HVUp, 2, 7)),
+
+    TRow(276, sea * 6, mtainSavannah * 2, sea * 4, mtainSavannah, hillySavannah),
     VRow(275, OrigLt(1526, HVUp, 7)),
-    TRow(274, sea * 8, sea * 5, sea, sea),
+    TRow(274, sea * 13, mtainSavannah),
     )
   }
   help.run
