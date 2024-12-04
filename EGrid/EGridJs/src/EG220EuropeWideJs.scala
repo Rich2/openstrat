56c1d2cd51dc7d3add795eeb607edf703687acd8
Rich2 <rr1@tutanota.com>
2024-12-04 10:33:57 +0000

    More Mill Js files.

--- EGrid/EGridJs/src/EG220EuropeWideJs.scala
+++ EGrid/EGridJs/src/EG220EuropeWideJs.scala
@@ -2,7 +2,6 @@
 package ostrat; package pSJs
 import scalajs.js.annotation._, egrid._, eg220._, prid.phex._
 
-@JSExportTopLevel("EG220EuropeWideJs")
 object EG220EuropeWideJs
-{ @JSExport def main(): Unit = { EGTerrOnlyGui(CanvasJs, Scen220Africa, HGView(148, 1522, 25), false); () }
+{ @JSExport def main(args: Array[String]): Unit = { EGTerrOnlyGui(CanvasJs, Scen220Africa, HGView(148, 1522, 25), false); () }
 } 
--- EGrid/EGridJs/src/EG220NAmericaJs.scala
+++ EGrid/EGridJs/src/EG220NAmericaJs.scala
@@ -2,7 +2,6 @@
 package ostrat; package pSJs
 import scalajs.js.annotation._, egrid._, eg220._, prid.phex._
 
-@JSExportTopLevel("EG220NAmericaJs")
 object EG220NAmericaJs
-{ @JSExport def main(): Unit = { EGTerrOnlyGui(CanvasJs, Scen220NorthAmerica, HGView(154, 9742, 25), false); () }
+{ @JSExport def main(args: Array[String]): Unit = { EGTerrOnlyGui(CanvasJs, Scen220NorthAmerica, HGView(154, 9742, 25), false); () }
 } 
