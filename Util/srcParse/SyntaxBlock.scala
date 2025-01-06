36017d1fd674fa40324760e9e06082e25b0ad538
Rich2 <rr1@tutanota.com>
2025-01-06 15:26:15 +0000

    ScalaDoc.

--- Util/srcParse/SyntaxBlock.scala
+++ Util/srcParse/SyntaxBlock.scala
@@ -1,21 +1,21 @@
-/* Copyright 2018-23 Richard Oliver. Licensed under Apache Licence version 2.0. */
+/* Copyright 2018-25 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pParse
 
-/** This trait includes all the tokens except braces plus the Bracket syntactic blocks. The Block in the name block member is either the top level
- *  statements in the file or the statements with in a bracket block. Conceptually the source file is considered a special case of bracket block where 
- *  the beginning of substitutes for the opening bracket and the end of file substitutes for the closing bracket. BlockMember has only two sub traits
- *  StatementMember and SemiToken. So a sequence of TokenOrBlocks is simply a series of tokens which has been parsed into Blocks. */
+/** This trait includes all the tokens except braces plus the Bracket syntactic blocks. The Block in the name block member is either the top level statements in
+ * the file or the statements with in a bracket block. Conceptually the source file is considered a special case of bracket block where the beginning of
+ * substitutes for the opening bracket and the end of file substitutes for the closing bracket. BlockMember has only two sub traits StatementMember and
+ * SemiToken. So a sequence of TokenOrBlocks is simply a series of tokens which has been parsed into Blocks. */
 trait BlockMem extends TextSpan
 
-/** A statement member is a Token except the semi colon, which is the statement delimiter and the braces, plus it includes blocks, As blocks contain a
- *  sequence of Statements but can themselves be part of a statement. */
+/** A statement member is a Token except the semicolon, which is the statement delimiter and the braces, plus it includes blocks, As blocks contain a sequence
+ * of Statements but can themselves be part of a statement. */
 trait StatementMem extends BlockMem
 
-/** An Assignment member can appear in the expressions either side of an asignment operator. */
+/** An Assignment member can appear in the expressions either side of an assignment operator. */
 trait AssignMem extends StatementMem
 
-/** I think its helpful to have an expression member trait for syntax that is not expressions. So I don't think it would be helpful if say an opening
- *  brace was an expression. All Expressions are Expression members. */
+/** I think its helpful to have an expression member trait for syntax that is not expressions. So I don't think it would be helpful if say an opening brace was
+ * an expression. All Expressions are Expression members. */
 trait ColonOpMem extends AssignMem
 
 /** Can be a member of either side of a ColonExpr */
@@ -37,26 +37,32 @@ sealed trait BracketCloseToken extends BracketToken
   final override def srcStr: String = braces.closeChar.toString
 }
 
+/** Parenthesis open token. */
 case class ParenthOpenToken(startPosn: TextPosn) extends BracketOpen
 { override def braces: Braces = Parentheses
 }
 
+/** Parenthesis close token. */
 case class ParenthCloseToken(startPosn: TextPosn) extends BracketCloseToken
 { override def braces: Braces = Parentheses
 }
 
+/** Square bracket open token. */
 case class SquareOpenToken(startPosn: TextPosn) extends BracketOpen
 { override def braces: Braces = SquareBraces
 }
 
+/** Square bracket close token. */
 case class SquareCloseToken(startPosn: TextPosn) extends BracketCloseToken
 { override def braces: Braces = SquareBraces
 }
 
+/** Curly brace open token. */
 case class CurlyOpenToken(startPosn: TextPosn) extends BracketOpen
 { override def braces: Braces = CurlyBraces
 }
 
+/** Curly brace close token. */
 case class CurlyCloseToken(startPosn: TextPosn) extends BracketCloseToken
 { override def braces: Braces = CurlyBraces
 }
@@ -67,25 +73,29 @@ sealed trait Braces
   def closeChar: Char
   def name: String
 }
+
+/** Parentheses, holds the open and close characters. */
 case object Parentheses extends Braces
 { override def openChar: Char = '('
   override def closeChar: Char = ')'
   override def name: String =  "Round"
 }
 
+/** Square braces, holds the open and close characters. */
 case object SquareBraces extends Braces
 { override def openChar: Char = '['
   override def closeChar: Char = ']'
   override def name: String = "Square"
 }
 
+/** Curly braces, holds the open and close characters. */
 case object CurlyBraces extends Braces
 { override def openChar: Char = '{'
   override def closeChar: Char = '}'
   override def name: String = "Curly"
 }
 
-case class BracketedRaws(statements: RArr[TextSpan], braces: Braces, startBracket: TextPosn, endBracket: TextPosn)// extends StatementsHolder
+case class BracketedRaws(statements: RArr[TextSpan], braces: Braces, startBracket: TextPosn, endBracket: TextPosn)
 { def exprName: String = braces.name + "BlockExpr"
 }
 
