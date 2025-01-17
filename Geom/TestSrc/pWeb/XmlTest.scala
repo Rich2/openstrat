1c87966e82e70eae646250bf3579937726e62319
Rich2 <rr1@tutanota.com>
2025-01-17 17:36:14 +0000

    Added another file to XML resources.

--- Geom/TestSrc/pWeb/XmlTest.scala
+++ Geom/TestSrc/pWeb/XmlTest.scala
@@ -13,30 +13,17 @@ object XmlTest extends TestSuite
 
   val wash = City("Washington")
   val bost = City("Boston")
-
-  val cities2 = Cities(RArr(wash, bost))
-
   val cities0 = Cities(RArr())
   val cities0Out = """<Cities/>"""
-
   val cities1 = Cities(RArr(wash))
-  val cities1Out =
-    """<Cities>
-      |  <City>Washington</City>
-      |</Cities>""".stripMargin
-
-  val cities2Out =
-    """<Cities>
-      |  <City>Washington</City>
-      |  <City>Boston</City>
-      |</Cities>""".stripMargin
+  val cities2 = Cities(RArr(wash, bost))
 
   val tests = Tests {
     test("Test1")
     { wash.out() ==> "<City>Washington</City>"
       cities0.out() ==> cities0Out
       Succ(cities1.out()) ==>  strFromResource("cities1.xml")
-      cities2.out() ==> cities2Out
+      Succ(cities2.out()) ==> strFromResource("cities2.xml")
     }
   }
 }
