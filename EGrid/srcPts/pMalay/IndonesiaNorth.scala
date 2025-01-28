33108997738d7439cebac84cf45866ee64869d88
Rich2 <rr1@tutanota.com>
2025-01-28 19:13:21 +0000

    Created Halmahera.

--- EGrid/srcPts/pMalay/IndonesiaNorth.scala
+++ EGrid/srcPts/pMalay/IndonesiaNorth.scala
@@ -1,6 +1,6 @@
 /* Copyright 2018-25 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pEarth; package pMalay
-import geom._, pglobe._, egrid._, WTiles._
+import geom.*, pglobe.*, egrid.*, WTiles.*
 
 /** A reference object for the Island of Borneo,rperesented by multiple [[PolygonLL]] */
 object Borneo extends IslandPolys("Borneo", 743330.kilares)
@@ -61,73 +61,20 @@ object Sulawesi extends EarthPoly("Sulawesi", -2.16 ll 120.58, mtainJungle)
   override val polygonLL: PolygonLL = PolygonLL(northEast, p5, ambesia, poso, teku, southWest, nGulfBoni, southEast, northWest, p92)
 }
 
-/** [[PolygonLL]] graphical representation of the island of New Guinea. Depends on nothing. */
-object GuineaWest extends EarthPoly("West Guinea", -5.19 ll 141.03, hillyJungle)
-{ val waigeoWest: LatLong = -0.113 ll 130.295
-  val waigeoNorth: LatLong = -0.007 ll 130.814
-  val manokwari: LatLong = -0.73 ll 133.98
-  val sCenderawasih: LatLong = -3.39 ll 135.33
-  val tebe: LatLong = -1.46 ll 137.93
-  val northEast: LatLong = -2.606 ll 141
+/** [[polygonLL]] graphical representation of the island of Halmahera Java 124413km² + Bali 5780km². Depends on nothing. */
+object Halmahera extends IslandPoly("Halmahera", 0.662 ll 127.976, hillyJungle)
+{ val halmahera: Kilares = 17780.kilares
+  val morotai: Kilares = 2336.6.kilares
+  val bacan: Kilares = 2719.03.kilares
 
-  val southEast: LatLong = -9.126 ll 141
-  val p55: LatLong = -9.231 ll 141.135
-  val p60: LatLong = -8.113 ll 139.951
-  val southWest: LatLong = -8.431 ll 137.655
-  val p70: LatLong = -7.518 ll 138.145
-  val heilwigMouth: LatLong = -5.359 ll 137.866
-  val aindua: LatLong = -4.46 ll 135.21
-  val p85: LatLong = -4.083 ll 132.915
-  val wNewGuinea: LatLong = -0.82 ll 130.45
+  override def area: Kilares = halmahera + morotai + bacan
 
-  override val polygonLL: PolygonLL = PolygonLL(waigeoWest, waigeoNorth, manokwari, sCenderawasih, tebe, northEast, southEast, p55, p60, southWest, p70,
-    heilwigMouth, aindua, p85, wNewGuinea)
-}
+  val morotaiNorth: LatLong = 2.645 ll 128.568
+  val east: LatLong = 0.213 ll 128.895
+  val p30: LatLong = 0.466 ll 127.928
+  val southEast: LatLong = -0.904 ll 128.452
+  val mandioli: LatLong = -0.753 ll 127.162
+  val ternate = 0.808 ll 127.293
 
-/** [[PolygonLL]] graphical representation for Papua New Guinea. Depends on nothing. */
-object PapuaNewGuinea extends EarthPoly("Papua New Guinea", -5.448 ll 143.578, hillyJungle)
-{ val madang: LatLong = -4.85 ll 145.78
-  val saidor: LatLong = -5.614 ll 146.473
-  val p10: LatLong = -5.918 ll 147.339
-  val p15: LatLong = -6.402 ll 147.843
-  val p22: LatLong = -6.642 ll 147.856
-  val markhamMouth: LatLong = -6.745 ll 146.970
-  val deboinMission: LatLong = -8.056 ll 148.123
-  val gavida: LatLong = -9.019 ll 149.292
-  val east: LatLong = -10.23 ll 150.87
-
-  val hulaBlackSand: LatLong = -10.103 ll 147.726
-  val p53: LatLong = -8.067 ll 146.031
-  val morigo: LatLong = -7.83 ll 143.98
-  val saibai: LatLong = -9.32 ll 142.63
-
-  override val polygonLL: PolygonLL = PolygonLL(GuineaWest.northEast, madang, saidor, p10, p15, p22, markhamMouth, deboinMission, gavida, east,
-    hulaBlackSand, p53, morigo, saibai, GuineaWest.southEast
-  )
-}
-
-/** [[PolygonLL]] graphical representation of New Britain 35144.6km². Depends on nothing. */
-object NewBritain extends IslandPoly("New Britain", -5.251 ll 151.402, hillyJungle)
-{ override val area: Kilares = 35144.6.kilares
-
-  val north: LatLong = -4.133 ll 152.166
-  val northEast: LatLong = -4.336 ll 152.404
-  val baronga: LatLong = -6.246 ll 150.463
-  val umbolWest: LatLong = -5.502 ll 147.754
-  val p75: LatLong = -5.480 ll 150.908
-  val takis: LatLong = -4.213 ll 151.489
-
-  override val polygonLL: PolygonLL = PolygonLL(north, northEast, baronga, umbolWest, p75, takis)
-}
-
-/** [[PolygonLL]] graphical representation 8990km² of New Ireland 7404km² + 1186km² + 400km². Depends on nothing. */
-object NewIreland extends IslandPoly("New Ireland", -5.251 ll 151.402, hillyJungle)
-{ override val area: Kilares = 8990.kilares
-
-  val newHanoverIsland: LatLong = -2.360 ll 150.190
-  val newIreland20: LatLong = -3.977 ll 152.926
-  val south: LatLong = -4.840 ll 152.882
-  val p60: LatLong = -4.182 ll 152.685
-
-  override val polygonLL: PolygonLL = PolygonLL(newHanoverIsland, newIreland20, south, p60)
+  override val polygonLL: PolygonLL = PolygonLL(morotaiNorth, east, p30, southEast, mandioli, ternate)
 }
---
+++ EGrid/srcPts/pMalay/NewGuineeSolomans.scala
@@ -0,0 +1,74 @@
+/* Copyright 2018-25 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package pEarth; package pMalay
+import geom.*, pglobe.*, egrid.*, WTiles.*
+
+/** [[PolygonLL]] graphical representation of the island of New Guinea. Depends on nothing. */
+object GuineaWest extends EarthPoly("West Guinea", -5.19 ll 141.03, hillyJungle)
+{ val waigeoWest: LatLong = -0.113 ll 130.295
+  val waigeoNorth: LatLong = -0.007 ll 130.814
+  val manokwari: LatLong = -0.73 ll 133.98
+  val sCenderawasih: LatLong = -3.39 ll 135.33
+  val tebe: LatLong = -1.46 ll 137.93
+  val northEast: LatLong = -2.606 ll 141
+
+  val southEast: LatLong = -9.126 ll 141
+  val p55: LatLong = -9.231 ll 141.135
+  val p60: LatLong = -8.113 ll 139.951
+  val southWest: LatLong = -8.431 ll 137.655
+  val p70: LatLong = -7.518 ll 138.145
+  val heilwigMouth: LatLong = -5.359 ll 137.866
+  val aindua: LatLong = -4.46 ll 135.21
+  val p85: LatLong = -4.083 ll 132.915
+  val wNewGuinea: LatLong = -0.82 ll 130.45
+
+  override val polygonLL: PolygonLL = PolygonLL(waigeoWest, waigeoNorth, manokwari, sCenderawasih, tebe, northEast, southEast, p55, p60, southWest, p70,
+    heilwigMouth, aindua, p85, wNewGuinea)
+}
+
+/** [[PolygonLL]] graphical representation for Papua New Guinea. Depends on nothing. */
+object PapuaNewGuinea extends EarthPoly("Papua New Guinea", -5.448 ll 143.578, hillyJungle)
+{ val madang: LatLong = -4.85 ll 145.78
+  val saidor: LatLong = -5.614 ll 146.473
+  val p10: LatLong = -5.918 ll 147.339
+  val p15: LatLong = -6.402 ll 147.843
+  val p22: LatLong = -6.642 ll 147.856
+  val markhamMouth: LatLong = -6.745 ll 146.970
+  val deboinMission: LatLong = -8.056 ll 148.123
+  val gavida: LatLong = -9.019 ll 149.292
+  val east: LatLong = -10.23 ll 150.87
+
+  val hulaBlackSand: LatLong = -10.103 ll 147.726
+  val p53: LatLong = -8.067 ll 146.031
+  val morigo: LatLong = -7.83 ll 143.98
+  val saibai: LatLong = -9.32 ll 142.63
+
+  override val polygonLL: PolygonLL = PolygonLL(GuineaWest.northEast, madang, saidor, p10, p15, p22, markhamMouth, deboinMission, gavida, east,
+    hulaBlackSand, p53, morigo, saibai, GuineaWest.southEast
+  )
+}
+
+/** [[PolygonLL]] graphical representation of New Britain 35144.6km². Depends on nothing. */
+object NewBritain extends IslandPoly("New Britain", -5.251 ll 151.402, hillyJungle)
+{ override val area: Kilares = 35144.6.kilares
+
+  val north: LatLong = -4.133 ll 152.166
+  val northEast: LatLong = -4.336 ll 152.404
+  val baronga: LatLong = -6.246 ll 150.463
+  val umbolWest: LatLong = -5.502 ll 147.754
+  val p75: LatLong = -5.480 ll 150.908
+  val takis: LatLong = -4.213 ll 151.489
+
+  override val polygonLL: PolygonLL = PolygonLL(north, northEast, baronga, umbolWest, p75, takis)
+}
+
+/** [[PolygonLL]] graphical representation 8990km² of New Ireland 7404km² + 1186km² + 400km². Depends on nothing. */
+object NewIreland extends IslandPoly("New Ireland", -5.251 ll 151.402, hillyJungle)
+{ override val area: Kilares = 8990.kilares
+
+  val newHanoverIsland: LatLong = -2.360 ll 150.190
+  val newIreland20: LatLong = -3.977 ll 152.926
+  val south: LatLong = -4.840 ll 152.882
+  val p60: LatLong = -4.182 ll 152.685
+
+  override val polygonLL: PolygonLL = PolygonLL(newHanoverIsland, newIreland20, south, p60)
+}
