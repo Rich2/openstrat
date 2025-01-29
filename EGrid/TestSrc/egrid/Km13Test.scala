cfc2e092230c96eb3dd6edaa3031a8ebc3ad6545
Rich2 <rr1@tutanota.com>
2025-01-29 13:10:33 +0000

    1300km Australia terrain.

---
+++ EGrid/TestSrc/egrid/Km13Test.scala
@@ -0,0 +1,17 @@
+/* Copyright 2018-25 Richard Oliver. Licensed under Apache Licence version 2.0. */
+package ostrat; package egrid
+import utest.{Show => *, *}, prid.phex.*, eg13.*
+
+object Km13Test extends TestSuite
+{
+  val tests = Tests{
+    val scen = Scen13All
+    val gs = scen.gridSys
+    val corners = scen.corners
+
+    val sPoly: PolygonHvOffset = corners.sidePoly(HSep(92, 6654))(gs)
+    test("SidePoly")
+    { sPoly.numVerts ==> 5
+    }
+  }
+}
--- EGrid/TestSrc/egrid/Km460Test.scala
+++ EGrid/TestSrc/egrid/Km460Test.scala
@@ -2,18 +2,16 @@
 package ostrat; package egrid
 import utest.{Show => *, *}, prid.phex.*, eg460.*
 
-object Km460Test
-  extends TestSuite
-  {
+object Km460Test extends TestSuite
+{
   val tests = Tests{
     val scen = Scen460Atlantic
     val gs = scen.gridSys
     val corners = scen.corners
-
+  
     val sPoly: PolygonHvOffset = corners.sidePoly(HSep(72, 10750))(gs)
     test("SidePoly"){
       sPoly.numVerts ==> 6
     }
-
   }
 }
