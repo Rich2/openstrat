5c17ad82bbdac1fd275bd241e73b9ac6bc71c725
Rich2 <rr1@tutanota.com>
2025-02-05 15:07:18 +0000

    Corrections to Html.

--- Geom/TestSrc/pWeb/HtmlTest.scala
+++ Geom/TestSrc/pWeb/HtmlTest.scala
@@ -6,17 +6,28 @@ object HtmlTest extends TestSuite
 {
   val tests = Tests {
     val p1 = HtmlP("Hi")
+    val p2 = HtmlP("Hi again!")
     val sectStr0 = HtmlSection(p1).out(0)
     val sectStr0c = "<section>\n  <p>Hi</p>\n</section>"
     val sectStr1 = HtmlSection(p1).out(2)
-    val sectStr1c = "  <section>\n    <p>Hi</p>\n  </section>"
-    test("Test Htlml")
-    {
-      p1.out(0) ==> "<p>Hi</p>"
+    val sectStr1c = "<section>\n    <p>Hi</p>\n  </section>"
+    val sectStr2 = HtmlSection(p1, p2).out(0)
+    val sectStr2c = "<section>\n  <p>Hi</p>\n  <p>Hi again!</p>\n</section>"
+
+    test("Test Html")
+    { p1.out(0) ==> "<p>Hi</p>"
       sectStr0.length ==> sectStr0c.length
       sectStr0 ==> sectStr0c
       sectStr1.length ==> sectStr1c.length
       sectStr1 ==> sectStr1c
+      sectStr2.length ==> sectStr2c.length
+      sectStr2 ==> sectStr2c
+    }
+
+    val li0 = HtmlLi("Pot")
+    test("Lists")
+    {
+      li0.out(0) ==> "<li>Pot</li>"
     }
   }
 }
