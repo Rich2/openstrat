70a844678df732e04123e109a05c829c51d9e9e3
Rich2 <rr1@tutanota.com>
2025-03-07 13:31:41 +0000

    Improved Pom staging.

--- Dev/JvmSrc/PomsApp.scala
+++ Dev/JvmSrc/PomsApp.scala
@@ -1,26 +1,24 @@
 /* Copyright 2018-25 Richard Oliver. Licensed under Apache Licence version 2.0. */
-package ostrat
+package ostrat; package pDev
 import utiljvm._
 
-/** Application for producing Openstrat POM files. */
+/** Application for producing Openstrat POM files. Takes the target folder as a paremter. */
 object PomsApp
 {
   def main(args: Array[String]): Unit =
   { deb("Starting PomsApp")
-    val versionStr = "0.3.5"
+    val versionStr = "0.3.6snap"
     val oDir = args.headOption
-    println(oDir)
+    debvar(oDir)
 
     def makePom(dirStr: String, name: String, versionStr: String, depStrs: String*): ErrBi[Exception, FileWritten] =
       fileWrite(dirStr / name + "-" + versionStr + ".pom", new OpenStratPomProject(name, versionStr, depStrs.toArr).out())
 
     oDir.foreach { dirStr =>
-      println(dirStr.length)
-      println(makePom(dirStr, "rutil", versionStr))
-      println(makePom(dirStr, "geom", versionStr, "rutil"))
-      println(makePom(dirStr, "tiling", versionStr, "rutil", "geom"))
-      println(makePom(dirStr, "egrid", versionStr, "rutil", "geom", "tiling"))
-      println(makePom(dirStr, "apps", versionStr, "rutil", "geom", "tiling", "egrid"))
+      val res = ErrBiAcc(makePom(dirStr, "rutil", versionStr), makePom(dirStr, "geom", versionStr, "rutil"),
+        makePom(dirStr, "tiling", versionStr, "rutil", "geom"), makePom(dirStr, "egrid", versionStr, "rutil", "geom", "tiling"),
+        makePom(dirStr, "apps", versionStr, "rutil", "geom", "tiling", "egrid"))
+     println(res.errsSummary)
     }
   }
 }
