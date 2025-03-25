1f0e5b1c87d4dc49635babfa289b72b612efa505
Rich2 <rr1@tutanota.com>
2025-03-25 13:26:49 +0000

    #286 Created PolygonLen2 companion object.

--- Geom/srcUnits/PolygonLen2.scala
+++ Geom/srcUnits/PolygonLen2.scala
@@ -19,3 +19,19 @@ trait PolygonLen2[VT <: PtLen2] extends Any, GeomLen2Elem, PolygonLikeDbl2[VT]
   def rotate180If(cond: Boolean): PolygonLen2[VT]
   def rotate180IfNot(cond: Boolean): PolygonLen2[VT]
 }
+
+/** Companion object for [[PolygonLen2]]. Does not provide factory methods. Use the specific [[Length]] unit classes such as [[PolyonM2]] and [[PolygonKm2]].
+ * Does contain geometric transformation type class instances. */
+object PolygonLen2
+{ /** implicit [[StateLen2]] type class instance / evidence for [[PolygonLen2]]. */
+  implicit val slateLen2Ev: SlateLen2[PolygonLen2[PtLen2]] = (obj, operand) => obj.slate(operand)
+
+  /** implicit [[StateLenXY]] type class instance / evidence for [[PolygonLen2]]. */
+  implicit val slateLenXYEv: SlateLenXY[PolygonLen2[PtLen2]] = (obj, xOp, yOp) => obj.slate(xOp, yOp)
+
+  /** implicit [[Scale]] type class instance / evidence for [[PolygonLen2]]. */
+  implicit val scaleEv: Scale[PolygonLen2[PtLen2]] = (obj, operand) => obj.scale(operand)
+
+  /** implicit [[MapGeom2]] type class instance / evidence for [[PolygonLen2]]. */
+  implicit val mapGeom2Ev: MapGeom2[PolygonLen2[PtLen2], Polygon] = (obj, operand) => obj.mapGeom2(operand)
+}
