4db5e6ee425790167b2bcdfc617e67b94aa8eead
Rich2 <rr1@tutanota.com>
2025-09-16 09:52:28 +0100

    Reformatted Scala code for IndexPage.

--- Dev/srcDoc/IndexPage.scala
+++ Dev/srcDoc/IndexPage.scala
@@ -8,7 +8,7 @@ object IndexPage extends HtmlPage
   def topMenu: HtmlUl = AppPage.topMenu(AppPage.defaultTopPairs)
   def body: HtmlBody = HtmlBody(topMenu, central)
 
-  def central: HtmlDiv = HtmlDiv.classAtt("main", iconsHtml, focus, devInfo, HtmlBr, begInfo, menu)
+  def central: HtmlDiv = HtmlDiv.classAtt("main", iconsHtml, focus, devInfo, HtmlBr, begInfo, intro, menu)
 
   def focus: HtmlOlWithLH = HtmlOlWithLH.h2("This project has 3 main focuses", strat, hist, geom1)
   def strat: HtmlLi = HtmlLi("Historical strategy games, particularly focused on simultaneous-turn, tile-based games.")
@@ -18,47 +18,45 @@ object IndexPage extends HtmlPage
   def devInfo = HtmlA("Documentation/dev.html", "Info for developers here.")
   def begInfo = HtmlA("Documentation/newdevs.html", "Info for new developers here.")
 
-  def menu: HtmlOlWithLH = HtmlOlWithLH(intro, util, geom, tile, egrid, apps, dev)
+  def menu: HtmlOl = HtmlOl(util, geom, tile, egrid, apps, dev)
 
-  def intro: String = "<b>The Code is currently organised into 7 modules.</b> Each module can build artifacts for Jvm and JavaFx and for the" +
-    " JavaScript platform and the Web."
+  def intro = HtmlP("The Code is currently organised into 6 modules.".htmlB, """Each module can build artifacts for Jvm and JavaFx and for the JavaScript
+  |platform and the Web.""".stripMargin)
 
-  def util: HtmlLi = HtmlLi.linkAndText("Documentation/util.html", "Util", "Contains a number of utilities. This includes a parsing and persistence" +
-    " system.")
+  def util: HtmlLi = HtmlLi(HtmlA("Documentation/util.html", "Util"), "Contains a number of utilities. This includes a parsing and persistence system.")
 
   def geom: HtmlLi = HtmlLi.linkAndText("Documentation/geom.html", "Geom Module", "Depends on Util. Basic 2D and 3D geometry, functional Graphics.")
 
-  def tile: HtmlLi = HtmlLi.linkAndText("Documentation/tiling.html", "Tiling Module", "Depends on Util and Geom modules. Encodes tile Grids, both" +
-    " square and hexagonal, as well as systems of grids.")
+  def tile: HtmlLi = HtmlLi(HtmlA("Documentation/tiling.html", "Tiling Module"), """Depends on Util and Geom modules. Encodes tile Grids, both square and
+  |hexagonal, as well as systems of grids.""".stripMargin)
   
-  def egrid: HtmlLi = HtmlLi.linkAndText("Documentation/egrid.html", "EGrid Module", "Tiling of the whole world in Hex grids, defining the changes" +
-    " over the course of history. This will be a data orientated module. It will also include terrain types to model terrain, both real and imagined" +
-    " for local maps and higher scales right up to 0.5 metres per tile However it won't generally include the data for these. The data for the real" +
-    " world will be organised according to a number of levels, which are likely to change over increasingly shorter historical time frames.")
+  def egrid: HtmlLi = HtmlLi.linkAndText("Documentation/egrid.html", "EGrid Module", """Tiling of the whole world in Hex grids, defining the changes over the
+  |course of history. This will be a data orientated module. It will also include terrain types to model terrain, both real and imagined for local maps and
+  |higher scales right up to 0.5 metres per tile However it won't generally include the data for these. The data for the real world will be organised according
+  |to a number of levels, which are likely to change over increasingly shorter historical time frames.""".stripMargin)
 
-  def apps: HtmlLi = HtmlLi.linkAndText("Documentation/apps.html", "Apps Module", "This module for end-user applications, that may eventually end up" +
-    " in their own repositories")
+  def apps: HtmlLi = HtmlLi.linkAndText("Documentation/apps.html", "Apps Module", """This module for end-user applications, that may eventually end up in their
+  |own repositories""".stripMargin)
 
   def dev: HtmlLi = HtmlLi(HtmlA("Documentation/dev.html", "Dev Module"), """Depends on all the other modules. This module is for the use of developer tools and
   |settings and illustrate provide tutorials, and to some extent test the modules core code.""",
-    SpanLine(HtmlA("Documentation/tomcat.html", "Apache Tomcat")))
+  SpanLine(HtmlA("Documentation/tomcat.html", "Apache Tomcat Documentaion")))
 
-  def iconsHtml: XmlAsString ="""
-   |<p><a href="https://github.com/Rich2/openstrat"><svg xmlns="http://www.w3.org/2000/svg" width="92" height="20">
-      |  <linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/>
-      |  </linearGradient><mask id="a"><rect width="92" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h34v20H0z"/><path
-      |    fill="#46BC99" d="M34 0h58v20H34z"/><path fill="url(#b)" d="M0 0h92v20H0z"/></g>
-      |    <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="17" y="15" fill="#010101"
-      |     fill-opacity=".3">code</text><text x="17" y="14">code</text><text x="62" y="15" fill="#010101" fill-opacity=".3">on github</text><
-      |     text x="62" y="14">on github</text>
-      |   </g>
-      |</svg></a>&nbsp;<a href="https://gitter.im/typestrat/Lobby"><svg xmlns="http://www.w3.org/2000/svg" width="92" height="20">
-      |  <linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/>
-      |  </linearGradient><mask id="a"><rect width="92" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h34v20H0z"/>
-      |    <path fill="#46BC99" d="M34 0h58v20H34z"/><path fill="url(#b)" d="M0 0h92v20H0z"/></g>
-      |    <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-      |      <text x="17" y="15" fill="#010101" fill-opacity=".3">chat</text><text x="17" y="14">chat</text>
-      |      <text x="62" y="15" fill="#010101" fill-opacity=".3">on gitter</text><text x="62" y="14">on gitter</text>
-      |    </g>
-      |</svg></a><br /></p>""".stripMargin.xmlAsString
+  def iconsHtml: XmlAsString ="""<p><a href="https://github.com/Rich2/openstrat"><svg xmlns="http://www.w3.org/2000/svg" width="92" height="20">
+  |  <linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/>
+  |  </linearGradient><mask id="a"><rect width="92" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h34v20H0z"/><path
+  |    fill="#46BC99" d="M34 0h58v20H34z"/><path fill="url(#b)" d="M0 0h92v20H0z"/></g>
+  |    <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="17" y="15" fill="#010101"
+  |     fill-opacity=".3">code</text><text x="17" y="14">code</text><text x="62" y="15" fill="#010101" fill-opacity=".3">on github</text><
+  |     text x="62" y="14">on github</text>
+  |   </g>
+  |</svg></a>&nbsp;<a href="https://gitter.im/typestrat/Lobby"><svg xmlns="http://www.w3.org/2000/svg" width="92" height="20">
+  |  <linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/>
+  |  </linearGradient><mask id="a"><rect width="92" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h34v20H0z"/>
+  |    <path fill="#46BC99" d="M34 0h58v20H34z"/><path fill="url(#b)" d="M0 0h92v20H0z"/></g>
+  |    <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
+  |      <text x="17" y="15" fill="#010101" fill-opacity=".3">chat</text><text x="17" y="14">chat</text>
+  |      <text x="62" y="15" fill="#010101" fill-opacity=".3">on gitter</text><text x="62" y="14">on gitter</text>
+  |    </g>
+  |</svg></a><br /></p>""".stripMargin.xmlAsString
 }
