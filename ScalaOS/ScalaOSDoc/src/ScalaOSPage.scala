175510a510fe17669978b870761d78371f487a3a
Rich2 <rr1@tutanota.com>
2025-10-04 15:31:35 +0100

    ScalaOS documentation.

--- ScalaOS/ScalaOSDoc/src/ScalaOSPage.scala
+++ ScalaOS/ScalaOSDoc/src/ScalaOSPage.scala
@@ -1,20 +1,22 @@
 /* Copyright 2025 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pDoc
-import pWeb.*
+import pWeb.*, wcode.*
 
 /** Documentation page for ScalaOS Module." */
 object ScalaOSPage extends OSDocumentationPage
 {
   override def titleStr: String = "Scala Operating System"
   override def body: HtmlBody = HtmlBody(HtmlH1("Geom Module"), central)
-  def central: HtmlDiv = HtmlDiv.classAtt("central", str1)
+  def central: HtmlDiv = HtmlDiv.classAtt("central", p1)
 
-  val str1 = """An operating system built from scratch with just a Linux Kernel. Don't know if this project wil ever get anywhere.
-  <br>git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-  <br>cd linux-stable
-  <br>git checkout -b stable v6.16.9
-  <br>make defconfig
-  <br>y to include n to exclude
-  <br>sudo apt install libelf-dev
-  <br>sudo apt install elfutils"""
+  val p1 = HtmlP("An operating system built from scratch with just a Linux Kernel. Don't know if this project wil ever get anywhere.",
+  BashLine("sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm"),
+  BashLine("git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"),
+  BashLine("cd linux-stable"),
+  BashLine("git checkout -b stable v6.16.9"),
+  BashLine("make defconfig"),
+  SpanLine("y to include n to exclude"),
+  BashLine("sudo apt install libelf-dev"),
+  BashLine("sudo apt install elfutils")
+  )
 }
