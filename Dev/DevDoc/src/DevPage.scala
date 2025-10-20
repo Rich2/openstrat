4c3d6e7eb94a7a0d8ec7a4796b2762365685bcae
Rich2 <rr1@tutanota.com>
2025-10-20 15:04:29 +0100

    Updates to Dev pages.

--- Dev/DevDoc/src/DevPage.scala
+++ Dev/DevDoc/src/DevPage.scala
@@ -7,14 +7,12 @@ object DevPage extends HtmlPage
 { override def head: HtmlHead = HtmlHead.titleCss("Dev Module", "documentation")
   override def body: HtmlBody = HtmlBody(HtmlH1("Dev Module"), central)
 
-  def central: HtmlDiv = HtmlDiv.classAtt("central", list, p1, p2, p3, p4, sbt1, p5, sbt3D, intellij, miscTitle, p6, table, p7, credits)
+  def central: HtmlDiv = HtmlDiv.classAtt("central", list, p1, p2, p3, p4, sbt1, p5, sbt3D, DevMisc, credits)
   def list: HtmlOlWithLH = HtmlOlWithLH(HtmlH2("The Dev module contains"),
     HtmlLi("JavaFx application selection and developer settings for the different apps.", HtmlA("osapp.jar", "Runnable jar")),
     HtmlLi("Generates the HTML and CSS files for the website, including this file."),
     HtmlLi("Creates Servlets for Tomcat / Jetty Servers.")
   )
-  
-  def miscTitle = HtmlH2("Place to put various notes, so as stuff doesn't get lost. It can be sorted into proper documentation later.")
 
   def p1: HtmlP = HtmlP("It currently works on JavaFx and web page. Using canvas on both platforms. See", "../api/index.html", "Scala Docs".htmlPath, "and see",
     linkOut("../apiJs/index.html", "Scala Docs for JavaScript target", "."))
@@ -61,30 +59,6 @@ object DevPage extends HtmlPage
   stripMargin)
 
   def sbt3D = HtmlP("For JavaFx 3D ", HtmlSbtInline("""set DevFx/reStart/mainClass:= Some("ostrat.pFx.App3D")"""))
-
-  def intellij: HtmlUlWithLH = HtmlUlWithLH("For IntelliJ useful options:",
-    HtmlLi("File => Editor => General -> Other -> tick Show quick documentation on mouse move."),
-    HtmlLi("File => 'Build, Execution, Deployment' => Compiler -> Build project automatically"),
-    HtmlLi("Project-Pane => Options -> 'Flatten packages'")
-  )
-
-  def p6: HtmlP = HtmlP("So at least recent versions of Kubuntu the java command on the path, is at", dirOut("/usr/bin/java", "."), "It is a link to",
-  dirOut("/etc/alternatives/java", "."), "This is also a link. To install a different java, install the JDK root folder in", dirOut("usr/lib/jvm", "."), """It
-  |doesn't have to be here, but it makes it easier to go with convention. Run""".stripMargin,
-  BashLine("sudo update-alternatives --config java"),
-  "In my example this gives")
-
-  def table = HtmlTable(
-    HtmlRowHead.strs4("Selection", "Path", "Priority", "Status"),
-    HtmlRow.strs4("0", "/usr/lib/jvm/java-11-openjdk-amd64/bin/java", "1111", "auto mode"),
-    HtmlRow.strs4("1", "/usr/lib/jvm/java-11-openjdk-amd64/bin/java", "1111", "manual mode"),
-    HtmlRow.strs4("2", "/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java", "1081", "manual mode")
-  )
-
-  def p7 = HtmlP("So leave the number as it is, then add to alternatives. I put the number 3 at then end because in my case slots 0 to 2 are already taken.",
-  BashLine("sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.8.0_212/bin/java 3"),
-  "then repeat",
-  BashLine("sudo update-alternatives --config java"))
   
   def credits: HtmlUlWithLH = HtmlUlWithLH("<h3>Credits</h3>",
     HtmlLi.linkAndText("https://lampwww.epfl.ch/~doeraene/thesis/", "Sébastien Doeraene, Ph.D. thesis", "for Scala.js"),
