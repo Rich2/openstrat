3e7c270c01e911773acfbee0e55ee07e552f3d01
Rich2 <rr1@tutanota.com>
2026-01-24 10:22:51 +0000

    Created ScaleLike type class.

--- Geom/srcTrans/Scale.scala
+++ Geom/srcTrans/Scale.scala
@@ -2,27 +2,35 @@
 package ostrat; package geom
 import reflect.ClassTag
 
-/** Type class for scale 2-dimensional vector transformations. Each transformation method has been given its own Type class and associated extension class.
- * Different sets of transformations can then be combined. */
-trait Scale[T]
+/** Type class for scale geometric transformations. Can be 2D or 3D, scalars or [[Length]] units, pure geometric or graphical types. Allows the scale extension
+ * method on objects that can summon the type class instance. */
+trait ScaleLike[A, B]
 { /** Scales object of type T. */
-  def scaleT(obj: T, operand: Double): T
+  def scaleT(obj: A, operand: Double): B
 }
 
-/** Companion object for the Scale type class. Contains instances. 2-dimensional vector transformations type class. */
-object Scale
-{ /** Implicit [[Scale]] type class instances / evidence for [[Arr]]. */
-  given arrEv[A, ArrA <: Arr[A]](using build: BuilderArrMap[A, ArrA], ev: Scale[A]): Scale[ArrA] = (obj, offset) => obj.map(ev.scaleT(_, offset))
+/** Companion object for the [[ScaleLike]] transformations type class Contains instances for [[Functor]]s [[Arr]]s and [[Array]]s. */
+object ScaleLike
+{ /** Implicit [[ScaleLike]] type class instances / evidence for [[Arr]]. */
+  given arrEv[A, B, ArrA <: Arr[A], ArrB <: Arr[B]](using evAB: ScaleLike[A, B], build: BuilderArrMap[B, ArrB]): ScaleLike[ArrA, ArrB] =
+    (obj, offset) => obj.map(evAB.scaleT(_, offset))
 
-  /** Implicit [[Scale]] type class instances / evidence for [[Functor]]. Provides instances for [[List]], [[Option]] etc. */
-  given functorEv[A, F[_]](using evF: Functor[F], evA: Scale[A]): Scale[F[A]] = (obj, operand) => evF.mapT(obj, evA.scaleT(_, operand))
+  /** Implicit [[ScaleLike]] type class instances / evidence for [[Functor]]. Provides instances for [[List]], [[Option]] etc. */
+  given functorEv[A, B, F[_]](using evF: Functor[F], evAB: ScaleLike[A, B]): ScaleLike[F[A], F[B]] = (obj, operand) => evF.mapT(obj, evAB.scaleT(_, operand))
 
-  /** Implicit [[Scale]] type class instances / evidence for [[Array]]. */
-  given arrayEv[A](using ct: ClassTag[A], ev: Scale[A]): Scale[Array[A]] = (obj, operand) => obj.map(ev.scaleT(_, operand))
+  /** Implicit [[ScaleLike]] type class instances / evidence for [[Array]]. */
+  given arrayEv[A, B](using evAB: ScaleLike[A, B], ctB: ClassTag[B]): ScaleLike[Array[A], Array[B]] = (obj, operand) => obj.map(evAB.scaleT(_, operand))
+
+  /** Subtype type class instances / evidence for [[ScaleLike]]. */
+  given subTypesEv[A, B >: A](using evB: Scale[B]): ScaleLike[A, B] = (obj, op) => evB.scaleT(obj, op)
 }
+
+/** Special cases of [[ScaleLike]] Type class where the type of the object is preserved for scale geometric transformations. Can be 2D or 3D, scalars or
+ * [[Length]] units, pure geometric or graphical types. Allows the scale extension method on objects that can summon the type class instance. */
+trait Scale[A] extends ScaleLike[A, A]
 
 /** Extension methods for the [[Scale]] type class. */
-extension[T](value: T)(using ev: Scale[T])
+extension[A, B](value: A)(using ev: ScaleLike[A, B])
 { /** Performs scale transformation on objects of type T. */
-  def scale(operand: Double): T = ev.scaleT(value, operand)
+  def scale(operand: Double): B = ev.scaleT(value, operand)
 }
