63780273bd4260dcc192214320ddabdc4e1bba48
Rich2 <rr1@tutanota.com>
2026-04-22 12:25:53 +0100

    Created more file name types.

--- Geom/srcWeb/FileName.scala
+++ Geom/srcWeb/FileName.scala
@@ -15,10 +15,54 @@ object FileName
   class FileNameGen(val str: String) extends FileName
 }
 
-class FileNameStem(str: String)
+/** A file name stem, missing the last part of the file name. */
+case class FileNameStem(str: String)
+{ /** Appends operand [[String]] to this file name stem. */
+  def / (operand: String): FileNameStem = FileNameStem(str + operand)
 
+  /** Adds a ",html" extension to the file name. */
+  def html: HtmlFileName = HtmlFileName(str)
+}
+
+/** File name with an extension usch as .txt or .html file name. */
 trait FileNameExtended extends FileName
-{
+{ /** The [[String]] for the file name stem. */
   def fileNameStemStr: String
+
+  /** The file name stem without its extension such as ".txt" or ".html". */
   def fileNameStem: FileNameStem = FileNameStem(str)
+}
+
+object FileNameExtended
+{
+  /** Implementation class for the gneral case of a file name with an extension. */
+  class FileNameExtendedGen(val arrayUnsafe: Array[String]) extends FileNameExtended
+  { override def fileNameStemStr: String = arrayUnsafe(0)
+    override def str: String = arrayUnsafe.mkString(".")
+  }
+}
+
+/** A text file name. */
+case class TextFileName(fileNameStemStr: String) extends FileNameExtended
+{ override def str: String = fileNameStemStr + ".txt"
+}
+
+/** An HTML file name. */
+case class HtmlFileName(fileNameStemStr: String) extends FileNameExtended
+{ override def str: String = fileNameStemStr + ".html"
+}
+
+/** A JavaScript file name. */
+case class JsFileName(fileNameStemStr: String) extends FileNameExtended
+{ override def str: String = fileNameStemStr + ".js"
+}
+
+/** A JavaScript Map file name. */
+case class JsMapFileName(fileNameStemStr: String) extends FileNameExtended
+{ override def str: String = fileNameStemStr + ".js.map"
+}
+
+/** A CSS file name. */
+case class CssFileName(fileNameStemStr: String) extends FileNameExtended
+{ override def str: String = fileNameStemStr + ".css"
 }
