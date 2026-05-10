11de90c8f8117a1c0daff4f719977bdaf33f4312
Rich2 <rr1@tutanota.com>
2026-05-10 13:37:05 +0100

    Created IFramePx class.

--- Geom/srcHtml/IFrame.scala
+++ Geom/srcHtml/IFrame.scala
@@ -1,6 +1,7 @@
 /* Copyright 2018-26 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ostrat; package pweb
 
+/** HTML iframe element. */
 trait IFrame extends HtmlElem
 { override def tagName: String = "iframe"
 
@@ -18,13 +19,41 @@ trait IFrame extends HtmlElem
 }
 
 object IFrame
-{ /** Creates an HTML iframe element with a 16:9 ratio from the height attribute.  */
+{ /** Factory apply method to create an iframe HTML element. */
+  def apply(srcStr: String, widthAtt: WidthCss, heightAtt: HeightCss, otherAttribs: RArr[XAtt]): IFrame = IFrameGen(srcStr, widthAtt, heightAtt, otherAttribs)
+
+  /** Factory apply method to create an iframe HTML element. */
+  def apply(srcStr: String, widthAtt: WidthCss, heightAtt: HeightCss, otherAttribs: XAtt*): IFrame = IFrameGen(srcStr, widthAtt, heightAtt, otherAttribs.toRArr)
+
+  /** Creates an HTML iframe element with a 16:9 ratio from the height attribute.  */
   def h169(srcStr: String, heightAtt: HeightCss, otherAttribs: XAtt*): IFrame = IFrameGen(srcStr, heightAtt.widthAtt(16.0 / 9), heightAtt, otherAttribs.toRArr)
 
   /** Creates an HTML iframe element with a 16:9 ratio from the width attribute. */
   def w169(srcStr: String, widthAtt: WidthCss, otherAttribs: XAtt*): IFrame = IFrameGen(srcStr, widthAtt, widthAtt.heightAtt(9.0 /16), otherAttribs.toRArr)
 
+  /** Implementation class for the gneral case of an HTML iframe element. */
   case class IFrameGen(srcStr: String, widthAtt: WidthCss, heightAtt: HeightCss, otherAttribs: RArr[XAtt]) extends IFrame, HtmlOwnLine
   { override def srcAtt: SrcAtt = SrcAtt(srcStr)
   }
+}
+
+/** HTML iframe element, with the width and height specified in pixels. */
+class IFramePx(val srcStr: String, val widthNum: Double, val heightNum: Double, val otherAttribs: RArr[XAtt]) extends IFrame, HtmlOwnLine
+{ override def srcAtt: SrcAtt = SrcAtt(srcStr)
+  override def widthAtt: WidthCss = WidthPx(widthNum)
+  override def heightAtt: HeightCss = HeightPx(heightNum)
+}
+
+object IFramePx
+{ /** Factory apply method to create an iframe HTML element with width and height specified in pixels. */
+  def apply(srcStr: String, widthNum: Double, heightNum: Double, otherAttribs: RArr[XAtt]): IFramePx = new IFramePx(srcStr, widthNum, heightNum, otherAttribs)
+
+  /** Factory apply method to create an iframe HTML element with width and height specified in pixels. */
+  def apply(srcStr: String, widthNum: Double, heightNum: Double, otherAttribs: XAtt*): IFramePx = IFramePx(srcStr, widthNum, heightNum, otherAttribs.toRArr)
+
+  /** Creates an HTML iframe element with a 16:9 ratio from the height attributewith width and height specified in pixels.  */
+  def h169(srcStr: String, heightNum: Double, otherAttribs: XAtt*): IFramePx = IFramePx(srcStr, heightNum * 16.0 / 9, heightNum, otherAttribs.toRArr)
+
+  /** Creates an HTML iframe element with a 16:9 ratio from the width attribute with width and height specified in pixels. */
+  def w169(srcStr: String, widthNum: Double, otherAttribs: XAtt*): IFramePx = IFramePx(srcStr, widthNum, widthNum * 9.0 /16, otherAttribs.toRArr)
 }
